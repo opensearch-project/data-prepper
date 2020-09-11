a43e58b80d97910ac62e9d7dd112fb60ab1c00fb
qchea <qchea@amazon.com>
2020-09-11 15:39:39 -0500

    MNT: replace checkArgument with checkNotNull

--- situp-plugins/elasticsearch/src/main/java/com/amazon/situp/plugins/sink/elasticsearch/RetryConfiguration.java
+++ situp-plugins/elasticsearch/src/main/java/com/amazon/situp/plugins/sink/elasticsearch/RetryConfiguration.java
@@ -1,6 +1,6 @@
 package com.amazon.situp.plugins.sink.elasticsearch;
 
-import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 public class RetryConfiguration {
   public static final String DLQ_FILE = "dlq_file";
@@ -16,7 +16,7 @@ public class RetryConfiguration {
     private String dlqFile;
 
     public Builder withDlqFile(final String dlqFile) {
-      checkArgument(dlqFile != null, "dlqFile cannot be null.");
+      checkNotNull(dlqFile, "dlqFile cannot be null.");
       this.dlqFile = dlqFile;
       return this;
     }
