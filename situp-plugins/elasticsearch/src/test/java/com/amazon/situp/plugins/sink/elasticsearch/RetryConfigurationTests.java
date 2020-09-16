c658d1c3ce88c90a9b5db030dfdb62edebe7645d
qchea <qchea@amazon.com>
2020-09-16 15:58:50 -0500

    TST: unit test on retry status

--- situp-plugins/elasticsearch/src/test/java/com/amazon/situp/plugins/sink/elasticsearch/RetryConfigurationTests.java
+++ situp-plugins/elasticsearch/src/test/java/com/amazon/situp/plugins/sink/elasticsearch/RetryConfigurationTests.java
@@ -1,6 +1,12 @@
 package com.amazon.situp.plugins.sink.elasticsearch;
 
+import org.elasticsearch.rest.RestStatus;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -8,16 +14,27 @@ import static org.junit.Assert.assertNull;
 public class RetryConfigurationTests {
     @Test
     public void testDefaultConfigurationIsNotNull() {
-        RetryConfiguration retryConfiguration = new RetryConfiguration.Builder().build();
+        final RetryConfiguration retryConfiguration = new RetryConfiguration.Builder().build();
         assertNull(retryConfiguration.getDlqFile());
+        final Integer expStatus = RestStatus.TOO_MANY_REQUESTS.getStatus();
+        assertEquals(new HashSet<>(Collections.singletonList(expStatus)), retryConfiguration.getRetryStatus());
     }
 
     @Test
     public void testConfigurationWithCustomDlqFilePath() {
-        String fakeDlqFilePath = "foo.txt";
-        RetryConfiguration retryConfiguration = new RetryConfiguration.Builder()
+        final String fakeDlqFilePath = "foo.txt";
+        final RetryConfiguration retryConfiguration = new RetryConfiguration.Builder()
                 .withDlqFile(fakeDlqFilePath)
                 .build();
         assertEquals(fakeDlqFilePath, retryConfiguration.getDlqFile());
+    }
+
+    @Test
+    public void testConfigurationWithCustomRestStatus() {
+        final List<Integer> testStatus = Arrays.asList(400, 404, 429);
+        final RetryConfiguration retryConfiguration = new RetryConfiguration.Builder()
+                .withRetryStatus(testStatus)
+                .build();
+        assertEquals(new HashSet<>(testStatus), retryConfiguration.getRetryStatus());
     }
 }
