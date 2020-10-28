4e2549dcfc2f5d111b67c49504c1f524be836545
qchea <qchea@amazon.com>
2020-10-28 14:24:09 -0500

    MAINT: use grpc

--- situp-core/src/integrationTest/java/com/amazon/situp/integration/EndToEndIntegrationTest.java
+++ situp-core/src/integrationTest/java/com/amazon/situp/integration/EndToEndIntegrationTest.java
@@ -17,6 +17,7 @@ import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
+import com.linecorp.armeria.client.Clients;
 import com.linecorp.armeria.client.WebClient;
 import com.linecorp.armeria.common.HttpData;
 import com.linecorp.armeria.common.HttpMethod;
@@ -24,6 +25,7 @@ import com.linecorp.armeria.common.MediaType;
 import com.linecorp.armeria.common.RequestHeaders;
 import com.linecorp.armeria.common.SessionProtocol;
 import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
+import io.opentelemetry.proto.collector.trace.v1.TraceServiceGrpc;
 import io.opentelemetry.proto.common.v1.AnyValue;
 import io.opentelemetry.proto.common.v1.KeyValue;
 import io.opentelemetry.proto.resource.v1.Resource;
@@ -96,15 +98,9 @@ public class EndToEndIntegrationTest {
         restHighLevelClient.indices().refresh(requestAll, RequestOptions.DEFAULT);
     }
 
-    private void sendExportTraceServiceRequestToSource(ExportTraceServiceRequest request) throws InvalidProtocolBufferException {
-        WebClient.of().execute(RequestHeaders.builder()
-                        .scheme(SessionProtocol.HTTP)
-                        .authority("127.0.0.1:21890")
-                        .method(HttpMethod.POST)
-                        .path("/opentelemetry.proto.collector.trace.v1.TraceService/Export")
-                        .contentType(MediaType.JSON_UTF_8)
-                        .build(),
-                HttpData.copyOf(JsonFormat.printer().print(request).getBytes())).aggregate().join();
+    private void sendExportTraceServiceRequestToSource(final ExportTraceServiceRequest request) {
+        Clients.newClient(
+                "gproto+http://127.0.0.1:21890/", TraceServiceGrpc.TraceServiceBlockingStub.class).export(request);
     }
 
     private List<Map<String, Object>> getSourcesFromSearchHits(final SearchHits searchHits) {
@@ -198,7 +194,7 @@ public class EndToEndIntegrationTest {
     }
 
     private String getServiceName(final ResourceSpans resourceSpans) {
-        return resourceSpans.getResource().getAttributesList().stream().filter(kv -> kv.getKey() == "service.name")
+        return resourceSpans.getResource().getAttributesList().stream().filter(kv -> kv.getKey().equals("service.name"))
                 .findFirst().get().getValue().getStringValue();
     }
 }
