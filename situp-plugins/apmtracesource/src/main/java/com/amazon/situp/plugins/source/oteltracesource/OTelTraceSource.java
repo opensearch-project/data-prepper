20f58805f73c25ebe2d7957a903664cd24b2e978
Kowshik Nagarajaan <kowshikn@amazon.com>
2020-10-29 08:16:24 -0700

    Add healthcheck too source.

--- situp-plugins/apmtracesource/src/main/java/com/amazon/situp/plugins/source/oteltracesource/OTelTraceSource.java
+++ situp-plugins/apmtracesource/src/main/java/com/amazon/situp/plugins/source/oteltracesource/OTelTraceSource.java
@@ -9,6 +9,7 @@ import com.amazon.situp.model.source.Source;
 import com.linecorp.armeria.server.Server;
 import com.linecorp.armeria.server.ServerBuilder;
 import com.linecorp.armeria.server.grpc.GrpcService;
+import com.linecorp.armeria.server.healthcheck.HealthCheckService;
 import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +20,7 @@ import java.util.concurrent.ExecutionException;
 @SitupPlugin(name = "otel_trace_source", type = PluginType.SOURCE)
 public class OTelTraceSource implements Source<Record<ExportTraceServiceRequest>> {
     private static final Logger LOG = LoggerFactory.getLogger(OTelTraceSource.class);
+    private static final String HEALTH_CHECK_PATH = "/health_check";
     private final OTelTraceSourceConfig oTelTraceSourceConfig;
     private Server server;
 
@@ -43,6 +45,9 @@ public class OTelTraceSource implements Source<Record<ExportTraceServiceRequest>
                         new File(oTelTraceSourceConfig.getSslKeyFile()));
             } else {
                 sb.http(oTelTraceSourceConfig.getPort());
+            }
+            if(oTelTraceSourceConfig.isHealthCheck()) {
+                sb.service(HEALTH_CHECK_PATH, HealthCheckService.of());
             }
             //TODO: Expose BlockingTaskExecutor/Connections
             server = sb.build();
