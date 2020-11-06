2f5f1e5de70a5ae91e9a4642877ee6ff99a11e26
Kowshik Nagarajaan <kowshikn@amazon.com>
2020-11-05 19:17:10 -0600

    Migrate AOT and fix sample app.

--- examples/odfe-pipes-trace-app/analytics-service/src/main/java/com/example/restservice/LoggingController.java
+++ examples/odfe-pipes-trace-app/analytics-service/src/main/java/com/example/restservice/LoggingController.java
@@ -11,18 +11,14 @@ import org.springframework.web.bind.annotation.*;
 import com.example.restservice.Logging;
 import java.io.IOException;
 
+import java.util.Optional;
+
 @RestController()
 public class LoggingController {
 
-    @Autowired
-    private RestHighLevelClient client;
-
     @PostMapping("/logs")
     public String save(@RequestBody Logging logging) throws IOException {
-        IndexRequest request = new IndexRequest("logs");
-        request.id(logging.getService());
-        request.source(new ObjectMapper().writeValueAsString(logging), XContentType.JSON);
-        IndexResponse indexResponse = client.index(request, RequestOptions.DEFAULT);
-        return indexResponse.getResult().name();
+        //TODO send logs to another system.
+        return "Dummy";
     }
 }
