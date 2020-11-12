617fbedd41f772a73728496de5ce8c69b1b801da
qchea <qchea@amazon.com>
2020-11-12 11:45:03 -0600

    ADD: xms and xmx

--- examples/situp-wait-for-odfe-and-start.sh
+++ examples/situp-wait-for-odfe-and-start.sh
@@ -5,13 +5,4 @@ until [[ $(curl --write-out %{http_code} --output /dev/null --silent --head --fa
   sleep 1
 done
 
-java \
--Xms128m -Xmx128m \
--Dcom.sun.management.jmxremote=true \
--Dcom.sun.management.jmxremote.port=8849 \
--Dcom.sun.management.jmxremote.authenticate=false \
--Dcom.sun.management.jmxremote.ssl=false \
--Dcom.sun.management.jmxremote.local.only=false \
--Dcom.sun.management.jmxremote.rmi.port=8849 \
--Djava.rmi.server.hostname=localhost \
--jar situp.jar /app/transformation-instance.yml
+java -Xms128m -Xmx128m -jar situp.jar /app/transformation-instance.yml
