1957ca88be96b24ae995dd55adc910f1a10caacf
qchea <qchea@amazon.com>
2020-11-16 14:43:57 -0600

    REF: delete unused classes and add new plugin modules

--- situp-plugins/apmtracesource/src/main/java/com/amazon/situp/plugins/source/apmtracesource/http/server/ServerRequestProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.amazon.situp.plugins.source.apmtracesource.http.server;
-
-import io.netty.handler.codec.http.HttpResponseStatus;
-
-public interface ServerRequestProcessor<T> {
-
-  HttpResponseStatus processMessage(T message);
-}
