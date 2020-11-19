bcd3699430ed7a4cbe6c9fc198f90d0d4180ee18
qchea <qchea@amazon.com>
2020-11-19 14:12:07 -0600

    initialize test classes

---
+++ situp-plugins/peer-forwarder/src/test/java/com/amazon/situp/plugins/processor/peerforwarder/PeerForwarderConfigTest.java
@@ -0,0 +1,11 @@
+package com.amazon.situp.plugins.processor.peerforwarder;
+
+import org.junit.Test;
+
+public class PeerForwarderConfigTest {
+
+    @Test
+    public void testSimple() {
+
+    }
+}
---
+++ situp-plugins/peer-forwarder/src/test/java/com/amazon/situp/plugins/processor/peerforwarder/PeerForwarderTest.java
@@ -0,0 +1,11 @@
+package com.amazon.situp.plugins.processor.peerforwarder;
+
+import org.junit.Test;
+
+public class PeerForwarderTest {
+
+    @Test
+    public void testSimple() {
+
+    }
+}
