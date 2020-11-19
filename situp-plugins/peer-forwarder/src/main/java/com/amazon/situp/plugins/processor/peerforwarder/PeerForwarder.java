0d281e9eecc8cee9d19e661640de913f11a06110
qchea <qchea@amazon.com>
2020-11-19 13:53:21 -0600

    ADD: wrap up PeerForwarder

--- situp-plugins/peer-forwarder/src/main/java/com/amazon/situp/plugins/processor/peerforwarder/PeerForwarder.java
+++ situp-plugins/peer-forwarder/src/main/java/com/amazon/situp/plugins/processor/peerforwarder/PeerForwarder.java
@@ -5,13 +5,22 @@ import com.amazon.situp.model.annotations.SitupPlugin;
 import com.amazon.situp.model.configuration.PluginSetting;
 import com.amazon.situp.model.processor.Processor;
 import com.amazon.situp.model.record.Record;
+import com.linecorp.armeria.client.Clients;
+import com.linecorp.armeria.internal.shaded.bouncycastle.util.encoders.Hex;
 import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
+import io.opentelemetry.proto.collector.trace.v1.TraceServiceGrpc;
+import io.opentelemetry.proto.trace.v1.InstrumentationLibrarySpans;
 import io.opentelemetry.proto.trace.v1.ResourceSpans;
+import io.opentelemetry.proto.trace.v1.Span;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.time.Duration;
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -22,11 +31,13 @@ import java.util.Map;
 @SitupPlugin(name = "peer_forwarder", type = PluginType.PROCESSOR)
 public class PeerForwarder implements Processor<Record<ExportTraceServiceRequest>, Record<ExportTraceServiceRequest>> {
 
+    private static final Logger LOG = LoggerFactory.getLogger(PeerForwarder.class);
+
     private final PeerForwarderConfig peerForwarderConfig;
 
     private final List<String> peerIps;
 
-    private String localIp;
+    private final Map<String, TraceServiceGrpc.TraceServiceBlockingStub> peerClients;
 
     public static String getLocalPublicIp() throws IOException {
         // Find public IP address
@@ -41,14 +52,24 @@ public class PeerForwarder implements Processor<Record<ExportTraceServiceRequest
     public PeerForwarder(final PluginSetting pluginSetting) {
         peerForwarderConfig = PeerForwarderConfig.buildConfig(pluginSetting);
         peerIps = new ArrayList<>(peerForwarderConfig.getPeerIps());
+        peerClients = new HashMap<>();
+        for (final String peerIp: peerIps) {
+            peerClients.put(peerIp, createGRPCClient(peerIp));
+        }
         try {
-            localIp = getLocalPublicIp();
-            peerIps.add(localIp);
+            peerIps.add(getLocalPublicIp());
         } catch (IOException e) {
             // TODO: handle this error properly
             e.printStackTrace();
         }
         Collections.sort(peerIps);
+    }
+
+    private TraceServiceGrpc.TraceServiceBlockingStub createGRPCClient(final String ipAddress) {
+        // TODO: replace hardcoded port with customization
+        return Clients.builder(String.format("gproto+http://%s:21890/", ipAddress))
+                .writeTimeout(Duration.ofSeconds(peerForwarderConfig.getTimeOut()))
+                .build(TraceServiceGrpc.TraceServiceBlockingStub.class);
     }
 
     @Override
@@ -59,9 +80,8 @@ public class PeerForwarder implements Processor<Record<ExportTraceServiceRequest
         }
         for (final Record<ExportTraceServiceRequest> record: records) {
             for (final ResourceSpans rs: record.getData().getResourceSpansList()) {
-                final Map<String, ResourceSpans> rsBatch = splitByTrace(rs);
-                assert rsBatch != null;
-                for (final Map.Entry<String, ResourceSpans> entry: rsBatch.entrySet()) {
+                final List<Map.Entry<String, ResourceSpans>> rsBatch = splitByTrace(rs);
+                for (final Map.Entry<String, ResourceSpans> entry: rsBatch) {
                     final String traceId = entry.getKey();
                     final ResourceSpans newRS = entry.getValue();
                     final String peerIp = getHostByConsistentHashing(traceId);
@@ -70,15 +90,77 @@ public class PeerForwarder implements Processor<Record<ExportTraceServiceRequest
             }
         }
 
-        // TODO: send ExportTraceServiceRequest to designated peer
-        return null;
+        // Send ExportTraceServiceRequest to designated peer
+        final List<Record<ExportTraceServiceRequest>> results = new ArrayList<>();
+        for (final String peerIp: peerIps) {
+            final TraceServiceGrpc.TraceServiceBlockingStub client = peerClients.getOrDefault(peerIp, null);
+            ExportTraceServiceRequest.Builder currRequestBuilder = ExportTraceServiceRequest.newBuilder();
+            int currSpansCount = 0;
+            for (final ResourceSpans rs: peerGroupedRS.get(peerIp)) {
+                final int rsSize = getResourceSpansSize(rs);
+                if (currSpansCount >= peerForwarderConfig.getMaxNumSpansPerRequest()) {
+                    final ExportTraceServiceRequest currRequest = currRequestBuilder.build();
+                    processRequest(client, currRequest, results);
+                    currRequestBuilder = ExportTraceServiceRequest.newBuilder();
+                    currSpansCount = 0;
+                }
+                currRequestBuilder.addResourceSpans(rs);
+                currSpansCount += rsSize;
+            }
+            // last request
+            final ExportTraceServiceRequest currRequest = currRequestBuilder.build();
+            processRequest(client, currRequest, results);
+        }
+        return results;
     }
 
-    private Map<String, ResourceSpans> splitByTrace(final ResourceSpans rs) {
-        return null;
+    private void processRequest(final TraceServiceGrpc.TraceServiceBlockingStub client,
+                                final ExportTraceServiceRequest request,
+                                final List<Record<ExportTraceServiceRequest>> results) {
+        if (client != null) {
+            try {
+                client.export(request);
+            } catch (Exception e) {
+                results.add(new Record<>(request));
+            }
+        } else {
+            results.add(new Record<>(request));
+        }
+    }
+
+    private List<Map.Entry<String, ResourceSpans>> splitByTrace(final ResourceSpans rs) {
+        final List<Map.Entry<String, ResourceSpans>> result = new ArrayList<>();
+        for (final InstrumentationLibrarySpans ils: rs.getInstrumentationLibrarySpansList()) {
+            final Map<String, ResourceSpans.Builder> batches = new HashMap<>();
+            for (final Span span: ils.getSpansList()) {
+                final String sTraceId = Hex.toHexString(span.getTraceId().toByteArray());
+                if (!batches.containsKey(sTraceId)) {
+                    final ResourceSpans.Builder newRSBuilder = ResourceSpans.newBuilder()
+                            .setResource(rs.getResource());
+                    newRSBuilder.addInstrumentationLibrarySpansBuilder().setInstrumentationLibrary(ils.getInstrumentationLibrary());
+                    batches.put(sTraceId, newRSBuilder);
+                }
+
+                // there is only one instrumentation library per batch
+                batches.get(sTraceId).getInstrumentationLibrarySpansBuilder(0).addSpans(span);
+            }
+
+            batches.forEach((traceId, rsBuilder) -> result.add(new AbstractMap.SimpleEntry<>(traceId, rsBuilder.build())));
+        }
+
+        return result;
     }
 
     private String getHostByConsistentHashing(final String traceId) {
-        return null;
+        // TODO: better consistent hashing algorithm, e.g. ring hashing
+        return peerIps.get(traceId.hashCode() % peerIps.size());
+    }
+
+    private int getResourceSpansSize(final ResourceSpans rs) {
+        int spanCount = 0;
+        for (final InstrumentationLibrarySpans ils: rs.getInstrumentationLibrarySpansList()) {
+            spanCount += ils.getSpansCount();
+        }
+        return spanCount;
     }
 }
