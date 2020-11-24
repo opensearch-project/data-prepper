4065f72fe7fbaea1107d53faab0de258277cbf8e
qchea <qchea@amazon.com>
2020-11-24 10:51:39 -0600

    ENH: use powermock to cover remote Ip forward test

--- situp-plugins/peer-forwarder/src/test/java/com/amazon/situp/plugins/processor/peerforwarder/PeerForwarderTest.java
+++ situp-plugins/peer-forwarder/src/test/java/com/amazon/situp/plugins/processor/peerforwarder/PeerForwarderTest.java
@@ -4,6 +4,7 @@ import com.amazon.situp.model.configuration.PluginSetting;
 import com.amazon.situp.model.record.Record;
 import com.google.protobuf.ByteString;
 import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
+import io.opentelemetry.proto.collector.trace.v1.TraceServiceGrpc;
 import io.opentelemetry.proto.common.v1.InstrumentationLibrary;
 import io.opentelemetry.proto.resource.v1.Resource;
 import io.opentelemetry.proto.trace.v1.InstrumentationLibrarySpans;
@@ -11,13 +12,32 @@ import io.opentelemetry.proto.trace.v1.ResourceSpans;
 import io.opentelemetry.proto.trace.v1.Span;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.doAnswer;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.spy;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PeerForwarder.class)
+@PowerMockIgnore({"javax.net.ssl.*","javax.security.*"})
 public class PeerForwarderTest {
 
     private static final Span SPAN_1 = Span.newBuilder()
@@ -56,7 +76,7 @@ public class PeerForwarderTest {
 
     @Test
     public void testLocalIpOnly() {
-        final PeerForwarder testPeerForwarder = generatePeerForwarder(Collections.emptyList());
+        final PeerForwarder testPeerForwarder = generatePeerForwarder(Collections.emptyList(), 2);
         final List<Record<ExportTraceServiceRequest>> exportedRecords =
                 testPeerForwarder.execute(Arrays.asList(new Record<>(REQUEST_1), new Record<>(REQUEST_2)));
         Assert.assertTrue(exportedRecords.size() >= 3);
@@ -74,10 +94,71 @@ public class PeerForwarderTest {
         Assert.assertTrue(expectedResourceSpans.containsAll(exportedResourceSpans));
     }
 
-    private PeerForwarder generatePeerForwarder(final List<String> peerIps) {
+    @Test
+    public void testProcessRequest() throws Exception {
+        final List<String> testIps = generateTestIps(1);
+        final PeerForwarder testPeerForwarder = spy(generatePeerForwarder(testIps, 3));
+        final TraceServiceGrpc.TraceServiceBlockingStub mockClient = mock(TraceServiceGrpc.TraceServiceBlockingStub.class);
+        doReturn(null).when(mockClient).export(any());
+        final List<Record<ExportTraceServiceRequest>> localBuffer = new ArrayList<>();
+        final Object[] args1 = new Object[]{null, REQUEST_1, localBuffer};
+        Whitebox.invokeMethod(testPeerForwarder, "processRequest", args1);
+        Assert.assertEquals(1, localBuffer.size());
+        Assert.assertEquals(REQUEST_1, localBuffer.get(0).getData());
+        final Object[] args2 = new Object[]{mockClient, REQUEST_2, localBuffer};
+        Whitebox.invokeMethod(testPeerForwarder, "processRequest", args2);
+        Assert.assertEquals(1, localBuffer.size());
+        verify(mockClient, times(1)).export(REQUEST_2);
+    }
+
+    @Test
+    public void testSingleRemoteIp() throws Exception {
+        final List<String> testIps = generateTestIps(1);
+        final PeerForwarder testPeerForwarder = spy(generatePeerForwarder(testIps, 3));
+        final Map<String, List<ExportTraceServiceRequest>> requestsByIp = testIps.stream()
+                .collect(Collectors.toMap(ip-> ip, ip-> new ArrayList<>()));
+        // Mock processRequest
+        doAnswer(invocation -> {
+            final TraceServiceGrpc.TraceServiceBlockingStub client = invocation.getArgument(0);
+            final ExportTraceServiceRequest request = invocation.getArgument(1);
+            if (client != null) {
+                final String host =  client.getChannel().authority().split(":")[0];
+                requestsByIp.get(host).add(request);
+            }
+            return null;
+        }).when(testPeerForwarder, "processRequest",
+                any(),
+                any(ExportTraceServiceRequest.class),
+                any(List.class));
+
+        testPeerForwarder.execute(Arrays.asList(new Record<>(REQUEST_1), new Record<>(REQUEST_2)));
+
+        final List<ResourceSpans> expectedResourceSpans = Arrays.asList(
+                generateResourceSpans(SPAN_5, SPAN_6),
+                generateResourceSpans(SPAN_4)
+        );
+        Assert.assertEquals(1, requestsByIp.get(testIps.get(0)).size());
+        final ExportTraceServiceRequest forwardedRequest = requestsByIp.get(testIps.get(0)).get(0);
+        final List<ResourceSpans> forwardedResourceSpans = forwardedRequest.getResourceSpansList();
+        Assert.assertTrue(forwardedResourceSpans.containsAll(expectedResourceSpans));
+        Assert.assertTrue(expectedResourceSpans.containsAll(forwardedResourceSpans));
+    }
+
+    private List<String> generateTestIps(int num) throws IOException {
+        final String localPublicIp = PeerForwarder.getLocalPublicIp();
+        final String[] ipArray = localPublicIp.split("\\.");
+        List<String> results = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            ipArray[ipArray.length-1] = String.valueOf((Integer.parseInt(ipArray[ipArray.length-1]) + 1));
+            results.add(String.join(".", ipArray));
+        }
+        return results;
+    }
+
+    private PeerForwarder generatePeerForwarder(final List<String> peerIps, final int spansPerRequest) {
         final HashMap<String, Object> settings = new HashMap<>();
         settings.put(PeerForwarderConfig.PEER_IPS, peerIps);
-        settings.put(PeerForwarderConfig.MAX_NUM_SPANS_PER_REQUEST, 2);
+        settings.put(PeerForwarderConfig.MAX_NUM_SPANS_PER_REQUEST, spansPerRequest);
         settings.put(PeerForwarderConfig.TIME_OUT, 300);
         return new PeerForwarder(new PluginSetting("peer_forwarder", settings));
     }
