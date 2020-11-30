a413037f9eb47dc717bdb547c61b090b2fcda83c
Kowshik Nagarajaan <kowshikn@amazon.com>
2020-11-30 11:15:44 -0600

    Rename sample directory

--- examples/situp-wait-for-elastic-oss.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-until [[ $(curl --write-out %{http_code} --output /dev/null --silent --head --fail http://elastic:9200) == 200 ]]; do
-  echo "Waiting for Elastic OSS to be ready"
-  sleep 1
-done
-
-exec java -jar situp.jar /app/transformation-instance.yml
