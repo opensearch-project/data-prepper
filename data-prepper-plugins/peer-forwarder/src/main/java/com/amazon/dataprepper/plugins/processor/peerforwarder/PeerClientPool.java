c6c254227415d2933832a7daa6283ec8b7bdd97a
qchea <qchea@amazon.com>
2020-12-15 17:13:00 -0600

    FIX: add ssl and keyCertchainfile into peerclient pool

--- data-prepper-plugins/peer-forwarder/src/main/java/com/amazon/dataprepper/plugins/processor/peerforwarder/PeerClientPool.java
+++ data-prepper-plugins/peer-forwarder/src/main/java/com/amazon/dataprepper/plugins/processor/peerforwarder/PeerClientPool.java
@@ -1,8 +1,12 @@
 package com.amazon.dataprepper.plugins.processor.peerforwarder;
 
+import com.linecorp.armeria.client.ClientBuilder;
+import com.linecorp.armeria.client.ClientFactory;
+import com.linecorp.armeria.client.ClientFactoryBuilder;
 import com.linecorp.armeria.client.Clients;
 import io.opentelemetry.proto.collector.trace.v1.TraceServiceGrpc;
 
+import java.io.File;
 import java.time.Duration;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -12,6 +16,8 @@ public class PeerClientPool {
     private final Map<String, TraceServiceGrpc.TraceServiceBlockingStub> peerClients;
 
     private int clientTimeoutSeconds = 3;
+    private boolean ssl;
+    private File sslKeyCertChainFile;
 
     private PeerClientPool() {
         peerClients = new ConcurrentHashMap<>();
@@ -24,6 +30,12 @@ public class PeerClientPool {
     public void setClientTimeoutSeconds(int clientTimeoutSeconds) {
         this.clientTimeoutSeconds = clientTimeoutSeconds;
     }
+    public void setIsSsl(boolean ssl) {
+        this.ssl = ssl;
+    }
+    public void setSslKeyCertChainFile(File sslKeyCertChainFile) {
+        this.sslKeyCertChainFile = sslKeyCertChainFile;
+    }
 
     public TraceServiceGrpc.TraceServiceBlockingStub getClient(final String address) {
         // TODO: Resolve to IP first, or is hostname good enough?
@@ -32,8 +44,12 @@ public class PeerClientPool {
 
     private TraceServiceGrpc.TraceServiceBlockingStub createGRPCClient(final String ipAddress) {
         // TODO: replace hardcoded port with customization
-        return Clients.builder(String.format("gproto+http://%s:21890/", ipAddress))
-                .writeTimeout(Duration.ofSeconds(clientTimeoutSeconds))
-                .build(TraceServiceGrpc.TraceServiceBlockingStub.class);
+        final ClientBuilder clientBuilder = Clients.builder(String.format("gproto+http://%s:21890/", ipAddress))
+                .writeTimeout(Duration.ofSeconds(clientTimeoutSeconds));
+        if (ssl) {
+            clientBuilder.factory(ClientFactory.builder()
+                    .tlsCustomizer(sslContextBuilder -> sslContextBuilder.trustManager(sslKeyCertChainFile)).build());
+        }
+        return clientBuilder.build(TraceServiceGrpc.TraceServiceBlockingStub.class);
     }
 }
--- data-prepper-plugins/peer-forwarder/src/main/java/com/amazon/dataprepper/plugins/processor/peerforwarder/PeerForwarderConfig.java
+++ data-prepper-plugins/peer-forwarder/src/main/java/com/amazon/dataprepper/plugins/processor/peerforwarder/PeerForwarderConfig.java
@@ -4,6 +4,8 @@ import com.amazon.dataprepper.model.configuration.PluginSetting;
 import com.amazon.dataprepper.plugins.processor.peerforwarder.discovery.PeerListProvider;
 import com.amazon.dataprepper.plugins.processor.peerforwarder.discovery.PeerListProviderFactory;
 
+import java.io.File;
+
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class PeerForwarderConfig {
@@ -15,24 +17,17 @@ public class PeerForwarderConfig {
     public static final String STATIC_ENDPOINTS = "static_endpoints";
     public static final String SSL = "ssl";
     public static final String SSL_KEY_CERT_FILE = "sslKeyCertChainFile";
-    public static final String SSL_KEY_FILE = "sslKeyFile";
     private static final boolean DEFAULT_SSL = false;
 
     private final HashRing hashRing;
     private final PeerClientPool peerClientPool;
     private final int timeOut;
     private final int maxNumSpansPerRequest;
-    private final boolean ssl;
-    private final String sslKeyCertChainFile;
-    private final String sslKeyFile;
 
     private PeerForwarderConfig(final PeerClientPool peerClientPool,
                                 final HashRing hashRing,
                                 final int timeOut,
-                                final int maxNumSpansPerRequest,
-                                final boolean isSSL,
-                                final String sslKeyCertChainFile,
-                                final String sslKeyFile) {
+                                final int maxNumSpansPerRequest) {
         checkNotNull(peerClientPool);
         checkNotNull(hashRing);
 
@@ -40,31 +35,26 @@ public class PeerForwarderConfig {
         this.hashRing = hashRing;
         this.timeOut = timeOut;
         this.maxNumSpansPerRequest = maxNumSpansPerRequest;
-        this.ssl = isSSL;
-        this.sslKeyCertChainFile = sslKeyCertChainFile;
-        this.sslKeyFile = sslKeyFile;
-        if (ssl && (sslKeyCertChainFile == null || sslKeyCertChainFile.isEmpty())) {
-            throw new IllegalArgumentException(String.format("%s is enable, %s can not be empty or null", SSL, SSL_KEY_CERT_FILE));
-        }
-        if (ssl && (sslKeyFile == null || sslKeyFile.isEmpty())) {
-            throw new IllegalArgumentException(String.format("%s is enable, %s can not be empty or null", SSL, SSL_KEY_CERT_FILE));
-        }
     }
 
     public static PeerForwarderConfig buildConfig(final PluginSetting pluginSetting) {
+        final boolean ssl = pluginSetting.getBooleanOrDefault(SSL, DEFAULT_SSL);
+        final String sslKeyCertChainFile = pluginSetting.getStringOrDefault(SSL_KEY_CERT_FILE, null);
+        if (ssl && (sslKeyCertChainFile == null || sslKeyCertChainFile.isEmpty())) {
+            throw new IllegalArgumentException(String.format("%s is enable, %s can not be empty or null", SSL, SSL_KEY_CERT_FILE));
+        }
         final PeerListProvider peerListProvider = new PeerListProviderFactory().createProvider(pluginSetting);
         final HashRing hashRing = new HashRing(peerListProvider, NUM_VIRTUAL_NODES);
         final PeerClientPool peerClientPool = PeerClientPool.getInstance();
         peerClientPool.setClientTimeoutSeconds(3);
+        peerClientPool.setIsSsl(ssl);
+        peerClientPool.setSslKeyCertChainFile(new File(sslKeyCertChainFile));
 
         return new PeerForwarderConfig(
                 peerClientPool,
                 hashRing,
                 pluginSetting.getIntegerOrDefault(TIME_OUT, 3),
-                pluginSetting.getIntegerOrDefault(MAX_NUM_SPANS_PER_REQUEST, 48),
-                pluginSetting.getBooleanOrDefault(SSL, DEFAULT_SSL),
-                pluginSetting.getStringOrDefault(SSL_KEY_CERT_FILE, null),
-                pluginSetting.getStringOrDefault(SSL_KEY_FILE, null));
+                pluginSetting.getIntegerOrDefault(MAX_NUM_SPANS_PER_REQUEST, 48));
     }
 
     public HashRing getHashRing() {
@@ -81,17 +71,5 @@ public class PeerForwarderConfig {
 
     public int getMaxNumSpansPerRequest() {
         return maxNumSpansPerRequest;
-    }
-
-    public boolean isSsl() {
-        return ssl;
-    }
-
-    public String getSslKeyCertChainFile() {
-        return sslKeyCertChainFile;
-    }
-
-    public String getSslKeyFile() {
-        return sslKeyFile;
     }
 }
