d84ed17baa3981d84e8ea6add26021195225168a
qchea <qchea@amazon.com>
2020-12-15 15:10:29 -0600

    ADD: ssl parameters in PeerForwarderConfig

--- data-prepper-plugins/peer-forwarder/src/main/java/com/amazon/dataprepper/plugins/processor/peerforwarder/PeerForwarderConfig.java
+++ data-prepper-plugins/peer-forwarder/src/main/java/com/amazon/dataprepper/plugins/processor/peerforwarder/PeerForwarderConfig.java
@@ -13,16 +13,26 @@ public class PeerForwarderConfig {
     public static final String DISCOVERY_MODE = "discovery_mode";
     public static final String HOSTNAME_FOR_DNS_LOOKUP = "hostname_for_dns_lookup";
     public static final String STATIC_ENDPOINTS = "static_endpoints";
+    public static final String SSL = "ssl";
+    public static final String SSL_KEY_CERT_FILE = "sslKeyCertChainFile";
+    public static final String SSL_KEY_FILE = "sslKeyFile";
+    private static final boolean DEFAULT_SSL = false;
 
     private final HashRing hashRing;
     private final PeerClientPool peerClientPool;
     private final int timeOut;
     private final int maxNumSpansPerRequest;
+    private final boolean ssl;
+    private final String sslKeyCertChainFile;
+    private final String sslKeyFile;
 
     private PeerForwarderConfig(final PeerClientPool peerClientPool,
                                 final HashRing hashRing,
                                 final int timeOut,
-                                final int maxNumSpansPerRequest) {
+                                final int maxNumSpansPerRequest,
+                                final boolean isSSL,
+                                final String sslKeyCertChainFile,
+                                final String sslKeyFile) {
         checkNotNull(peerClientPool);
         checkNotNull(hashRing);
 
@@ -30,6 +40,15 @@ public class PeerForwarderConfig {
         this.hashRing = hashRing;
         this.timeOut = timeOut;
         this.maxNumSpansPerRequest = maxNumSpansPerRequest;
+        this.ssl = isSSL;
+        this.sslKeyCertChainFile = sslKeyCertChainFile;
+        this.sslKeyFile = sslKeyFile;
+        if (ssl && (sslKeyCertChainFile == null || sslKeyCertChainFile.isEmpty())) {
+            throw new IllegalArgumentException(String.format("%s is enable, %s can not be empty or null", SSL, SSL_KEY_CERT_FILE));
+        }
+        if (ssl && (sslKeyFile == null || sslKeyFile.isEmpty())) {
+            throw new IllegalArgumentException(String.format("%s is enable, %s can not be empty or null", SSL, SSL_KEY_CERT_FILE));
+        }
     }
 
     public static PeerForwarderConfig buildConfig(final PluginSetting pluginSetting) {
@@ -42,7 +61,10 @@ public class PeerForwarderConfig {
                 peerClientPool,
                 hashRing,
                 pluginSetting.getIntegerOrDefault(TIME_OUT, 3),
-                pluginSetting.getIntegerOrDefault(MAX_NUM_SPANS_PER_REQUEST, 48));
+                pluginSetting.getIntegerOrDefault(MAX_NUM_SPANS_PER_REQUEST, 48),
+                pluginSetting.getBooleanOrDefault(SSL, DEFAULT_SSL),
+                pluginSetting.getStringOrDefault(SSL_KEY_CERT_FILE, null),
+                pluginSetting.getStringOrDefault(SSL_KEY_FILE, null));
     }
 
     public HashRing getHashRing() {
@@ -59,5 +81,17 @@ public class PeerForwarderConfig {
 
     public int getMaxNumSpansPerRequest() {
         return maxNumSpansPerRequest;
+    }
+
+    public boolean isSsl() {
+        return ssl;
+    }
+
+    public String getSslKeyCertChainFile() {
+        return sslKeyCertChainFile;
+    }
+
+    public String getSslKeyFile() {
+        return sslKeyFile;
     }
 }
