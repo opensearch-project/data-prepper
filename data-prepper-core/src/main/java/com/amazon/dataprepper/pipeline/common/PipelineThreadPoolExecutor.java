ba9307ef6d7d0a90ebbd486a2493b7644ad45ad7
Jeff Wright <74204404+wrijeff@users.noreply.github.com>
2021-01-04 19:12:54 +0000

    Increased code coverage threshold for data-prepper-core project.

--- data-prepper-core/src/main/java/com/amazon/dataprepper/pipeline/common/PipelineThreadPoolExecutor.java
+++ data-prepper-core/src/main/java/com/amazon/dataprepper/pipeline/common/PipelineThreadPoolExecutor.java
@@ -37,13 +37,6 @@ public class PipelineThreadPoolExecutor extends ThreadPoolExecutor {
         this.pipeline = pipeline;
     }
 
-    public static PipelineThreadPoolExecutor newSingleThreadExecutor(
-            final ThreadFactory threadFactory,
-            final Pipeline pipeline) {
-        return new PipelineThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<>(), threadFactory, pipeline);
-    }
-
     public static PipelineThreadPoolExecutor newFixedThreadPool(
             final int nThreads,
             final ThreadFactory threadFactory,
