6d6b72cfc85e1803b1fba06c04e61915b0631798
qchea <qchea@amazon.com>
2021-01-06 11:22:38 -0600

    TST: more test cases to cover RetryConfigurationTests

--- data-prepper-plugins/elasticsearch/src/test/java/com/amazon/dataprepper/plugins/sink/elasticsearch/RetryConfigurationTests.java
+++ data-prepper-plugins/elasticsearch/src/test/java/com/amazon/dataprepper/plugins/sink/elasticsearch/RetryConfigurationTests.java
@@ -1,6 +1,10 @@
 package com.amazon.dataprepper.plugins.sink.elasticsearch;
 
+import com.amazon.dataprepper.model.configuration.PluginSetting;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -13,11 +17,24 @@ public class RetryConfigurationTests {
     }
 
     @Test
-    public void testConfigurationWithCustomDlqFilePath() {
+    public void testReadRetryConfigNoDLQFilePath() {
+        final RetryConfiguration retryConfiguration = RetryConfiguration.readRetryConfig(generatePluginSetting(null));
+        assertNull(retryConfiguration.getDlqFile());
+    }
+
+    @Test
+    public void testReadRetryConfigWithDLQFilePath() {
         final String fakeDlqFilePath = "foo.txt";
-        final RetryConfiguration retryConfiguration = new RetryConfiguration.Builder()
-                .withDlqFile(fakeDlqFilePath)
-                .build();
+        final RetryConfiguration retryConfiguration = RetryConfiguration.readRetryConfig(generatePluginSetting(fakeDlqFilePath));
         assertEquals(fakeDlqFilePath, retryConfiguration.getDlqFile());
+    }
+
+    private PluginSetting generatePluginSetting(final String dlqFilePath) {
+        final Map<String, Object> metadata = new HashMap<>();
+        if (dlqFilePath != null) {
+            metadata.put(RetryConfiguration.DLQ_FILE, dlqFilePath);
+        }
+
+        return new PluginSetting("elasticsearch", metadata);
     }
 }
