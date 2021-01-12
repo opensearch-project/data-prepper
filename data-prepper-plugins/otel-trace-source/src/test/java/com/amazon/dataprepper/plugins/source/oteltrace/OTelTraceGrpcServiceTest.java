84a96903696bfdbf3739acb74673dc81bbec4115
Shivani Shukla <sshkamz@amazon.com>
2021-01-12 09:37:47 -0600

    Adding test cases for the OTel Trace Grpc Service metrics

---
+++ data-prepper-plugins/otel-trace-source/src/test/java/com/amazon/dataprepper/plugins/source/oteltrace/OTelTraceGrpcServiceTest.java
@@ -0,0 +1,83 @@
+package com.amazon.dataprepper.plugins.source.oteltrace;
+
+import com.amazon.dataprepper.metrics.MetricsTestUtil;
+import com.amazon.dataprepper.metrics.PluginMetrics;
+import com.amazon.dataprepper.model.buffer.Buffer;
+import com.amazon.dataprepper.model.configuration.PluginSetting;
+import com.amazon.dataprepper.model.record.Record;
+import io.grpc.stub.StreamObserver;
+import io.micrometer.core.instrument.Counter;
+import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
+import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.concurrent.TimeoutException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+
+public class OTelTraceGrpcServiceTest {
+
+    private OTelTraceGrpcService oTelTraceGrpcService;
+    private static PluginSetting pluginSetting;
+    private final int bufferWriteTimeoutInMillis = 100000;
+
+    @BeforeEach
+    public void setup() {
+        pluginSetting = new PluginSetting("OTelTraceGrpcService", Collections.EMPTY_MAP);
+        pluginSetting.setPipelineName("pipeline");
+    }
+
+    @Test
+    public void testRecordsWrittenCounter() {
+
+        PluginMetrics mockPluginMetrics = mock(PluginMetrics.class);
+        MetricsTestUtil.initMetrics();
+
+        ExportTraceServiceRequest request = ExportTraceServiceRequest.newBuilder().build();
+        StreamObserver response = mock(StreamObserver.class);
+        Counter mockRecordsWrittenCounter = mock(Counter.class);
+        Counter mockTimeoutCounter = mock(Counter.class);
+        when(mockPluginMetrics.counter(OTelTraceGrpcService.RECORDS_WRITTEN)).thenReturn(mockRecordsWrittenCounter);
+        when(mockPluginMetrics.counter(OTelTraceGrpcService.REQUEST_TIMEOUTS)).thenReturn(mockTimeoutCounter);
+
+        oTelTraceGrpcService = new OTelTraceGrpcService(bufferWriteTimeoutInMillis, mock(Buffer.class), mockPluginMetrics);
+        oTelTraceGrpcService.export(request, response);
+
+        verify(response, times(1)).onNext(ExportTraceServiceResponse.newBuilder().build());
+        verify(response, times(1)).onCompleted();
+        verify(mockRecordsWrittenCounter, times(1)).increment();
+        verifyZeroInteractions(mockTimeoutCounter);
+    }
+
+    @Test
+    public void testTimeoutCounter() throws TimeoutException {
+
+        PluginMetrics mockPluginMetrics = mock(PluginMetrics.class);
+        Buffer mockBuffer = mock(Buffer.class);
+        MetricsTestUtil.initMetrics();
+        ExportTraceServiceRequest request = ExportTraceServiceRequest.newBuilder().build();
+        StreamObserver response = mock(StreamObserver.class);
+        Counter mockRecordsWrittenCounter = mock(Counter.class);
+        Counter mockTimeoutCounter = mock(Counter.class);
+        doThrow(new TimeoutException()).when(mockBuffer).write(any(Record.class), anyInt());
+        when(mockPluginMetrics.counter(OTelTraceGrpcService.REQUEST_TIMEOUTS)).thenReturn(mockTimeoutCounter);
+
+        oTelTraceGrpcService = new OTelTraceGrpcService(bufferWriteTimeoutInMillis, mockBuffer, mockPluginMetrics);
+        oTelTraceGrpcService.export(request, response);
+
+        verify(response, times(0)).onNext(ExportTraceServiceResponse.newBuilder().build());
+        verify(response, times(0)).onCompleted();
+        verify(mockTimeoutCounter, times(1)).increment();
+        verifyZeroInteractions(mockRecordsWrittenCounter);
+    }
+}
