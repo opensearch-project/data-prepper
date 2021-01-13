79ad3416775d6e9c8d5b1fce4714de6b0169dcbe
Jeff Wright <74204404+wrijeff@users.noreply.github.com>
2021-01-13 21:54:11 +0000

    Changed k8s project to build Data Prepper locally. Changed /app to /usr/share/data-prepper in build files.

--- examples/dev/k8s/build_images_for_minikube.sh
+++ examples/dev/k8s/build_images_for_minikube.sh
@@ -2,3 +2,4 @@
 eval $(minikube docker-env)
 docker build ../../trace-analytics-sample-app/sample-app -t example-k8s/sample-app
 docker build ../../kibana-trace-analytics -t example-k8s/kibana
+docker build ../../.. -f ../../../examples/dev/trace-analytics-sample-app/Dockerfile -t example-k8s/data-prepper
