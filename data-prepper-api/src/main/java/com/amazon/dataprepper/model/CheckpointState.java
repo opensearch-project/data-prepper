908cb87b4381fdeafadeadb41af1dc11cf6f793a
qchea <qchea@amazon.com>
2021-01-25 15:25:55 -0600

    MAINT: CheckpointState docstring

--- data-prepper-api/src/main/java/com/amazon/dataprepper/model/CheckpointState.java
+++ data-prepper-api/src/main/java/com/amazon/dataprepper/model/CheckpointState.java
@@ -1,5 +1,8 @@
 package com.amazon.dataprepper.model;
 
+/**
+ * CheckpointState keeps track of a summary of records processed by a data-prepper worker thread.
+ */
 public class CheckpointState {
     private final int numCheckedRecords;
 
