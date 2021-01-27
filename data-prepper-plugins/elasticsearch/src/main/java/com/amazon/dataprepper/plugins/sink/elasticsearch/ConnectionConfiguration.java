c7cde2602621243208586dc3d643709e47d21843
Kowshik Nagarajaan <kowshikn@amazon.com>
2021-01-26 21:29:32 -0600

    Add AWSSigV4 signing

--- data-prepper-plugins/elasticsearch/src/main/java/com/amazon/dataprepper/plugins/sink/elasticsearch/ConnectionConfiguration.java
+++ data-prepper-plugins/elasticsearch/src/main/java/com/amazon/dataprepper/plugins/sink/elasticsearch/ConnectionConfiguration.java
@@ -1,7 +1,12 @@
 package com.amazon.dataprepper.plugins.sink.elasticsearch;
 
 import com.amazon.dataprepper.model.configuration.PluginSetting;
+import com.amazonaws.auth.AWS4Signer;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.http.AWSRequestSigningApacheInterceptor;
 import org.apache.http.HttpHost;
+import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
@@ -32,6 +37,9 @@ import static com.google.common.base.Preconditions.checkArgument;
 public class ConnectionConfiguration {
   private static final Logger LOG = LoggerFactory.getLogger(ElasticsearchSink.class);
 
+  private static final String SERVICE_NAME = "es";
+  private static final String DEFAULT_AWS_REGION = "us-east-1";
+
   public static final String HOSTS = "hosts";
   public static final String USERNAME = "username";
   public static final String PASSWORD = "password";
@@ -39,6 +47,8 @@ public class ConnectionConfiguration {
   public static final String CONNECT_TIMEOUT = "connect_timeout";
   public static final String CERT_PATH = "cert";
   public static final String INSECURE = "insecure";
+  public static final String AWS_SIGV4 = "aws_sigv4";
+  public static final String AWS_REGION = "aws_region";
 
   private final List<String> hosts;
   private final String username;
@@ -47,6 +57,8 @@ public class ConnectionConfiguration {
   private final Integer socketTimeout;
   private final Integer connectTimeout;
   private final boolean insecure;
+  private final boolean awsSigv4;
+  private final String awsRegion;
 
   public List<String> getHosts() {
     return hosts;
@@ -58,6 +70,14 @@ public class ConnectionConfiguration {
 
   public String getPassword() {
     return password;
+  }
+
+  public boolean isAwsSigv4() {
+    return awsSigv4;
+  }
+
+  public String getAwsRegion() {
+    return awsRegion;
   }
 
   public Integer getSocketTimeout() {
@@ -76,6 +96,8 @@ public class ConnectionConfiguration {
     this.connectTimeout = builder.connectTimeout;
     this.certPath = builder.certPath;
     this.insecure = builder.insecure;
+    this.awsSigv4 = builder.awsSigv4;
+    this.awsRegion = builder.awsRegion;
   }
 
   public static ConnectionConfiguration readConnectionConfiguration(final PluginSetting pluginSetting){
@@ -98,6 +120,10 @@ public class ConnectionConfiguration {
     if (connectTimeout != null) {
       builder = builder.withConnectTimeout(connectTimeout);
     }
+
+    builder.withAwsSigv4(pluginSetting.getBooleanOrDefault(AWS_SIGV4, false));
+    builder.withAwsRegion(pluginSetting.getStringOrDefault(AWS_REGION, DEFAULT_AWS_REGION));
+
     final String certPath = pluginSetting.getStringOrDefault(CERT_PATH, null);
     final boolean insecure = pluginSetting.getBooleanOrDefault(INSECURE, false);
     if (certPath != null) {
@@ -121,6 +147,43 @@ public class ConnectionConfiguration {
       i++;
     }
     final RestClientBuilder restClientBuilder = RestClient.builder(httpHosts);
+    /**
+     * Given that this is a patch release, we will support only the IAM based access policy AES domains.
+     * We will not support FGAC and Custom endpoint domains. This will be followed in the next version.
+     */
+    if(awsSigv4) {
+      attachAWSSigV4Callback(restClientBuilder);
+    } else {
+      attachSSLUsernameContext(restClientBuilder);
+    }
+    restClientBuilder.setRequestConfigCallback(
+            requestConfigBuilder -> {
+              if (connectTimeout != null) {
+                requestConfigBuilder.setConnectTimeout(connectTimeout);
+              }
+              if (socketTimeout != null) {
+                requestConfigBuilder.setSocketTimeout(socketTimeout);
+              }
+              return requestConfigBuilder;
+            });
+    return new RestHighLevelClient(restClientBuilder);
+  }
+
+  private void attachAWSSigV4Callback(final RestClientBuilder restClientBuilder) {
+    //if aws signing is enabled we will add AWSRequestSigningApacheInterceptor interceptor,
+    //if not follow regular credentials process
+    LOG.info("{} is set, will sign requests using AWSRequestSigningApacheInterceptor", AWS_SIGV4);
+    final AWS4Signer aws4Signer = new AWS4Signer();
+    aws4Signer.setServiceName(SERVICE_NAME);
+    aws4Signer.setRegionName(awsRegion);
+    final AWSCredentialsProvider credentialsProvider = new DefaultAWSCredentialsProviderChain();
+    final HttpRequestInterceptor httpRequestInterceptor = new AWSRequestSigningApacheInterceptor(SERVICE_NAME, aws4Signer,
+            credentialsProvider);
+    restClientBuilder.setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.addInterceptorLast(
+            httpRequestInterceptor));
+  }
+
+  private void attachSSLUsernameContext(final RestClientBuilder restClientBuilder) {
     final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
     if (username != null) {
       LOG.info("Using the username provided in the config.");
@@ -139,17 +202,6 @@ public class ConnectionConfiguration {
               return httpClientBuilder;
             }
     );
-    restClientBuilder.setRequestConfigCallback(
-            requestConfigBuilder -> {
-              if (connectTimeout != null) {
-                requestConfigBuilder.setConnectTimeout(connectTimeout);
-              }
-              if (socketTimeout != null) {
-                requestConfigBuilder.setSocketTimeout(socketTimeout);
-              }
-              return requestConfigBuilder;
-            });
-    return new RestHighLevelClient(restClientBuilder);
   }
 
   private SSLContext getCAStrategy(Path certPath) {
@@ -189,6 +241,8 @@ public class ConnectionConfiguration {
     private Integer connectTimeout;
     private Path certPath;
     private boolean insecure;
+    private boolean awsSigv4;
+    private String awsRegion;
 
 
     public Builder(final List<String> hosts) {
@@ -229,6 +283,17 @@ public class ConnectionConfiguration {
 
     public Builder withInsecure(final boolean insecure) {
       this.insecure = insecure;
+      return this;
+    }
+
+    public Builder withAwsSigv4(final boolean awsSigv4) {
+      this.awsSigv4 = awsSigv4;
+      return this;
+    }
+
+    public Builder withAwsRegion(final String awsRegion) {
+      checkArgument(awsRegion != null, "awsRegion cannot be null");
+      this.awsRegion = awsRegion;
       return this;
     }
 
--- data-prepper-plugins/elasticsearch/src/test/java/com/amazon/dataprepper/plugins/sink/elasticsearch/ConnectionConfigurationTests.java
+++ data-prepper-plugins/elasticsearch/src/test/java/com/amazon/dataprepper/plugins/sink/elasticsearch/ConnectionConfigurationTests.java
@@ -1,58 +1,139 @@
 package com.amazon.dataprepper.plugins.sink.elasticsearch;
 
 import com.amazon.dataprepper.model.configuration.PluginSetting;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 public class ConnectionConfigurationTests {
+    private final List<String> TEST_HOSTS = Collections.singletonList("http://localhost:9200");
+    private final String TEST_USERNAME = "admin";
+    private final String TEST_PASSWORD = "admin";
+    private final Integer TEST_CONNECT_TIMEOUT = 5;
+    private final Integer TEST_SOCKET_TIMEOUT = 10;
+    private final String TEST_CERT_PATH = Objects.requireNonNull(getClass().getClassLoader().getResource("test-ca.pem")).getFile();
+
     @Test
     public void testReadConnectionConfigurationDefault() {
-        final List<String> testHosts = Collections.singletonList("http://localhost:9200");
         final PluginSetting pluginSetting = generatePluginSetting(
-                testHosts, null, null, null, null);
+                TEST_HOSTS, null, null, null, null, false, null, null, false);
         final ConnectionConfiguration connectionConfiguration =
                 ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
-        assertEquals(testHosts, connectionConfiguration.getHosts());
+        assertEquals(TEST_HOSTS, connectionConfiguration.getHosts());
         assertNull(connectionConfiguration.getUsername());
         assertNull(connectionConfiguration.getPassword());
+        assertFalse(connectionConfiguration.isAwsSigv4());
+        assertNull(connectionConfiguration.getCertPath());
         assertNull(connectionConfiguration.getConnectTimeout());
         assertNull(connectionConfiguration.getSocketTimeout());
     }
 
     @Test
-    public void testReadConnectionConfigurationCustom() {
-        final List<String> testHosts = Collections.singletonList("http://localhost:9200");
-        final String username = "admin";
-        final String password = "admin";
-        final Integer connectTimeout = 5;
-        final Integer socketTimeout = 10;
+    public void testCreateClientDefault() throws IOException {
         final PluginSetting pluginSetting = generatePluginSetting(
-                testHosts, username, password, connectTimeout, socketTimeout);
+                TEST_HOSTS, null, null, null, null, false, null, null, false);
         final ConnectionConfiguration connectionConfiguration =
                 ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
-        assertEquals(testHosts, connectionConfiguration.getHosts());
-        assertEquals(username, connectionConfiguration.getUsername());
-        assertEquals(password, connectionConfiguration.getPassword());
-        assertEquals(connectTimeout, connectionConfiguration.getConnectTimeout());
-        assertEquals(socketTimeout, connectionConfiguration.getSocketTimeout());
+        final RestHighLevelClient client = connectionConfiguration.createClient();
+        assertNotNull(client);
+        client.close();
+    }
+
+    @Test
+    public void testReadConnectionConfigurationNoCert() {
+        final PluginSetting pluginSetting = generatePluginSetting(
+                TEST_HOSTS, TEST_USERNAME, TEST_PASSWORD, TEST_CONNECT_TIMEOUT, TEST_SOCKET_TIMEOUT, false, null, null, false);
+        final ConnectionConfiguration connectionConfiguration =
+                ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
+        assertEquals(TEST_HOSTS, connectionConfiguration.getHosts());
+        assertEquals(TEST_USERNAME, connectionConfiguration.getUsername());
+        assertEquals(TEST_PASSWORD, connectionConfiguration.getPassword());
+        assertEquals(TEST_CONNECT_TIMEOUT, connectionConfiguration.getConnectTimeout());
+        assertEquals(TEST_SOCKET_TIMEOUT, connectionConfiguration.getSocketTimeout());
+        assertFalse(connectionConfiguration.isAwsSigv4());
+    }
+
+    @Test
+    public void testCreateClientNoCert() throws IOException {
+        final PluginSetting pluginSetting = generatePluginSetting(
+                TEST_HOSTS, TEST_USERNAME, TEST_PASSWORD, TEST_CONNECT_TIMEOUT, TEST_SOCKET_TIMEOUT, false, null, null, false);
+        final ConnectionConfiguration connectionConfiguration =
+                ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
+        final RestHighLevelClient client = connectionConfiguration.createClient();
+        assertNotNull(client);
+        client.close();
+    }
+
+    @Test
+    public void testCreateClientInsecure() throws IOException {
+        final PluginSetting pluginSetting = generatePluginSetting(
+                TEST_HOSTS, TEST_USERNAME, TEST_PASSWORD, TEST_CONNECT_TIMEOUT, TEST_SOCKET_TIMEOUT, false, null, null, true);
+        final ConnectionConfiguration connectionConfiguration =
+                ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
+        final RestHighLevelClient client = connectionConfiguration.createClient();
+        assertNotNull(client);
+        client.close();
+    }
+
+    @Test
+    public void testCreateClientWithCertPath() throws IOException {
+        final PluginSetting pluginSetting = generatePluginSetting(
+                TEST_HOSTS, TEST_USERNAME, TEST_PASSWORD, TEST_CONNECT_TIMEOUT, TEST_SOCKET_TIMEOUT, false, null, TEST_CERT_PATH, false);
+        final ConnectionConfiguration connectionConfiguration =
+                ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
+        final RestHighLevelClient client = connectionConfiguration.createClient();
+        assertNotNull(client);
+        client.close();
+    }
+
+    @Test
+    public void testCreateClientWithAWSSigV4AndRegion() throws IOException {
+        final PluginSetting pluginSetting = generatePluginSetting(
+                TEST_HOSTS, null, null, null, null, true, "us-west-2", null, false);
+        final ConnectionConfiguration connectionConfiguration =
+                ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
+        assertEquals("us-west-2", connectionConfiguration.getAwsRegion());
+        assertTrue(connectionConfiguration.isAwsSigv4());;
+    }
+
+    @Test
+    public void testCreateClientWithAWSSigV4DefaultRegion() throws IOException {
+        final PluginSetting pluginSetting = generatePluginSetting(
+                TEST_HOSTS, null, null, null, null, true, null, null, false);
+        final ConnectionConfiguration connectionConfiguration =
+                ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
+        assertEquals("us-east-1", connectionConfiguration.getAwsRegion());
+        assertTrue(connectionConfiguration.isAwsSigv4());;
     }
 
     private PluginSetting generatePluginSetting(
             final List<String> hosts, final String username, final String password,
-            final Integer connectTimeout, final Integer socketTimeout) {
+            final Integer connectTimeout, final Integer socketTimeout, final boolean awsSigv4, final String awsRegion,
+            final String certPath, final boolean insecure) {
         final Map<String, Object> metadata = new HashMap<>();
         metadata.put("hosts", hosts);
         metadata.put("username", username);
         metadata.put("password", password);
         metadata.put("connect_timeout", connectTimeout);
         metadata.put("socket_timeout", socketTimeout);
+        metadata.put("aws_sigv4", awsSigv4);
+        if (awsRegion != null) {
+            metadata.put("aws_region", awsRegion);
+        }
+        metadata.put("cert", certPath);
+        metadata.put("insecure", insecure);
 
         return new PluginSetting("elasticsearch", metadata);
     }
