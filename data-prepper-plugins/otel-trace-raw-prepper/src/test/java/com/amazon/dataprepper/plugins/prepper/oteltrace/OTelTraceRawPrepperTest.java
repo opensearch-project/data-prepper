fac55204c057a171da01ca41b69fd4bfdb816f23
qchea <qchea@amazon.com>
2021-03-09 17:20:04 -0600

    TST: flushbyMixedAndMultiThread

--- data-prepper-plugins/otel-trace-raw-prepper/src/test/java/com/amazon/dataprepper/plugins/prepper/oteltrace/OTelTraceRawPrepperTest.java
+++ data-prepper-plugins/otel-trace-raw-prepper/src/test/java/com/amazon/dataprepper/plugins/prepper/oteltrace/OTelTraceRawPrepperTest.java
@@ -124,9 +124,7 @@ public class OTelTraceRawPrepperTest {
             processedRecords.addAll(future.get());
         }
         Thread.sleep(TEST_PARENT_SPAN_FLUSH_DELAY * SEC_TO_MILLIS);
-        futures = new ArrayList<>();
-        futures.addAll(submitExportTraceServiceRequests(executorService, Collections.emptyList()));
-        futures.addAll(submitExportTraceServiceRequests(executorService, Collections.emptyList()));
+        futures = submitExportTraceServiceRequests(executorService, Collections.emptyList());
         for (Future<Collection<Record<String>>> future : futures) {
             processedRecords.addAll(future.get());
         }
@@ -146,6 +144,36 @@ public class OTelTraceRawPrepperTest {
         Thread.sleep((TEST_GC_INTERVAL - TEST_PARENT_SPAN_FLUSH_DELAY) * SEC_TO_MILLIS);
         processedRecords = (List<Record<String>>) oTelTraceRawPrepper.doExecute(Collections.emptyList());
         Assertions.assertThat(processedRecords.size()).isEqualTo(4);
+    }
+
+    @Test
+    public void testExportRequestFlushByMixedMultiThread() throws InvalidProtocolBufferException, InterruptedException, ExecutionException {
+        final String sampleRequest1 = "{\"resourceSpans\":[{\"resource\":{\"attributes\":[{\"key\":\"service.name\",\"value\":{\"stringValue\":\"analytics-service\"}},{\"key\":\"telemetry.sdk.language\",\"value\":{\"stringValue\":\"java\"}},{\"key\":\"telemetry.sdk.name\",\"value\":{\"stringValue\":\"opentelemetry\"}},{\"key\":\"telemetry.sdk.version\",\"value\":{\"stringValue\":\"0.8.0-SNAPSHOT\"}}],\"droppedAttributesCount\":0},\"instrumentationLibrarySpans\":[{\"instrumentationLibrary\":{\"name\":\"io.opentelemetry.auto.spring-webmvc-3.1\",\"version\":\"\"},\"spans\":[{\"traceId\":\"/6V20yEXOsbO82Acj0vedQ==\",\"spanId\":\"CFrAgv/Pv40=\",\"traceState\":\"\",\"parentSpanId\":\"yxwHNNFJQP0=\",\"name\":\"LoggingController.save\",\"kind\":\"SPAN_KIND_INTERNAL\",\"startTimeUnixNano\":\"1597902043168792500\",\"endTimeUnixNano\":\"1597902043215953100\",\"attributes\":[],\"droppedAttributesCount\":0,\"events\":[],\"droppedEventsCount\":0,\"links\":[],\"droppedLinksCount\":0,\"status\":{\"code\":\"STATUS_CODE_OK\",\"message\":\"\"}}]},{\"instrumentationLibrary\":{\"name\":\"io.opentelemetry.auto.apache-httpasyncclient-4.0\",\"version\":\"\"},\"spans\":[{\"traceId\":\"/6V20yEXOsbO82Acj0vedQ==\",\"spanId\":\"G4PRdsC1+0Y=\",\"traceState\":\"\",\"parentSpanId\":\"CFrAgv/Pv40=\",\"name\":\"HTTP PUT\",\"kind\":\"SPAN_KIND_CLIENT\",\"startTimeUnixNano\":\"1597902043175204700\",\"endTimeUnixNano\":\"1597902043205117100\",\"attributes\":[{\"key\":\"http.status_code\",\"value\":{\"intValue\":\"200\"}},{\"key\":\"http.url\",\"value\":{\"stringValue\":\"/logs/_doc/service_1?timeout\\u003d1m\"}},{\"key\":\"http.method\",\"value\":{\"stringValue\":\"PUT\"}}],\"droppedAttributesCount\":0,\"events\":[],\"droppedEventsCount\":0,\"links\":[],\"droppedLinksCount\":0,\"status\":{\"code\":\"STATUS_CODE_OK\",\"message\":\"\"}}]},{\"instrumentationLibrary\":{\"name\":\"io.opentelemetry.auto.servlet-3.0\",\"version\":\"\"},\"spans\":[{\"traceId\":\"/6V20yEXOsbO82Acj0vedQ==\",\"spanId\":\"yxwHNNFJQP0=\",\"traceState\":\"\",\"parentSpanId\":\"\",\"name\":\"/logs\",\"kind\":\"SPAN_KIND_SERVER\",\"startTimeUnixNano\":\"1597902043168010200\",\"endTimeUnixNano\":\"1597902043217170200\",\"attributes\":[{\"key\":\"http.status_code\",\"value\":{\"intValue\":\"200\"}},{\"key\":\"net.peer.port\",\"value\":{\"intValue\":\"41164\"}},{\"key\":\"servlet.path\",\"value\":{\"stringValue\":\"/logs\"}},{\"key\":\"http.response_content_length\",\"value\":{\"intValue\":\"7\"}},{\"key\":\"http.user_agent\",\"value\":{\"stringValue\":\"curl/7.54.0\"}},{\"key\":\"http.flavor\",\"value\":{\"stringValue\":\"HTTP/1.1\"}},{\"key\":\"servlet.context\",\"value\":{\"stringValue\":\"\"}},{\"key\":\"http.url\",\"value\":{\"stringValue\":\"http://0.0.0.0:8087/logs\"}},{\"key\":\"net.peer.ip\",\"value\":{\"stringValue\":\"172.29.0.1\"}},{\"key\":\"http.method\",\"value\":{\"stringValue\":\"POST\"}},{\"key\":\"http.client_ip\",\"value\":{\"stringValue\":\"172.29.0.1\"}}],\"droppedAttributesCount\":0,\"events\":[],\"droppedEventsCount\":0,\"links\":[],\"droppedLinksCount\":0,\"status\":{\"code\":\"STATUS_CODE_OK\",\"message\":\"\"}}]}]}]}";
+        final String sampleRequest2 = "{\"resourceSpans\":[{\"resource\":{\"attributes\":[{\"key\":\"service.name\",\"value\":{\"stringValue\":\"analytics-service\"}},{\"key\":\"telemetry.sdk.language\",\"value\":{\"stringValue\":\"java\"}},{\"key\":\"telemetry.sdk.name\",\"value\":{\"stringValue\":\"opentelemetry\"}},{\"key\":\"telemetry.sdk.version\",\"value\":{\"stringValue\":\"0.8.0-SNAPSHOT\"}}],\"droppedAttributesCount\":0},\"instrumentationLibrarySpans\":[{\"instrumentationLibrary\":{\"name\":\"io.opentelemetry.auto.spring-webmvc-3.1\",\"version\":\"\"},\"spans\":[{\"traceId\":\"bQ/2NNEmtuwsGAOR5ntCNw==\",\"spanId\":\"WsV2GFh9pxw=\",\"traceState\":\"\",\"parentSpanId\":\"mnO/qUT5ye4=\",\"name\":\"LoggingController.save\",\"kind\":\"SPAN_KIND_INTERNAL\",\"startTimeUnixNano\":\"1597902046041803300\",\"endTimeUnixNano\":\"1597902046088892200\",\"attributes\":[],\"droppedAttributesCount\":0,\"events\":[],\"droppedEventsCount\":0,\"links\":[],\"droppedLinksCount\":0,\"status\":{\"code\":\"STATUS_CODE_OK\",\"message\":\"\"}}]},{\"instrumentationLibrary\":{\"name\":\"io.opentelemetry.auto.apache-httpasyncclient-4.0\",\"version\":\"\"},\"spans\":[{\"traceId\":\"bQ/2NNEmtuwsGAOR5ntCNw==\",\"spanId\":\"na9fwApQsYs=\",\"traceState\":\"\",\"parentSpanId\":\"WsV2GFh9pxw=\",\"name\":\"HTTP PUT\",\"kind\":\"SPAN_KIND_CLIENT\",\"startTimeUnixNano\":\"1597902046052809200\",\"endTimeUnixNano\":\"1597902046084822500\",\"attributes\":[{\"key\":\"http.status_code\",\"value\":{\"intValue\":\"200\"}},{\"key\":\"http.url\",\"value\":{\"stringValue\":\"/logs/_doc/service_1?timeout\\u003d1m\"}},{\"key\":\"http.method\",\"value\":{\"stringValue\":\"PUT\"}}],\"droppedAttributesCount\":0,\"events\":[],\"droppedEventsCount\":0,\"links\":[],\"droppedLinksCount\":0,\"status\":{\"code\":\"STATUS_CODE_OK\",\"message\":\"\"}}]}]}]}";
+        final ExportTraceServiceRequest exportTraceServiceRequest1 = buildExportTraceServiceRequestFromJson(sampleRequest1);
+        final ExportTraceServiceRequest exportTraceServiceRequest2 = buildExportTraceServiceRequestFromJson(sampleRequest2);
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        List<Record<String>> processedRecords = new ArrayList<>();
+        List<Future<Collection<Record<String>>>> futures = new ArrayList<>();
+        futures.addAll(submitExportTraceServiceRequests(executorService, Collections.singletonList(exportTraceServiceRequest1)));
+        futures.addAll(submitExportTraceServiceRequests(executorService, Collections.singletonList(exportTraceServiceRequest2)));
+        for (Future<Collection<Record<String>>> future : futures) {
+            processedRecords.addAll(future.get());
+        }
+        Thread.sleep(TEST_PARENT_SPAN_FLUSH_DELAY * SEC_TO_MILLIS);
+        futures = submitExportTraceServiceRequests(executorService, Collections.emptyList());
+        for (Future<Collection<Record<String>>> future : futures) {
+            processedRecords.addAll(future.get());
+        }
+        Assertions.assertThat(processedRecords.size()).isEqualTo(3);
+        Thread.sleep((TEST_GC_INTERVAL - TEST_PARENT_SPAN_FLUSH_DELAY) * SEC_TO_MILLIS);
+        futures = submitExportTraceServiceRequests(executorService, Collections.emptyList());
+        for (Future<Collection<Record<String>>> future : futures) {
+            processedRecords.addAll(future.get());
+        }
+        Assertions.assertThat(processedRecords.size()).isEqualTo(5);
+        executorService.shutdown();
+        executorService.awaitTermination(3, TimeUnit.SECONDS);
     }
 
     private ExportTraceServiceRequest buildExportTraceServiceRequestFromJson(String requestJson) throws InvalidProtocolBufferException {
