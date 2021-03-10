2686ebd49faacf0191bc33b944e0ddc6048ba013
qchea <qchea@amazon.com>
2021-03-09 19:52:28 -0600

    TST: enhance test cases

--- data-prepper-plugins/otel-trace-raw-prepper/src/test/java/com/amazon/dataprepper/plugins/prepper/oteltrace/OTelTraceRawPrepperTest.java
+++ data-prepper-plugins/otel-trace-raw-prepper/src/test/java/com/amazon/dataprepper/plugins/prepper/oteltrace/OTelTraceRawPrepperTest.java
@@ -5,6 +5,8 @@ import com.amazon.dataprepper.model.configuration.PluginSetting;
 import com.amazon.dataprepper.model.record.Record;
 import com.amazon.dataprepper.metrics.MetricsTestUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
 import io.micrometer.core.instrument.Measurement;
@@ -15,6 +17,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Before;
 
+import static org.awaitility.Awaitility.await;
 import static org.mockito.Mockito.mock;
 
 import java.util.ArrayList;
@@ -23,6 +26,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringJoiner;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -36,6 +40,7 @@ import static org.mockito.Mockito.when;
 
 public class OTelTraceRawPrepperTest {
 
+    private final static ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private static final long SEC_TO_MILLIS = 1_000L;
     private static final long TEST_GC_INTERVAL = 3L;
     private static final long TEST_PARENT_SPAN_FLUSH_DELAY = 1L;
@@ -102,11 +107,13 @@ public class OTelTraceRawPrepperTest {
     public void testExportRequestFlushByParentSpan() throws InvalidProtocolBufferException, JsonProcessingException, InterruptedException {
         final String sampleRequest = "{\"resourceSpans\":[{\"resource\":{\"attributes\":[{\"key\":\"service.name\",\"value\":{\"stringValue\":\"analytics-service\"}},{\"key\":\"telemetry.sdk.language\",\"value\":{\"stringValue\":\"java\"}},{\"key\":\"telemetry.sdk.name\",\"value\":{\"stringValue\":\"opentelemetry\"}},{\"key\":\"telemetry.sdk.version\",\"value\":{\"stringValue\":\"0.8.0-SNAPSHOT\"}}],\"droppedAttributesCount\":0},\"instrumentationLibrarySpans\":[{\"instrumentationLibrary\":{\"name\":\"io.opentelemetry.auto.spring-webmvc-3.1\",\"version\":\"\"},\"spans\":[{\"traceId\":\"/6V20yEXOsbO82Acj0vedQ==\",\"spanId\":\"CFrAgv/Pv40=\",\"traceState\":\"\",\"parentSpanId\":\"yxwHNNFJQP0=\",\"name\":\"LoggingController.save\",\"kind\":\"SPAN_KIND_INTERNAL\",\"startTimeUnixNano\":\"1597902043168792500\",\"endTimeUnixNano\":\"1597902043215953100\",\"attributes\":[],\"droppedAttributesCount\":0,\"events\":[],\"droppedEventsCount\":0,\"links\":[],\"droppedLinksCount\":0,\"status\":{\"code\":\"STATUS_CODE_OK\",\"message\":\"\"}},{\"traceId\":\"bQ/2NNEmtuwsGAOR5ntCNw==\",\"spanId\":\"WsV2GFh9pxw=\",\"traceState\":\"\",\"parentSpanId\":\"mnO/qUT5ye4=\",\"name\":\"LoggingController.save\",\"kind\":\"SPAN_KIND_INTERNAL\",\"startTimeUnixNano\":\"1597902046041803300\",\"endTimeUnixNano\":\"1597902046088892200\",\"attributes\":[],\"droppedAttributesCount\":0,\"events\":[],\"droppedEventsCount\":0,\"links\":[],\"droppedLinksCount\":0,\"status\":{\"code\":\"STATUS_CODE_OK\",\"message\":\"\"}}]},{\"instrumentationLibrary\":{\"name\":\"io.opentelemetry.auto.apache-httpasyncclient-4.0\",\"version\":\"\"},\"spans\":[{\"traceId\":\"/6V20yEXOsbO82Acj0vedQ==\",\"spanId\":\"G4PRdsC1+0Y=\",\"traceState\":\"\",\"parentSpanId\":\"CFrAgv/Pv40=\",\"name\":\"HTTP PUT\",\"kind\":\"SPAN_KIND_CLIENT\",\"startTimeUnixNano\":\"1597902043175204700\",\"endTimeUnixNano\":\"1597902043205117100\",\"attributes\":[{\"key\":\"http.status_code\",\"value\":{\"intValue\":\"200\"}},{\"key\":\"http.url\",\"value\":{\"stringValue\":\"/logs/_doc/service_1?timeout\\u003d1m\"}},{\"key\":\"http.method\",\"value\":{\"stringValue\":\"PUT\"}}],\"droppedAttributesCount\":0,\"events\":[],\"droppedEventsCount\":0,\"links\":[],\"droppedLinksCount\":0,\"status\":{\"code\":\"STATUS_CODE_OK\",\"message\":\"\"}},{\"traceId\":\"bQ/2NNEmtuwsGAOR5ntCNw==\",\"spanId\":\"na9fwApQsYs=\",\"traceState\":\"\",\"parentSpanId\":\"WsV2GFh9pxw=\",\"name\":\"HTTP PUT\",\"kind\":\"SPAN_KIND_CLIENT\",\"startTimeUnixNano\":\"1597902046052809200\",\"endTimeUnixNano\":\"1597902046084822500\",\"attributes\":[{\"key\":\"http.status_code\",\"value\":{\"intValue\":\"200\"}},{\"key\":\"http.url\",\"value\":{\"stringValue\":\"/logs/_doc/service_1?timeout\\u003d1m\"}},{\"key\":\"http.method\",\"value\":{\"stringValue\":\"PUT\"}}],\"droppedAttributesCount\":0,\"events\":[],\"droppedEventsCount\":0,\"links\":[],\"droppedLinksCount\":0,\"status\":{\"code\":\"STATUS_CODE_OK\",\"message\":\"\"}}]},{\"instrumentationLibrary\":{\"name\":\"io.opentelemetry.auto.servlet-3.0\",\"version\":\"\"},\"spans\":[{\"traceId\":\"/6V20yEXOsbO82Acj0vedQ==\",\"spanId\":\"yxwHNNFJQP0=\",\"traceState\":\"\",\"parentSpanId\":\"\",\"name\":\"/logs\",\"kind\":\"SPAN_KIND_SERVER\",\"startTimeUnixNano\":\"1597902043168010200\",\"endTimeUnixNano\":\"1597902043217170200\",\"attributes\":[{\"key\":\"http.status_code\",\"value\":{\"intValue\":\"200\"}},{\"key\":\"net.peer.port\",\"value\":{\"intValue\":\"41164\"}},{\"key\":\"servlet.path\",\"value\":{\"stringValue\":\"/logs\"}},{\"key\":\"http.response_content_length\",\"value\":{\"intValue\":\"7\"}},{\"key\":\"http.user_agent\",\"value\":{\"stringValue\":\"curl/7.54.0\"}},{\"key\":\"http.flavor\",\"value\":{\"stringValue\":\"HTTP/1.1\"}},{\"key\":\"servlet.context\",\"value\":{\"stringValue\":\"\"}},{\"key\":\"http.url\",\"value\":{\"stringValue\":\"http://0.0.0.0:8087/logs\"}},{\"key\":\"net.peer.ip\",\"value\":{\"stringValue\":\"172.29.0.1\"}},{\"key\":\"http.method\",\"value\":{\"stringValue\":\"POST\"}},{\"key\":\"http.client_ip\",\"value\":{\"stringValue\":\"172.29.0.1\"}}],\"droppedAttributesCount\":0,\"events\":[],\"droppedEventsCount\":0,\"links\":[],\"droppedLinksCount\":0,\"status\":{\"code\":\"STATUS_CODE_OK\",\"message\":\"\"}},{\"traceId\":\"bQ/2NNEmtuwsGAOR5ntCNw==\",\"spanId\":\"mnO/qUT5ye4=\",\"traceState\":\"\",\"parentSpanId\":\"\",\"name\":\"/logs\",\"kind\":\"SPAN_KIND_SERVER\",\"startTimeUnixNano\":\"1597902046041011600\",\"endTimeUnixNano\":\"1597902046089556800\",\"attributes\":[{\"key\":\"http.status_code\",\"value\":{\"intValue\":\"200\"}},{\"key\":\"net.peer.port\",\"value\":{\"intValue\":\"41168\"}},{\"key\":\"servlet.path\",\"value\":{\"stringValue\":\"/logs\"}},{\"key\":\"http.response_content_length\",\"value\":{\"intValue\":\"7\"}},{\"key\":\"http.user_agent\",\"value\":{\"stringValue\":\"curl/7.54.0\"}},{\"key\":\"http.flavor\",\"value\":{\"stringValue\":\"HTTP/1.1\"}},{\"key\":\"servlet.context\",\"value\":{\"stringValue\":\"\"}},{\"key\":\"http.url\",\"value\":{\"stringValue\":\"http://0.0.0.0:8087/logs\"}},{\"key\":\"net.peer.ip\",\"value\":{\"stringValue\":\"172.29.0.1\"}},{\"key\":\"http.method\",\"value\":{\"stringValue\":\"POST\"}},{\"key\":\"http.client_ip\",\"value\":{\"stringValue\":\"172.29.0.1\"}}],\"droppedAttributesCount\":0,\"events\":[],\"droppedEventsCount\":0,\"links\":[],\"droppedLinksCount\":0,\"status\":{\"code\":\"STATUS_CODE_OK\",\"message\":\"\"}}]}]}]}";
         final ExportTraceServiceRequest exportTraceServiceRequest = buildExportTraceServiceRequestFromJson(sampleRequest);
-        List<Record<String>> processedRecords = (List<Record<String>>) oTelTraceRawPrepper.doExecute(Collections.singletonList(new Record<>(exportTraceServiceRequest)));
-        Assertions.assertThat(processedRecords.size()).isEqualTo(0);
+        oTelTraceRawPrepper.doExecute(Collections.singletonList(new Record<>(exportTraceServiceRequest)));
         Thread.sleep(TEST_PARENT_SPAN_FLUSH_DELAY * SEC_TO_MILLIS);
-        processedRecords = (List<Record<String>>) oTelTraceRawPrepper.doExecute(Collections.emptyList());
-        Assertions.assertThat(processedRecords.size()).isEqualTo(6);
+        await().atMost(2 * TEST_PARENT_SPAN_FLUSH_DELAY, TimeUnit.SECONDS).untilAsserted(() -> {
+            final List<Record<String>> processedRecords = (List<Record<String>>) oTelTraceRawPrepper.doExecute(Collections.emptyList());
+            Assertions.assertThat(processedRecords.size()).isEqualTo(6);
+            Assertions.assertThat(getOrphanedSpanCount(processedRecords)).isEqualTo(0);
+        });
     }
 
     @Test
@@ -116,19 +123,21 @@ public class OTelTraceRawPrepperTest {
         final ExportTraceServiceRequest exportTraceServiceRequest1 = buildExportTraceServiceRequestFromJson(sampleRequest1);
         final ExportTraceServiceRequest exportTraceServiceRequest2 = buildExportTraceServiceRequestFromJson(sampleRequest2);
         ExecutorService executorService = Executors.newFixedThreadPool(2);
-        List<Record<String>> processedRecords = new ArrayList<>();
+        final List<Record<String>> processedRecords = new ArrayList<>();
         List<Future<Collection<Record<String>>>> futures = new ArrayList<>();
         futures.addAll(submitExportTraceServiceRequests(executorService, Collections.singletonList(exportTraceServiceRequest1)));
         futures.addAll(submitExportTraceServiceRequests(executorService, Collections.singletonList(exportTraceServiceRequest2)));
         for (Future<Collection<Record<String>>> future : futures) {
             processedRecords.addAll(future.get());
         }
-        Thread.sleep(TEST_PARENT_SPAN_FLUSH_DELAY * SEC_TO_MILLIS);
-        futures = submitExportTraceServiceRequests(executorService, Collections.emptyList());
-        for (Future<Collection<Record<String>>> future : futures) {
-            processedRecords.addAll(future.get());
-        }
-        Assertions.assertThat(processedRecords.size()).isEqualTo(6);
+        await().atMost(2 * TEST_PARENT_SPAN_FLUSH_DELAY, TimeUnit.SECONDS).untilAsserted(() -> {
+            List<Future<Collection<Record<String>>>> futureList = submitExportTraceServiceRequests(executorService, Collections.emptyList());
+            for (Future<Collection<Record<String>>> future : futureList) {
+                processedRecords.addAll(future.get());
+            }
+            Assertions.assertThat(processedRecords.size()).isEqualTo(6);
+            Assertions.assertThat(getOrphanedSpanCount(processedRecords)).isEqualTo(0);
+        });
         executorService.shutdown();
         executorService.awaitTermination(3, TimeUnit.SECONDS);
     }
@@ -139,11 +148,12 @@ public class OTelTraceRawPrepperTest {
         final ExportTraceServiceRequest exportTraceServiceRequest = buildExportTraceServiceRequestFromJson(sampleRequestWithOrphanedSpans);
         oTelTraceRawPrepper.doExecute(Collections.singletonList(new Record<>(exportTraceServiceRequest)));
         Thread.sleep(TEST_PARENT_SPAN_FLUSH_DELAY * SEC_TO_MILLIS);
-        List<Record<String>> processedRecords = (List<Record<String>>) oTelTraceRawPrepper.doExecute(Collections.emptyList());
-        Assertions.assertThat(processedRecords.size()).isEqualTo(0);
-        Thread.sleep((TEST_GC_INTERVAL - TEST_PARENT_SPAN_FLUSH_DELAY) * SEC_TO_MILLIS);
-        processedRecords = (List<Record<String>>) oTelTraceRawPrepper.doExecute(Collections.emptyList());
-        Assertions.assertThat(processedRecords.size()).isEqualTo(4);
+        oTelTraceRawPrepper.doExecute(Collections.emptyList());
+        await().atMost(2 * TEST_GC_INTERVAL, TimeUnit.SECONDS).untilAsserted(() -> {
+            final List<Record<String>> processedRecords = (List<Record<String>>) oTelTraceRawPrepper.doExecute(Collections.emptyList());
+            Assertions.assertThat(processedRecords.size()).isEqualTo(4);
+            Assertions.assertThat(getOrphanedSpanCount(processedRecords)).isEqualTo(4);
+        });
     }
 
     @Test
@@ -160,18 +170,14 @@ public class OTelTraceRawPrepperTest {
         for (Future<Collection<Record<String>>> future : futures) {
             processedRecords.addAll(future.get());
         }
-        Thread.sleep(TEST_PARENT_SPAN_FLUSH_DELAY * SEC_TO_MILLIS);
-        futures = submitExportTraceServiceRequests(executorService, Collections.emptyList());
-        for (Future<Collection<Record<String>>> future : futures) {
-            processedRecords.addAll(future.get());
-        }
-        Assertions.assertThat(processedRecords.size()).isEqualTo(3);
-        Thread.sleep((TEST_GC_INTERVAL - TEST_PARENT_SPAN_FLUSH_DELAY) * SEC_TO_MILLIS);
-        futures = submitExportTraceServiceRequests(executorService, Collections.emptyList());
-        for (Future<Collection<Record<String>>> future : futures) {
-            processedRecords.addAll(future.get());
-        }
-        Assertions.assertThat(processedRecords.size()).isEqualTo(5);
+        await().atMost(2 * TEST_GC_INTERVAL, TimeUnit.SECONDS).untilAsserted(() -> {
+            List<Future<Collection<Record<String>>>> futureList = submitExportTraceServiceRequests(executorService, Collections.emptyList());
+            for (Future<Collection<Record<String>>> future : futureList) {
+                processedRecords.addAll(future.get());
+            }
+            Assertions.assertThat(processedRecords.size()).isEqualTo(5);
+            Assertions.assertThat(getOrphanedSpanCount(processedRecords)).isEqualTo(2);
+        });
         executorService.shutdown();
         executorService.awaitTermination(3, TimeUnit.SECONDS);
     }
@@ -187,6 +193,18 @@ public class OTelTraceRawPrepperTest {
         final List<Record<ExportTraceServiceRequest>> records = exportTraceServiceRequests.stream().map(Record::new).collect(Collectors.toList());
         futures.add(executorService.submit(() -> oTelTraceRawPrepper.doExecute(records)));
         return futures;
+    }
+
+    private int getOrphanedSpanCount(List<Record<String>> records) throws JsonProcessingException {
+        int count = 0;
+        for (Record<String> record: records) {
+            String spanJson = record.getData();
+            Map<String, Object> spanMap = OBJECT_MAPPER.readValue(spanJson, new TypeReference<Map<String, Object>>() {});
+            if (spanMap.get("traceGroup").equals("ERROR")) {
+                count += 1;
+            }
+        }
+        return count;
     }
 }
 
