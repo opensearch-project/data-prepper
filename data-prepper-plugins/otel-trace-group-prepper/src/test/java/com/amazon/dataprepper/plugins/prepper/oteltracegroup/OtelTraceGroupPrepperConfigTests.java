7aceb5dbd0cec262344628a24567167e55fd665b
qchea <qchea@amazon.com>
2021-03-23 14:14:38 -0500

    TST: config test

---
+++ data-prepper-plugins/otel-trace-group-prepper/src/test/java/com/amazon/dataprepper/plugins/prepper/oteltracegroup/OtelTraceGroupPrepperConfigTests.java
@@ -0,0 +1,33 @@
+package com.amazon.dataprepper.plugins.prepper.oteltracegroup;
+
+import com.amazon.dataprepper.model.configuration.PluginSetting;
+import com.amazon.dataprepper.plugins.sink.elasticsearch.ConnectionConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OtelTraceGroupPrepperConfigTests {
+
+    @Mock
+    private ConnectionConfiguration connectionConfigurationMock;
+
+    @Test
+    public void testInitialize() {
+        try (MockedStatic<ConnectionConfiguration> connectionConfigurationMockedStatic = Mockito.mockStatic(ConnectionConfiguration.class)) {
+            connectionConfigurationMockedStatic.when(() -> ConnectionConfiguration.readConnectionConfiguration(any(PluginSetting.class)))
+                    .thenReturn(connectionConfigurationMock);
+            PluginSetting testPluginSetting = new PluginSetting("otel_trace_group_prepper", new HashMap<>());
+            OtelTraceGroupPrepperConfig otelTraceGroupPrepperConfig = OtelTraceGroupPrepperConfig.buildConfig(testPluginSetting);
+            assertNotNull(otelTraceGroupPrepperConfig.getEsConnectionConfig());
+        }
+    }
+}
