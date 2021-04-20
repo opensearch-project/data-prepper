3f28b69e51f64064cb5b16f903eff165e47c67c5
qchea <qchea@amazon.com>
2021-04-19 20:17:42 -0500

    MAINT: check validity of domain name and endpoints before initializing PeerListProvider
    Signed-off-by: qchea <qchea@amazon.com>

--- data-prepper-plugins/peer-forwarder/src/main/java/com/amazon/dataprepper/plugins/prepper/peerforwarder/discovery/PeerListProviderFactory.java
+++ data-prepper-plugins/peer-forwarder/src/main/java/com/amazon/dataprepper/plugins/prepper/peerforwarder/discovery/PeerListProviderFactory.java
@@ -3,12 +3,20 @@ package com.amazon.dataprepper.plugins.prepper.peerforwarder.discovery;
 import com.amazon.dataprepper.metrics.PluginMetrics;
 import com.amazon.dataprepper.model.configuration.PluginSetting;
 import com.amazon.dataprepper.plugins.prepper.peerforwarder.PeerForwarderConfig;
+import com.google.common.base.Preconditions;
 import com.linecorp.armeria.client.endpoint.dns.DnsAddressEndpointGroup;
+import org.apache.commons.validator.routines.DomainValidator;
+import org.apache.commons.validator.routines.InetAddressValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Objects;
 
 public class PeerListProviderFactory {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PeerListProviderFactory.class);
+
     // TODO: Make these configurable?
     private static final int MIN_TTL = 10;
     private static final int MAX_TTL = 20;
@@ -27,6 +35,7 @@ public class PeerListProviderFactory {
             case DNS:
                 final String domainName = pluginSetting.getStringOrDefault(PeerForwarderConfig.DOMAIN_NAME, null);
                 Objects.requireNonNull(domainName, String.format("Missing '%s' configuration value",PeerForwarderConfig. DOMAIN_NAME));
+                Preconditions.checkArgument(validateEndpoint(domainName), "Invalid domain name: %s", domainName);
 
                 final DnsAddressEndpointGroup endpointGroup = DnsAddressEndpointGroup.builder(domainName)
                         .ttl(MIN_TTL, MAX_TTL)
@@ -36,10 +45,15 @@ public class PeerListProviderFactory {
             case STATIC:
                 final List<String> endpoints = (List<String>) pluginSetting.getAttributeOrDefault(PeerForwarderConfig.STATIC_ENDPOINTS, null);
                 Objects.requireNonNull(endpoints, String.format("Missing '%s' configuration value", PeerForwarderConfig.STATIC_ENDPOINTS));
+                Preconditions.checkArgument(endpoints.stream().allMatch(this::validateEndpoint), "Including invalid endpoints: %s", endpoints);
 
                 return new StaticPeerListProvider(endpoints, pluginMetrics);
             default:
                 throw new UnsupportedOperationException(String.format("Unsupported discovery mode: %s", discoveryMode));
         }
+    }
+
+    private boolean validateEndpoint(final String endpoint) {
+        return DomainValidator.getInstance(true).isValid(endpoint) || InetAddressValidator.getInstance().isValid(endpoint);
     }
 }
