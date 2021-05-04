18fd66e36953b4d4cbc4d4fcd1d3a356bb6f3b55
qchea <qchea@amazon.com>
2021-05-04 11:14:56 -0500

    MAINT: more renaming on odfe or opendistro
    Signed-off-by: qchea <qchea@amazon.com>

--- examples/dev/trace-analytics-sample-app/resources/data-prepper-wait-for-os-and-start.sh
+++ examples/dev/trace-analytics-sample-app/resources/data-prepper-wait-for-os-and-start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 until [[ $(curl --write-out %{http_code} --output /dev/null --silent --head --fail https://node-0.example.com:9200 -u admin:admin --insecure) == 200 ]]; do
-  echo "Waiting for ODFE to be ready"
+  echo "Waiting for OpenSearch to be ready"
   sleep 1
 done
 
