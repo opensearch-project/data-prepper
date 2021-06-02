e97b9bb3441dd8ac2a07c70a1d666ec86f8b3f02
qchea <qchea@amazon.com>
2021-06-02 18:19:58 -0500

    MAINT: upgrade setting and API
    Signed-off-by: qchea <qchea@amazon.com>

--- data-prepper-plugins/opensearch/src/test/java/com/amazon/dataprepper/plugins/sink/opensearch/OpenSearchSinkIT.java
+++ data-prepper-plugins/opensearch/src/test/java/com/amazon/dataprepper/plugins/sink/opensearch/OpenSearchSinkIT.java
@@ -401,13 +401,13 @@ public class OpenSearchSinkIT extends OpenSearchRestTestCase {
 
   private String getIndexPolicyId(final String index) throws IOException {
     // TODO: replace with new _opensearch API
-    final Request request = new Request(HttpMethod.GET, "/_opendistro/_ism/explain/" + index);
+    final Request request = new Request(HttpMethod.GET, "/_plugins/_ism/explain/" + index);
     final Response response = client().performRequest(request);
     final String responseBody = EntityUtils.toString(response.getEntity());
 
     @SuppressWarnings("unchecked")
     final String policyId = (String) ((Map<String, Object>)createParser(XContentType.JSON.xContent(),
-            responseBody).map().get(index)).get("index.opendistro.index_state_management.policy_id");
+            responseBody).map().get(index)).get("index.plugins.index_state_management.policy_id");
     return policyId;
   }
 
