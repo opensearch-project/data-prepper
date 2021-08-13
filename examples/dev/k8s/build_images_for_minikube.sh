6ba7e662978aedc746fabfd220a058c16d88503c
Qi Chen <qchea@amazon.com>
2021-08-13 16:16:45 -0500

    MAINT: use os images in examples (#73)
    Signed-off-by: qchea <qchea@amazon.com>

--- examples/dev/k8s/build_images_for_minikube.sh
+++ examples/dev/k8s/build_images_for_minikube.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 eval $(minikube docker-env)
 docker build ../../trace-analytics-sample-app/sample-app -t example-k8s/sample-app
-docker build ../../kibana-trace-analytics -t example-k8s/kibana
 docker build ../../.. -f ../../../examples/dev/trace-analytics-sample-app/Dockerfile -t example-k8s/data-prepper
--- examples/dev/trace-analytics-sample-app/resources/data-prepper-wait-for-odfe-and-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-until [[ $(curl --write-out %{http_code} --output /dev/null --silent --head --fail https://node-0.example.com:9200 -u admin:admin --insecure) == 200 ]]; do
-  echo "Waiting for ODFE to be ready"
-  sleep 1
-done
-
-java -Dlog4j.configurationFile=log4j.properties -Xms128m -Xmx128m -jar data-prepper.jar pipelines.yaml data-prepper-config.yaml
