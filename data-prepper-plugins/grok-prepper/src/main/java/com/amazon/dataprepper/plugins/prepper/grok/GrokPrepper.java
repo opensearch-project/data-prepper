fe993180b0a23539617696c4c24a3f2c3fe1a13e
graytaylor0 <33740195+graytaylor0@users.noreply.github.com>
2021-09-27 15:48:29 -0500

    Beginning testing of Grok Prepper class
    Signed-off-by: graytaylor0 <33740195+graytaylor0@users.noreply.github.com>

--- data-prepper-plugins/grok-prepper/src/main/java/com/amazon/dataprepper/plugins/prepper/grok/GrokPrepper.java
+++ data-prepper-plugins/grok-prepper/src/main/java/com/amazon/dataprepper/plugins/prepper/grok/GrokPrepper.java
@@ -19,22 +19,49 @@ import com.amazon.dataprepper.model.configuration.PluginSetting;
 import com.amazon.dataprepper.model.record.Record;
 import com.amazon.dataprepper.model.prepper.AbstractPrepper;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 
+import io.krakens.grok.api.Grok;
+import io.krakens.grok.api.GrokCompiler;
+import io.krakens.grok.api.Match;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @DataPrepperPlugin(name = "grok", type = PluginType.PREPPER)
 public class GrokPrepper extends AbstractPrepper<Record<String>, Record<String>> {
 
     private static final Logger LOG = LoggerFactory.getLogger(GrokPrepper.class);
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final TypeReference<Map<String, Object>> MAP_TYPE_REFERENCE = new TypeReference<Map<String, Object>>() {};
+
+    private final GrokCompiler grokCompiler;
+    private final Map<String, List<Grok>> fieldToGrok;
     private final GrokPrepperConfig grokPrepperConfig;
 
     public GrokPrepper(final PluginSetting pluginSetting) {
         super(pluginSetting);
         grokPrepperConfig = GrokPrepperConfig.buildConfig(pluginSetting);
+        grokCompiler = GrokCompiler.newInstance();
+        fieldToGrok = new LinkedHashMap<>();
+
+        registerPatterns();
+
+        for (Map.Entry<String, List<String>> entry : grokPrepperConfig.getMatch().entrySet()) {
+            fieldToGrok.put(entry.getKey(), entry.getValue().stream().map(item -> grokCompiler.compile(item, grokPrepperConfig.isNamedCapturesOnly())).collect(Collectors.toList()));
+        }
     }
 
     /**
@@ -43,10 +70,38 @@ public class GrokPrepper extends AbstractPrepper<Record<String>, Record<String>>
      *
      * @param records Input records that will be modified/processed
      * @return Record  modified output records
-    */
+     */
     @Override
     public Collection<Record<String>> doExecute(Collection<Record<String>> records) {
-        return records;
+        final List<Record<String>> recordsOut = new LinkedList<>();
+
+        for (Record<String> record : records) {
+            try {
+                final Map<String, Object> recordMap = OBJECT_MAPPER.readValue(record.getData(), MAP_TYPE_REFERENCE);
+                final Map<String, Object> grokkedLog = new HashMap<>();
+
+                for (Map.Entry<String, List<Grok>> entry : fieldToGrok.entrySet()) {
+                    for (Grok grok : entry.getValue()) {
+                        if (recordMap.containsKey(entry.getKey())) {
+                            Match match = grok.match(recordMap.get(entry.getKey()).toString());
+                            match.setKeepEmptyCaptures(grokPrepperConfig.isKeepEmptyCaptures());
+
+                            mergeUpdateWithOriginalMap(grokkedLog, match.capture());
+                        }
+                    }
+                }
+
+                mergeUpdateWithOriginalMap(recordMap, grokkedLog);
+
+                final Record<String> grokkedRecord = new Record<>(OBJECT_MAPPER.writeValueAsString(recordMap), record.getMetadata());
+                recordsOut.add(grokkedRecord);
+
+            } catch (JsonProcessingException e) {
+                LOG.error("Failed to parse the record [{}]", record.getData());
+                recordsOut.add(record);
+            }
+        }
+        return recordsOut;
     }
 
     @Override
@@ -62,5 +117,35 @@ public class GrokPrepper extends AbstractPrepper<Record<String>, Record<String>>
     @Override
     public void shutdown() {
 
+    }
+
+    private void registerPatterns() {
+        grokCompiler.registerDefaultPatterns();
+    }
+
+    /**
+     * merge key value pairs from updates Map with the original Map. If keys in the updates Map already exist in the original Map,
+     * the value for that key in the original Map will be made a List<String>, and the values from updates will be appended. For collisions
+     * where the value is not of type String, updates will be ignored
+     *
+     * @param original The original Map to be updated with values from key value pairs from updates
+     * @param updates The updates Map that contains key value pairs to be merged with original
+     */
+    private void mergeUpdateWithOriginalMap(final Map<String, Object> original, final Map<String, Object> updates) {
+        for (Map.Entry<String, Object> updateEntry : updates.entrySet()) {
+
+            if (!(original.containsKey(updateEntry.getKey()))) {
+                original.put(updateEntry.getKey(), updateEntry.getValue());
+                continue;
+            }
+
+            if (List.class.isAssignableFrom(original.get(updateEntry.getKey()).getClass())) {
+                ((List<Object>) original.get(updateEntry.getKey())).add(updateEntry.getValue());
+            } else {
+                List<Object> values = new ArrayList<>(Collections.singletonList(original.get(updateEntry.getKey())));
+                values.add(updateEntry.getValue());
+                original.put(updateEntry.getKey(), values);
+            }
+        }
     }
 }
--- data-prepper-plugins/grok-prepper/src/test/java/com/amazon/dataprepper/plugins/prepper/grok/GrokPrepperTests.java
+++ data-prepper-plugins/grok-prepper/src/test/java/com/amazon/dataprepper/plugins/prepper/grok/GrokPrepperTests.java
@@ -11,34 +11,78 @@
 
 package com.amazon.dataprepper.plugins.prepper.grok;
 
-import com.amazon.dataprepper.model.record.Record;
 
 import com.amazon.dataprepper.model.configuration.PluginSetting;
+import io.krakens.grok.api.Grok;
+import io.krakens.grok.api.GrokCompiler;
+import io.krakens.grok.api.Match;
+import com.amazon.dataprepper.model.record.Record;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.Mock;
+import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
 
 @ExtendWith(MockitoExtension.class)
 public class GrokPrepperTests {
 
-    PluginSetting pluginSetting;
+    private PluginSetting pluginSetting;
     private GrokPrepper grokPrepper;
+    private Collection<Record<String>> records;
+
+    @Mock
+    private GrokCompiler grokCompiler;
+
+    @Mock
+    private Match match;
+
+    @Mock
+    private Grok grok;
+
+    private final String PLUGIN_NAME = "grok";
+    private Map<String, Object> capture;
 
     @BeforeEach
     public void setup() {
-        pluginSetting = new PluginSetting(
-                "grok",
-                null
-                );
-        pluginSetting.setPipelineName("grokPipeline");
+
+        capture = new HashMap<>();
+        capture.put("field_capture_1", "value_capture_1");
+        capture.put("field_capture_2", "value_capture_2");
+        capture.put("field_capture_3", "value_capture_3");
+
+        final Map<String, List<String>> matchConfig = new HashMap<>();
+        matchConfig.put("message", new ArrayList<String>(){{ add("%{COMMONAPACHELOG"); }});
+
+        when(GrokCompiler.newInstance()).thenReturn(grokCompiler);
+        when(grokCompiler.compile(anyString(), anyBoolean())).thenReturn(grok);
+        when(grok.match(anyString())).thenReturn(match);
+        when(match.capture()).thenReturn(capture);
+
+        pluginSetting = completePluginSettingForGrokPrepper(
+                GrokPrepperConfig.DEFAULT_BREAK_ON_MATCH,
+                GrokPrepperConfig.DEFAULT_KEEP_EMPTY_CAPTURES,
+                matchConfig,
+                GrokPrepperConfig.DEFAULT_NAMED_CAPTURES_ONLY,
+                Collections.emptyList(),
+                Collections.emptyList(),
+                GrokPrepperConfig.DEFAULT_PATTERNS_FILES_GLOB,
+                Collections.emptyMap(),
+                GrokPrepperConfig.DEFAULT_TIMEOUT_MILLIS,
+                GrokPrepperConfig.TARGET);
+
         grokPrepper = new GrokPrepper(pluginSetting);
     }
 
@@ -48,6 +92,34 @@ public class GrokPrepperTests {
     }
 
     @Test
+
+
+    private PluginSetting completePluginSettingForGrokPrepper(final boolean breakOnMatch,
+                                                              final boolean keepEmptyCaptures,
+                                                              final Map<String, List<String>> match,
+                                                              final boolean namedCapturesOnly,
+                                                              final List<String> overwrite,
+                                                              final List<String> patternsDir,
+                                                              final String patternsFilesGlob,
+                                                              final Map<String, String> patternDefinitions,
+                                                              final int timeoutMillis,
+                                                              final String target) {
+        final Map<String, Object> settings = new HashMap<>();
+        settings.put(GrokPrepperConfig.BREAK_ON_MATCH, breakOnMatch);
+        settings.put(GrokPrepperConfig.NAMED_CAPTURES_ONLY, namedCapturesOnly);
+        settings.put(GrokPrepperConfig.MATCH, match);
+        settings.put(GrokPrepperConfig.KEEP_EMPTY_CAPTURES, keepEmptyCaptures);
+        settings.put(GrokPrepperConfig.OVERWRITE, overwrite);
+        settings.put(GrokPrepperConfig.PATTERNS_DIR, patternsDir);
+        settings.put(GrokPrepperConfig.PATTERN_DEFINITIONS, patternDefinitions);
+        settings.put(GrokPrepperConfig.PATTERNS_FILES_GLOB, patternsFilesGlob);
+        settings.put(GrokPrepperConfig.TIMEOUT_MILLIS, timeoutMillis);
+        settings.put(GrokPrepperConfig.TARGET, target);
+
+        return new PluginSetting(PLUGIN_NAME, settings);
+    }
+
+   /* @Test
     public void testGrokPrepper() {
         String testData = "{\"message\": \"127.0.0.1 user-identifier frank [10/Oct/2000:13:55:36 -0700] BEF25A72965 \\\"GET /apache_pb.gif HTTP/1.0\\\" 200 2326\"}";
         Record<String> record = new Record<>(testData);
@@ -56,5 +128,5 @@ public class GrokPrepperTests {
 
         assertThat(grokkedRecords.size(), equalTo(1));
         assertThat(grokkedRecords.get(0), equalTo(record));
-    }
+    }*/
 }
