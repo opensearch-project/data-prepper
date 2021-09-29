9ae696b92d34ac6acdde5e4f8de628201c81d5f6
qchea <qchea@amazon.com>
2021-09-29 11:13:36 -0500

    MAINT: docstring for SizeOverflowException
    Signed-off-by: qchea <qchea@amazon.com>

--- data-prepper-api/src/main/java/com/amazon/dataprepper/model/buffer/SizeOverflowException.java
+++ data-prepper-api/src/main/java/com/amazon/dataprepper/model/buffer/SizeOverflowException.java
@@ -11,6 +11,11 @@
 
 package com.amazon.dataprepper.model.buffer;
 
+/**
+ * Thrown to indicate the size of the data to be written into the {@link Buffer} is
+ * beyond Buffer's maximum capacity.
+ * <p>
+ */
 public class SizeOverflowException extends Exception {
     public SizeOverflowException(final String message) {
         super(message);
