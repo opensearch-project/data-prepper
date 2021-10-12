5bf9ec871a6d8174fd922ddaed6cf007ad1be72b
David Venable <dlv@amazon.com>
2021-10-12 07:47:29 -0500

    Use a Function to determine the number of instances to load in PluginFactory. Changed PluginProvider to return Optional<Class> instead of null. Added TODO to test constructor.
    Signed-off-by: David Venable <dlv@amazon.com>
    Signed-off-by: Arunachalam Lakshmanan <arunachalam.l@gmail.com>

--- data-prepper-core/src/test/java/com/amazon/dataprepper/plugin/DefaultPluginFactoryTest.java
+++ data-prepper-core/src/test/java/com/amazon/dataprepper/plugin/DefaultPluginFactoryTest.java
@@ -1,20 +1,29 @@
 package com.amazon.dataprepper.plugin;
 
+import com.amazon.dataprepper.model.configuration.PluginSetting;
+import com.amazon.dataprepper.model.plugin.NoPluginFoundException;
 import com.amazon.dataprepper.model.sink.Sink;
 import com.amazon.dataprepper.plugins.TestSink;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 class DefaultPluginFactoryTest {
 
@@ -24,6 +33,7 @@ class DefaultPluginFactoryTest {
     private PluginProvider firstPluginProvider;
     private Class<?> baseClass;
     private String pluginName;
+    private PluginSetting pluginSetting;
 
     @BeforeEach
     void setUp() {
@@ -37,6 +47,8 @@ class DefaultPluginFactoryTest {
 
         baseClass = Sink.class;
         pluginName = UUID.randomUUID().toString();
+        pluginSetting = mock(PluginSetting.class);
+        given(pluginSetting.getName()).willReturn(pluginName);
     }
 
     private DefaultPluginFactory createObjectUnderTest() {
@@ -75,16 +87,129 @@ class DefaultPluginFactoryTest {
                 this::createObjectUnderTest);
     }
 
-    @Test
-    @SuppressWarnings("unchecked")
-    void getPluginClass_should_return_first_plugin_found() {
+    @Nested
+    class WithoutPlugin {
+
+        @BeforeEach
+        void setUp() {
+            given(firstPluginProvider.findPluginClass(baseClass, pluginName))
+                    .willReturn(Optional.empty());
+        }
+
+        @Test
+        void loadPlugin_should_throw_if_no_plugin_found() {
+            final DefaultPluginFactory objectUnderTest = createObjectUnderTest();
+
+            assertThrows(NoPluginFoundException.class,
+                    () -> objectUnderTest.loadPlugin(baseClass, pluginSetting));
+
+            verifyNoInteractions(pluginCreator);
+        }
+
+        @Test
+        void loadPlugins_should_throw_when_no_plugin_found() {
+            final DefaultPluginFactory objectUnderTest = createObjectUnderTest();
+
+            assertThrows(NoPluginFoundException.class,
+                    () -> objectUnderTest.loadPlugins(baseClass, pluginSetting,
+                            c -> 1));
+
+            verifyNoInteractions(pluginCreator);
+        }
+    }
+
+    @Nested
+    class WithFoundPlugin {
+
         @SuppressWarnings("rawtypes")
-        final Class expectedPluginClass = TestSink.class;
+        private Class expectedPluginClass;
 
-        given(firstPluginProvider.findPluginClass(baseClass, pluginName))
-                .willReturn(expectedPluginClass);
+        @BeforeEach
+        void setUp() {
+            expectedPluginClass = TestSink.class;
 
-        assertThat(createObjectUnderTest().getPluginClass(baseClass, pluginName),
-                equalTo(expectedPluginClass));
+            given(firstPluginProvider.findPluginClass(baseClass, pluginName))
+                    .willReturn(Optional.of(expectedPluginClass));
+        }
+
+        @Test
+        void loadPlugin_should_create_a_new_instance_of_the_first_plugin_found() {
+
+            final TestSink expectedInstance = mock(TestSink.class);
+            given(pluginCreator.newPluginInstance(expectedPluginClass, pluginSetting))
+                    .willReturn(expectedInstance);
+
+            assertThat(createObjectUnderTest().loadPlugin(baseClass, pluginSetting),
+                    equalTo(expectedInstance));
+        }
+
+        @Test
+        void loadPlugins_should_throw_for_null_number_of_instances() {
+
+            final DefaultPluginFactory objectUnderTest = createObjectUnderTest();
+            assertThrows(IllegalArgumentException.class, () -> objectUnderTest.loadPlugins(
+                    baseClass, pluginSetting, c -> null));
+
+            verifyNoInteractions(pluginCreator);
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = {-100, -2, -1})
+        void loadPlugins_should_throw_for_invalid_number_of_instances(int numberOfInstances) {
+
+            final DefaultPluginFactory objectUnderTest = createObjectUnderTest();
+            assertThrows(IllegalArgumentException.class, () -> objectUnderTest.loadPlugins(
+                    baseClass, pluginSetting, c -> numberOfInstances));
+
+            verifyNoInteractions(pluginCreator);
+        }
+
+        @Test
+        void loadPlugins_should_return_an_empty_list_when_the_number_of_instances_is_0() {
+            final List<?> plugins = createObjectUnderTest().loadPlugins(
+                    baseClass, pluginSetting, c -> 0);
+
+            assertThat(plugins, notNullValue());
+            assertThat(plugins.size(), equalTo(0));
+
+            verifyNoInteractions(pluginCreator);
+        }
+
+        @Test
+        void loadPlugins_should_return_a_single_instance_when_the_the_numberOfInstances_is_1() {
+            final TestSink expectedInstance = mock(TestSink.class);
+            given(pluginCreator.newPluginInstance(expectedPluginClass, pluginSetting))
+                    .willReturn(expectedInstance);
+
+            final List<?> plugins = createObjectUnderTest().loadPlugins(
+                    baseClass, pluginSetting, c -> 1);
+
+            assertThat(plugins, notNullValue());
+            assertThat(plugins.size(), equalTo(1));
+            assertThat(plugins.get(0), equalTo(expectedInstance));
+        }
+
+        @Test
+        void loadPlugins_should_return_an_instance_for_the_total_count() {
+            final TestSink expectedInstance1 = mock(TestSink.class);
+            final TestSink expectedInstance2 = mock(TestSink.class);
+            final TestSink expectedInstance3 = mock(TestSink.class);
+            given(pluginCreator.newPluginInstance(expectedPluginClass, pluginSetting))
+                    .willReturn(expectedInstance1)
+                    .willReturn(expectedInstance2)
+                    .willReturn(expectedInstance3);
+
+            given(pluginSetting.getNumberOfProcessWorkers())
+                    .willReturn(3);
+
+            final List<?> plugins = createObjectUnderTest().loadPlugins(
+                    baseClass, pluginSetting, c -> 3);
+
+            assertThat(plugins, notNullValue());
+            assertThat(plugins.size(), equalTo(3));
+            assertThat(plugins.get(0), equalTo(expectedInstance1));
+            assertThat(plugins.get(1), equalTo(expectedInstance2));
+            assertThat(plugins.get(2), equalTo(expectedInstance3));
+        }
     }
 }
