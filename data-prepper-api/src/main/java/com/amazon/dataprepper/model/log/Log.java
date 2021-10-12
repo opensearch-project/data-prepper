35df391bc8b846c7e9ae20ad78f4e33f2bcf438a
Christopher Manning <cmanning09@users.noreply.github.com>
2021-10-12 14:00:55 -0500

    addressing review comments, adding @since, adjusting return types and increasing dependency versions
    Signed-off-by: Christopher Manning <cmanning09@users.noreply.github.com>

--- data-prepper-api/src/main/java/com/amazon/dataprepper/model/log/Log.java
+++ data-prepper-api/src/main/java/com/amazon/dataprepper/model/log/Log.java
@@ -5,6 +5,7 @@ import com.amazon.dataprepper.model.event.Event;
 /**
  * A log event in Data Prepper represents a single log line. A log event does not require any specific keys allowing this event type to
  * support any log structure.
+ * @since 1.2
  */
 public interface Log extends Event {
 
