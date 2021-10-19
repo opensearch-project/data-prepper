da7a37bcae74669d0f11efd9af3842715721e2df
Asif Sohail Mohammed <nsifmoh@amazon.com>
2021-10-19 18:07:23 -0400

    added javadocs to logstash model classes
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>

--- data-prepper-logstash-configuration/src/main/java/com/amazon/dataprepper/logstash/model/LogstashConfiguration.java
+++ data-prepper-logstash-configuration/src/main/java/com/amazon/dataprepper/logstash/model/LogstashConfiguration.java
@@ -3,8 +3,13 @@ package com.amazon.dataprepper.logstash.model;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Class to hold Logstash configuration {@link LogstashPluginType} and corresponding list of {@link LogstashPlugin}
+ *
+ * @since 1.2
+ */
 public class LogstashConfiguration {
-    Map<LogstashPluginType, List<LogstashPlugin>> pluginSections;
+    private Map<LogstashPluginType, List<LogstashPlugin>> pluginSections;
 
     public List<LogstashPlugin> getPluginSection(LogstashPluginType pluginType) {
         return pluginSections.get(pluginType);
--- data-prepper-logstash-configuration/src/main/java/com/amazon/dataprepper/logstash/model/LogstashPluginType.java
+++ data-prepper-logstash-configuration/src/main/java/com/amazon/dataprepper/logstash/model/LogstashPluginType.java
@@ -1,5 +1,10 @@
 package com.amazon.dataprepper.logstash.model;
 
+/**
+ * Types of plugins in Logstash configuration
+ *
+ * @since 1.2
+ */
 public enum LogstashPluginType {
     INPUT,
     FILTER,
