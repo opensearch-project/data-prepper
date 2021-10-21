3c8c706c019946099ce32516e68e946a47e0337b
Asif Sohail Mohammed <nsifmoh@amazon.com>
2021-10-21 15:13:26 -0400

    updated model classes to be immutable using builder pattern
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>
    Signed-off-by: Arunachalam Lakshmanan <arunachalam.l@gmail.com>

--- data-prepper-logstash-configuration/src/main/java/com/amazon/dataprepper/logstash/model/LogstashAttributeValue.java
+++ data-prepper-logstash-configuration/src/main/java/com/amazon/dataprepper/logstash/model/LogstashAttributeValue.java
@@ -6,22 +6,38 @@ package com.amazon.dataprepper.logstash.model;
  * @since 1.2
  */
 public class LogstashAttributeValue {
-    private LogstashValueType attributeValueType;
-    private Object value;
+    private final LogstashValueType attributeValueType;
+    private final Object value;
 
     public LogstashValueType getAttributeValueType() {
         return attributeValueType;
-    }
-
-    public void setAttributeValueType(LogstashValueType attributeValueType) {
-        this.attributeValueType = attributeValueType;
     }
 
     public Object getValue() {
         return value;
     }
 
-    public void setValue(Object value) {
-        this.value = value;
+    private LogstashAttributeValue (LogstashAttributeValueBuilder builder) {
+        this.attributeValueType = builder.attributeValueType;
+        this.value = builder.value;
+    }
+
+    public static class LogstashAttributeValueBuilder {
+        private LogstashValueType attributeValueType;
+        private Object value;
+
+        public LogstashAttributeValueBuilder attributeValueType(LogstashValueType attributeValueType) {
+            this.attributeValueType = attributeValueType;
+            return this;
+        }
+
+        public LogstashAttributeValueBuilder value(Object value) {
+            this.value = value;
+            return this;
+        }
+
+        public LogstashAttributeValue build() {
+            return new LogstashAttributeValue(this);
+        }
     }
 }
