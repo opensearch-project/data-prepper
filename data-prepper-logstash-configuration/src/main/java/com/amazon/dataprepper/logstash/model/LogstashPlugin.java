f9aef2d2e0c5d22f128aa174571c73ce577585fe
Asif Sohail Mohammed <nsifmoh@amazon.com>
2021-10-22 12:20:16 -0400

    added static method for builder and null checks
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>

--- data-prepper-logstash-configuration/src/main/java/com/amazon/dataprepper/logstash/model/LogstashPlugin.java
+++ data-prepper-logstash-configuration/src/main/java/com/amazon/dataprepper/logstash/model/LogstashPlugin.java
@@ -2,6 +2,8 @@ package com.amazon.dataprepper.logstash.model;
 
 import java.util.List;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  * Class to hold Logstash configuration plugin name and list of {@link LogstashAttribute}
  *
@@ -19,21 +21,33 @@ public class LogstashPlugin {
         return attributes;
     }
 
-    private LogstashPlugin(LogstashPluginBuilder builder) {
+    private LogstashPlugin(Builder builder) {
+        checkNotNull(builder.pluginName, "plugin name cannot be null");
+        checkNotNull(builder.attributes, "attributes cannot be null");
+
         this.pluginName = builder.pluginName;
         this.attributes = builder.attributes;
     }
 
-    public static class LogstashPluginBuilder {
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * A Builder for creating {@link LogstashPlugin} instances.
+     *
+     * @since 1.2
+     */
+    public static class Builder {
         private String pluginName;
         private List<LogstashAttribute> attributes;
 
-        public LogstashPluginBuilder pluginName(final String pluginName) {
+        public Builder pluginName(final String pluginName) {
             this.pluginName = pluginName;
             return this;
         }
 
-        public LogstashPluginBuilder attributes(final List<LogstashAttribute> attributes) {
+        public Builder attributes(final List<LogstashAttribute> attributes) {
             this.attributes = attributes;
             return this;
         }
