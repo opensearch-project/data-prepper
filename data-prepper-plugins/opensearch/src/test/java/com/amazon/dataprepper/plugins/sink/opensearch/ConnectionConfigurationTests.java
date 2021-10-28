95d7567160d1309acf6e65501881338a7041e566
Han Jiang <jianghan@amazon.com>
2021-10-28 15:56:16 -0500

    Enable OpenSearch Sink to go through a forward http proxy to communicate with an OpenSearch or ODFE server. (#479)
    * Enable OpenSearch Sink to go through a forward http proxy to communicate with a OpenSearch or ODFE server.
    Signed-off-by: Han Jiang <jianghan@amazon.com>
    
    * Add a check on proxy server port number
    Signed-off-by: Han Jiang <jianghan@amazon.com>
    
    * Have a default value for ConnectionConfiguration.Builder's proxy attribute to avoid NPE
    Signed-off-by: Han Jiang <jianghan@amazon.com>
    
    * Add a new unit test to cover a proxy host string with an explicit scheme: http
    Signed-off-by: Han Jiang <jianghan@amazon.com>
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

--- data-prepper-plugins/opensearch/src/test/java/com/amazon/dataprepper/plugins/sink/opensearch/ConnectionConfigurationTests.java
+++ data-prepper-plugins/opensearch/src/test/java/com/amazon/dataprepper/plugins/sink/opensearch/ConnectionConfigurationTests.java
@@ -21,14 +21,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 public class ConnectionConfigurationTests {
+    private static final String PROXY_PARAMETER = "proxy";
     private final List<String> TEST_HOSTS = Collections.singletonList("http://localhost:9200");
     private final String TEST_USERNAME = "admin";
     private final String TEST_PASSWORD = "admin";
@@ -167,7 +170,120 @@ public class ConnectionConfigurationTests {
         assertEquals(TEST_PIPELINE_NAME, connectionConfiguration.getPipelineName());
     }
 
+    @Test
+    public void testCreateClient_WithValidHttpProxy_HostIP() throws IOException {
+        final Map<String, Object> metadata = generateConfigurationMetadata(
+                TEST_HOSTS, TEST_USERNAME, TEST_PASSWORD, TEST_CONNECT_TIMEOUT, TEST_SOCKET_TIMEOUT, false, null, null, TEST_CERT_PATH, false);
+        final String testHttpProxy = "121.121.121.121:80";
+        metadata.put(PROXY_PARAMETER, testHttpProxy);
+        final PluginSetting pluginSetting = getPluginSettingByConfigurationMetadata(metadata);
+        final ConnectionConfiguration connectionConfiguration =
+                ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
+        assertEquals(connectionConfiguration.getProxy().get(), testHttpProxy);
+        final RestHighLevelClient client = connectionConfiguration.createClient();
+        assertNotNull(client);
+        client.close();
+    }
+
+    @Test
+    public void testCreateClient_WithValidHttpProxy_HostName() throws IOException {
+        final Map<String, Object> metadata = generateConfigurationMetadata(
+                TEST_HOSTS, TEST_USERNAME, TEST_PASSWORD, TEST_CONNECT_TIMEOUT, TEST_SOCKET_TIMEOUT, false, null, null, TEST_CERT_PATH, false);
+        final String testHttpProxy = "example.com:80";
+        metadata.put(PROXY_PARAMETER, testHttpProxy);
+        final PluginSetting pluginSetting = getPluginSettingByConfigurationMetadata(metadata);
+        final ConnectionConfiguration connectionConfiguration =
+                ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
+        assertEquals(connectionConfiguration.getProxy().get(), testHttpProxy);
+        final RestHighLevelClient client = connectionConfiguration.createClient();
+        assertNotNull(client);
+        client.close();
+    }
+
+    @Test
+    public void testCreateClient_WithValidHttpProxy_SchemeProvided() throws IOException {
+        final Map<String, Object> metadata = generateConfigurationMetadata(
+                TEST_HOSTS, TEST_USERNAME, TEST_PASSWORD, TEST_CONNECT_TIMEOUT, TEST_SOCKET_TIMEOUT, false, null, null, TEST_CERT_PATH, false);
+        final String testHttpProxy = "http://example.com:4350";
+        metadata.put(PROXY_PARAMETER, testHttpProxy);
+        final PluginSetting pluginSetting = getPluginSettingByConfigurationMetadata(metadata);
+        final ConnectionConfiguration connectionConfiguration =
+                ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
+        assertEquals(connectionConfiguration.getProxy().get(), testHttpProxy);
+        final RestHighLevelClient client = connectionConfiguration.createClient();
+        assertNotNull(client);
+        client.close();
+    }
+
+    @Test
+    public void testCreateClient_WithInvalidHttpProxy_InvalidPort() {
+        final Map<String, Object> metadata = generateConfigurationMetadata(
+                TEST_HOSTS, TEST_USERNAME, TEST_PASSWORD, TEST_CONNECT_TIMEOUT, TEST_SOCKET_TIMEOUT, false, null, null, TEST_CERT_PATH, false);
+        final String testHttpProxy = "example.com:port";
+        metadata.put(PROXY_PARAMETER, testHttpProxy);
+        final PluginSetting pluginSetting = getPluginSettingByConfigurationMetadata(metadata);
+        final ConnectionConfiguration connectionConfiguration =
+                ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
+        assertEquals(connectionConfiguration.getProxy().get(), testHttpProxy);
+        assertThrows(IllegalArgumentException.class, () -> connectionConfiguration.createClient());
+    }
+
+    @Test
+    public void testCreateClient_WithInvalidHttpProxy_NoPort() {
+        final Map<String, Object> metadata = generateConfigurationMetadata(
+                TEST_HOSTS, TEST_USERNAME, TEST_PASSWORD, TEST_CONNECT_TIMEOUT, TEST_SOCKET_TIMEOUT, false, null, null, TEST_CERT_PATH, false);
+        final String testHttpProxy = "example.com";
+        metadata.put(PROXY_PARAMETER, testHttpProxy);
+        final PluginSetting pluginSetting = getPluginSettingByConfigurationMetadata(metadata);
+        final ConnectionConfiguration connectionConfiguration =
+                ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
+        assertThrows(IllegalArgumentException.class, () -> connectionConfiguration.createClient());
+    }
+
+    @Test
+    public void testCreateClient_WithInvalidHttpProxy_PortNotInRange() {
+        final Map<String, Object> metadata = generateConfigurationMetadata(
+                TEST_HOSTS, TEST_USERNAME, TEST_PASSWORD, TEST_CONNECT_TIMEOUT, TEST_SOCKET_TIMEOUT, false, null, null, TEST_CERT_PATH, false);
+        final String testHttpProxy = "example.com:888888";
+        metadata.put(PROXY_PARAMETER, testHttpProxy);
+        final PluginSetting pluginSetting = getPluginSettingByConfigurationMetadata(metadata);
+        final ConnectionConfiguration connectionConfiguration =
+                ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
+        assertThrows(IllegalArgumentException.class, () -> connectionConfiguration.createClient());
+    }
+
+    @Test
+    public void testCreateClient_WithInvalidHttpProxy_NotHttp() {
+        final Map<String, Object> metadata = generateConfigurationMetadata(
+                TEST_HOSTS, TEST_USERNAME, TEST_PASSWORD, TEST_CONNECT_TIMEOUT, TEST_SOCKET_TIMEOUT, false, null, null, TEST_CERT_PATH, false);
+        final String testHttpProxy = "socket://example.com:port";
+        metadata.put(PROXY_PARAMETER, testHttpProxy);
+        final PluginSetting pluginSetting = getPluginSettingByConfigurationMetadata(metadata);
+        final ConnectionConfiguration connectionConfiguration =
+                ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
+        assertEquals(connectionConfiguration.getProxy().get(), testHttpProxy);
+        assertThrows(IllegalArgumentException.class, () -> connectionConfiguration.createClient());
+    }
+
+    @Test
+    public void testCreateClient_WithConnectionConfigurationBuilder_ProxyOptionalObjectShouldNotBeNull() throws IOException {
+        final ConnectionConfiguration.Builder builder = new ConnectionConfiguration.Builder(TEST_HOSTS);
+        final ConnectionConfiguration connectionConfiguration = builder.build();
+        assertEquals(Optional.empty(), connectionConfiguration.getProxy());
+        final RestHighLevelClient client = connectionConfiguration.createClient();
+        assertNotNull(client);
+        client.close();
+    }
+
     private PluginSetting generatePluginSetting(
+            final List<String> hosts, final String username, final String password,
+            final Integer connectTimeout, final Integer socketTimeout, final boolean awsSigv4, final String awsRegion,
+            final String awsStsRoleArn, final String certPath, final boolean insecure) {
+        final Map<String, Object> metadata = generateConfigurationMetadata(hosts, username, password, connectTimeout, socketTimeout, awsSigv4, awsRegion, awsStsRoleArn, certPath, insecure);
+        return getPluginSettingByConfigurationMetadata(metadata);
+    }
+
+    private Map<String, Object> generateConfigurationMetadata(
             final List<String> hosts, final String username, final String password,
             final Integer connectTimeout, final Integer socketTimeout, final boolean awsSigv4, final String awsRegion,
             final String awsStsRoleArn, final String certPath, final boolean insecure) {
@@ -184,6 +300,10 @@ public class ConnectionConfigurationTests {
         metadata.put("aws_sts_role_arn", awsStsRoleArn);
         metadata.put("cert", certPath);
         metadata.put("insecure", insecure);
+        return metadata;
+    }
+
+    private PluginSetting getPluginSettingByConfigurationMetadata(final Map<String, Object> metadata) {
         final PluginSetting pluginSetting = new PluginSetting("opensearch", metadata);
         pluginSetting.setPipelineName(TEST_PIPELINE_NAME);
         return pluginSetting;
