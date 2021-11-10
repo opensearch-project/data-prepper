5414891246ef9843d00c9f199a17a5391b688622
David Venable <dlv@amazon.com>
2021-11-09 20:08:05 -0600

    Support HTTP Basic authentication on the core Data Prepper APIs (#558)
    Support HTTP Basic authentication on the core Data Prepper APIs. This uses plugin support so that it can be customized if needed. Includes documentation for configuring or disabling authentication. #312
    
    Signed-off-by: David Venable <dlv@amazon.com>
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

---
+++ data-prepper-core/src/main/java/com/amazon/dataprepper/plugins/HttpBasicDataPrepperCoreAuthenticationProvider.java
@@ -0,0 +1,52 @@
+package com.amazon.dataprepper.plugins;
+
+import com.amazon.dataprepper.model.annotations.DataPrepperPlugin;
+import com.amazon.dataprepper.model.annotations.DataPrepperPluginConstructor;
+import com.amazon.dataprepper.pipeline.server.DataPrepperCoreAuthenticationProvider;
+import com.amazon.dataprepper.pipeline.server.HttpBasicAuthenticationConfig;
+import com.sun.net.httpserver.Authenticator;
+import com.sun.net.httpserver.BasicAuthenticator;
+
+import java.util.Objects;
+
+/**
+ * The plugin for HTTP Basic authentication of the core Data Prepper APIs.
+ *
+ * @since 1.2
+ */
+@DataPrepperPlugin(name = "http_basic",
+        pluginType = DataPrepperCoreAuthenticationProvider.class,
+        pluginConfigurationType = HttpBasicAuthenticationConfig.class)
+public class HttpBasicDataPrepperCoreAuthenticationProvider implements DataPrepperCoreAuthenticationProvider {
+
+    private final HttpBasicAuthenticationConfig httpBasicAuthenticationConfig;
+    private final CoreAuthenticator coreAuthenticator;
+
+    @DataPrepperPluginConstructor
+    public HttpBasicDataPrepperCoreAuthenticationProvider(final HttpBasicAuthenticationConfig httpBasicAuthenticationConfig) {
+        this.httpBasicAuthenticationConfig = httpBasicAuthenticationConfig;
+        Objects.requireNonNull(this.httpBasicAuthenticationConfig);
+        Objects.requireNonNull(httpBasicAuthenticationConfig.getUsername());
+        Objects.requireNonNull(httpBasicAuthenticationConfig.getPassword());
+
+        coreAuthenticator = new CoreAuthenticator();
+    }
+
+    @Override
+    public Authenticator getAuthenticator() {
+        return coreAuthenticator;
+    }
+
+    private class CoreAuthenticator extends BasicAuthenticator {
+
+        CoreAuthenticator() {
+            super("core");
+        }
+
+        @Override
+        public boolean checkCredentials(final String username, final String password) {
+            return httpBasicAuthenticationConfig.getUsername().equals(username) &&
+                    httpBasicAuthenticationConfig.getPassword().equals(password);
+        }
+    }
+}
