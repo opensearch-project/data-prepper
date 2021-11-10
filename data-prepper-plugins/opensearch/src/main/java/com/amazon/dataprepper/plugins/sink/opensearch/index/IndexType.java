090acba77550842289f80e085221ff29b048c39a
Han Jiang <jianghan@amazon.com>
2021-11-10 09:48:48 -0600

    Support index-type parameter (#480)
    * Support index-type parameter
    Signed-off-by: Han Jiang <jianghan@amazon.com>
    
    * Rename IndexType attribute to be more succict and intuitive.
    Signed-off-by: Han Jiang <jianghan@amazon.com>
    
    * Add more unit tests and optimize getting value from Optional object
    Signed-off-by: Han Jiang <jianghan@amazon.com>
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

--- data-prepper-plugins/opensearch/src/main/java/com/amazon/dataprepper/plugins/sink/opensearch/index/IndexType.java
+++ data-prepper-plugins/opensearch/src/main/java/com/amazon/dataprepper/plugins/sink/opensearch/index/IndexType.java
@@ -1,7 +1,54 @@
 package com.amazon.dataprepper.plugins.sink.opensearch.index;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public enum IndexType {
-    TRACE_ANALYTICS_RAW,
-    TRACE_ANALYTICS_SERVICE_MAP,
-    CUSTOM;
+    TRACE_ANALYTICS_RAW("trace-analytics-raw"),
+    TRACE_ANALYTICS_SERVICE_MAP("trace-analytics-service-map"),
+    CUSTOM("custom");
+
+    private final String value;
+
+    /**
+     * This is a reverse-lookup map for getting a IndexType from a value.
+     */
+    private static final Map<String, IndexType> STRING_TO_INDEX_TYPE_MAP = new HashMap<>();
+
+    static {
+        Arrays.stream(IndexType.values())
+                .forEach(indexType -> STRING_TO_INDEX_TYPE_MAP.put(indexType.value, indexType));
+    }
+
+    IndexType(final String value){
+        this.value = value;
+    }
+
+    String getValue(){
+        return value;
+    }
+
+    /**
+     * This is for getting an IndexType enum from a given string value.
+     * @param value The string value of an IndexType enum
+     * @return IndexType enum matching the string value
+     */
+    static Optional<IndexType> getByValue(final String value) {
+        return Optional.ofNullable(STRING_TO_INDEX_TYPE_MAP.get(value));
+    }
+
+    /**
+     * This flattens all values into a string which can be used for logging or showing what values are supported
+     * for the index_type parameter
+     * @return a string containing all values that are supported for the index_type parameter
+     */
+    static String getIndexTypeValues() {
+        return Arrays.stream(IndexType.values())
+                .map(IndexType::getValue)
+                .collect(Collectors.toList())
+                .toString();
+    }
 }
