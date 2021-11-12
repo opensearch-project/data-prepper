4fb2563bb25ff38c8b372e5dcaa210623d436616
Asif Sohail Mohammed <nsifmoh@amazon.com>
2021-11-12 17:12:25 -0600

    Updated configConverter and removed the tests
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>

--- data-prepper-logstash-configuration/src/main/java/org/opensearch/dataprepper/logstash/LogstashConfigConverter.java
+++ data-prepper-logstash-configuration/src/main/java/org/opensearch/dataprepper/logstash/LogstashConfigConverter.java
@@ -1,4 +1,4 @@
-package org.opensearch.dataprepper.logstash.parser;
+package org.opensearch.dataprepper.logstash;
 
 import com.amazon.dataprepper.model.configuration.PipelineModel;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -7,14 +7,13 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
-import org.opensearch.dataprepper.logstash.LogstashLexer;
-import org.opensearch.dataprepper.logstash.LogstashParser;
 import org.opensearch.dataprepper.logstash.mapping.LogstashMapper;
 import org.opensearch.dataprepper.logstash.model.LogstashConfiguration;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.Map;
@@ -26,31 +25,35 @@ import java.util.Map;
  */
 public class LogstashConfigConverter {
     public String convertLogstashConfigurationToPipeline(String logstashConfigurationPath, String outputDirectory) throws IOException {
-        final String logstashConfigAsString = new String(Files.readAllBytes(Paths.get(logstashConfigurationPath)));
+        final Path configurationFilePath = Paths.get(logstashConfigurationPath);
+        final String logstashConfigAsString = new String(Files.readAllBytes(configurationFilePath));
 
         LogstashLexer lexer = new LogstashLexer(CharStreams.fromString(logstashConfigAsString));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         LogstashParser parser = new LogstashParser(tokens);
-        ParseTree tree = parser.config();
+        final ParseTree tree = parser.config();
 
-        LogstashVisitor visitor = new LogstashVisitor();
+        org.opensearch.dataprepper.logstash.parser.LogstashVisitor visitor = new org.opensearch.dataprepper.logstash.parser.LogstashVisitor();
         LogstashConfiguration logstashConfiguration = (LogstashConfiguration) visitor.visit(tree);
 
         LogstashMapper logstashMapper = new LogstashMapper();
         PipelineModel pipelineModel = logstashMapper.mapPipeline(logstashConfiguration);
-        Map<String, Object> pipeline = Collections.singletonMap("log-pipeline", pipelineModel);
+        final Map<String, Object> pipeline = Collections.singletonMap("logstash-converted-pipeline", pipelineModel);
 
-        YAMLFactory factory = new YAMLFactory();
-        factory.disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER);
-        factory.enable(YAMLGenerator.Feature.INDENT_ARRAYS_WITH_INDICATOR);
-        factory.disable(YAMLGenerator.Feature.SPLIT_LINES);
-        factory.enable(YAMLGenerator.Feature.LITERAL_BLOCK_STYLE);
+        ObjectMapper mapper = new ObjectMapper(YAMLFactory.builder()
+                .disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER)
+                .enable(YAMLGenerator.Feature.INDENT_ARRAYS_WITH_INDICATOR)
+                .disable(YAMLGenerator.Feature.SPLIT_LINES)
+                .enable(YAMLGenerator.Feature.LITERAL_BLOCK_STYLE)
+                .build());
 
-        ObjectMapper mapper = new ObjectMapper(factory);
+        final String confFileName = configurationFilePath.getFileName().toString();
+        final String yamlFileName = confFileName.substring(0, confFileName.lastIndexOf("."));
 
-        String yamlFilePath = outputDirectory + "logstash.yaml";
-        mapper.writeValue(new File(yamlFilePath), pipeline);
+        final Path yamlFilePath = Paths.get(outputDirectory , yamlFileName + ".yaml");
 
-        return yamlFilePath;
+        mapper.writeValue(new File(String.valueOf(yamlFilePath)), pipeline);
+
+        return String.valueOf(yamlFilePath);
     }
 }
