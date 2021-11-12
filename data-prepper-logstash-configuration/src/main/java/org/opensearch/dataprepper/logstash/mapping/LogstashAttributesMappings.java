08f17ab37193ce09d403eaaebb4d44aedd1bbbda
David Venable <dlv@amazon.com>
2021-11-12 12:47:11 -0600

    NPE bug fixes in the DefaultLogstashPluginAttributesMapper (#574)
    Bug fixes in the DefaultLogstashPluginAttributesMapper related to null values. The LogstashMappingModel now returns empty maps when they are null or absent in the YAML. Part of #568.
    
    Signed-off-by: David Venable <dlv@amazon.com>

--- data-prepper-logstash-configuration/src/main/java/org/opensearch/dataprepper/logstash/mapping/LogstashAttributesMappings.java
+++ data-prepper-logstash-configuration/src/main/java/org/opensearch/dataprepper/logstash/mapping/LogstashAttributesMappings.java
@@ -13,7 +13,25 @@ import java.util.Map;
  * @since 1.2
  */
 public interface LogstashAttributesMappings {
+    /**
+     * A map of attribute names in the Logstash configuration to
+     * the correct property name in Data Prepper.
+     * <p>
+     * This should not return null and should return empty if not defined.
+     *
+     * @return A Map
+     * @since 1.2
+     */
     Map<String, String> getMappedAttributeNames();
 
+    /**
+     * A map of Data Prepper property names to values to set on those
+     * properties.
+     * <p>
+     * This should not return null and should return empty if not defined.
+     *
+     * @return A Map
+     * @since 1.2
+     */
     Map<String, Object> getAdditionalAttributes();
 }
--- data-prepper-logstash-configuration/src/test/java/org/opensearch/dataprepper/logstash/mapping/LogstashMappingModelTest.java
+++ data-prepper-logstash-configuration/src/test/java/org/opensearch/dataprepper/logstash/mapping/LogstashMappingModelTest.java
@@ -41,4 +41,17 @@ class LogstashMappingModelTest {
         assertThat(logstashMappingModel.getAdditionalAttributes().get("addA"), equalTo(true));
         assertThat(logstashMappingModel.getAdditionalAttributes().get("addB"), equalTo("staticValueB"));
     }
+
+    @Test
+    void deserialize_from_JSON_with_null_values_has_empty_lists() throws IOException {
+
+        final LogstashMappingModel logstashMappingModel = objectMapper.readValue(this.getClass().getResourceAsStream("sample-with-nulls.mapping.yaml"), LogstashMappingModel.class);
+
+        assertThat(logstashMappingModel.getPluginName(), equalTo("samplePlugin"));
+        assertThat(logstashMappingModel.getAttributesMapperClass(), equalTo("org.opensearch.dataprepper.Placeholder"));
+        assertThat(logstashMappingModel.getMappedAttributeNames(), notNullValue());
+        assertThat(logstashMappingModel.getMappedAttributeNames().size(), equalTo(0));
+        assertThat(logstashMappingModel.getAdditionalAttributes(), notNullValue());
+        assertThat(logstashMappingModel.getAdditionalAttributes().size(), equalTo(0));
+    }
 }
