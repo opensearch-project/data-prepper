7cba10fe5b698fdee2191fe99a675d427b61867c
David Venable <dlv@amazon.com>
2021-11-12 08:01:49 -0600

    Added LogstashPluginAttributesMapper for custom mapping (#568)
    Refactored the DefaultPluginMapper to use LogstashPluginAttributesMapper as the mechanism for mapping plugins. This can be configure in the mapping YAML to allow detailed mapping configurations. #466
    
    Signed-off-by: David Venable <dlv@amazon.com>
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

--- data-prepper-logstash-configuration/src/main/java/org/opensearch/dataprepper/logstash/mapping/DefaultPluginMapper.java
+++ data-prepper-logstash-configuration/src/main/java/org/opensearch/dataprepper/logstash/mapping/DefaultPluginMapper.java
@@ -5,12 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.opensearch.dataprepper.logstash.exception.LogstashMappingException;
 import org.opensearch.dataprepper.logstash.model.LogstashPlugin;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -19,8 +16,16 @@ import java.util.Map;
  * @since 1.2
  */
 public class DefaultPluginMapper implements LogstashPluginMapper {
-    private static final Logger LOG = LoggerFactory.getLogger(DefaultPluginMapper.class);
     private final ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+    private final AttributesMapperProvider attributesMapperProvider;
+
+    public DefaultPluginMapper() {
+        this(new AttributesMapperProvider());
+    }
+
+    DefaultPluginMapper(final AttributesMapperProvider attributesMapperProvider) {
+        this.attributesMapperProvider = attributesMapperProvider;
+    }
 
     @Override
     public PluginModel mapPlugin(LogstashPlugin logstashPlugin) {
@@ -43,19 +48,10 @@ public class DefaultPluginMapper implements LogstashPluginMapper {
         if (logstashMappingModel.getPluginName() == null) {
             throw new LogstashMappingException("The mapping file " + mappingResourceName + " has a null value for 'pluginName'.");
         }
-        Map<String, Object> pluginSettings = new LinkedHashMap<>(logstashMappingModel.getAdditionalAttributes());
 
-        logstashPlugin.getAttributes().forEach(logstashAttribute -> {
-            if (logstashMappingModel.getMappedAttributeNames().containsKey(logstashAttribute.getAttributeName())) {
-                pluginSettings.put(
-                        logstashMappingModel.getMappedAttributeNames().get(logstashAttribute.getAttributeName()),
-                        logstashAttribute.getAttributeValue().getValue()
-                );
-            }
-            else {
-                LOG.warn("Attribute name {} is not found in mapping file.", logstashAttribute.getAttributeName());
-            }
-        });
+        final LogstashPluginAttributesMapper pluginAttributesMapper = attributesMapperProvider.getAttributesMapper(logstashMappingModel);
+
+        final Map<String, Object> pluginSettings = pluginAttributesMapper.mapAttributes(logstashPlugin.getAttributes(), logstashMappingModel);
 
         return new PluginModel(logstashMappingModel.getPluginName(), pluginSettings);
     }
---
+++ data-prepper-logstash-configuration/src/test/java/org/opensearch/dataprepper/logstash/mapping/LogstashMappingModelTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.logstash.mapping;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class LogstashMappingModelTest {
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper(new YAMLFactory());
+
+    }
+
+    @Test
+    void deserialize_from_JSON_should_have_expected_value() throws IOException {
+
+        final LogstashMappingModel logstashMappingModel = objectMapper.readValue(this.getClass().getResourceAsStream("sample.mapping.yaml"), LogstashMappingModel.class);
+
+        assertThat(logstashMappingModel.getPluginName(), equalTo("samplePlugin"));
+        assertThat(logstashMappingModel.getAttributesMapperClass(), equalTo("org.opensearch.dataprepper.Placeholder"));
+        assertThat(logstashMappingModel.getMappedAttributeNames(), notNullValue());
+        assertThat(logstashMappingModel.getMappedAttributeNames().size(), equalTo(2));
+        assertThat(logstashMappingModel.getMappedAttributeNames().get("valueA"), equalTo("keyA"));
+        assertThat(logstashMappingModel.getMappedAttributeNames().get("valueB"), equalTo("keyB"));
+        assertThat(logstashMappingModel.getAdditionalAttributes(), notNullValue());
+        assertThat(logstashMappingModel.getAdditionalAttributes().size(), equalTo(2));
+        assertThat(logstashMappingModel.getAdditionalAttributes().get("addA"), equalTo(true));
+        assertThat(logstashMappingModel.getAdditionalAttributes().get("addB"), equalTo("staticValueB"));
+    }
+}
