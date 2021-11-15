faa6c74620cd203eb8938d998d5c497c5fd35598
Asif Sohail Mohammed <mdasifsohail7@gmail.com>
2021-11-15 10:05:19 -0600

    Integrated logstash converter into Data Prepper core (#591)
    Integrated logstash converter into Data Prepper core
    
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>
    
    Co-authored-by: Asif Sohail Mohammed <nsifmoh@amazon.com>
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

--- data-prepper-core/src/main/java/com/amazon/dataprepper/DataPrepperExecute.java
+++ data-prepper-core/src/main/java/com/amazon/dataprepper/DataPrepperExecute.java
@@ -11,8 +11,13 @@
 
 package com.amazon.dataprepper;
 
+import org.opensearch.dataprepper.logstash.LogstashConfigConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Execute entry into Data Prepper.
@@ -30,10 +35,26 @@ public class DataPrepperExecute {
         }
         final DataPrepper dataPrepper = DataPrepper.getInstance();
         if (args.length > 0) {
-            dataPrepper.execute(args[0]);
+            String configurationFileLocation = checkForLogstashConfigurationAndConvert(args[0]);
+            dataPrepper.execute(configurationFileLocation);
         } else {
             LOG.error("Configuration file is required");
             System.exit(1);
         }
+    }
+
+    private static String checkForLogstashConfigurationAndConvert(String configurationFileLocation) {
+        if (configurationFileLocation.endsWith(".conf")) {
+            final LogstashConfigConverter logstashConfigConverter = new LogstashConfigConverter();
+            final Path configurationDirectory = Paths.get(configurationFileLocation).toAbsolutePath().getParent();
+
+            try {
+                configurationFileLocation = logstashConfigConverter.convertLogstashConfigurationToPipeline(
+                        configurationFileLocation, String.valueOf(configurationDirectory));
+            } catch (IOException e) {
+                LOG.error("Unable to read the Logstash configuration file", e);
+            }
+        }
+        return configurationFileLocation;
     }
 }
