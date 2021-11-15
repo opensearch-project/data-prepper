ab61d573fa57e9eb1d7b942f8afb369b5a51cfd0
David Venable <dlv@amazon.com>
2021-11-15 11:49:05 -0600

    Added integration tests for the Logstash Configuration Converter (#592)
    Added integration tests for the LogstashConfigConverter class. These test the entire conversion process by converting and comparing the results to expected YAML files. Includes some updates to the PipelineDataFlowModel.
    
    Signed-off-by: David Venable <dlv@amazon.com>

--- data-prepper-api/src/main/java/com/amazon/dataprepper/model/configuration/PipelinesDataFlowModel.java
+++ data-prepper-api/src/main/java/com/amazon/dataprepper/model/configuration/PipelinesDataFlowModel.java
@@ -1,14 +1,34 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.amazon.dataprepper.model.configuration;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonCreator;
 
+import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Model for the Pipelines data flow. This has the format of the Data Prepper
+ * pipelines.yaml file.
+ *
+ * @since 1.2
+ */
 public class PipelinesDataFlowModel {
 
     @JsonAnySetter
-    private Map<String, PipelineModel> pipelines;
+    private Map<String, PipelineModel> pipelines = new HashMap<>();
+
+    /**
+     * Jackson will use this constructor.
+     */
+    @JsonCreator
+    @SuppressWarnings("unused")
+    private PipelinesDataFlowModel() { }
 
     public PipelinesDataFlowModel(final Map<String, PipelineModel> pipelines) {
         this.pipelines = pipelines;
@@ -18,5 +38,4 @@ public class PipelinesDataFlowModel {
     public Map<String, PipelineModel> getPipelines() {
         return pipelines;
     }
-
 }
--- data-prepper-api/src/test/java/com/amazon/dataprepper/model/configuration/PipelinesDataFlowModelTest.java
+++ data-prepper-api/src/test/java/com/amazon/dataprepper/model/configuration/PipelinesDataFlowModelTest.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
@@ -14,9 +15,11 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasKey;
 
 class PipelinesDataFlowModelTest {
 
+    private static final String RESOURCE_PATH = "/pipelines_data_flow_serialized.yaml";
     private ObjectMapper objectMapper;
 
     @BeforeEach
@@ -39,11 +42,43 @@ class PipelinesDataFlowModelTest {
 
         final String serializedString = objectMapper.writeValueAsString(pipelinesDataFlowModel);
 
-        InputStream inputStream = this.getClass().getResourceAsStream("/pipelines_data_flow_serialized.yaml");
+        InputStream inputStream = this.getClass().getResourceAsStream(RESOURCE_PATH);
 
         final String expectedYaml = PluginModelTests.convertInputStreamToString(inputStream);
 
         assertThat(serializedString, notNullValue());
         assertThat(serializedString, equalTo(expectedYaml));
+    }
+
+    @Test
+    void deserialize_PipelinesDataFlowModel() throws IOException {
+
+        final InputStream inputStream = this.getClass().getResourceAsStream(RESOURCE_PATH);
+
+        final PipelinesDataFlowModel actualModel = objectMapper.readValue(inputStream, PipelinesDataFlowModel.class);
+
+        final String pipelineName = "test-pipeline";
+
+        assertThat(actualModel, notNullValue());
+        assertThat(actualModel.getPipelines(), notNullValue());
+        assertThat(actualModel.getPipelines().size(), equalTo(1));
+        assertThat(actualModel.getPipelines(), hasKey(pipelineName));
+
+        final PipelineModel pipelineModel = actualModel.getPipelines().get(pipelineName);
+
+        assertThat(pipelineModel, notNullValue());
+
+        assertThat(pipelineModel.getSource(), notNullValue());
+        assertThat(pipelineModel.getSource().getPluginName(), equalTo("testSource"));
+
+        assertThat(pipelineModel.getPreppers(), notNullValue());
+        assertThat(pipelineModel.getPreppers().size(), equalTo(1));
+        assertThat(pipelineModel.getPreppers().get(0), notNullValue());
+        assertThat(pipelineModel.getPreppers().get(0).getPluginName(), equalTo("testPrepper"));
+
+        assertThat(pipelineModel.getSinks(), notNullValue());
+        assertThat(pipelineModel.getSinks().size(), equalTo(1));
+        assertThat(pipelineModel.getSinks().get(0), notNullValue());
+        assertThat(pipelineModel.getSinks().get(0).getPluginName(), equalTo("testSink"));
     }
 }
