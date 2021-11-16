45c4fce303e48e941cdc5a8d5390703a13e903f2
Taylor Gray <33740195+graytaylor0@users.noreply.github.com>
2021-11-15 19:53:48 -0600

    Refactored file source, added type and format configuration options for json and plaintext
    Signed-off-by: Taylor Gray <33740195+graytaylor0@users.noreply.github.com>

---
+++ data-prepper-plugins/common/src/test/java/com/amazon/dataprepper/plugins/source/file/FileSourceTests.java
@@ -0,0 +1,224 @@
+/*
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  The OpenSearch Contributors require contributions made to
+ *  this file be licensed under the Apache-2.0 license or a
+ *  compatible open source license.
+ *
+ *  Modifications Copyright OpenSearch Contributors. See
+ *  GitHub history for details.
+ */
+
+package com.amazon.dataprepper.plugins.source.file;
+
+import com.amazon.dataprepper.metrics.PluginMetrics;
+import com.amazon.dataprepper.model.buffer.Buffer;
+import com.amazon.dataprepper.model.configuration.PluginSetting;
+import com.amazon.dataprepper.model.event.Event;
+import com.amazon.dataprepper.model.event.JacksonEvent;
+import com.amazon.dataprepper.model.plugin.PluginFactory;
+import com.amazon.dataprepper.model.record.Record;
+import com.amazon.dataprepper.plugins.buffer.blockingbuffer.BlockingBuffer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+public class FileSourceTests {
+    private static final Logger LOG = LoggerFactory.getLogger(FileSourceTests.class);
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final TypeReference<Map<String, Object>> MAP_TYPE_REFERENCE = new TypeReference<Map<String, Object>>() {};
+
+    private static final String TEST_PIPELINE_NAME = "pipeline";
+    private static final int TEST_WRITE_TIMEOUT = 100;
+    private static final String TEST_FILE_PATH_PLAIN = "src/test/resources/test-file-source-plain.tst";
+    private static final String TEST_FILE_PATH_JSON = "src/test/resources/test-file-source-json.tst";
+    private static final String TEST_FILE_PATH_INVALID_JSON = "src/test/resources/test-file-source-invalid-json.tst";
+    private static final String FILE_DOES_NOT_EXIST = "file_does_not_exist";
+
+    private FileSourceConfig fileSourceConfig;
+    private FileSource fileSource;
+
+    @Mock
+    private PluginMetrics pluginMetrics;
+
+    @Mock
+    private PluginFactory pluginFactory;
+
+    private Buffer<Record<Event>> buffer;
+
+    final Map<String, Object> pluginSettings = new HashMap<>();
+
+    final List<Record<Event>> expectedEventsPlain = new ArrayList<>();
+    final List<Record<Event>> expectedEventsJson = new ArrayList<>();
+    final List<Record<Event>> expectedEventsInvalidJson = new ArrayList<>();
+
+
+    @BeforeEach
+    public void setup() {
+        pluginSettings.put(FileSourceConfig.ATTRIBUTE_PATH, TEST_FILE_PATH_PLAIN);
+        pluginSettings.put(FileSourceConfig.ATTRIBUTE_TYPE, "new_type");
+        pluginSettings.put(FileSourceConfig.ATTRIBUTE_TIMEOUT, TEST_WRITE_TIMEOUT);
+
+        // plain
+        final String expectedPlainFirstLine = "THIS IS A PLAINTEXT LINE";
+        final String expectedPlainSecondLine = "THIS IS ANOTHER PLAINTEXT LINE";
+
+
+        final Record<Event> firstEventPlain = createRecordEventWithKeyValuePair(FileSource.MESSAGE_KEY, expectedPlainFirstLine);
+        final Record<Event> secondEventPlain = createRecordEventWithKeyValuePair(FileSource.MESSAGE_KEY, expectedPlainSecondLine);
+
+        expectedEventsPlain.add(firstEventPlain);
+        expectedEventsPlain.add(secondEventPlain);
+
+        //json
+        final String expectedJsonFirstLine = "{\"test_key: \"test_value\"}";
+        final String expectedJsonSecondLine = "{\"second_test_key\": \"second_test_value\"}";
+
+        final Record<Event> firstEventJson = createRecordEventWithKeyValuePair("test_key", "test_value");
+        final Record<Event> secondEventJson = createRecordEventWithKeyValuePair("second_test_key", "second_test_value");
+
+        expectedEventsJson.add(firstEventJson);
+        expectedEventsJson.add(secondEventJson);
+
+        // invalid json
+        final String expectedInvalidJsonFirstLine = "{\"test_key: test_value\"}";
+        final String expectedInvalidJsonSecondLine = "{\"second_test_key\": \"second_test_value\"";
+
+
+        final Record<Event> firstEventInvalidJson = createRecordEventWithKeyValuePair(FileSource.MESSAGE_KEY, expectedInvalidJsonFirstLine);
+        final Record<Event> secondEventInvalidJson = createRecordEventWithKeyValuePair(FileSource.MESSAGE_KEY, expectedInvalidJsonSecondLine);
+
+        expectedEventsInvalidJson.add(firstEventInvalidJson);
+        expectedEventsInvalidJson.add(secondEventInvalidJson);
+
+
+
+        buffer = getBuffer();
+    }
+
+    private FileSource createObjectUnderTest() {
+        fileSourceConfig = OBJECT_MAPPER.convertValue(pluginSettings, FileSourceConfig.class);
+        return new FileSource(fileSourceConfig, pluginMetrics, pluginFactory);
+    }
+
+    private BlockingBuffer<Record<Event>> getBuffer() {
+        final HashMap<String, Object> integerHashMap = new HashMap<>();
+        integerHashMap.put("buffer_size", 2);
+        integerHashMap.put("batch_size", 2);
+        final PluginSetting pluginSetting = new PluginSetting("blocking_buffer", integerHashMap) {{
+            setPipelineName(TEST_PIPELINE_NAME);
+        }};
+        return new BlockingBuffer<>(pluginSetting);
+    }
+
+    @Test
+    public void testFileSourceWithEmptyFilePathThrowsRuntimeException() {
+        pluginSettings.put(FileSourceConfig.ATTRIBUTE_PATH, "");
+        fileSource = createObjectUnderTest();
+        assertThrows(RuntimeException.class, () -> fileSource.start(buffer));
+    }
+
+    @Test
+    public void testFileSourceWithNonexistentFilePathThrowsRuntimeException() {
+        pluginSettings.put(FileSourceConfig.ATTRIBUTE_PATH, FILE_DOES_NOT_EXIST);
+        fileSource = createObjectUnderTest();
+        assertThrows(RuntimeException.class, () -> fileSource.start(buffer));
+    }
+
+    @Test
+    public void testFileSourceWithNullFilePathThrowsNullPointerException() {
+        pluginSettings.put(FileSourceConfig.ATTRIBUTE_PATH, null);
+        assertThrows(NullPointerException.class, this::createObjectUnderTest);
+    }
+
+    @Test
+    public void testFileWithPlainTextAddsEventsToBufferCorrectly() {
+        fileSource = createObjectUnderTest();
+        fileSource.start(buffer);
+
+        final List<Record<Event>> bufferEvents = new ArrayList<>(buffer.read(1000).getKey());
+
+        assertThat(bufferEvents.size(), equalTo(expectedEventsPlain.size()));
+        assertExpectedRecordsAreEqual(expectedEventsPlain, bufferEvents);
+    }
+
+    @Test
+    public void testFileWithJSONAddsEventsToBufferCorrectly() {
+        pluginSettings.put(FileSourceConfig.ATTRIBUTE_PATH, TEST_FILE_PATH_JSON);
+        pluginSettings.put(FileSourceConfig.ATTRIBUTE_FORMAT, FileFormat.getByName("json"));
+        fileSource = createObjectUnderTest();
+        fileSource.start(buffer);
+
+        final List<Record<Event>> bufferEvents = new ArrayList<>(buffer.read(1000).getKey());
+
+        assertThat(bufferEvents.size(), equalTo(expectedEventsJson.size()));
+        assertExpectedRecordsAreEqual(expectedEventsJson, bufferEvents);
+    }
+
+    @Test
+    public void testFileWithInvalidJSONAddsEventsToBufferAsPlainText() {
+        pluginSettings.put(FileSourceConfig.ATTRIBUTE_PATH, TEST_FILE_PATH_INVALID_JSON);
+        pluginSettings.put(FileSourceConfig.ATTRIBUTE_FORMAT, FileFormat.getByName("json"));
+        fileSource = createObjectUnderTest();
+        fileSource.start(buffer);
+
+        final List<Record<Event>> bufferEvents = new ArrayList<>(buffer.read(1000).getKey());
+
+        assertThat(bufferEvents.size(), equalTo(expectedEventsInvalidJson.size()));
+        assertExpectedRecordsAreEqual(expectedEventsInvalidJson, bufferEvents);
+    }
+
+    @Test
+    public void testNonSupportedFileFormatThrowsNullPointerException() {
+        pluginSettings.put(FileSourceConfig.ATTRIBUTE_FORMAT, FileFormat.getByName("unsupported"));
+        assertThrows(NullPointerException.class, this::createObjectUnderTest);
+    }
+
+    static void assertExpectedRecordsAreEqual(final List<Record<Event>> expectedEvents, final List<Record<Event>> actualEvents) {
+        for (int i = 0; i < expectedEvents.size(); i++) {
+            assertThat(actualEvents.get(i), notNullValue());
+            assertThat(actualEvents.get(i).getData(), notNullValue());
+            assertRecordsAreEqual(actualEvents.get(i), expectedEvents.get(i));
+        }
+    }
+
+    static void assertRecordsAreEqual(final Record<Event> first, final Record<Event> second) {
+        try {
+            final Map<String, Object> recordMapFirst = OBJECT_MAPPER.readValue(first.getData().toJsonString(), MAP_TYPE_REFERENCE);
+            final Map<String, Object> recordMapSecond = OBJECT_MAPPER.readValue(second.getData().toJsonString(), MAP_TYPE_REFERENCE);
+            assertThat(recordMapFirst, is(equalTo(recordMapSecond)));
+        } catch (JsonProcessingException e) {
+            LOG.error("Unable to parse Event as JSON");
+        }
+    }
+
+    private Record<Event> createRecordEventWithKeyValuePair(final String key, final String value) {
+        final Map<String, Object> eventData = new HashMap<>();
+        eventData.put(key, value);
+
+        return new Record<>(JacksonEvent
+                .builder()
+                .withEventType("event")
+                .withData(eventData)
+                .build());
+    }
+}
