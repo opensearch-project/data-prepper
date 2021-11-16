64d9fd504bc9dff037bfa25ee51a1fba4feaaec8
Steven Bayer <smbayer@amazon.com>
2021-11-16 11:14:44 -0600

    move deprecated prepper warning to always log if prepper is defined
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

--- data-prepper-api/src/main/java/com/amazon/dataprepper/model/configuration/PipelineModel.java
+++ data-prepper-api/src/main/java/com/amazon/dataprepper/model/configuration/PipelineModel.java
@@ -43,7 +43,6 @@ public class PipelineModel {
             throw new IllegalArgumentException(message);
         }
         else if (preppers != null) {
-            LOG.warn("prepper configurations are deprecated, processor configurations will be required in 1.2");
             return preppers;
         }
         else {
