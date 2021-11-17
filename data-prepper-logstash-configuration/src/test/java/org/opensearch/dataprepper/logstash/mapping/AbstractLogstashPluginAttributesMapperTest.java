5f5cb41f1e26a7d5f01af42d94a056b9c2c600dc
Asif Sohail Mohammed <mdasifsohail7@gmail.com>
2021-11-17 16:27:44 -0600

    Updated Logstash attributes mapper to use template pattern (#617)
    * Updated Logstash attributes mapper to use template pattern
    
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>
    
    Co-authored-by: Asif Sohail Mohammed <nsifmoh@amazon.com>

---
+++ data-prepper-logstash-configuration/src/test/java/org/opensearch/dataprepper/logstash/mapping/AbstractLogstashPluginAttributesMapperTest.java
@@ -0,0 +1,59 @@
+package org.opensearch.dataprepper.logstash.mapping;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.opensearch.dataprepper.logstash.model.LogstashAttribute;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.LinkedHashMap;
+import java.util.HashSet;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.never;
+
+class AbstractLogstashPluginAttributesMapperTest {
+
+    private List<LogstashAttribute> logstashAttributes;
+    private LogstashAttributesMappings mappings;
+    private Map<String, Object> pluginSettings;
+
+    @BeforeEach
+    void setUp() {
+        final LogstashAttribute logstashAttribute = mock(LogstashAttribute.class);
+
+        logstashAttributes = Collections.singletonList(logstashAttribute);
+        mappings = mock(LogstashAttributesMappings.class);
+        pluginSettings = new LinkedHashMap<>(mappings.getAdditionalAttributes());
+    }
+
+    @Test
+    void mapAttributes_with_no_custom_attributes_does_not_invoke_mapCustomAttributes_Test() {
+        AbstractLogstashPluginAttributesMapper abstractLogstashPluginAttributesMapper = Mockito
+                .spy(AbstractLogstashPluginAttributesMapper.class);
+
+        when(abstractLogstashPluginAttributesMapper.getCustomMappedAttributeNames()).thenReturn(new HashSet<>());
+
+        Map<String, Object> pluginSettings = abstractLogstashPluginAttributesMapper.mapAttributes(logstashAttributes, mappings);
+        verify(abstractLogstashPluginAttributesMapper, never()).mapCustomAttributes(
+                logstashAttributes, mappings, pluginSettings);
+    }
+
+
+    @Test
+    void mapAttributes_with_custom_attributes_invokes_mapCustomAttributes_Test() {
+        AbstractLogstashPluginAttributesMapper abstractLogstashPluginAttributesMapper = Mockito
+                .spy(AbstractLogstashPluginAttributesMapper.class);
+
+        when(abstractLogstashPluginAttributesMapper.getCustomMappedAttributeNames())
+                .thenReturn(new HashSet<>(Collections.singletonList("customAttribute")));
+
+        Map<String, Object> pluginSettings = abstractLogstashPluginAttributesMapper.mapAttributes(logstashAttributes, mappings);
+        verify(abstractLogstashPluginAttributesMapper).mapCustomAttributes(logstashAttributes, mappings, pluginSettings);
+    }
+
+}
