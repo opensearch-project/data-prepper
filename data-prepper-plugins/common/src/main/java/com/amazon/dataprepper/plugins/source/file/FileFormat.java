e1713ccddf1c3e97eaf900fe5ede7208cfd9af66
Taylor Gray <33740195+graytaylor0@users.noreply.github.com>
2021-11-18 11:49:36 -0600

    File source improvements and change to event model (#601)
    Refactored file source, added record_type and format configuration options for json and plaintext, support for both Event And String
    
    Signed-off-by: Taylor Gray <33740195+graytaylor0@users.noreply.github.com>
    Signed-off-by: Arunachalam Lakshmanan <arunachalam.l@gmail.com>

---
+++ data-prepper-plugins/common/src/main/java/com/amazon/dataprepper/plugins/source/file/FileFormat.java
@@ -0,0 +1,33 @@
+package com.amazon.dataprepper.plugins.source.file;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * An enumm to represent the file formats supported in Data Prepper's file source.
+ * @since 1.2
+ */
+public enum FileFormat {
+
+    PLAIN("plain"),
+    JSON("json");
+
+    private static final Map<String, FileFormat> NAMES_MAP = Arrays.stream(FileFormat.values())
+            .collect(Collectors.toMap(FileFormat::toString, Function.identity()));
+
+    private final String name;
+
+    FileFormat(final String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+
+    public static FileFormat getByName(final String name) {
+        return NAMES_MAP.get(name.toLowerCase());
+    }
+}
