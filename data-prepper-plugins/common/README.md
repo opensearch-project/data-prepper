515fa4d9cb0cf74a4d9102c81736a2bbb3d7ff13
Taylor Gray <33740195+graytaylor0@users.noreply.github.com>
2021-11-18 11:49:36 -0600

    File source improvements and change to event model (#601)
    Refactored file source, added record_type and format configuration options for json and plaintext, support for both Event And String
    
    Signed-off-by: Taylor Gray <33740195+graytaylor0@users.noreply.github.com>

--- data-prepper-plugins/common/README.md
+++ data-prepper-plugins/common/README.md
@@ -8,9 +8,33 @@ A prepper plugin to generate new string records with upper or lower case convers
 
 ## `file` (source)
 
-A source plugin to read input data from the specified file path.
+A source plugin to read input data from the specified file path. The file source creates a new Record for each line of data in the file.
 
-- path (String): absolute input data file path
+* `path` (String): absolute input data file path. It is required
+
+* `format` (String): The format of each line of the file. Valid options are `json` or `plain`. Default is `plain`.
+  <br></br>
+    * `plain`: Reads plaintext data from files. Internally, a plain text line from a file will be given a key of `message` as shown below.
+    ```
+    Example log line in file
+    ```
+  becomes 
+    ```
+    { "message": "Example log line in file" }
+    ```
+  
+    * `json`: Reads data that is in the form of a JSON string from a file. If the json string is unable to be parsed, the file source will treat it as a plaintext line.
+  Expects json lines as follows:
+      ```
+      { "key1": "val1" }
+      { "key2": "val2" }
+      { "key3": "val3" }
+      ```
+            
+  
+* `record_type` (String): The Event type that will be stored in the metadata of the Event. Default is `string`. 
+Temporarily, `type` can either be `event` or `string`. If you would like to use the file source for log analytics use cases like grok, 
+  change this to `event`.
 
 ## `file` (sink)
 
---
+++ data-prepper-plugins/common/src/main/java/com/amazon/dataprepper/plugins/source/file/FileSource.java
@@ -0,0 +1,121 @@
+/*
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  The OpenSearch Contributors require contributions made to
+ *  this file be licensed under the Apache-2.0 license or a
+ *  compatible open source license.
+ *
+ *  Modifications Copyright OpenSearch Contributors. See
+ *  GitHub history for details.
+ */
+
+package com.amazon.dataprepper.plugins.source.file;
+
+import com.amazon.dataprepper.metrics.PluginMetrics;
+import com.amazon.dataprepper.model.annotations.DataPrepperPlugin;
+import com.amazon.dataprepper.model.annotations.DataPrepperPluginConstructor;
+import com.amazon.dataprepper.model.buffer.Buffer;
+import com.amazon.dataprepper.model.event.JacksonEvent;
+import com.amazon.dataprepper.model.plugin.PluginFactory;
+import com.amazon.dataprepper.model.record.Record;
+import com.amazon.dataprepper.model.source.Source;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static java.lang.String.format;
+
+@DataPrepperPlugin(name = "file", pluginType = Source.class, pluginConfigurationType = FileSourceConfig.class)
+public class FileSource implements Source<Record<Object>> {
+
+    static final String MESSAGE_KEY = "message";
+    private static final Logger LOG = LoggerFactory.getLogger(FileSource.class);
+    private static final TypeReference<Map<String, Object>> MAP_TYPE_REFERENCE = new TypeReference<Map<String, Object>>() {};
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private final FileSourceConfig fileSourceConfig;
+
+    private boolean isStopRequested;
+    private final int writeTimeout;
+
+    @DataPrepperPluginConstructor
+    public FileSource(final FileSourceConfig fileSourceConfig, final PluginMetrics pluginMetrics, final PluginFactory pluginFactory) {
+        fileSourceConfig.validate();
+        this.fileSourceConfig = fileSourceConfig;
+        this.isStopRequested = false;
+        this.writeTimeout = FileSourceConfig.DEFAULT_TIMEOUT;
+    }
+
+
+    @Override
+    public void start(final Buffer<Record<Object>> buffer) {
+        checkNotNull(buffer, "Buffer cannot be null for file source to start");
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(fileSourceConfig.getFilePathToRead()), StandardCharsets.UTF_8)) {
+            String line;
+            while ((line = reader.readLine()) != null && !isStopRequested) {
+                writeLineAsEventOrString(line, buffer);
+            }
+        } catch (IOException | TimeoutException | IllegalArgumentException ex) {
+            LOG.error("Error processing the input file path [{}]", fileSourceConfig.getFilePathToRead(), ex);
+            throw new RuntimeException(format("Error processing the input file %s",
+                    fileSourceConfig.getFilePathToRead()), ex);
+        }
+    }
+
+    @Override
+    public void stop() {
+        isStopRequested = true;
+    }
+
+    private Record<Object> getEventRecordFromLine(final String line) {
+        Map<String, Object> structuredLine = new HashMap<>();
+
+        switch(fileSourceConfig.getFormat()) {
+            case JSON:
+                structuredLine = parseJson(line);
+                break;
+            case PLAIN:
+                structuredLine.put(MESSAGE_KEY, line);
+                break;
+        }
+
+        return new Record<>(JacksonEvent
+                .builder()
+                .withEventType(fileSourceConfig.getRecordType())
+                .withData(structuredLine)
+                .build());
+    }
+
+    private Map<String, Object> parseJson(final String jsonString) {
+        try {
+            return OBJECT_MAPPER.readValue(jsonString, MAP_TYPE_REFERENCE);
+        } catch (JsonProcessingException e) {
+            LOG.error("Unable to parse json data [{}], assuming plain text", jsonString, e);
+            final Map<String, Object> plainMap = new HashMap<>();
+            plainMap.put(MESSAGE_KEY, jsonString);
+            return plainMap;
+        }
+    }
+
+    // Temporary function to support both trace and log ingestion pipelines.
+    // TODO: This function should be removed with the completion of: https://github.com/opensearch-project/data-prepper/issues/546
+    private void writeLineAsEventOrString(final String line, final Buffer<Record<Object>> buffer) throws TimeoutException, IllegalArgumentException {
+        if (fileSourceConfig.getRecordType().equals(FileSourceConfig.EVENT_TYPE)) {
+            buffer.write(getEventRecordFromLine(line), writeTimeout);
+        } else if (fileSourceConfig.getRecordType().equals(FileSourceConfig.DEFAULT_TYPE)) {
+            buffer.write(new Record<>(line), writeTimeout);
+        }
+    }
+}
