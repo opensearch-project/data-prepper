7ea98faa2cf526a1d17805f424bf4f9149561829
Steven Bayer <sbayer55@gmail.com>
2021-11-30 11:51:44 -0600

    Rename prepper to processor (#655)
    Removed references to Preppers in data-prepper-core and docs except where required for plugin compatibility.
    Added testing for support of both Prepper and Processor plugins.
    
    Signed-off-by: Steven Bayer <smbayer@amazon.com>
    
    Co-authored-by: Steven Bayer <smbayer@amazon.com>
    Signed-off-by: Arunachalam Lakshmanan <arunachalam.l@gmail.com>

---
+++ data-prepper-core/src/test/java/com/amazon/dataprepper/pipeline/common/TestProcessor.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.amazon.dataprepper.pipeline.common;
+
+import com.amazon.dataprepper.model.annotations.DataPrepperPlugin;
+import com.amazon.dataprepper.model.annotations.SingleThread;
+import com.amazon.dataprepper.model.configuration.PluginSetting;
+import com.amazon.dataprepper.model.processor.Processor;
+import com.amazon.dataprepper.model.record.Record;
+
+import java.util.Collection;
+
+@SingleThread
+@DataPrepperPlugin(name = "test_processor", pluginType = Processor.class)
+public class TestProcessor implements Processor<Record<String>, Record<String>> {
+    public boolean isShutdown = false;
+
+    public TestProcessor(final PluginSetting pluginSetting) {}
+
+    @Override
+    public Collection<Record<String>> execute(Collection<Record<String>> records) {
+        return records;
+    }
+
+    @Override
+    public void prepareForShutdown() {
+
+    }
+
+    @Override
+    public boolean isReadyForShutdown() {
+        return true;
+    }
+
+    @Override
+    public void shutdown() {
+        isShutdown = true;
+    }
+}
