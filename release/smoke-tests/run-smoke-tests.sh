9cf2f4a6041fd035a601542d3c0f3fcd2dbae2ac
Steven Bayer <smbayer@amazon.com>
2021-12-03 17:15:40 -0600

    Added clearer output to smoke tests
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

--- release/smoke-tests/run-smoke-tests.sh
+++ release/smoke-tests/run-smoke-tests.sh
@@ -13,6 +13,20 @@ OPENSEARCH_OTEL_INDEX="otel-v1-apm-span-000001"
 
 cd ${REPO_ROOT}/release/smoke-tests
 
+function end_tests () {
+    local EXIT_CODE=$1
+    docker-compose down
+    cd "${REPO_ROOT}"
+
+    if [ "${EXIT_CODE}" -ne 0 ]
+    then
+        echo -e "\033[0;31mSmoke tests failed\033[0m"
+    else
+        echo -e "\033[0;32mSmoke tests passed\033[0m"
+    fi
+    exit "${EXIT_CODE}"
+}
+
 function usage() {
     echo ""
     echo "This script is used to build the Docker image. It prepares the files required by the Dockerfile in a temporary directory, then builds and tags the Docker image. Script expects to be run from the project root directory."
@@ -41,14 +55,15 @@ function query_hits_gt_zero () {
         LOG_COUNT=$(docker run alpine /bin/sh -c "apk -q --no-cache add jq && echo '${SEARCH_RESPONSE}' | jq '.hits.total.value'")
     fi
 
-    if [ $LOG_COUNT -gt 0 ]
+    if [ "${LOG_COUNT}" -gt 0 ]
     then
         echo "Open Search is receiving logs from Data Prepper"
         echo "Found at least ${LOG_COUNT} hits"
+        echo -e "\033[0;32mTest passed\033[0m"
     else
-        echo "No hits found with query url ${}"
+        echo "No hits found with query url ${URL}"
         echo "Smoke test failed"
-        exit 1
+        end_tests 1
     fi
 }
 
@@ -56,7 +71,7 @@ while getopts "hv:r:o::" arg; do
     case $arg in
         h)
             usage
-            exit 1
+            end_tests 1
             ;;
         v)
             export TAG_NAME=$OPTARG
@@ -69,7 +84,7 @@ while getopts "hv:r:o::" arg; do
             ;;
         ?)
             echo "Invalid option: -${arg}"
-            exit 1
+            end_tests 1
             ;;
     esac
 done
@@ -78,16 +93,18 @@ if ! docker pull "${REPOSITORY}/data-prepper:${TAG_NAME}" > /dev/null
 then
     echo "--------------------------------------------------------------------------"
     echo "Unable to pull image \"${REPOSITORY}/data-prepper:${TAG_NAME}\" are you sure it exists?"
-    exit 1
+    end_tests 1
 fi
 
 echo "Will smoke test image \"${REPOSITORY}/data-prepper:${TAG_NAME}\""
+
+docker-compose down > /dev/null 2> /dev/null
 
 if ! docker-compose up --detach
 then
     echo "--------------------------------------------------------------------------"
     echo "Failed to start all docker-compose services"
-    exit 1
+    end_tests 1
 fi
 
 #Ping Data Prepper until response:
@@ -99,21 +116,25 @@ do
         WAITING_FOR_DATAPREPPER=false
     else
         echo "Waiting for Data Prepper to start"
+        sleep 5s
     fi
-    sleep 1s
 done
 
 echo "Data Prepper started!"
-echo "Ready to begin smoke tests. Running cURL commands."
 
+sleep 30s
+
+echo "Ready to begin smoke tests. Running cURL commands."
+echo ""
+
+echo -e "\033[0;33mTest:\033[0m Verify logs received via HTTP were processed"
 query_hits_gt_zero "https://${OPENSEARCH_HOST}/${OPENSEARCH_GROK_INDEX}/_search"
 echo "Open Search successfully received logs from Data Prepper!"
+echo ""
 
+echo -e "\033[0;33mTest:\033[0m Verify metrics received via grpc were processed"
 query_hits_gt_zero "https://${OPENSEARCH_HOST}/${OPENSEARCH_OTEL_INDEX}/_search?q=PythonService"
 echo "Open Search successfully received OTel spans from Data Prepper!"
+echo ""
 
-docker-compose down
-
-cd ${REPO_ROOT}
-
-echo "All smoke tests passed"
+end_tests 0
