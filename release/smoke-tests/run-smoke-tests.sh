fdef791a42897d13be7ca5bb836ffe5b89c68769
Steven Bayer <smbayer@amazon.com>
2021-12-03 16:38:42 -0600

    Enhanced smoke test documentation
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

--- release/smoke-tests/run-smoke-tests.sh
+++ release/smoke-tests/run-smoke-tests.sh
@@ -3,9 +3,10 @@
 # Copyright OpenSearch Contributors
 # SPDX-License-Identifier: Apache-2.0
 
-set -e --trace
+set -e
 
 REPO_ROOT=`git rev-parse --show-toplevel`
+export OPENSEARCH_VERSION="1.0.1"
 OPENSEARCH_HOST="localhost:9200"
 OPENSEARCH_GROK_INDEX="test-grok-index"
 OPENSEARCH_OTEL_INDEX="otel-v1-apm-span-000001"
@@ -24,6 +25,7 @@ function usage() {
     echo ""
     echo "Optional arguments:"
     echo -e "-h\t\tPrint this message."
+    echo -e "-o OPENSEARCH_VERSION\tOverride the default Open Search version used in smoke tests"
     echo "--------------------------------------------------------------------------"
 }
 
@@ -50,7 +52,7 @@ function query_hits_gt_zero () {
     fi
 }
 
-while getopts "hv:r:" arg; do
+while getopts "hv:r:o::" arg; do
     case $arg in
         h)
             usage
@@ -62,6 +64,9 @@ while getopts "hv:r:" arg; do
         r)
             export REPOSITORY=$OPTARG
             ;;
+        o)
+            export OPENSEARCH_VERSION=$OPTARG
+            ;;
         ?)
             echo "Invalid option: -${arg}"
             exit 1
@@ -69,18 +74,29 @@ while getopts "hv:r:" arg; do
     esac
 done
 
+if ! docker pull "${REPOSITORY}/data-prepper:${TAG_NAME}" > /dev/null
+then
+    echo "--------------------------------------------------------------------------"
+    echo "Unable to pull image \"${REPOSITORY}/data-prepper:${TAG_NAME}\" are you sure it exists?"
+    exit 1
+fi
+
 echo "Will smoke test image \"${REPOSITORY}/data-prepper:${TAG_NAME}\""
 
-docker-compose up --detach
-
+if ! docker-compose up --detach
+then
+    echo "--------------------------------------------------------------------------"
+    echo "Failed to start all docker-compose services"
+    exit 1
+fi
 
 #Ping Data Prepper until response:
-WAITING_FOR_OPENSEARCH=true
-while ${WAITING_FOR_OPENSEARCH}
+WAITING_FOR_DATAPREPPER=true
+while ${WAITING_FOR_DATAPREPPER}
 do
     if curl -s -k -u 'admin:admin' 'https://localhost:9200/_cat/indices' > /dev/null && curl -s -k -H "Content-Type: application/json" -d '[{"log": "smoke test log "}]' 'http://localhost:2021/log/ingest' > /dev/null
     then
-        WAITING_FOR_OPENSEARCH=false
+        WAITING_FOR_DATAPREPPER=false
     else
         echo "Waiting for Data Prepper to start"
     fi
@@ -88,6 +104,7 @@ do
 done
 
 echo "Data Prepper started!"
+echo "Ready to begin smoke tests. Running cURL commands."
 
 query_hits_gt_zero "https://${OPENSEARCH_HOST}/${OPENSEARCH_GROK_INDEX}/_search"
 echo "Open Search successfully received logs from Data Prepper!"
@@ -98,3 +115,5 @@ echo "Open Search successfully received OTel spans from Data Prepper!"
 docker-compose down
 
 cd ${REPO_ROOT}
+
+echo "All smoke tests passed"
