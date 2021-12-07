925861a588333dd84bf7000faab6833d40b4d628
Taylor Gray <33740195+graytaylor0@users.noreply.github.com>
2021-12-06 18:57:59 -0600

    Refactored event checkKey to use pre-compiled regex pattern. refactored RandomStringSource
    Signed-off-by: Taylor Gray <33740195+graytaylor0@users.noreply.github.com>

--- data-prepper-plugins/common/src/test/java/com/amazon/dataprepper/plugins/source/RandomStringSourceTests.java
+++ data-prepper-plugins/common/src/test/java/com/amazon/dataprepper/plugins/source/RandomStringSourceTests.java
@@ -14,31 +14,39 @@ package com.amazon.dataprepper.plugins.source;
 import com.amazon.dataprepper.model.event.Event;
 import com.amazon.dataprepper.model.record.Record;
 import com.amazon.dataprepper.plugins.buffer.TestBuffer;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
 
 public class RandomStringSourceTests {
 
     @Test
-    public void testPutRecord() throws InterruptedException {
-        final RandomStringSource randomStringSource =
-                new RandomStringSource();
-        final Queue<Record<Event>> bufferQueue = new ConcurrentLinkedQueue<>();
+    public void testPutRecord() throws ExecutionException, InterruptedException {
+        final RandomStringSource randomStringSource = new RandomStringSource();
+        final Queue<Record<Event>> bufferQueue = new LinkedBlockingQueue<>();
         final TestBuffer buffer = new TestBuffer(bufferQueue, 1);
-        //Start source, and sleep for 100 millis
-        randomStringSource.start(buffer);
-        Thread.sleep(500);
-        //Make sure that 1 record is in buffer
-        Assert.assertEquals(1, buffer.size());
-        //Stop the source, and wait long enough that another message would be sent
-        //if the source was running
-        randomStringSource.stop();
-        Thread.sleep(500);
-        //Make sure there is still only 1 record in buffer
-        Assert.assertEquals(1, buffer.size());
+
+        final ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<?> task = executorService.submit(() -> randomStringSource.start(buffer));
+
+        try {
+            // this timeout is increased to 3000 to ensure that at least 1 Record has time to be made by the RandomStringSource
+            task.get(3000, TimeUnit.MILLISECONDS);
+        } catch (TimeoutException e) {
+            assertThat(buffer.size(), greaterThan(0));
+            randomStringSource.stop();
+            assertThat(buffer.size(), greaterThan(0));
+        }
     }
 
 }
