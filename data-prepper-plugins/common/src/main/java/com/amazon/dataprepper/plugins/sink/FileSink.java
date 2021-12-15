32c4c5884006225dce164ccb88e263407e2bd6fb
Chen <19492223+chenqi0805@users.noreply.github.com>
2021-12-15 16:49:56 -0600

    MAINT: use generic type in FileSink
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>

--- data-prepper-plugins/common/src/main/java/com/amazon/dataprepper/plugins/sink/FileSink.java
+++ data-prepper-plugins/common/src/main/java/com/amazon/dataprepper/plugins/sink/FileSink.java
@@ -13,6 +13,7 @@ package com.amazon.dataprepper.plugins.sink;
 
 import com.amazon.dataprepper.model.annotations.DataPrepperPlugin;
 import com.amazon.dataprepper.model.configuration.PluginSetting;
+import com.amazon.dataprepper.model.event.Event;
 import com.amazon.dataprepper.model.record.Record;
 import com.amazon.dataprepper.model.sink.Sink;
 
@@ -26,7 +27,7 @@ import java.util.Collection;
 import static java.lang.String.format;
 
 @DataPrepperPlugin(name = "file", pluginType = Sink.class)
-public class FileSink implements Sink<Record<String>> {
+public class FileSink implements Sink<Record<Object>> {
     private static final String SAMPLE_FILE_PATH = "src/resources/file-test-sample-output.txt";
 
     public static final String FILE_PATH = "path";
@@ -56,15 +57,28 @@ public class FileSink implements Sink<Record<String>> {
     }
 
     @Override
-    public void output(Collection<Record<String>> records) {
+    public void output(Collection<Record<Object>> records) {
         try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(outputFilePath),
                 StandardCharsets.UTF_8)) {
-            for (final Record<String> record : records) {
-                writer.write(record.getData());
-                writer.newLine();
+            for (final Record<Object> record : records) {
+                checkTypeAndWriteObject(record.getData(), writer);
             }
         } catch (IOException ex) {
             throw new RuntimeException(format("Encountered exception opening/creating file %s", outputFilePath), ex);
+        }
+    }
+
+    // Temporary function to support both trace and log ingestion pipelines.
+    // TODO: This function should be removed with the completion of: https://github.com/opensearch-project/data-prepper/issues/546
+    private void checkTypeAndWriteObject(final Object object, final BufferedWriter writer) throws IOException {
+        if (object instanceof String) {
+            writer.write((String) object);
+            writer.newLine();
+        } else if (object instanceof Event) {
+            writer.write(((Event) object).toJsonString());
+            writer.newLine();
+        } else {
+            throw new RuntimeException("Invalid record type. StdOutSink only supports String and Events");
         }
     }
 
