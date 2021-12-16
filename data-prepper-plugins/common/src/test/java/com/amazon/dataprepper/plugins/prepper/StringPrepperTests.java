8814dfdfbd1af31d730f83e5dbae42e781b96be3
Qi Chen <19492223+chenqi0805@users.noreply.github.com>
2021-12-16 15:44:56 -0600

    Maintenance: StringPrepper use Event model (#753)
    * MAINT: StringPrepper use Event model
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: Only convert string value
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * STY: import
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: redundant type
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * STY: java 8
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    Signed-off-by: Arunachalam Lakshmanan <arunachalam.l@gmail.com>

--- data-prepper-plugins/common/src/test/java/com/amazon/dataprepper/plugins/prepper/StringPrepperTests.java
+++ data-prepper-plugins/common/src/test/java/com/amazon/dataprepper/plugins/prepper/StringPrepperTests.java
@@ -11,25 +11,40 @@
 
 package com.amazon.dataprepper.plugins.prepper;
 
+import com.amazon.dataprepper.model.event.Event;
+import com.amazon.dataprepper.model.event.JacksonEvent;
 import com.amazon.dataprepper.model.record.Record;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringPrepperTests {
 
+    private final String TEST_EVENT_TYPE = "test_event_type";
+    private final String TEST_KEY = "test_key";
     private final String UPPERCASE_TEST_STRING = "data_prepper";
     private final String LOWERCASE_TEST_STRING = "STRING_CONVERTER";
-    private final Record<String> TEST_RECORD_1 = new Record<>(UPPERCASE_TEST_STRING);
-    private final Record<String> TEST_RECORD_2 = new Record<>(LOWERCASE_TEST_STRING);
-    private final List<Record<String>> TEST_RECORDS = Arrays.asList(TEST_RECORD_1, TEST_RECORD_2);
+    private final Record<Event> TEST_RECORD_1 = new Record<>(
+            JacksonEvent.builder()
+                    .withEventType(TEST_EVENT_TYPE)
+                    .withData(Map.of(TEST_KEY, UPPERCASE_TEST_STRING))
+                    .build()
+    );
+    private final Record<Event> TEST_RECORD_2 = new Record<>(
+            JacksonEvent.builder()
+                    .withEventType(TEST_EVENT_TYPE)
+                    .withData(Map.of(TEST_KEY, LOWERCASE_TEST_STRING))
+                    .build()
+    );
+    private final List<Record<Event>> TEST_RECORDS = Arrays.asList(TEST_RECORD_1, TEST_RECORD_2);
     private StringPrepper.Configuration configuration;
 
     @BeforeEach
@@ -45,40 +60,60 @@ public class StringPrepperTests {
     public void testStringPrepperDefault() {
 
         final StringPrepper stringPrepper = createObjectUnderTest();
-        final List<Record<String>> modifiedRecords = (List<Record<String>>) stringPrepper.execute(TEST_RECORDS);
+        final List<Record<Event>> modifiedRecords = (List<Record<Event>>) stringPrepper.execute(TEST_RECORDS);
         stringPrepper.shutdown();
 
-        final List<String> modifiedRecordData = modifiedRecords.stream().map(Record::getData).collect(Collectors.toList());
-        final List<String> expectedRecordData = Arrays.asList(UPPERCASE_TEST_STRING.toUpperCase(), LOWERCASE_TEST_STRING);
+        final List<Event> modifiedRecordEvents = modifiedRecords.stream().map(Record::getData).collect(Collectors.toList());
 
-        assertThat(modifiedRecordData, equalTo(expectedRecordData));
+        assertThat(modifiedRecordEvents.size(), equalTo(2));
+        final Event firstEvent = modifiedRecordEvents.get(0);
+        final Event secondEvent = modifiedRecordEvents.get(1);
+        assertTrue(firstEvent.containsKey(TEST_KEY));
+        assertThat(firstEvent.getMetadata().getEventType(), equalTo(TEST_EVENT_TYPE));
+        assertThat(firstEvent.get(TEST_KEY, String.class), equalTo(UPPERCASE_TEST_STRING.toUpperCase()));
+        assertTrue(secondEvent.containsKey(TEST_KEY));
+        assertThat(secondEvent.getMetadata().getEventType(), equalTo(TEST_EVENT_TYPE));
+        assertThat(secondEvent.get(TEST_KEY, String.class), equalTo(LOWERCASE_TEST_STRING));
     }
 
     @Test
     public void testStringPrepperLowerCase() {
         configuration.setUpperCase(false);
         final StringPrepper stringPrepper = createObjectUnderTest();
-        final List<Record<String>> modifiedRecords = (List<Record<String>>) stringPrepper.execute(TEST_RECORDS);
+        final List<Record<Event>> modifiedRecords = (List<Record<Event>>) stringPrepper.execute(TEST_RECORDS);
         stringPrepper.shutdown();
 
-        final List<String> modifiedRecordData = modifiedRecords.stream().map(Record::getData).collect(Collectors.toList());
-        final List<String> expectedRecordData = Arrays.asList(UPPERCASE_TEST_STRING, LOWERCASE_TEST_STRING.toLowerCase());
+        final List<Event> modifiedRecordEvents = modifiedRecords.stream().map(Record::getData).collect(Collectors.toList());
 
-        assertThat(modifiedRecordData.size(), equalTo(2));
-        assertThat(modifiedRecordData, hasItems(UPPERCASE_TEST_STRING, LOWERCASE_TEST_STRING.toLowerCase()));
+        assertThat(modifiedRecordEvents.size(), equalTo(2));
+        final Event firstEvent = modifiedRecordEvents.get(0);
+        final Event secondEvent = modifiedRecordEvents.get(1);
+        assertTrue(firstEvent.containsKey(TEST_KEY));
+        assertThat(firstEvent.getMetadata().getEventType(), equalTo(TEST_EVENT_TYPE));
+        assertThat(firstEvent.get(TEST_KEY, String.class), equalTo(UPPERCASE_TEST_STRING));
+        assertTrue(secondEvent.containsKey(TEST_KEY));
+        assertThat(secondEvent.getMetadata().getEventType(), equalTo(TEST_EVENT_TYPE));
+        assertThat(secondEvent.get(TEST_KEY, String.class), equalTo(LOWERCASE_TEST_STRING.toLowerCase()));
     }
 
     @Test
     public void testStringPrepperUpperCase() {
         configuration.setUpperCase(true);
         final StringPrepper stringPrepper = createObjectUnderTest();
-        final List<Record<String>> modifiedRecords = (List<Record<String>>) stringPrepper.execute(TEST_RECORDS);
+        final List<Record<Event>> modifiedRecords = (List<Record<Event>>) stringPrepper.execute(TEST_RECORDS);
         stringPrepper.shutdown();
 
-        final List<String> modifiedRecordData = modifiedRecords.stream().map(Record::getData).collect(Collectors.toList());
-        final List<String> expectedRecordData = Arrays.asList(UPPERCASE_TEST_STRING.toUpperCase(), LOWERCASE_TEST_STRING);
+        final List<Event> modifiedRecordEvents = modifiedRecords.stream().map(Record::getData).collect(Collectors.toList());
 
-        assertThat(modifiedRecordData, equalTo(expectedRecordData));
+        assertThat(modifiedRecordEvents.size(), equalTo(2));
+        final Event firstEvent = modifiedRecordEvents.get(0);
+        final Event secondEvent = modifiedRecordEvents.get(1);
+        assertTrue(firstEvent.containsKey(TEST_KEY));
+        assertThat(firstEvent.getMetadata().getEventType(), equalTo(TEST_EVENT_TYPE));
+        assertThat(firstEvent.get(TEST_KEY, String.class), equalTo(UPPERCASE_TEST_STRING.toUpperCase()));
+        assertTrue(secondEvent.containsKey(TEST_KEY));
+        assertThat(secondEvent.getMetadata().getEventType(), equalTo(TEST_EVENT_TYPE));
+        assertThat(secondEvent.get(TEST_KEY, String.class), equalTo(LOWERCASE_TEST_STRING));
     }
 
     @Test
