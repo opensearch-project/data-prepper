069c5a2b3004978df6884e1d8985bb4fd591b1a2
Chen <19492223+chenqi0805@users.noreply.github.com>
2021-12-16 14:56:33 -0600

    STY: import
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>

--- data-prepper-plugins/common/src/main/java/com/amazon/dataprepper/plugins/prepper/StringPrepper.java
+++ data-prepper-plugins/common/src/main/java/com/amazon/dataprepper/plugins/prepper/StringPrepper.java
@@ -21,7 +21,6 @@ import com.amazon.dataprepper.model.record.Record;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
