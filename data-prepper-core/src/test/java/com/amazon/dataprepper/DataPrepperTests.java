9fae6308b91bdd90a487c1183ec24fb1046015ee
Steven Bayer <smbayer@amazon.com>
2021-12-22 15:45:56 -0600

    Added DataPrepper unit tests
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

--- data-prepper-core/src/test/java/com/amazon/dataprepper/DataPrepperTests.java
+++ data-prepper-core/src/test/java/com/amazon/dataprepper/DataPrepperTests.java
@@ -7,19 +7,18 @@ package com.amazon.dataprepper;
 
 import com.amazon.dataprepper.model.plugin.PluginFactory;
 import com.amazon.dataprepper.parser.PipelineParser;
-import com.amazon.dataprepper.parser.config.DataPrepperArgs;
-import com.amazon.dataprepper.parser.model.DataPrepperConfiguration;
 import com.amazon.dataprepper.pipeline.Pipeline;
 import com.amazon.dataprepper.pipeline.server.DataPrepperServer;
-import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,27 +33,34 @@ import static org.mockito.Mockito.when;
 public class DataPrepperTests {
     private static Map<String, Pipeline> parseConfigurationFixture;
     private static PipelineParser pipelineParser;
+    private static Pipeline pipeline;
 
     @Mock
-    private Pipeline pipeline;
-    @Mock
     private PluginFactory pluginFactory;
-    @Mock
-    private DataPrepperConfiguration configuration;
     @Mock
     private DataPrepperServer dataPrepperServer;
     @InjectMocks
     private DataPrepper dataPrepper;
 
     @BeforeAll
-    public static void beforeAll() {
+    public static void beforeAll() throws NoSuchFieldException {
         pipelineParser = mock(PipelineParser.class);
+        pipeline = mock(Pipeline.class);
 
         parseConfigurationFixture = new HashMap<>();
-        parseConfigurationFixture.put("testKey", mock(Pipeline.class));
+        parseConfigurationFixture.put("testKey", pipeline);
 
         when(pipelineParser.parseConfiguration())
                 .thenReturn(parseConfigurationFixture);
+    }
+
+    @BeforeEach
+    public void before() throws NoSuchFieldException, IllegalAccessException {
+        // Use reflection to set dataPrepper.dataPrepperServer because @InjectMock will not use field injection.
+        final Field dataPrepperServerField = dataPrepper.getClass().getDeclaredField("dataPrepperServer");
+        dataPrepperServerField.setAccessible(true);
+        dataPrepperServerField.set(dataPrepper, dataPrepperServer);
+        dataPrepperServerField.setAccessible(false);
     }
 
     @Test
@@ -71,13 +77,18 @@ public class DataPrepperTests {
 
         assertThrows(
                 RuntimeException.class,
-                () -> new DataPrepper(configuration, pipelineParser, pluginFactory),
+                () -> new DataPrepper(pipelineParser, pluginFactory),
                 "Exception should be thrown if pipeline parser has no pipeline configuration");
     }
 
     @Test
     public void testGivenInstantiatedWithPluginFactoryWhenGetPluginFactoryCalledThenReturnSamePluginFactory() {
         assertThat(dataPrepper.getPluginFactory(), Matchers.is(pluginFactory));
+    }
+
+    @Test
+    public void testGivenValidPipelineParserThenReturnResultOfParseConfiguration() {
+        assertThat(dataPrepper.getTransformationPipelines(), Matchers.is(parseConfigurationFixture));
     }
 
     @Test
@@ -88,108 +99,42 @@ public class DataPrepperTests {
         verify(dataPrepperServer, times(1)).start();
     }
 
-//    @Before
-//    public void setup() throws Exception {
-//        actualSecurityManager = System.getSecurityManager();
-//        System.setSecurityManager(new CustomSecurityManager());
-//        DataPrepper.configure(TestDataProvider.VALID_DATA_PREPPER_CONFIG_FILE);
-//    }
-//
-//    @After
-//    public void teardown() {
-//        System.setSecurityManager(actualSecurityManager);
-//    }
-//
-//    @Test
-//    public void testInstanceCreation() {
-//        DataPrepper testDataPrepper1 = DataPrepper.getInstance();
-//        assertThat("Failed to retrieve a valid Data Prepper instance", testDataPrepper1, is(notNullValue()));
-//        DataPrepper testDataPrepper2 = DataPrepper.getInstance();
-//        assertThat("Data Prepper has to be singleton", testDataPrepper2, is(testDataPrepper1));
-//    }
-//
-//    @Test
-//    public void testDataPrepperSystemMetrics() {
-//        // Test retrieve gauge in ClassLoaderMetrics
-//        final List<Measurement> classesLoaded = getSystemMeasurementList("jvm.classes.loaded");
-//        Assert.assertEquals(1, classesLoaded.size());
-//        // Test retrieve gauge in JvmMemoryMetrics
-//        final List<Measurement> jvmBufferCount = getSystemMeasurementList("jvm.buffer.count");
-//        Assert.assertEquals(1, jvmBufferCount.size());
-//        // Test retrieve gauge in JvmGcMetrics
-//        final List<Measurement> jvmGcMaxDataSize = getSystemMeasurementList("jvm.gc.max.data.size");
-//        Assert.assertEquals(1, jvmGcMaxDataSize.size());
-//        // Test retrieve gauge in ProcessorMetrics
-//        final List<Measurement> sysCPUCount = getSystemMeasurementList("system.cpu.count");
-//        Assert.assertEquals(1, sysCPUCount.size());
-//        // Test retrieve gauge in JvmThreadMetrics
-//        final List<Measurement> jvmThreadsPeak = getSystemMeasurementList("jvm.threads.peak");
-//        Assert.assertEquals(1, jvmThreadsPeak.size());
-//    }
-//
-//    @Test
-//    public void testCustomConfiguration() {
-//        DataPrepper testInstance = DataPrepper.getInstance();
-//        Assert.assertEquals(5678, DataPrepper.getConfiguration().getServerPort());
-//    }
-//
-//    @Test
-//    public void testNoPipelinesToExecute() {
-//        try {
-//            DataPrepper testDataPrepper = DataPrepper.getInstance();
-//            testDataPrepper.execute(NO_PIPELINES_EXECUTE_CONFIG_FILE);
-//        } catch (SystemExitException ex) {
-//            assertThat("Data Prepper should exit with status 1", ex.getExitStatus(), is(1));
-//            assertThat("Data Prepper exit message is incorrect",
-//                    ex.getMessage().contains("System exit was initiated"));
-//        }
-//    }
-//
-//    @Test
-//    public void testDataPrepperExecuteAndShutdown() {
-//        final DataPrepper testDataPrepper = DataPrepper.getInstance();
-//        boolean executionStatus = testDataPrepper.execute(VALID_MULTIPLE_PIPELINE_CONFIG_FILE);
-//        assertThat("Failed to initiate execution", executionStatus);
-//        testDataPrepper.shutdown();
-//        //call shutdown() twice to ensure nothing breaks
-//        testDataPrepper.shutdown();
-//        VALID_MULTIPLE_PIPELINE_NAMES.forEach(testDataPrepper::shutdown);
-//        testDataPrepper.shutdown("pipeline_does_not_exist"); //this does nothing
-//    }
-//
-//
-//    public static class CustomSecurityManager extends SecurityManager {
-//        @Override
-//        public void checkPermission(Permission perm) {
-//        }
-//
-//        @Override
-//        public void checkPermission(Permission perm, Object context) {
-//            // allow anything.
-//        }
-//
-//        @Override
-//        public void checkExit(int status) {
-//            super.checkExit(status);
-//            throw new SystemExitException(status);
-//        }
-//    }
-//
-//    public static class SystemExitException extends SecurityException {
-//        private final int status;
-//
-//        public SystemExitException(int status) {
-//            super("System exit was initiated");
-//            this.status = status;
-//        }
-//
-//        public int getExitStatus() {
-//            return this.status;
-//        }
-//    }
-//
-//    private static List<Measurement> getSystemMeasurementList(final String meterName) {
-//        return StreamSupport.stream(DataPrepper.getSystemMeterRegistry().find(meterName).meter().measure().spliterator(), false)
-//                .collect(Collectors.toList());
-//    }
+    @Test
+    public void testDataPrepperShutdown() {
+        dataPrepper.shutdown();
+        verify(pipeline, times(1)).shutdown();
+    }
+
+    @Test
+    public void testDataPrepperShutdownPipeline() {
+        Pipeline randomPipeline = mock(Pipeline.class);
+        parseConfigurationFixture.put("Random Pipeline", randomPipeline);
+        dataPrepper.shutdown("Random Pipeline");
+
+        verify(randomPipeline, times(1)).shutdown();
+    }
+
+    @Test
+    public void testDataPrepperShutdownNonExistentPipelineWithoutException() {
+        dataPrepper.shutdown("Missing Pipeline");
+    }
+
+    @Test
+    public void testShutdownDataPrepperServer() {
+        dataPrepper.shutdownDataPrepperServer();
+
+        verify(dataPrepperServer, times(1)).stop();
+    }
+    
+    @Test
+    public void foo() throws NoSuchFieldException, IllegalAccessException {
+        final Field dataPrepperServerField = DataPrepper.class.getDeclaredField("DEFAULT_SERVICE_NAME");
+        dataPrepperServerField.setAccessible(true);
+        String expected = (String) dataPrepperServerField.get(null);
+        dataPrepperServerField.setAccessible(false);
+
+        String actual = DataPrepper.getServiceNameForMetrics();
+
+        assertThat(actual, Matchers.is(expected));
+    }
 }
