cdc18a9779e4f45d3bc5c0a7ee827d82215ef174
David Powers <ddpowers@amazon.com>
2021-12-27 14:37:14 -0600

    Create Drop Processor and a simple test
    Signed-off-by: David Powers <ddpowers@amazon.com>

---
+++ data-prepper-plugins/drop-processor/src/main/java/com/amazon/dataprepper/plugins/processor/drop/DropProcessor.java
@@ -0,0 +1,60 @@
+/*
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  The OpenSearch Contributors require contributions made to
+ *  this file be licensed under the Apache-2.0 license or a
+ *  compatible open source license.
+ *
+ *  Modifications Copyright OpenSearch Contributors. See
+ *  GitHub history for details.
+ */
+
+package com.amazon.dataprepper.plugins.processor.drop;
+
+import com.amazon.dataprepper.metrics.PluginMetrics;
+import com.amazon.dataprepper.model.annotations.DataPrepperPlugin;
+import com.amazon.dataprepper.model.annotations.DataPrepperPluginConstructor;
+import com.amazon.dataprepper.model.annotations.SingleThread;
+import com.amazon.dataprepper.model.configuration.PluginSetting;
+import com.amazon.dataprepper.model.event.Event;
+import com.amazon.dataprepper.model.record.Record;
+import com.amazon.dataprepper.model.processor.AbstractProcessor;
+import com.amazon.dataprepper.model.processor.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+@SingleThread
+@DataPrepperPlugin(name = "drop_events", pluginType = Processor.class)
+public class DropProcessor extends AbstractProcessor<Record<Event>, Record<Event>> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DropProcessor.class);
+
+    @DataPrepperPluginConstructor
+    public DropProcessor(final PluginSetting pluginSetting) {
+        super(pluginSetting);
+    }
+
+    @Override
+    public Collection<Record<Event>> doExecute(Collection<Record<Event>> records) {
+        List<Record<Event>> emptySet = new LinkedList<>();
+        return emptySet;
+    }
+
+    @Override
+    public void prepareForShutdown() {
+
+    }
+
+    @Override
+    public boolean isReadyForShutdown() {
+        return true;
+    }
+
+    @Override
+    public void shutdown() {
+    }
+}
---
+++ data-prepper-plugins/drop-processor/src/test/java/com/amazon/dataprepper/plugins/processor/drop/DropProcessorTests.java
@@ -0,0 +1,49 @@
+package com.amazon.dataprepper.plugins.processor.drop;
+
+import com.amazon.dataprepper.metrics.PluginMetrics;
+import com.amazon.dataprepper.model.configuration.PluginSetting;
+import com.amazon.dataprepper.model.event.Event;
+import com.amazon.dataprepper.model.event.JacksonEvent;
+import com.amazon.dataprepper.model.record.Record;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+import java.util.*;
+
+import static org.mockito.Mockito.mockStatic;
+
+public class DropProcessorTests {
+    private String messageInput;
+    private DropProcessor dropProcessor;
+    private PluginSetting pluginSetting;
+    private final String PLUGIN_NAME = "drop_events";
+
+    @Test
+    void doExecute() {
+        pluginSetting = getDefaultPluginSetting();
+        pluginSetting.setPipelineName("dropProcessorPipeline");
+        dropProcessor = new DropProcessor(pluginSetting);
+
+        final Map<String, Object> testData = new HashMap();
+        messageInput = UUID.randomUUID().toString();
+        testData.put("message", messageInput);
+        final Record<Event> record = buildRecordWithEvent(testData);
+
+        final List<Record<Event>> droppedRecords = (List<Record<Event>>) dropProcessor.doExecute(Collections.singletonList(record));
+
+        Assert.assertEquals(0, droppedRecords.size());
+    }
+
+    private PluginSetting getDefaultPluginSetting() {
+        final Map<String, Object> settings = new HashMap<>();
+        return new PluginSetting(PLUGIN_NAME, settings);
+    }
+
+    static Record<Event> buildRecordWithEvent(final Map<String, Object> data) {
+        return new Record<>(JacksonEvent.builder()
+                .withData(data)
+                .withEventType("event")
+                .build());
+    }
+}
