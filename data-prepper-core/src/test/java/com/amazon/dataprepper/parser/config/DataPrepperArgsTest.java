6bea543b7b6dcd7cb82836e5dd28c8aa19863aaa
Steven Bayer <smbayer@amazon.com>
2022-01-04 14:37:19 -0600

    Added unit tests for command line args
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

---
+++ data-prepper-core/src/test/java/com/amazon/dataprepper/parser/config/DataPrepperArgsTest.java
@@ -0,0 +1,71 @@
+package com.amazon.dataprepper.parser.config;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+class DataPrepperArgsTest {
+    private static final String PIPELINE_FILE_PATH = "pipeline.yml";
+    private static final String DP_CONFIG_YAML_FILE_PATH = "config.yml";
+    private static final String LOGSTASH_PIPELINE_FILE_PATH = "src/test/resources/logstash-filter.conf";
+
+    @Test
+    public void testGivenSingleArgumentThenAssignedToPipelineConfig() {
+        DataPrepperArgs args = new DataPrepperArgs(PIPELINE_FILE_PATH);
+
+        assertThat(args, is(notNullValue()));
+        assertThat(args.getPipelineConfigFileLocation(), is(PIPELINE_FILE_PATH));
+        assertThat(args.getDataPrepperConfigFileLocation(), is(nullValue()));
+    }
+
+    @Test
+    public void testGivenTwoArgumentThenAssignedCorrectly() {
+        DataPrepperArgs args = new DataPrepperArgs(PIPELINE_FILE_PATH, DP_CONFIG_YAML_FILE_PATH);
+
+        assertThat(args, is(notNullValue()));
+        assertThat(args.getPipelineConfigFileLocation(), is(PIPELINE_FILE_PATH));
+        assertThat(args.getDataPrepperConfigFileLocation(), is(DP_CONFIG_YAML_FILE_PATH));
+    }
+
+    @Test
+    public void testGivenThreeArgumentThenThrowException() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new DataPrepperArgs(PIPELINE_FILE_PATH, LOGSTASH_PIPELINE_FILE_PATH, DP_CONFIG_YAML_FILE_PATH));
+    }
+
+    @Test
+    public void testGivenNoArgumentThenThrowException() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new DataPrepperArgs());
+    }
+
+    @Test
+    public void testGivenLogstashConfigPathThenPipelineConfigCreated() {
+        DataPrepperArgs args = new DataPrepperArgs(LOGSTASH_PIPELINE_FILE_PATH, DP_CONFIG_YAML_FILE_PATH);
+
+        assertThat(args, is(notNullValue()));
+        assertThat(
+                args.getPipelineConfigFileLocation(),
+                endsWith("src/test/resources/logstash-filter.yaml"));
+        assertThat(args.getDataPrepperConfigFileLocation(), is(DP_CONFIG_YAML_FILE_PATH));
+    }
+
+    @Test
+    public void testGivenInvalidLogstashConfigPathThenThrowException() {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> new DataPrepperArgs("bad-file-path.conf"));
+    }
+}
