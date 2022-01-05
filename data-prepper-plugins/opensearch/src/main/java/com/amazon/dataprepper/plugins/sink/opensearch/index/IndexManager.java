9df9d32042710b263a96e9e573938954b17e0382
Han Jiang <jianghan@amazon.com>
2022-01-05 10:30:26 -0600

    Support time pattern in index names. (#788)
    * Support time pattern in index names.
    Signed-off-by: Han Jiang <jianghan@amazon.com>
    
    * Add validations for index time pattern
    Signed-off-by: Han Jiang <jianghan@amazon.com>
    
    * Only allow one of time pattern in an index.
    Signed-off-by: Han Jiang <jianghan@amazon.com>
    
    * Correct the check on the count of time patterns.
    Signed-off-by: Han Jiang <jianghan@amazon.com>
    
    * Check on nested time patterns in index.
    Signed-off-by: Han Jiang <jianghan@amazon.com>

--- data-prepper-plugins/opensearch/src/main/java/com/amazon/dataprepper/plugins/sink/opensearch/index/IndexManager.java
+++ data-prepper-plugins/opensearch/src/main/java/com/amazon/dataprepper/plugins/sink/opensearch/index/IndexManager.java
@@ -6,6 +6,7 @@
 package com.amazon.dataprepper.plugins.sink.opensearch.index;
 
 import com.amazon.dataprepper.plugins.sink.opensearch.OpenSearchSinkConfiguration;
+import com.google.common.collect.ImmutableSet;
 import org.opensearch.OpenSearchException;
 import org.opensearch.action.admin.cluster.settings.ClusterGetSettingsRequest;
 import org.opensearch.action.admin.cluster.settings.ClusterGetSettingsResponse;
@@ -21,9 +22,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -31,17 +40,107 @@ public abstract class IndexManager {
 
     public static final String INDEX_ALIAS_USED_AS_INDEX_ERROR
             = "Invalid alias name [%s], an index exists with the same name as the alias";
+    private static final String TIME_PATTERN_STARTING_SYMBOLS = "%{";
     protected RestHighLevelClient restHighLevelClient;
     protected OpenSearchSinkConfiguration openSearchSinkConfiguration;
     protected IsmPolicyManagementStrategy ismPolicyManagementStrategy;
+    protected String indexPrefix;
 
     private static final Logger LOG = LoggerFactory.getLogger(IndexManager.class);
+
+    //For matching a string that begins with a "%{" and ends with a "}".
+    //For a string like "data-prepper-%{yyyy-MM-dd}", "%{yyyy-MM-dd}" is matched.
+    private final static String TIME_PATTERN_REGULAR_EXPRESSION  = "%\\{.*?\\}";
+
+    //For matching a string enclosed by "%{" and "}".
+    //For a string like "data-prepper-%{yyyy-MM}", "yyyy-MM" is matched.
+    private final static String TIME_PATTERN_INTERNAL_EXTRACTOR_REGULAR_EXPRESSION  = "%\\{(.*?)\\}";
+
+    private Optional<DateTimeFormatter> indexTimeSuffixFormatter;
+    private final static ZoneId UTC_ZONE_ID = ZoneId.of(TimeZone.getTimeZone("UTC").getID());
 
     protected IndexManager(final RestHighLevelClient restHighLevelClient, final OpenSearchSinkConfiguration openSearchSinkConfiguration){
         checkNotNull(restHighLevelClient);
         checkNotNull(openSearchSinkConfiguration);
         this.restHighLevelClient = restHighLevelClient;
         this.openSearchSinkConfiguration = openSearchSinkConfiguration;
+        initializeIndexPrefixAndSuffix();
+    }
+
+    private void initializeIndexPrefixAndSuffix(){
+        final String indexAliasFromConfig = openSearchSinkConfiguration.getIndexConfiguration().getIndexAlias();
+
+        final Pattern pattern = Pattern.compile(TIME_PATTERN_INTERNAL_EXTRACTOR_REGULAR_EXPRESSION);
+        final Matcher timePatternMatcher = pattern.matcher(indexAliasFromConfig);
+        if (timePatternMatcher.find()) {
+            final String timePattern = timePatternMatcher.group(1);
+            if (timePatternMatcher.find()) { // check if there is a one more match.
+                throw new IllegalArgumentException("An index only allows one date-time pattern.");
+            }
+            if(timePattern.contains(TIME_PATTERN_STARTING_SYMBOLS)){ //check if it is a nested pattern such as "data-prepper-%{%{yyyy.MM.dd}}"
+                throw new IllegalArgumentException("An index doesn't allow nested date-time patterns.");
+            }
+            validateTimePatternIsAtTheEnd(indexAliasFromConfig, timePattern);
+            validateNoSpecialCharsInTimePattern(timePattern);
+            validateTimePatternGranularity(timePattern);
+            indexTimeSuffixFormatter = Optional.of(DateTimeFormatter.ofPattern(timePattern));
+        } else {
+            indexTimeSuffixFormatter = Optional.empty();
+        }
+        indexPrefix = indexAliasFromConfig.replaceAll(TIME_PATTERN_REGULAR_EXPRESSION, "");
+    }
+
+    /*
+      Data Prepper only allows time pattern as a suffix.
+     */
+    private void validateTimePatternIsAtTheEnd(final String indexAliasFromConfig, final String timePattern) {
+        if (!indexAliasFromConfig.endsWith(timePattern + "}")) {
+            throw new IllegalArgumentException("Time pattern can only be a suffix of an index.");
+        }
+    }
+
+    /*
+     * Special characters can cause failures in creating indexes.
+     * */
+    private static final Set<Character> INVALID_CHARS = ImmutableSet.of('#', '\\', '/', '*', '?', '"', '<', '>', '|', ',', ':');
+
+    private void validateNoSpecialCharsInTimePattern(final String timePattern) {
+        final boolean containsInvalidCharacter = timePattern.chars()
+                .mapToObj(c -> (char) c)
+                .anyMatch(character -> INVALID_CHARS.contains(character));
+        if (containsInvalidCharacter) {
+            throw new IllegalArgumentException("Index time pattern contains one or multiple special characters: " + INVALID_CHARS);
+        }
+    }
+
+    /*
+     * Data Prepper doesn't support creating indexes with time patterns that are too granular, e.g. minute, second, millisecond, nanosecond.
+     * https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
+     * */
+    private static final Set<Character> UNSUPPORTED_TIME_GRANULARITY_CHARS = ImmutableSet.of('m', 's', 'S', 'A', 'n', 'N');
+
+    private void validateTimePatternGranularity(final String timePattern) {
+        final boolean containsUnsupportedTimeSymbol = timePattern.chars()
+                .mapToObj(c -> (char) c)
+                .anyMatch(character -> UNSUPPORTED_TIME_GRANULARITY_CHARS.contains(character));
+        if (containsUnsupportedTimeSymbol) {
+            throw new IllegalArgumentException("Index time pattern contains time patterns that are less than one hour: "
+                    + UNSUPPORTED_TIME_GRANULARITY_CHARS);
+        }
+    }
+
+    public final String getIndexAlias() {
+        if (indexTimeSuffixFormatter.isPresent()) {
+            final String formattedTimeString = indexTimeSuffixFormatter.get()
+                    .format(getCurrentUtcTime());
+            return indexPrefix + formattedTimeString;
+        } else {
+            return indexPrefix;
+        }
+    }
+
+    private ZonedDateTime getCurrentUtcTime() {
+        return LocalDateTime.now().atZone(ZoneId.systemDefault()).withZoneSameInstant(UTC_ZONE_ID);
     }
 
     public final boolean checkISMEnabled() throws IOException {
@@ -53,8 +152,9 @@ public abstract class IndexManager {
     }
 
     public final void checkAndCreateIndexTemplate(final boolean isISMEnabled, final String ismPolicyId) throws IOException {
-        final String indexAlias = openSearchSinkConfiguration.getIndexConfiguration().getIndexAlias();
-        final String indexTemplateName = indexAlias + "-index-template";
+        //If index prefix has a ending dash, then remove it to avoid two consecutive dashes.
+        final String indexPrefixWithoutTrailingDash = indexPrefix.replaceAll("-$", "");
+        final String indexTemplateName = indexPrefixWithoutTrailingDash  + "-index-template";
 
         // Check existing index template version - only overwrite if version is less than or does not exist
         if (!shouldCreateIndexTemplate(indexTemplateName)) {
@@ -63,10 +163,10 @@ public abstract class IndexManager {
 
         final PutIndexTemplateRequest putIndexTemplateRequest = new PutIndexTemplateRequest(indexTemplateName);
 
-        putIndexTemplateRequest.patterns(ismPolicyManagementStrategy.getIndexPatterns(indexAlias));
+        putIndexTemplateRequest.patterns(ismPolicyManagementStrategy.getIndexPatterns(indexPrefixWithoutTrailingDash));
 
         if (isISMEnabled) {
-            attachPolicy(openSearchSinkConfiguration.getIndexConfiguration(), ismPolicyId, indexAlias);
+            attachPolicy(openSearchSinkConfiguration.getIndexConfiguration(), ismPolicyId, indexPrefixWithoutTrailingDash);
         }
 
         putIndexTemplateRequest.source(openSearchSinkConfiguration.getIndexConfiguration().getIndexTemplate());
@@ -78,8 +178,8 @@ public abstract class IndexManager {
     }
 
     public void checkAndCreateIndex() throws IOException {
-        // Check alias exists
-        final String indexAlias = openSearchSinkConfiguration.getIndexConfiguration().getIndexAlias();
+        // Check if index name exists
+        final String indexAlias = getIndexAlias();
         final boolean indexExists = ismPolicyManagementStrategy.checkIfIndexExistsOnServer(indexAlias);
 
         if (!indexExists) {
