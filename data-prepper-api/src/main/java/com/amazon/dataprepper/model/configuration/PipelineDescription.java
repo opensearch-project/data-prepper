81d17d93d9c36f5093b0cf4b38e2ea6195ae3bb7
Taylor Gray <33740195+graytaylor0@users.noreply.github.com>
2022-01-05 12:52:32 -0600

    Add since 1.3 tag to PipelineDescription javadocs
    Signed-off-by: Taylor Gray <33740195+graytaylor0@users.noreply.github.com>

--- data-prepper-api/src/main/java/com/amazon/dataprepper/model/configuration/PipelineDescription.java
+++ data-prepper-api/src/main/java/com/amazon/dataprepper/model/configuration/PipelineDescription.java
@@ -7,15 +7,18 @@ package com.amazon.dataprepper.model.configuration;
 
 /**
  * Model that should be provided to plugins to provide certain details about the pipeline they belong to
+ * @since 1.3
  */
 public interface PipelineDescription {
     /**
      * Returns the name of the pipeline that a plugin belongs to
+     * @since 1.3
      */
     String getPipelineName();
 
     /**
      * Returns the number of process workers the pipeline is using; plugins can utilize this if synchronization between workers is necessary
+     * @since 1.3
      */
     int getNumberOfProcessWorkers();
 }
