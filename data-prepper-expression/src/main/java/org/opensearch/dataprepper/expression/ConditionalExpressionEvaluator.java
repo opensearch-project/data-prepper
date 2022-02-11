c05138d5870a363982e1f299bb6a917731259787
Steven Bayer <sbayer55@gmail.com>
2022-02-11 15:58:44 -0600

    Added Interfaces to define StatementEvaluator API for external packages (#1027)
    * Added Interfaces to define StatementEvaluator API for external packages
    
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

---
+++ data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/ConditionalExpressionEvaluator.java
@@ -0,0 +1,33 @@
+package org.opensearch.dataprepper.expression;
+
+import com.amazon.dataprepper.model.event.Event;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * Public class that {@link com.amazon.dataprepper.model.processor.Processor},
+ * {@link com.amazon.dataprepper.model.sink.Sink} and data-prepper-core objects can use to evaluate statements.
+ */
+@Named
+public class ConditionalExpressionEvaluator implements ExpressionEvaluator<Boolean> {
+    private final Parser<ParseTree> parser;
+    private final Evaluator<ParseTree, Event> evaluator;
+
+    @Inject
+    public ConditionalExpressionEvaluator(final Parser<ParseTree> parser, final Evaluator<ParseTree, Event> evaluator) {
+        this.parser = parser;
+        this.evaluator = evaluator;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <b>Method not implemented</b>
+     */
+    @Override
+    public Boolean evaluate(final String statement, final Event context) throws ClassCastException {
+        throw new RuntimeException("Method not implemented");
+    }
+}
