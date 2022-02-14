818eb613a7d6a0a354a9cb89f6616a5194639cbc
Steven Bayer <smbayer@amazon.com>
2022-02-14 11:06:14 -0600

    Added first parser test
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

--- data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/ParserTest.java
+++ data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/ParserTest.java
@@ -14,61 +14,99 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.opensearch.dataprepper.expression.antlr.DataPrepperExpressionParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import static org.hamcrest.CoreMatchers.isA;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 public class ParserTest {
+    private static final Logger LOG = LoggerFactory.getLogger(ParserTest.class);
 
-    @Mock
-    private Token token;
     @Mock
     private TokenStream tokenStream;
     @InjectMocks
     private DataPrepperExpressionParser parser;
 
-    private int streamPosition = 0;
+    private Queue<Token> tokens(final Integer ... types) {
+        final Queue<Token> tokenQueue = new LinkedBlockingQueue<>();
+        Arrays.stream(types)
+                .map(type -> {
+                    final Token token = mock(Token.class);
+//                    doReturn(type)
+//                            .when(token).getType();
+                    doAnswer(invocation -> {
+                        LOG.info("token.getType() -> {}", type);
+                        return type;
+                    }).when(token).getType();
 
-    @Test
-    void foo() {
-//        doAnswer(invocation -> {
-//            if (streamPosition == 0) {
-//                return "5";
-//            }
-//            else {
-//                return "<EOF>";
-//            }
-//        }).when(token).getText();
+                    doReturn("Type " + type)
+                            .when(token).toString();
+
+                    return token;
+                })
+                .forEach(tokenQueue::add);
+        return tokenQueue;
+    }
+
+    private TokenStream mockTokenStream(final Integer ... types) {
+        final TokenStream tokenStream = mock(TokenStream.class);
+        final Queue<Token> tokenQueue = tokens(types);
 
         doAnswer(invocation -> {
-            if (streamPosition == 0) {
-                return DataPrepperExpressionParser.Integer;
+            LOG.info("Consume !!!!!!!!!!!!!!!");
+            if (!tokenQueue.isEmpty()) {
+                final Token token = tokenQueue.remove();
+                LOG.warn("Popped token {} -> {}, queue has {} items remaining", token, token.getType(), tokenQueue.size());
             }
             else {
-                return DataPrepperExpressionParser.EOF;
+                LOG.warn("Consumed empty queue!");
             }
-        }).when(token).getType();
-
-        doAnswer(invocation -> {
-            streamPosition++;
             return null;
         }).when(tokenStream).consume();
 
-        doReturn(token)
+//        doReturn(tokenQueue.peek())
+        doAnswer(invocation -> {
+            final Token token = tokenQueue.peek();
+            LOG.info("\t\tTokenStream.LA() type: {} with queue size: {}", token.getType(), tokenQueue.size());
+            return token;
+        })
                 .when(tokenStream).LT(anyInt());
 
         doAnswer(invocation -> {
-            if (streamPosition == 0) {
-                return DataPrepperExpressionParser.Integer;
+            if (tokenQueue.isEmpty()) {
+                LOG.info("_input.LA() with empty queue");
+                return null;
             }
             else {
-                return DataPrepperExpressionParser.EOF;
+                final int type = tokenQueue.peek().getType();
+                LOG.info("_input.LA() -> {}", type);
+                return type;
             }
-        }).when(tokenStream).LA(anyInt());
+        })
+                .when(tokenStream).LA(anyInt());
+
+        return tokenStream;
+    }
+
+    @Test
+    void foo() {
+        final TokenStream input = mockTokenStream(
+                DataPrepperExpressionParser.Integer,
+                DataPrepperExpressionParser.EQUAL,
+                DataPrepperExpressionParser.Float,
+                DataPrepperExpressionParser.EOF
+        );
+        parser.setInputStream(input);
 
         final DataPrepperExpressionParser.ExpressionContext expression = parser.expression();
 
