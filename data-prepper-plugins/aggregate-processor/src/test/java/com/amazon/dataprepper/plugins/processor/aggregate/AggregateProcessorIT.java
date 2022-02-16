7f450e31c6823c08ddd395f3c044e1b6edc989be
graytaylor0 <tylgry@amazon.com>
2022-02-16 14:10:32 -0600

    Add copyright header to AggregateProcessorIT
    Signed-off-by: graytaylor0 <tylgry@amazon.com>

--- data-prepper-plugins/aggregate-processor/src/test/java/com/amazon/dataprepper/plugins/processor/aggregate/AggregateProcessorIT.java
+++ data-prepper-plugins/aggregate-processor/src/test/java/com/amazon/dataprepper/plugins/processor/aggregate/AggregateProcessorIT.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package com.amazon.dataprepper.plugins.processor.aggregate;
 
 import com.amazon.dataprepper.metrics.PluginMetrics;
