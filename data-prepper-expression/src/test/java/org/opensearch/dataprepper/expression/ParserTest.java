f84fe8ac1c2148dba65aa0eeb7204b4068654f38
Steven Bayer <sbayer55@gmail.com>
2022-02-16 09:11:58 -0600

    Added testing utils for testing antlr generated parser (#1037)
    * Added Interfaces to define StatementEvaluator API for external packages
    * Added parser test with mock token stream
    * Added first parser test
    * Added MockTokenStreamHelper
    
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

---
+++ data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/ParserTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.expression;
+
+import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.hamcrest.DiagnosingMatcher;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.opensearch.dataprepper.expression.antlr.DataPrepperExpressionParser;
+import org.opensearch.dataprepper.expression.util.MockTokenStreamHelper;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doAnswer;
+import static org.opensearch.dataprepper.expression.util.ContextMatcher.hasContext;
+import static org.opensearch.dataprepper.expression.util.ContextMatcherFactory.isParseTree;
+import static org.opensearch.dataprepper.expression.util.TerminalNodeMatcher.isTerminalNode;
+
+@ExtendWith(MockitoExtension.class)
+public class ParserTest {
+    private static final Class<? extends ParseTree> EXPRESSION = DataPrepperExpressionParser.ExpressionContext.class;
+    private static final Class<? extends ParseTree> CONDITIONAL_EXPRESSION = DataPrepperExpressionParser.ConditionalExpressionContext.class;
+    private static final Class<? extends ParseTree> EQUALITY_OPERATOR_EXPRESSION =
+            DataPrepperExpressionParser.EqualityOperatorExpressionContext.class;
+    private static final Class<? extends ParseTree> EQUALITY_OPERATOR = DataPrepperExpressionParser.EqualityOperatorContext.class;
+    private static final Class<? extends ParseTree> REGEX_OPERATOR_EXPRESSION =
+            DataPrepperExpressionParser.RegexOperatorExpressionContext.class;
+    private static final Class<? extends ParseTree> RELATIONAL_OPERATOR_EXPRESSION =
+            DataPrepperExpressionParser.RelationalOperatorExpressionContext.class;
+    private static final Class<? extends ParseTree> SET_OPERATOR_EXPRESSION =
+            DataPrepperExpressionParser.SetOperatorExpressionContext.class;
+    private static final Class<? extends ParseTree> UNARY_OPERATOR_EXPRESSION =
+            DataPrepperExpressionParser.UnaryOperatorExpressionContext.class;
+    private static final Class<? extends ParseTree> PRIMARY = DataPrepperExpressionParser.PrimaryContext.class;
+    private static final Class<? extends ParseTree> LITERAL = DataPrepperExpressionParser.LiteralContext.class;
+
+    @Mock
+    private TokenStream tokenStream;
+    @InjectMocks
+    private DataPrepperExpressionParser parser;
+
+    private void withTokenStream(final Integer ... types) {
+        final MockTokenStreamHelper helper = new MockTokenStreamHelper(types);
+
+        doAnswer(helper::consume).when(tokenStream).consume();
+        doAnswer(helper::LT).when(tokenStream).LT(anyInt());
+        doAnswer(helper::LA).when(tokenStream).LA(anyInt());
+
+        parser.setInputStream(tokenStream);
+    }
+
+    @Test
+    void testEqualityExpression() {
+        withTokenStream(
+                DataPrepperExpressionParser.Integer,
+                DataPrepperExpressionParser.EQUAL,
+                DataPrepperExpressionParser.Float,
+                DataPrepperExpressionParser.EOF
+        );
+
+        final ParseTree expression = parser.expression();
+
+        final DiagnosingMatcher<ParseTree> equals = isParseTree(EQUALITY_OPERATOR).containingTerminalNode();
+        final DiagnosingMatcher<ParseTree> endsWithInteger = isParseTree(
+                REGEX_OPERATOR_EXPRESSION,
+                RELATIONAL_OPERATOR_EXPRESSION,
+                SET_OPERATOR_EXPRESSION,
+                UNARY_OPERATOR_EXPRESSION,
+                PRIMARY,
+                LITERAL
+        ).containingTerminalNode();
+        final DiagnosingMatcher<ParseTree> leftHandSide = hasContext(EQUALITY_OPERATOR_EXPRESSION, endsWithInteger);
+        final DiagnosingMatcher<ParseTree> equalityExpression = isParseTree(
+                CONDITIONAL_EXPRESSION,
+                EQUALITY_OPERATOR_EXPRESSION
+        ).withChildrenMatching(leftHandSide, equals, endsWithInteger);
+
+        assertThat(expression, isParseTree(EXPRESSION).withChildrenMatching(equalityExpression, isTerminalNode())
+        );
+    }
+}
---
+++ data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/util/ContextMatcher.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.expression.util;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.hamcrest.Description;
+import org.hamcrest.DiagnosingMatcher;
+import org.hamcrest.Matcher;
+
+import javax.annotation.Nullable;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+/**
+ * @since 1.3
+ *
+ * <p>
+ *     ContextMatcher is a custom Hamcrest matcher to assert if a {@link ParseTree} is an instance of the expected
+ *     context and assert child node types and count. Should be used with {@link TerminalNodeMatcher}.
+ * </p>
+ * <p>
+ *     <b>Example</b><br>
+ *     Given tree:
+ *     <pre>
+ *     Expression<br>
+ *     ├─ ConditionalExpression<br>
+ *     │  ├─ EqualityOperatorExpression<br>
+ *     ├─ &lt;EOF&gt;<br>
+ *     </pre>
+ *
+ *     Matcher Assertion
+ *     <pre>
+ *         assertThat(parseTree, hasContext(Expression,<br>
+ *             hasContext(ConditionalExpression, hasContext(EqualityOperatorExpression)),<br>
+ *             hasContext(isTerminalNode())<br>
+ *         ))
+ *     </pre>
+ * </p>
+ */
+public class ContextMatcher extends DiagnosingMatcher<ParseTree> {
+    /**
+     * @since 1.3
+     * <p>Shortcut for constructor matching Hamcrest standard.</p>
+     * <p>
+     *     <b>Syntax</b><br>
+     *     <pre>assertThat(parseTree, hasContext(Expression, [child assertions]))</pre>
+     * </p>
+     * @param parserRuleContextType used to assert ParseTree branch is instance of parserRuleContextType
+     * @param childrenMatchers assertions to be used on child nodes. Matcher will also assert order and count
+     * @return matcher instance
+     */
+    @SafeVarargs
+    public static DiagnosingMatcher<ParseTree> hasContext(
+            final Class<? extends ParseTree> parserRuleContextType,
+            final DiagnosingMatcher<ParseTree>... childrenMatchers
+    ) {
+        return new ContextMatcher(parserRuleContextType, childrenMatchers);
+    }
+
+    private final DiagnosingMatcher<? extends ParseTree>[] childrenMatchers;
+    final Matcher<? extends ParseTree> isParserRuleContextType;
+    private final Matcher<Integer> listSizeMatcher;
+    @Nullable
+    private Matcher<?> failedAssertion;
+
+    @SafeVarargs
+    public ContextMatcher(
+            final Class<? extends ParseTree> parserRuleContextType,
+            final DiagnosingMatcher<? extends ParseTree> ... childrenMatchers
+    ) {
+        this.childrenMatchers = childrenMatchers;
+        isParserRuleContextType = is(instanceOf(parserRuleContextType));
+        listSizeMatcher = equalTo(childrenMatchers.length);
+    }
+
+    /**
+     * @since 1.3
+     * Asserts number of children equal to the number of childMatchers and, in order each child matches the
+     * corresponding matcher.
+     * @param ctx ParseTree branch to get children from
+     * @param mismatch Description used for printing Hamcrest mismatch messages
+     * @return true if all assertions pass
+     */
+    private boolean matchChildren(final ParseTree ctx, final Description mismatch) {
+        if (listSizeMatcher.matches(ctx.getChildCount())) {
+            for (int i = 0; i < childrenMatchers.length; i++) {
+                final ParseTree child = ctx.getChild(i);
+                final DiagnosingMatcher<? extends ParseTree> matcher = childrenMatchers[i];
+
+                if (!matcher.matches(child)) {
+                    mismatch.appendDescriptionOf(matcher)
+                            .appendText(" ");
+                    matcher.describeMismatch(child, mismatch);
+                    failedAssertion = matcher;
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        else {
+            mismatch.appendDescriptionOf(listSizeMatcher)
+                    .appendText(" ");
+            listSizeMatcher.describeMismatch(ctx.getChildCount(), mismatch);
+            failedAssertion = listSizeMatcher;
+            return false;
+        }
+    }
+
+    /**
+     * @since 1.3
+     * Asserts ParseTree branch matches assertion and all children match assertions, if any.
+     * @param item ParseTree branch to assert against
+     * @param mismatch Description used for printing Hamcrest mismatch messages
+     * @return true if all assertions pass
+     */
+    public boolean matches(final Object item, final Description mismatch) {
+        if (isParserRuleContextType.matches(item)) {
+            final ParseTree ctx = (ParseTree) item;
+            return matchChildren(ctx, mismatch);
+        }
+        else {
+            mismatch.appendDescriptionOf(isParserRuleContextType)
+                    .appendText("\n\t\tfinally ");
+            isParserRuleContextType.describeMismatch(item, mismatch);
+            failedAssertion = isParserRuleContextType;
+            return false;
+        }
+    }
+
+    /**
+     * @since 1.3
+     * Called by Hamcrest when match fails to print useful mismatch error message
+     * @param description Where output is collected
+     */
+    @Override
+    public void describeTo(final Description description) {
+        if (failedAssertion != null)
+            failedAssertion.describeTo(description);
+    }
+}
