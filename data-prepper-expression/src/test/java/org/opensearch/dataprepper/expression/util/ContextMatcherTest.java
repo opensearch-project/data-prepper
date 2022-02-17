b0a0577c100c12980341e347033391235682b79a
Steven Bayer <smbayer@amazon.com>
2022-02-17 15:51:55 -0600

    Added test cases for test utils
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

--- data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/util/ContextMatcherTest.java
+++ data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/util/ContextMatcherTest.java
@@ -7,9 +7,11 @@ package org.opensearch.dataprepper.expression.util;
 
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
 import org.hamcrest.Description;
 import org.hamcrest.DiagnosingMatcher;
 import org.junit.jupiter.api.Test;
+import org.opensearch.dataprepper.expression.antlr.DataPrepperExpressionParser;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -21,6 +23,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.opensearch.dataprepper.expression.util.ContextMatcher.hasContext;
+import static org.opensearch.dataprepper.expression.util.ContextMatcher.isUnaryTreeSet;
 
 public class ContextMatcherTest {
 
@@ -68,5 +71,28 @@ public class ContextMatcherTest {
     void testStaticConstructor() {
         final DiagnosingMatcher<ParseTree> matcher = hasContext(ParseTree.class);
         assertThat(matcher.matches(mock(ParserRuleContext.class)), is(true));
+    }
+
+    @Test
+    void testIsUnaryTreeSet() {
+        final ParseTree parseTree = mock(DataPrepperExpressionParser.SetInitializerContext.class, "Parse Tree");
+        final ParseTree terminal = mock(TerminalNode.class, "Terminal Node");
+        final ParseTree literal = mock(DataPrepperExpressionParser.LiteralContext.class, "Literal Node");
+        doReturn(7)
+                .when(parseTree)
+                .getChildCount();
+        doReturn(terminal).when(parseTree).getChild(0);
+        doReturn(literal).when(parseTree).getChild(1);
+        doReturn(terminal).when(parseTree).getChild(2);
+        doReturn(literal).when(parseTree).getChild(3);
+        doReturn(terminal).when(parseTree).getChild(4);
+        doReturn(literal).when(parseTree).getChild(5);
+        doReturn(terminal).when(parseTree).getChild(6);
+        doReturn(1).when(literal).getChildCount();
+        doReturn(terminal).when(literal).getChild(0);
+
+        final DiagnosingMatcher<ParseTree> unaryTreeSet = isUnaryTreeSet(3);
+
+        assertThat(unaryTreeSet.matches(parseTree), is(true));
     }
 }
