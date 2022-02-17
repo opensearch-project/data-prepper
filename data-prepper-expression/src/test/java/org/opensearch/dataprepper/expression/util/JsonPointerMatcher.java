47a113599599a5b820073d68f6bf0c0f323ac2ac
Steven Bayer <smbayer@amazon.com>
2022-02-17 15:21:00 -0600

    Added json pointer matcher
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

---
+++ data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/util/JsonPointerMatcher.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.expression.util;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.hamcrest.Description;
+import org.hamcrest.DiagnosingMatcher;
+import org.hamcrest.Matcher;
+import org.opensearch.dataprepper.expression.antlr.DataPrepperExpressionParser;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.opensearch.dataprepper.expression.util.ContextMatcher.describeContextTo;
+import static org.opensearch.dataprepper.expression.util.TerminalNodeMatcher.isTerminalNode;
+
+public class JsonPointerMatcher extends SimpleExpressionMatcher {
+    private static final Matcher<Integer> childCountMatcher = is(1);
+    private static final Matcher<ParseTree> terminalNodeMatcher = isTerminalNode();
+
+    public static DiagnosingMatcher<ParseTree> isJsonPointerUnaryTree() {
+        return new JsonPointerMatcher(VALID_JSON_POINTER_RULE_ORDER);
+    }
+
+    private static final List<Class<? extends ParseTree>> VALID_JSON_POINTER_RULE_ORDER = Arrays.asList(
+            DataPrepperExpressionParser.ExpressionContext.class,
+            DataPrepperExpressionParser.ConditionalExpressionContext.class,
+            DataPrepperExpressionParser.EqualityOperatorExpressionContext.class,
+            DataPrepperExpressionParser.RegexOperatorExpressionContext.class,
+            DataPrepperExpressionParser.RelationalOperatorExpressionContext.class,
+            DataPrepperExpressionParser.SetOperatorExpressionContext.class,
+            DataPrepperExpressionParser.UnaryOperatorExpressionContext.class,
+            DataPrepperExpressionParser.PrimaryContext.class,
+            DataPrepperExpressionParser.JsonPointerContext.class
+    );
+
+    private JsonPointerMatcher(final List<Class<? extends ParseTree>> validRuleOrder) {
+        super(validRuleOrder);
+    }
+
+    @Override
+    protected boolean baseCase(final ParseTree item, final Description mismatchDescription) {
+        if (!childCountMatcher.matches(item.getChildCount())) {
+            mismatchDescription.appendText("\n\t\t expected " + item.getText() + " to have 1 child node");
+            describeContextTo(item, mismatchDescription);
+            return false;
+        }
+        else if (!terminalNodeMatcher.matches(item.getChild(0))) {
+            mismatchDescription.appendText("\n\t\t expected " + item.getText() + " child to be of type TerminalNode");
+            describeContextTo(item, mismatchDescription);
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    @Override
+    public void describeTo(final Description description) {
+        description.appendText("Expected JsonPointerContext");
+    }
+}
