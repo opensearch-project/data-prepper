fa4464cf71f9b40b94b3f908f350cb8011350204
Steven Bayer <smbayer@amazon.com>
2022-02-23 13:15:46 -0600

    Added optional and required space test cases
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

--- data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/ParserTest.java
+++ data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/ParserTest.java
@@ -11,14 +11,12 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 import org.opensearch.dataprepper.expression.antlr.DataPrepperExpressionLexer;
 import org.opensearch.dataprepper.expression.antlr.DataPrepperExpressionParser;
 import org.opensearch.dataprepper.expression.util.ErrorListener;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ParserTest {
 
@@ -41,32 +39,35 @@ public class ParserTest {
         walker.walk(errorListener, context);
     }
 
-    private Executable assertThatHasParseError(final String expression) {
-        return () -> {
-            parseExpression(expression);
-            assertThat(
-                    "\"" + expression + "\" should have parsing errors",
-                    errorListener.isErrorFound(),
-                    is(true)
-            );
-        };
+    private void assertThatIsValid(final String expression) {
+        parseExpression(expression);
+        assertThat(
+                "\"" + expression + "\" should not have parsing errors",
+                errorListener.isErrorFound(),
+                is(false)
+        );
+    }
+
+    private void assertThatHasParseError(final String expression) {
+        parseExpression(expression);
+        assertThat(
+                "\"" + expression + "\" should have parsing errors",
+                errorListener.isErrorFound(),
+                is(true)
+        );
     }
 
     @Test
     void testGivenOperandIsNotStringWhenEvaluateThenErrorPresent() {
-        parseExpression("(true) =~ \"Hello?\"");
-
-        assertThat(errorListener.isErrorFound(), is(true));
+        assertThatHasParseError("(true) =~ \"Hello?\"");
     }
 
     @Test
     void testInvalidSetItem() {
-        assertAll(
-                assertThatHasParseError("{true or false}"),
-                assertThatHasParseError("{(5)}"),
-                assertThatHasParseError("{5 == 5}"),
-                assertThatHasParseError("{/node =~ \"[A-Z]*\"}")
-        );
+        assertThatHasParseError("{true or false}");
+        assertThatHasParseError("{(5)}");
+        assertThatHasParseError("{5 == 5}");
+        assertThatHasParseError("{/node =~ \"[A-Z]*\"}");
     }
 
     @Test
@@ -81,13 +82,11 @@ public class ParserTest {
 
     @Test
     void testInvalidFloatParsingRules() {
-        assertAll(
-                assertThatHasParseError("0."),
-                assertThatHasParseError("00."),
-                assertThatHasParseError(".00"),
-                assertThatHasParseError(".10"),
-                assertThatHasParseError("1.10")
-        );
+        assertThatHasParseError("0.");
+        assertThatHasParseError("00.");
+        assertThatHasParseError(".00");
+        assertThatHasParseError(".10");
+        assertThatHasParseError("1.10");
     }
 
     @Test
@@ -95,5 +94,49 @@ public class ParserTest {
         assertThatHasParseError("5==");
         assertThatHasParseError("==");
         assertThatHasParseError("not");
+    }
+
+    @Test
+    void testValidOperatorsRequireSpace() {
+        assertThatIsValid("/status in {200}");
+        assertThatIsValid("/a==(/b==200)");
+        assertThatIsValid("/a in {200}");
+        assertThatIsValid("/a not in {400}");
+        assertThatIsValid("/a<300 and /b>200");
+    }
+
+    @Test
+    void testInvalidSetOperatorArgs() {
+        assertThatHasParseError("/a in ({200})");
+        assertThatHasParseError("/a in 5");
+        assertThatHasParseError("/a in 3.14");
+        assertThatHasParseError("/a in false");
+        assertThatHasParseError("\"Hello\" not in ({200})");
+        assertThatHasParseError("\"Hello\" not in 5");
+        assertThatHasParseError("\"Hello\" not in 3.14");
+        assertThatHasParseError("\"Hello\" not in false");
+    }
+
+    @Test
+    void testInvalidOperatorsRequireSpace() {
+        assertThatHasParseError("/status in{200}");
+        assertThatHasParseError("/status in({200})");
+        assertThatHasParseError("/a in{200, 202}");
+        assertThatHasParseError("/a not in{400}");
+        assertThatHasParseError("/b<300and/b>200");
+        assertThatHasParseError("/a in {200,}");
+    }
+
+    @Test
+    void testValidOptionalSpaceOperators() {
+        assertThatIsValid("/status < 300");
+        assertThatIsValid("/status>=300");
+        assertThatIsValid("/msg =~ \"^\\\\w*\\$\"");
+        assertThatIsValid("/msg=~\"[A-Za-z]\"");
+        assertThatIsValid("/status == 200");
+        assertThatIsValid("/status_code==200");
+        assertThatIsValid("/a in {200, 202}");
+        assertThatIsValid("/a in {200,202}");
+        assertThatIsValid("/a in {200 , 202}");
     }
 }
