e69d904bb766ff9ff0837404edc88901c76854b7
Steven Bayer <smbayer@amazon.com>
2022-02-24 10:54:31 -0600

    Added RuleClassOrderedList
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

--- data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/util/SimpleExpressionMatcher.java
+++ data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/util/SimpleExpressionMatcher.java
@@ -10,92 +10,117 @@ import org.hamcrest.Description;
 import org.hamcrest.DiagnosingMatcher;
 import org.hamcrest.Matcher;
 
-import java.util.List;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.opensearch.dataprepper.expression.util.ContextMatcher.describeContextTo;
 
 /**
- * Base class for Matcher that asserts a unary tree ending in a node that matches {@link SimpleExpressionMatcher#lastNodeClass}
+ * Base class for Matcher that asserts a unary tree ending in a node that matches
+ * {@link RuleClassOrderedList#isInstanceOfLast(ParseTree)}
  */
 abstract class SimpleExpressionMatcher extends DiagnosingMatcher<ParseTree> {
     private static final Matcher<Integer> singleChildMatcher = is(1);
 
-    protected final List<Class<? extends ParseTree>> validRuleOrder;
-    protected final Class<? extends ParseTree> lastNodeClass;
+    protected final RuleClassOrderedList validRuleOrder;
 
-    protected SimpleExpressionMatcher(final List<Class<? extends ParseTree>> validRuleOrder) {
+    protected SimpleExpressionMatcher(final RuleClassOrderedList validRuleOrder) {
         this.validRuleOrder = validRuleOrder;
-        this.lastNodeClass = validRuleOrder.get(validRuleOrder.size() - 1);
-    }
-
-    private int getRuleIndex(final ParseTree item) {
-        for (int x = 0; x < validRuleOrder.size(); x++) {
-            if (validRuleOrder.get(x).isInstance(item)) {
-                return x;
-            }
-        }
-        return -1;
-    }
-
-    private boolean isValidRuleOrder(final ParseTree current, final ParseTree next, final Description mismatchDescription) {
-        final int index = getRuleIndex(current);
-        if (index < 0 || index >= validRuleOrder.size() - 1) {
-            mismatchDescription.appendText(current.getClass() + " is not a valid context ");
-            return false;
-        }
-        else {
-            if (validRuleOrder.get(index + 1).isInstance(next)) {
-                return true;
-            }
-            else {
-                mismatchDescription.appendText(current.getClass() + " -> " + next.getClass() + " not valid rule order");
-                describeContextTo(current, mismatchDescription);
-                return false;
-            }
-        }
     }
 
     /**
      * Called when {@link SimpleExpressionMatcher#matchesParseTree(ParseTree, Description)} finds a node matching
-     * {@link SimpleExpressionMatcher#lastNodeClass}
+     * {@link RuleClassOrderedList#isInstanceOfLast(ParseTree)}
+     *
      * @param item matching node
-     * @param mismatchDescription description to append failed assertion context.
+     * @param mismatch description to append failed assertion context.
      * @return if this matcher matches
      */
-    protected abstract boolean baseCase(final ParseTree item, final Description mismatchDescription);
+    protected abstract boolean baseCase(final ParseTree item, final Description mismatch);
 
-    private boolean matchesParseTree(final ParseTree item, final Description mismatch) {
-        if (lastNodeClass.isInstance(item)) {
-            if (baseCase(item, mismatch)) {
-                return true;
-            }
-            else {
-                describeContextTo(item, mismatch);
-                return false;
-            }
-        }
-        else if (singleChildMatcher.matches(item.getChildCount())) {
-            final ParseTree child = item.getChild(0);
-            if (!isValidRuleOrder(item, child, mismatch)) {
-                describeTo(mismatch);
-                return false;
-            }
-            return isValidRuleOrder(item, child, mismatch) && matchesParseTree(child, mismatch);
+    /**
+     * Wrapper method for {@link SimpleExpressionMatcher#baseCase(ParseTree, Description)} to ensure a mismatch
+     * description is added.
+     *
+     * @param item ParseTree to match
+     * @param mismatch description of matcher
+     * @return if item matches the expected value
+     */
+    private boolean matchesBaseCase(final ParseTree item, final Description mismatch) {
+        if (baseCase(item, mismatch)) {
+            return true;
         }
         else {
-            mismatch.appendText("Unexpected terminal node " + item.getText());
-            if (item.getParent() != null) {
-                mismatch.appendText(", child of parent node " + item.getParent().getText());
-            }
+            describeContextTo(item, mismatch);
             return false;
         }
     }
 
+    /**
+     * <p>
+     *     Checks if item has one child and that item's class and item's child's class are sequential in
+     *     {@link SimpleExpressionMatcher#validRuleOrder}. Then recursively starts matching item's child.
+     * </p>
+     * <p>
+     *     Valid Rule Order:<br>
+     *     A -> B -> C
+     * </p>
+     * <p>
+     *     True if <pre>item.getClass() == <? extends A> && item.getChild(0).getClass() == <? extends B></pre>
+     * </p>
+     *
+     * @param item ParseTree to match
+     * @param mismatch description of matcher
+     * @return if item matches the expected value
+     */
+    private boolean matchesChildren(final ParseTree item, final Description mismatch) {
+        if (singleChildMatcher.matches(item.getChildCount())) {
+            final ParseTree child = item.getChild(0);
+
+            if (validRuleOrder.isSequentialRules(item, child)) {
+                return matchesParseTree(child, mismatch);
+            }
+            else {
+                mismatch.appendText(item.getClass() + " -> " + child.getClass() + " not valid rule order");
+                describeContextTo(item, mismatch);
+                return false;
+            }
+        }
+        else {
+            singleChildMatcher.describeMismatch(item.getChildCount(), mismatch);
+            describeContextTo(item, mismatch);
+            return false;
+        }
+    }
+
+    /**
+     * Checks if item is the last rule in {@link SimpleExpressionMatcher#validRuleOrder}. If yes, trigger base case,
+     * otherwise match the nodes children.
+     *
+     * @param item ParseTree to match
+     * @param mismatch description of matcher
+     * @return if item matches the expected value
+     */
+    private boolean matchesParseTree(final ParseTree item, final Description mismatch) {
+        if (validRuleOrder.isInstanceOfLast(item)) {
+            return matchesBaseCase(item, mismatch);
+        }
+        else {
+            return matchesChildren(item, mismatch);
+        }
+    }
+
+    /**
+     * Verifies item is a ParseTree, then starts matching logic
+     *
+     * @see SimpleExpressionMatcher#matchesParseTree(ParseTree, Description)
+     *
+     * @param item parse tree to match against
+     * @param mismatch description of matcher
+     * @return if item matches the expected value
+     */
     @Override
-    protected boolean matches(final Object item, final Description mismatchDescription) {
+    protected boolean matches(final Object item, final Description mismatch) {
         if (item instanceof ParseTree) {
-            return matchesParseTree((ParseTree) item, mismatchDescription);
+            return matchesParseTree((ParseTree) item, mismatch);
         }
         else {
             return false;
