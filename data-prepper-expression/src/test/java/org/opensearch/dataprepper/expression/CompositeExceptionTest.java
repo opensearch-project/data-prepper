30bde1e751e81afa7f7da3767f1c9fa3802a5f1c
Steven Bayer <smbayer@amazon.com>
2022-02-25 14:20:14 -0600

    Simplified CompositeException
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

--- data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/CompositeExceptionTest.java
+++ data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/CompositeExceptionTest.java
@@ -7,39 +7,49 @@ package org.opensearch.dataprepper.expression;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+
 class CompositeExceptionTest {
 
-//    @Test
-//    void testNoCauses() {
-//        final Throwable throwable = new ParsingExceptions("test", Collections.emptyList());
-//
-//        final PrintStream printStream = mock(PrintStream.class);
-//
-//        throwable.printStackTrace(printStream);
-//
-//        verify(printStream, times(0)).println(eq("causes"));
-//    }
-//
-//    @Test
-//    void testMultipleExceptions() {
-//        final Throwable cause = mock(Throwable.class);
-//        final Throwable throwable = new ParsingExceptions("test", Collections.nCopies(5, cause));
-//
-//        final PrintStream printStream = mock(PrintStream.class);
-//
-//        throwable.printStackTrace(printStream);
-//
-//        verify(printStream, times(1)).println(eq("causes"));
-//        verify(cause, times(5)).printStackTrace(eq(printStream));
-//    }
+    @Test
+    void testNoCausesThrows() {
+        assertThrows(NullPointerException.class, () -> new CompositeException(null));
+    }
+
+    @Test
+    void testEmptyListThrows() {
+        assertThrows(IllegalArgumentException.class, () -> new CompositeException(Collections.emptyList()));
+    }
+
+    @Test
+    void testGivenSingleExceptionThenExceptionIsCause() {
+        final RuntimeException mock = mock(RuntimeException.class);
+        final CompositeException compositeException = new CompositeException(Arrays.asList(mock));
+
+        assertThat(compositeException.getCause(), is(mock));
+    }
 
     @Test
     void foo() throws CompositeException {
-        throw new CompositeException(
+        final CompositeException compositeException = new CompositeException(Arrays.asList(
                 new RuntimeException("Error"),
                 new RuntimeException("Error 2"),
                 new RuntimeException("Error 3")
-        );
-    }
+        ));
 
+        assertThat(compositeException.getCause() instanceof ExceptionOverview, is(true));
+
+        final String message = compositeException.getCause().getMessage();
+        assertThat(message, containsString("Multiple exceptions (3)"));
+        assertThat(message, containsString("|-- java.lang.RuntimeException: Error 3"));
+        assertThat(message, containsString("|-- java.lang.RuntimeException: Error 2"));
+        assertThat(message, containsString("|-- java.lang.RuntimeException: Error"));
+    }
 }
