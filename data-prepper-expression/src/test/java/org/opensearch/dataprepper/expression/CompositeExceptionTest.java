017885fce5e73570051cbb5a32bc8fa6d756d920
Steven Bayer <sbayer55@gmail.com>
2022-02-28 09:36:14 -0600

    Added ParseTreeParser (#1090)
    * Added ParseTreeParser
    
    Signed-off-by: Steven Bayer <smbayer@amazon.com>
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

---
+++ data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/CompositeExceptionTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.expression;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+
+class CompositeExceptionTest {
+
+    @Test
+    void testNoCausesThrows() {
+        assertThrows(NullPointerException.class, () -> new CompositeException(null));
+    }
+
+    @Test
+    void testEmptyListThrows() {
+        assertThrows(IllegalArgumentException.class, () -> new CompositeException(Collections.emptyList()));
+    }
+
+    @Test
+    void testGivenSingleExceptionThenExceptionIsCause() {
+        final RuntimeException mock = mock(RuntimeException.class);
+        final CompositeException compositeException = new CompositeException(Arrays.asList(mock));
+
+        assertThat(compositeException.getCause(), is(mock));
+    }
+
+    @Test
+    void testMultipleExceptionsPrinted() throws CompositeException {
+        final CompositeException compositeException = new CompositeException(Arrays.asList(
+                new RuntimeException("Error"),
+                new RuntimeException("Error 2"),
+                new RuntimeException("Error 3"),
+                null
+        ));
+
+        assertThat(compositeException.getCause() instanceof ExceptionOverview, is(true));
+
+        final String message = compositeException.getCause().getMessage();
+        assertThat(message, containsString("Multiple exceptions (4)"));
+        assertThat(message, containsString("|-- java.lang.RuntimeException: Error 3"));
+        assertThat(message, containsString("|-- java.lang.RuntimeException: Error 2"));
+        assertThat(message, containsString("|-- java.lang.RuntimeException: Error"));
+        assertThat(message, containsString("|-- java.lang.NullPointerException: Throwable was null!"));
+    }
+}
