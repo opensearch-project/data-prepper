0a5935762fb0ba0a578ab4ba3a133920a3a5fa32
Steven Bayer <smbayer@amazon.com>
2022-02-28 10:07:51 -0600

    Added test cases for ParenthesesExpressionMatcher
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

--- data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/util/ParenthesesExpressionMatcherTest.java
+++ data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/util/ParenthesesExpressionMatcherTest.java
@@ -11,6 +11,7 @@ import org.hamcrest.DiagnosingMatcher;
 import org.junit.jupiter.api.Test;
 import org.opensearch.dataprepper.expression.antlr.DataPrepperExpressionParser;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
@@ -21,7 +22,7 @@ import static org.opensearch.dataprepper.expression.util.ParenthesesExpressionMa
 class ParenthesesExpressionMatcherTest {
 
     @Test
-    void baseCase() {
+    void testGivenValidParseTreeMatchIsTrue() {
         final DiagnosingMatcher<ParseTree> isParenthesesExpression = isParenthesesExpression(isUnaryTree());
         final ParseTree relationalOperatorExpressionContext =
                 mock(DataPrepperExpressionParser.RelationalOperatorExpressionContext.class, "RelationalOperatorExpression");
@@ -52,5 +53,38 @@ class ParenthesesExpressionMatcherTest {
         doReturn(terminal).when(literal).getChild(eq(0));
 
         assertTrue(isParenthesesExpression.matches(relationalOperatorExpressionContext));
+    }
+
+    @Test
+    void testGivenInvalidParseTreeRootMatcherFails() {
+        final DiagnosingMatcher<ParseTree> isParenthesesExpression = isParenthesesExpression(isUnaryTree());
+
+        final ParseTree relationalOperatorExpressionContext =
+                mock(DataPrepperExpressionParser.ExpressionContext.class, "ExpressionContext");
+
+        assertFalse(isParenthesesExpression.matches(relationalOperatorExpressionContext));
+    }
+
+    @Test
+    void testGivenInvalidParseTreeMatchFails() {
+        final DiagnosingMatcher<ParseTree> isParenthesesExpression = isParenthesesExpression(isUnaryTree());
+        final ParseTree relationalOperatorExpressionContext =
+                mock(DataPrepperExpressionParser.RelationalOperatorExpressionContext.class, "RelationalOperatorExpression");
+        final ParseTree setOperatorExpressionContext =
+                mock(DataPrepperExpressionParser.SetOperatorExpressionContext.class, "SetOperatorExpression");
+        final ParseTree unaryOperatorExpressionContext =
+                mock(DataPrepperExpressionParser.UnaryOperatorExpressionContext.class, "UnaryOperatorExpression");
+        final ParseTree parenthesesExpressionContext =
+                mock(DataPrepperExpressionParser.ParenthesesExpressionContext.class, "ParenthesesExpression");
+
+        doReturn(1).when(relationalOperatorExpressionContext).getChildCount();
+        doReturn(1).when(setOperatorExpressionContext).getChildCount();
+        doReturn(1).when(unaryOperatorExpressionContext).getChildCount();
+        doReturn(2).when(parenthesesExpressionContext).getChildCount();
+
+        doReturn(setOperatorExpressionContext).when(relationalOperatorExpressionContext).getChild(eq(0));
+        doReturn(unaryOperatorExpressionContext).when(setOperatorExpressionContext).getChild(eq(0));
+
+        assertFalse(isParenthesesExpression.matches(relationalOperatorExpressionContext));
     }
 }
