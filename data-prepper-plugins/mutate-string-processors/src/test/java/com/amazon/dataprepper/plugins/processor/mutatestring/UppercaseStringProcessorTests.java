bb84552b0ca50ece7955fd9d58b489501ee93108
David Powers <ddpowers@amazon.com>
2022-03-02 11:04:19 -0600

    Additional Tests
    Signed-off-by: David Powers <ddpowers@amazon.com>

--- data-prepper-plugins/mutate-string-processors/src/test/java/com/amazon/dataprepper/plugins/processor/mutatestring/UppercaseStringProcessorTests.java
+++ data-prepper-plugins/mutate-string-processors/src/test/java/com/amazon/dataprepper/plugins/processor/mutatestring/UppercaseStringProcessorTests.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -46,6 +47,36 @@ public class UppercaseStringProcessorTests {
 
         assertThat(editedRecords.get(0).getData().containsKey("message"), is(true));
         assertThat(editedRecords.get(0).getData().get("message", Object.class), equalTo("THISISAMESSAGE"));
+    }
+
+    @Test
+    public void testHappyPathMultiUppercaseStringProcessor() {
+        when(config.getWithKeys()).thenReturn(Arrays.asList("message", "message2"));
+
+        final UppercaseStringProcessor processor = createObjectUnderTest();
+        final Record<Event> record = getEvent("thisisamessage");
+        record.getData().put("message2", "test2");
+        final List<Record<Event>> editedRecords = (List<Record<Event>>) processor.doExecute(Collections.singletonList(record));
+
+        assertThat(editedRecords.get(0).getData().containsKey("message"), is(true));
+        assertThat(editedRecords.get(0).getData().get("message", Object.class), equalTo("THISISAMESSAGE"));
+        assertThat(editedRecords.get(0).getData().containsKey("message2"), is(true));
+        assertThat(editedRecords.get(0).getData().get("message2", Object.class), equalTo("TEST2"));
+    }
+
+    @Test
+    public void testHappyPathMultiMixedUppercaseStringProcessor() {
+        when(config.getWithKeys()).thenReturn(Arrays.asList("message", "message2"));
+
+        final UppercaseStringProcessor processor = createObjectUnderTest();
+        final Record<Event> record = getEvent("thisisamessage");
+        record.getData().put("message2", 3);
+        final List<Record<Event>> editedRecords = (List<Record<Event>>) processor.doExecute(Collections.singletonList(record));
+
+        assertThat(editedRecords.get(0).getData().containsKey("message"), is(true));
+        assertThat(editedRecords.get(0).getData().get("message", Object.class), equalTo("THISISAMESSAGE"));
+        assertThat(editedRecords.get(0).getData().containsKey("message2"), is(true));
+        assertThat(editedRecords.get(0).getData().get("message2", Object.class), equalTo(3));
     }
 
     @Test
