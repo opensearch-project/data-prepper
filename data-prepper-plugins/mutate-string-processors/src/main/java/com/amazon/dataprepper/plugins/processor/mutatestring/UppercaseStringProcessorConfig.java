96da7c102b6b7cc80aaa32dbb39d4ceda55303ff
David Powers <ddpowers@amazon.com>
2022-03-02 12:08:02 -0600

    Delete UppercaseStringProcessorConfig file
    Signed-off-by: David Powers <ddpowers@amazon.com>

--- data-prepper-plugins/mutate-string-processors/src/main/java/com/amazon/dataprepper/plugins/processor/mutatestring/UppercaseStringProcessorConfig.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-package com.amazon.dataprepper.plugins.processor.mutatestring;
-
-public class UppercaseStringProcessorConfig extends WithKeysProcessorConfig {
-}
