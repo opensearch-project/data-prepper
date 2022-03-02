25544f02bf5e1155e1d79c14a0637a4142eb4a9d
Steven Bayer <smbayer@amazon.com>
2022-03-02 09:37:15 -0600

    Add missing copyright
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

--- data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/ExpressionEvaluator.java
+++ data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/ExpressionEvaluator.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.dataprepper.expression;
 
 import com.amazon.dataprepper.model.event.Event;
