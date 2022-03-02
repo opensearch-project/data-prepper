d0cbbbf466e6c051523579a044b0bf218a35a4ac
Steven Bayer <sbayer55@gmail.com>
2022-03-02 10:26:47 -0600

    Implemented ConditionalExpressionEvaluator methods (#1128)
    * Implemented ConditionalExpressionEvaluator methods
    
    Signed-off-by: Steven Bayer <smbayer@amazon.com>
    Signed-off-by: Arunachalam Lakshmanan <arunachalam.l@gmail.com>

--- data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/ConditionalExpressionEvaluatorTest.java
+++ data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/ConditionalExpressionEvaluatorTest.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.dataprepper.expression;
 
 import com.amazon.dataprepper.model.event.Event;
@@ -8,7 +13,18 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.UUID;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class ConditionalExpressionEvaluatorTest {
@@ -21,9 +37,62 @@ class ConditionalExpressionEvaluatorTest {
     private ConditionalExpressionEvaluator statementEvaluator;
 
     @Test
-    void testThrowsNotImplementedException() {
-        assertThrows(
-                RuntimeException.class,
-                () -> statementEvaluator.evaluate(null, null));
+    void testGivenValidParametersThenEvaluatorResultReturned() {
+        final String statement = UUID.randomUUID().toString();
+        final ParseTree parseTree = mock(ParseTree.class);
+        final Event event = mock(Event.class);
+        final Boolean expected = true;
+
+        doReturn(parseTree).when(parser).parse(eq(statement));
+        doReturn(expected).when(evaluator).evaluate(eq(parseTree), eq(event));
+
+        final Boolean actual = statementEvaluator.evaluate(statement, event);
+
+        assertThat(actual, is(expected));
+        verify(parser).parse(eq(statement));
+        verify(evaluator).evaluate(eq(parseTree), eq(event));
+    }
+
+    @Test
+    void testGivenUnexpectedEvaluatorResultTypeThenExceptionThrown() {
+        final String statement = UUID.randomUUID().toString();
+        final ParseTree parseTree = mock(ParseTree.class);
+        final Event event = mock(Event.class);
+        final Object result = mock(Object.class);
+
+        doReturn(parseTree).when(parser).parse(eq(statement));
+        doReturn(result).when(evaluator).evaluate(eq(parseTree), eq(event));
+
+        assertThrows(ExpressionEvaluationException.class, () -> statementEvaluator.evaluate(statement, event));
+
+        verify(parser).parse(eq(statement));
+        verify(evaluator).evaluate(eq(parseTree), eq(event));
+    }
+
+    @Test
+    void testGivenParserThrowsExceptionThenExceptionThrown() {
+        final String statement = UUID.randomUUID().toString();
+
+        doThrow(new RuntimeException()).when(parser).parse(eq(statement));
+
+        assertThrows(ExpressionEvaluationException.class, () -> statementEvaluator.evaluate(statement, null));
+
+        verify(parser).parse(eq(statement));
+        verify(evaluator, times(0)).evaluate(any(), any());
+    }
+
+    @Test
+    void testGivenEvaluatorThrowsExceptionThenExceptionThrown() {
+        final String statement = UUID.randomUUID().toString();
+        final ParseTree parseTree = mock(ParseTree.class);
+        final Event event = mock(Event.class);
+
+        doReturn(parseTree).when(parser).parse(eq(statement));
+        doThrow(new RuntimeException()).when(evaluator).evaluate(eq(parseTree), eq(event));
+
+        assertThrows(ExpressionEvaluationException.class, () -> statementEvaluator.evaluate(statement, event));
+
+        verify(parser).parse(eq(statement));
+        verify(evaluator).evaluate(eq(parseTree), eq(event));
     }
 }
