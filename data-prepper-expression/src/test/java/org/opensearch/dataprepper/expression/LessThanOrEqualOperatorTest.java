3831d57c836bcefe44f2c61d8324354258b28711
Qi Chen <19492223+chenqi0805@users.noreply.github.com>
2022-03-03 10:53:51 -0600

    Feature: operators in data-prepper expression (#1065)
    * FEAT: operators and tests
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: lower -> less
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: use int for symbol
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: include null in equality operators
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: operator classes package private
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * STY: eval -> evaluate
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: simplify numerical value comparison operators
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: DI annotation on operators
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: OperatorFactory to produce negate and numeric comparison operator
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: use NumericCompareOperator
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: generic operator to cover negate
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: generic operators
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * STY: static final
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MNT: Objects::equals
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>

---
+++ data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/LessThanOrEqualOperatorTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.expression;
+
+import org.junit.jupiter.api.Test;
+import org.opensearch.dataprepper.expression.antlr.DataPrepperExpressionParser;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class LessThanOrEqualOperatorTest {
+    final Operator<Boolean> objectUnderTest = new OperatorFactory().lessThanOrEqualOperator();
+
+    @Test
+    void testGetSymbol() {
+        assertThat(objectUnderTest.getSymbol(), is(DataPrepperExpressionParser.LTE));
+    }
+
+    @Test
+    void testEvalValidArgs() {
+        assertThat(objectUnderTest.evaluate(2, 1), is(false));
+        assertThat(objectUnderTest.evaluate(1, 2), is(true));
+        assertThat(objectUnderTest.evaluate(1, 1), is(true));
+        assertThat(objectUnderTest.evaluate(2f, 1), is(false));
+        assertThat(objectUnderTest.evaluate(1f, 2), is(true));
+        assertThat(objectUnderTest.evaluate(1f, 1), is(true));
+        assertThat(objectUnderTest.evaluate(2, 1f), is(false));
+        assertThat(objectUnderTest.evaluate(1, 2f), is(true));
+        assertThat(objectUnderTest.evaluate(1, 1f), is(true));
+        assertThat(objectUnderTest.evaluate(2f, 1f), is(false));
+        assertThat(objectUnderTest.evaluate(1f, 2f), is(true));
+        assertThat(objectUnderTest.evaluate(1f, 1f), is(true));
+    }
+
+    @Test
+    void testEvalInValidArgLength() {
+        assertThrows(IllegalArgumentException.class, () -> objectUnderTest.evaluate(1));
+        assertThrows(IllegalArgumentException.class, () -> objectUnderTest.evaluate(1, 2, 3));
+    }
+
+    @Test
+    void testEvalInValidArgType() {
+        assertThrows(IllegalArgumentException.class, () -> objectUnderTest.evaluate(1L, 1));
+        assertThrows(IllegalArgumentException.class, () -> objectUnderTest.evaluate(1.0, 1));
+        assertThrows(IllegalArgumentException.class, () -> objectUnderTest.evaluate(1, 1L));
+        assertThrows(IllegalArgumentException.class, () -> objectUnderTest.evaluate(1, 1.0));
+    }
+}
