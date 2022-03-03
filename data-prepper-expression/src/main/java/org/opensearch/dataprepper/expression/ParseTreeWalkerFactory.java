7fefc82cc07144a22a6a3c0bbf00a1f16c7e5a4b
Chen <19492223+chenqi0805@users.noreply.github.com>
2022-03-03 17:19:17 -0600

    ADD: ParseTreeWalkerFactory for DI
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>

---
+++ data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/ParseTreeWalkerFactory.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.expression;
+
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.springframework.context.annotation.Bean;
+
+import javax.inject.Named;
+
+@Named
+public class ParseTreeWalkerFactory {
+
+    @Bean()
+    public ParseTreeWalker parseTreeWalker() {
+        return new ParseTreeWalker();
+    }
+}
