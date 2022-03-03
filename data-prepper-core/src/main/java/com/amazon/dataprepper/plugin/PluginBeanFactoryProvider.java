6d1f2739f0ec73b1543bdf840451b62af55f4ef2
Steven Bayer <smbayer@amazon.com>
2022-03-03 17:33:53 -0600

    Add JavaDoc
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

--- data-prepper-core/src/main/java/com/amazon/dataprepper/plugin/PluginBeanFactoryProvider.java
+++ data-prepper-core/src/main/java/com/amazon/dataprepper/plugin/PluginBeanFactoryProvider.java
@@ -16,6 +16,16 @@ import javax.inject.Named;
 import javax.inject.Provider;
 import java.util.Objects;
 
+/**
+ * @since 1.3
+ * <p>
+ *     Used to create new instances of ApplicationContext that can be used to provide a per plugin instance isolated ApplicationContext
+ *     scope. CoreApplicationContext is unavailable to sharedPluginApplicationContext and its children.
+ * </p>
+ * <p>pluginIsolatedApplicationContext inherits from {@link PluginBeanFactoryProvider#sharedPluginApplicationContext}</p>
+ * <p>{@link PluginBeanFactoryProvider#sharedPluginApplicationContext} inherits from <i>publicContext</i></p>
+ * <p><i>publicContext</i> is the root {@link ApplicationContext}</p>
+ */
 @Named
 class PluginBeanFactoryProvider implements Provider<BeanFactory> {
     private static final Logger LOG = LoggerFactory.getLogger(PluginBeanFactoryProvider.class);
@@ -24,26 +34,17 @@ class PluginBeanFactoryProvider implements Provider<BeanFactory> {
     @Inject
     PluginBeanFactoryProvider(final ApplicationContext coreContext) {
         final ApplicationContext publicContext = Objects.requireNonNull(coreContext.getParent());
-        sharedPluginApplicationContext = new GenericApplicationContext(publicContext) {
-            @Override
-            public String toString() {
-                return "Plugin Shared Context!";
-            }
-        };
+        sharedPluginApplicationContext = new GenericApplicationContext(publicContext);
     }
 
+    /**
+     * @since 1.3
+     * Creates a new Application context that inherits from {@link PluginBeanFactoryProvider#sharedPluginApplicationContext} then returns
+     * new context's BeanFactory.
+     * @return BeanFactory A BeanFactory that inherits from {@link PluginBeanFactoryProvider#sharedPluginApplicationContext}
+     */
     public BeanFactory get() {
-        final GenericApplicationContext pluginIsolatedApplicationContext = new GenericApplicationContext(sharedPluginApplicationContext) {
-            @Override
-            public String toString() {
-                return "Plugin Isolated Context!";
-            }
-        };
+        final GenericApplicationContext pluginIsolatedApplicationContext = new GenericApplicationContext(sharedPluginApplicationContext);
         return pluginIsolatedApplicationContext.getBeanFactory();
-    }
-
-    @Override
-    public String toString() {
-        return sharedPluginApplicationContext.toString();
     }
 }
