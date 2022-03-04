3ec398065a92c419d7ba543515ea3ab61e56baff
Shivani Shukla <67481911+sshivanii@users.noreply.github.com>
2022-03-04 12:25:36 -0600

    Updates NoOpPrepper to implement Processor (#1142)
    Signed-off-by: Shivani Shukla <sshkamz@amazon.com>
    
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

--- data-prepper-plugins/common/src/main/java/com/amazon/dataprepper/plugins/prepper/NoOpPrepper.java
+++ data-prepper-plugins/common/src/main/java/com/amazon/dataprepper/plugins/prepper/NoOpPrepper.java
@@ -8,12 +8,13 @@ package com.amazon.dataprepper.plugins.prepper;
 import com.amazon.dataprepper.model.annotations.DataPrepperPlugin;
 import com.amazon.dataprepper.model.event.Event;
 import com.amazon.dataprepper.model.prepper.Prepper;
+import com.amazon.dataprepper.model.processor.Processor;
 import com.amazon.dataprepper.model.record.Record;
 
 import java.util.Collection;
 
 @DataPrepperPlugin(name = "no-op", pluginType = Prepper.class)
-public class NoOpPrepper implements Prepper<Record<Event>, Record<Event>> {
+public class NoOpPrepper implements Processor<Record<Event>, Record<Event>> {
 
     public NoOpPrepper() {
 
