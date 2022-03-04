50ccff5c2581b72b0bdd44d70c49fa9f2107dca0
David Powers <37314042+dapowers87@users.noreply.github.com>
2022-03-04 11:29:12 -0600

    Add UppercaseStringProcessor (#1137)
    * Add UppercaseStringProcessor and its associated base classes and config classes
    
    Signed-off-by: David Powers <ddpowers@amazon.com>
    Signed-off-by: Arunachalam Lakshmanan <arunachalam.l@gmail.com>

---
+++ data-prepper-plugins/mutate-string-processors/src/main/java/com/amazon/dataprepper/plugins/processor/mutatestring/UppercaseStringProcessor.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.amazon.dataprepper.plugins.processor.mutatestring;
+
+import com.amazon.dataprepper.metrics.PluginMetrics;
+import com.amazon.dataprepper.model.annotations.DataPrepperPlugin;
+import com.amazon.dataprepper.model.annotations.DataPrepperPluginConstructor;
+import com.amazon.dataprepper.model.event.Event;
+import com.amazon.dataprepper.model.processor.Processor;
+import java.util.Locale;
+
+@DataPrepperPlugin(name = "uppercase_string", pluginType = Processor.class, pluginConfigurationType = WithKeysProcessorConfig.class)
+public class UppercaseStringProcessor extends AbstractStringProcessor<String> {
+    @DataPrepperPluginConstructor
+    public UppercaseStringProcessor(final PluginMetrics pluginMetrics, final StringProcessorConfig<String> config) {
+        super(pluginMetrics, config);
+    }
+
+    @Override
+    protected String getKey(final String entry) {
+        return entry;
+    }
+
+    @Override
+    protected void performKeyAction(final Event recordEvent, final String entry, final String value)
+    {
+        recordEvent.put(entry, value.toUpperCase(Locale.ROOT));
+    }
+}
