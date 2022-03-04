8f226af3bfc9b0d1e3a60fe2dd7f4c8dd999baa9
Asif Sohail Mohammed <nsifmoh@amazon.com>
2022-03-04 16:51:47 -0600

    move initialization to constructor of Grok Mapper
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>

--- data-prepper-logstash-configuration/src/main/java/org/opensearch/dataprepper/logstash/mapping/GrokLogstashPluginAttributesMapper.java
+++ data-prepper-logstash-configuration/src/main/java/org/opensearch/dataprepper/logstash/mapping/GrokLogstashPluginAttributesMapper.java
@@ -21,6 +21,7 @@ class GrokLogstashPluginAttributesMapper extends AbstractLogstashPluginAttribute
     protected static final String LOGSTASH_GROK_MATCH_ATTRIBUTE_NAME = "match";
     protected static final String LOGSTASH_GROK_PATTERN_DEFINITIONS_ATTRIBUTE_NAME = "pattern_definitions";
     protected static final String LOGSTASH_GROK_OVERWRITE_ATTRIBUTE_NAME = "overwrite";
+    private static final Map<String, GrokLogstashPluginAttributeMapperHelper> HELPER_INSTANCES = new HashMap<>();
     private static  final HashSet<String> customAttributes = new HashSet<>(Arrays.asList(LOGSTASH_GROK_MATCH_ATTRIBUTE_NAME,
             LOGSTASH_GROK_PATTERN_DEFINITIONS_ATTRIBUTE_NAME,
             LOGSTASH_GROK_OVERWRITE_ATTRIBUTE_NAME));
@@ -29,24 +30,23 @@ class GrokLogstashPluginAttributesMapper extends AbstractLogstashPluginAttribute
         matchAttributesHelper = new GrokMatchAttributeHelper();
         patternDefinitionsAttributeHelper = new GrokPatternDefinitionsAttributeHelper();
         overwriteAttributeHelper = new GrokOverwriteAttributeHelper();
+
+        HELPER_INSTANCES.put(LOGSTASH_GROK_MATCH_ATTRIBUTE_NAME, matchAttributesHelper);
+        HELPER_INSTANCES.put(LOGSTASH_GROK_PATTERN_DEFINITIONS_ATTRIBUTE_NAME, patternDefinitionsAttributeHelper);
+        HELPER_INSTANCES.put(LOGSTASH_GROK_OVERWRITE_ATTRIBUTE_NAME, overwriteAttributeHelper);
     }
 
     @Override
     protected void mapCustomAttributes(List<LogstashAttribute> logstashAttributes, LogstashAttributesMappings logstashAttributesMappings, Map<String, Object> pluginSettings) {
-        final Map<String, GrokLogstashPluginAttributeMapperHelper> helperInstances = new HashMap<>();
-        helperInstances.put(LOGSTASH_GROK_MATCH_ATTRIBUTE_NAME, matchAttributesHelper);
-        helperInstances.put(LOGSTASH_GROK_PATTERN_DEFINITIONS_ATTRIBUTE_NAME, patternDefinitionsAttributeHelper);
-        helperInstances.put(LOGSTASH_GROK_OVERWRITE_ATTRIBUTE_NAME, overwriteAttributeHelper);
-
         logstashAttributes.forEach(logstashAttribute -> {
-            if (helperInstances.containsKey(logstashAttribute.getAttributeName()))
-                helperInstances.get(logstashAttribute.getAttributeName()).populateAttributes(logstashAttribute);
+            if (HELPER_INSTANCES.containsKey(logstashAttribute.getAttributeName()))
+                HELPER_INSTANCES.get(logstashAttribute.getAttributeName()).populateAttributes(logstashAttribute);
         });
 
         ((GrokPatternDefinitionsAttributeHelper) patternDefinitionsAttributeHelper).fixNamedCaptures(
                 ((GrokMatchAttributeHelper) matchAttributesHelper).mergeMatchAttributes());
 
-        helperInstances.values().forEach(attributeHelper ->
+        HELPER_INSTANCES.values().forEach(attributeHelper ->
                 attributeHelper.updatePluginSettings(logstashAttributesMappings, pluginSettings));
     }
 
