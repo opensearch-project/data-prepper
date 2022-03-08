5927cca8ce6774ad0d0ddb07290feab47e23899a
David Powers <ddpowers@amazon.com>
2022-03-08 12:51:18 -0600

    remove unused import
    Signed-off-by: David Powers <ddpowers@amazon.com>

--- data-prepper-plugins/mutate-string-processors/src/main/java/com/amazon/dataprepper/plugins/processor/mutatestring/WithKeysConfig.java
+++ data-prepper-plugins/mutate-string-processors/src/main/java/com/amazon/dataprepper/plugins/processor/mutatestring/WithKeysConfig.java
@@ -8,7 +8,6 @@ package com.amazon.dataprepper.plugins.processor.mutatestring;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jdk.nashorn.internal.ir.annotations.Ignore;
 
 import java.util.List;
 
