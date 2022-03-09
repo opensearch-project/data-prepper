bddf554c63fe2bf4613515281b8d470464be26e9
Qi Chen <19492223+chenqi0805@users.noreply.github.com>
2022-03-09 13:33:41 -0600

    Enhancement: rule index in operators and OperatorProvider (#1155)
    * ENH: add rule index in operators
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * ADD: OperatorProvider to encapsulate operator retrieval
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: address all PR comments
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: use more generic interface
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>

--- data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/GenericInSetOperator.java
+++ data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/GenericInSetOperator.java
@@ -5,6 +5,7 @@
 
 package org.opensearch.dataprepper.expression;
 
+import org.antlr.v4.runtime.RuleContext;
 import org.opensearch.dataprepper.expression.antlr.DataPrepperExpressionParser;
 
 import java.util.Set;
@@ -13,18 +14,23 @@ import java.util.function.BiPredicate;
 import static com.google.common.base.Preconditions.checkArgument;
 
 public class GenericInSetOperator implements Operator<Boolean> {
-    private final Integer symbol;
+    private final int symbol;
     private final String displayName;
     private final BiPredicate<Object, Object> operation;
 
-    public GenericInSetOperator(final Integer symbol, BiPredicate<Object, Object> operation) {
+    public GenericInSetOperator(final int symbol, BiPredicate<Object, Object> operation) {
         this.symbol = symbol;
         displayName = DataPrepperExpressionParser.VOCABULARY.getDisplayName(symbol);
         this.operation = operation;
     }
 
     @Override
-    public Integer getSymbol() {
+    public boolean shouldEvaluate(final RuleContext ctx) {
+        return ctx.getRuleIndex() == DataPrepperExpressionParser.RULE_setOperatorExpression;
+    }
+
+    @Override
+    public int getSymbol() {
         return symbol;
     }
 
--- data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/GenericRegexMatchOperator.java
+++ data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/GenericRegexMatchOperator.java
@@ -5,6 +5,7 @@
 
 package org.opensearch.dataprepper.expression;
 
+import org.antlr.v4.runtime.RuleContext;
 import org.opensearch.dataprepper.expression.antlr.DataPrepperExpressionParser;
 
 import java.util.function.BiPredicate;
@@ -13,18 +14,23 @@ import java.util.regex.PatternSyntaxException;
 import static com.google.common.base.Preconditions.checkArgument;
 
 public class GenericRegexMatchOperator implements Operator<Boolean> {
-    private final Integer symbol;
+    private final int symbol;
     private final String displayName;
     private final BiPredicate<Object, Object> operation;
 
-    public GenericRegexMatchOperator(final Integer symbol, BiPredicate<Object, Object> operation) {
+    public GenericRegexMatchOperator(final int symbol, BiPredicate<Object, Object> operation) {
         this.symbol = symbol;
         displayName = DataPrepperExpressionParser.VOCABULARY.getDisplayName(symbol);
         this.operation = operation;
     }
 
     @Override
-    public Integer getSymbol() {
+    public boolean shouldEvaluate(final RuleContext ctx) {
+        return ctx.getRuleIndex() == DataPrepperExpressionParser.RULE_regexOperatorExpression;
+    }
+
+    @Override
+    public int getSymbol() {
         return symbol;
     }
 
