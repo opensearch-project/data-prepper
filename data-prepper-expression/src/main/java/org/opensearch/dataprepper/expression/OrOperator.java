e41211cee082f5c9abafb26895a094d08ac3d439
Qi Chen <19492223+chenqi0805@users.noreply.github.com>
2022-03-09 13:33:41 -0600

    Enhancement: rule index in operators and OperatorProvider (#1155)
    * ENH: add rule index in operators
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * ADD: OperatorProvider to encapsulate operator retrieval
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: address all PR comments
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: use more generic interface
    
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

--- data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/OrOperator.java
+++ data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/OrOperator.java
@@ -5,6 +5,7 @@
 
 package org.opensearch.dataprepper.expression;
 
+import org.antlr.v4.runtime.RuleContext;
 import org.opensearch.dataprepper.expression.antlr.DataPrepperExpressionParser;
 
 import javax.inject.Named;
@@ -13,12 +14,17 @@ import static com.google.common.base.Preconditions.checkArgument;
 
 @Named
 class OrOperator implements Operator<Boolean> {
-    private static final Integer SYMBOL = DataPrepperExpressionParser.OR;
+    private static final int SYMBOL = DataPrepperExpressionParser.OR;
     private static final String DISPLAY_NAME = DataPrepperExpressionParser.VOCABULARY
             .getDisplayName(DataPrepperExpressionParser.OR);
 
     @Override
-    public Integer getSymbol() {
+    public boolean shouldEvaluate(final RuleContext ctx) {
+        return ctx.getRuleIndex() == DataPrepperExpressionParser.RULE_conditionalExpression;
+    }
+
+    @Override
+    public int getSymbol() {
         return SYMBOL;
     }
 
