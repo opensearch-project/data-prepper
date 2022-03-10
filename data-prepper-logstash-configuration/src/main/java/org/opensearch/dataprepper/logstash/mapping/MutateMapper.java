66c87cdbc0f32c81ff8a1123234a255047c036f0
David Powers <ddpowers@amazon.com>
2022-03-10 14:10:22 -0600

    Add processors to logstash converter
    Signed-off-by: David Powers <ddpowers@amazon.com>

--- data-prepper-logstash-configuration/src/main/java/org/opensearch/dataprepper/logstash/mapping/MutateMapper.java
+++ data-prepper-logstash-configuration/src/main/java/org/opensearch/dataprepper/logstash/mapping/MutateMapper.java
@@ -21,7 +21,7 @@ class MutateMapper implements LogstashPluginAttributesMapper {
         public final String key;
         public final Object value;
 
-        public AddEntryConfig(String key, Object value) {
+        public AddEntryConfig(final String key, final Object value) {
             this.key = key;
             this.value = value;
         }
@@ -31,21 +31,36 @@ class MutateMapper implements LogstashPluginAttributesMapper {
         public final String from_key;
         public final String to_key;
 
-        public RenameCopyConfig(String fromKey, String toKey) {
+        public RenameCopyConfig(final String fromKey, final String toKey) {
             this.from_key = fromKey;
             this.to_key = toKey;
         }
     }
 
-    public List<PluginModel> mapAttributes(List<LogstashAttribute> logstashAttributes, LogstashAttributesMappings logstashAttributesMappings) {
-        List<PluginModel> models = new LinkedList<>();
-        List<AddEntryConfig> adds = new LinkedList<>();
-        List<RenameCopyConfig> renames = new LinkedList<>();
-        List<String> deletes = new LinkedList<>();
-        List<RenameCopyConfig> copies = new LinkedList<>();
-        List<String> uppercases = new LinkedList<>();
+    public static class SubstituteConfig {
+        public final String source;
+        public final String from;
+        public final String to;
 
-        for(LogstashAttribute attr : logstashAttributes) {
+        public SubstituteConfig(final String source, final String from, final String to) {
+            this.source = source;
+            this.from = from;
+            this.to = to;
+        }
+    }
+
+    public List<PluginModel> mapAttributes(final List<LogstashAttribute> logstashAttributes, final LogstashAttributesMappings logstashAttributesMappings) {
+        final List<PluginModel> models = new LinkedList<>();
+        final List<AddEntryConfig> adds = new LinkedList<>();
+        final List<RenameCopyConfig> renames = new LinkedList<>();
+        final List<String> deletes = new LinkedList<>();
+        final List<RenameCopyConfig> copies = new LinkedList<>();
+        final List<String> uppercases = new LinkedList<>();
+        final List<String> lowercases = new LinkedList<>();
+        final List<String> trims = new LinkedList<>();
+        final List<SubstituteConfig> substitutes = new LinkedList<>();
+
+        for(final LogstashAttribute attr : logstashAttributes) {
             final String name = attr.getAttributeName();
             if(Objects.equals(name, "add_field")) {
                 ((Map<String, Object>) attr.getAttributeValue().getValue()).forEach(
@@ -64,52 +79,93 @@ class MutateMapper implements LogstashPluginAttributesMapper {
             } else if(Objects.equals(name, "uppercase")) {
                 uppercases.addAll(((ArrayList<String>) attr.getAttributeValue().getValue()).stream()
                         .map(NestedSyntaxConverter::convertNestedSyntaxToJsonPointer).collect(Collectors.toList()));
+            } else if(Objects.equals(name, "lowercase")) {
+                lowercases.addAll(((ArrayList<String>) attr.getAttributeValue().getValue()).stream()
+                        .map(NestedSyntaxConverter::convertNestedSyntaxToJsonPointer).collect(Collectors.toList()));
+            } else if(Objects.equals(name, "trim")) {
+                trims.addAll(((ArrayList<String>) attr.getAttributeValue().getValue()).stream()
+                        .map(NestedSyntaxConverter::convertNestedSyntaxToJsonPointer).collect(Collectors.toList()));
+            } else if(Objects.equals(name, "gsub")) {
+                final List<String> array = ((ArrayList<String>) attr.getAttributeValue().getValue()).stream()
+                        .map(NestedSyntaxConverter::convertNestedSyntaxToJsonPointer).collect(Collectors.toList());
+                for(int i = 0; i < array.size(); i += 0) {
+                    final SubstituteConfig newConfig = new SubstituteConfig(array.get(i), array.get(i + 1), array.get(i + 2));
+                    substitutes.add(newConfig);
+                    i += 3;
+                }
             }
         }
 
         if(!renames.isEmpty()) {
-            Map<String, Object> renameMap = new HashMap<>();
+            final Map<String, Object> renameMap = new HashMap<>();
             renameMap.put("entries", renames);
 
-            PluginModel renameModel = new PluginModel("rename_keys", renameMap);
+            final PluginModel renameModel = new PluginModel("rename_keys", renameMap);
 
             models.add(renameModel);
         }
 
         if(!copies.isEmpty()) {
-            Map<String, Object> copyMap = new HashMap<>();
+            final Map<String, Object> copyMap = new HashMap<>();
             copyMap.put("entries", copies);
 
-            PluginModel renameModel = new PluginModel("copy_values", copyMap);
+            final PluginModel renameModel = new PluginModel("copy_values", copyMap);
 
             models.add(renameModel);
         }
 
         if(!adds.isEmpty()) {
-            Map<String, Object> addMap = new HashMap<>();
+            final Map<String, Object> addMap = new HashMap<>();
             addMap.put("entries", adds);
 
-            PluginModel addModel = new PluginModel("add_entries", addMap);
+            final PluginModel addModel = new PluginModel("add_entries", addMap);
 
             models.add(addModel);
         }
 
         if(!deletes.isEmpty()) {
-            Map<String, Object> deleteMap = new HashMap<>();
+            final Map<String, Object> deleteMap = new HashMap<>();
             deleteMap.put("with_keys", deletes);
 
-            PluginModel deleteModel = new PluginModel("delete_entries", deleteMap);
+            final PluginModel deleteModel = new PluginModel("delete_entries", deleteMap);
 
             models.add(deleteModel);
         }
 
         if(!uppercases.isEmpty()) {
-            Map<String, Object> uppercaseMap = new HashMap<>();
+            final Map<String, Object> uppercaseMap = new HashMap<>();
             uppercaseMap.put("with_keys", uppercases);
 
-            PluginModel uppercaseModel = new PluginModel("uppercase_string", uppercaseMap);
+            final PluginModel uppercaseModel = new PluginModel("uppercase_string", uppercaseMap);
 
             models.add(uppercaseModel);
+        }
+
+        if(!lowercases.isEmpty()) {
+            final Map<String, Object> lowercaseMap = new HashMap<>();
+            lowercaseMap.put("with_keys", lowercases);
+
+            final PluginModel lowercaseModel = new PluginModel("lowercase_string", lowercaseMap);
+
+            models.add(lowercaseModel);
+        }
+
+        if(!trims.isEmpty()) {
+            final Map<String, Object> trimMap = new HashMap<>();
+            trimMap.put("with_keys", trims);
+
+            final PluginModel trimModel = new PluginModel("trim_string", trimMap);
+
+            models.add(trimModel);
+        }
+
+        if(!substitutes.isEmpty()) {
+            final Map<String, Object> substituteMap = new HashMap<>();
+            substituteMap.put("entries", substitutes);
+
+            final PluginModel substituteModel = new PluginModel("substitute_string", substituteMap);
+
+            models.add(substituteModel);
         }
 
         return models;
