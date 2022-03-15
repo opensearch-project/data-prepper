feb35c53f8091295e603e4b8a3623252dd636ae4
Steven Bayer <smbayer@amazon.com>
2022-03-15 12:20:35 -0500

    Added tar smoke test
    Signed-off-by: Steven Bayer <smbayer@amazon.com>

--- release/tar-smoke-tests/run-tar-smoke-test.sh
+++ release/tar-smoke-tests/run-tar-smoke-test.sh
@@ -1,9 +1,54 @@
 #
-# /*
-#  * Copyright OpenSearch Contributors
-#  * SPDX-License-Identifier: Apache-2.0
-#  */
+# Copyright OpenSearch Contributors
+# SPDX-License-Identifier: Apache-2.0
 #
 
-docker build -t data-prepper-tar:latest .
-docker run --rm data-prepper-tar:latest
+set -ex
+
+function copy_if_needed() {
+    local SOURCE_DIR=$1
+    local TARGET_DIR=$2
+    local FILE_NAME=$3
+
+    echo "Checking if file exists: ${TARGET_DIR}/${FILE_NAME}"
+
+    if [ -f "${TARGET_DIR}/${FILE_NAME}" ]; then
+        echo "File already exists, skipping copy"
+    else
+        echo "File missing, starting copy"
+        cp "${SOURCE_DIR}/${FILE_NAME}" "${TARGET_DIR}"
+    fi
+}
+
+export REPO_DIR
+REPO_DIR=$(pwd)
+
+export DOCKER_FILE_DIR="${REPO_DIR}/release/tar-smoke-tests"
+export FROM_IMAGE_NAME="openjdk"
+export FROM_IMAGE_TAG="14"
+
+export NAME="opensearch-data-prepper"
+#export NAME="opensearch-data-prepper-jdk"
+export DATA_PREPPER_VERSION="1.3.0-SNAPSHOT"
+export BUILD_NAME="${NAME}-${DATA_PREPPER_VERSION}-linux-x64"
+export TAR_FILE="${BUILD_NAME}.tar.gz"
+
+copy_if_needed "${REPO_DIR}/release/archives/linux/build/distributions" "${DOCKER_FILE_DIR}" "${TAR_FILE}"
+
+docker build \
+    --build-arg BUILD_NAME="${BUILD_NAME}" \
+    --build-arg DATA_PREPPER_VERSION="${DATA_PREPPER_VERSION}" \
+    --build-arg DOCKER_FILE_DIR="${DOCKER_FILE_DIR}" \
+    --build-arg FROM_IMAGE_NAME="${FROM_IMAGE_NAME}" \
+    --build-arg FROM_IMAGE_TAG="${FROM_IMAGE_TAG}" \
+    --build-arg TAR_FILE=${TAR_FILE} \
+    -t ${NAME}:${DATA_PREPPER_VERSION} \
+    "${DOCKER_FILE_DIR}"
+
+rm -f "${DOCKER_FILE_DIR}/${TAR_FILE}"
+
+echo "Build image ${NAME}:${DATA_PREPPER_VERSION}"
+echo "To run use \"docker run --rm -p 2021:2021 --name ${NAME} ${NAME}:${DATA_PREPPER_VERSION}\""
+echo "To run interactively use \"docker run -it --rm -p 2021:2021 --name ${NAME} ${NAME}:${DATA_PREPPER_VERSION} /bin/bash\""
+
+docker run --rm -p 2021:2021 --name ${NAME} ${NAME}:${DATA_PREPPER_VERSION}
