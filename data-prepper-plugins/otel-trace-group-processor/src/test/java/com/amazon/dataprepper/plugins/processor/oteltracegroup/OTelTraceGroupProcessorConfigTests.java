46b205a13178a95c3f48c5a3bd3cf27bafb14ea3
Qi Chen <19492223+chenqi0805@users.noreply.github.com>
2022-03-30 17:17:54 -0500

    Maintenance: add OTelTraceGroupProcessor from trace ingestion migration branch (#1224)
    * ADD: otel-trace-group-processor
    
    Signed-off-by: Qi Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: update header
    
    Signed-off-by: Qi Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: migrate to processor interface
    
    Signed-off-by: Qi Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: README and renaming plugin
    
    Signed-off-by: Qi Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * DOC: fix plugin names in README
    
    Signed-off-by: Qi Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * REF: normalizeDateTime
    
    Signed-off-by: Qi Chen <19492223+chenqi0805@users.noreply.github.com>

---
+++ data-prepper-plugins/otel-trace-group-processor/src/test/java/com/amazon/dataprepper/plugins/processor/oteltracegroup/OTelTraceGroupProcessorConfigTests.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.amazon.dataprepper.plugins.processor.oteltracegroup;
+
+import com.amazon.dataprepper.model.configuration.PluginSetting;
+import com.amazon.dataprepper.plugins.sink.opensearch.ConnectionConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+
+@RunWith(MockitoJUnitRunner.class)
+public class OTelTraceGroupProcessorConfigTests {
+
+    @Mock
+    private ConnectionConfiguration connectionConfigurationMock;
+
+    @Test
+    public void testInitialize() {
+        try (MockedStatic<ConnectionConfiguration> connectionConfigurationMockedStatic = Mockito.mockStatic(ConnectionConfiguration.class)) {
+            connectionConfigurationMockedStatic.when(() -> ConnectionConfiguration.readConnectionConfiguration(any(PluginSetting.class)))
+                    .thenReturn(connectionConfigurationMock);
+            PluginSetting testPluginSetting = new PluginSetting("otel_trace_group", new HashMap<>());
+            OTelTraceGroupProcessorConfig otelTraceGroupProcessorConfig = OTelTraceGroupProcessorConfig.buildConfig(testPluginSetting);
+            assertEquals(connectionConfigurationMock, otelTraceGroupProcessorConfig.getEsConnectionConfig());
+        }
+    }
+}
