d56c7ce19bac95e50476498a20995cc139e34e08
Qi Chen <19492223+chenqi0805@users.noreply.github.com>
2022-03-30 17:17:54 -0500

    Maintenance: add OTelTraceGroupProcessor from trace ingestion migration branch (#1224)
    * ADD: otel-trace-group-processor
    
    Signed-off-by: Qi Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: update header
    
    Signed-off-by: Qi Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: migrate to processor interface
    
    Signed-off-by: Qi Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * MAINT: README and renaming plugin
    
    Signed-off-by: Qi Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * DOC: fix plugin names in README
    
    Signed-off-by: Qi Chen <19492223+chenqi0805@users.noreply.github.com>
    
    * REF: normalizeDateTime
    
    Signed-off-by: Qi Chen <19492223+chenqi0805@users.noreply.github.com>
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

---
+++ data-prepper-plugins/otel-trace-group-processor/src/main/java/com/amazon/dataprepper/plugins/processor/oteltracegroup/OTelTraceGroupProcessorConfig.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.amazon.dataprepper.plugins.processor.oteltracegroup;
+
+import com.amazon.dataprepper.model.configuration.PluginSetting;
+import com.amazon.dataprepper.plugins.sink.opensearch.ConnectionConfiguration;
+import com.amazon.dataprepper.plugins.sink.opensearch.index.IndexConstants;
+import com.amazon.dataprepper.plugins.sink.opensearch.index.IndexType;
+
+public class OTelTraceGroupProcessorConfig {
+    protected static final String TRACE_ID_FIELD = "traceId";
+    protected static final String SPAN_ID_FIELD = "spanId";
+    protected static final String PARENT_SPAN_ID_FIELD = "parentSpanId";
+    protected static final String RAW_INDEX_ALIAS = IndexConstants.TYPE_TO_DEFAULT_ALIAS.get(IndexType.TRACE_ANALYTICS_RAW);
+    protected static final String STRICT_DATE_TIME = "strict_date_time";
+
+    private final ConnectionConfiguration esConnectionConfig;
+
+    public ConnectionConfiguration getEsConnectionConfig() {
+        return esConnectionConfig;
+    }
+
+    private OTelTraceGroupProcessorConfig(final ConnectionConfiguration esConnectionConfig) {
+        this.esConnectionConfig = esConnectionConfig;
+    }
+
+    public static OTelTraceGroupProcessorConfig buildConfig(final PluginSetting pluginSetting) {
+        final ConnectionConfiguration esConnectionConfig = ConnectionConfiguration.readConnectionConfiguration(pluginSetting);
+        return new OTelTraceGroupProcessorConfig(esConnectionConfig);
+    }
+}
