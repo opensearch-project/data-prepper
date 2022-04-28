3a374734359ea1cd829052366130f74c8375e740
David Venable <dlv@amazon.com>
2022-04-28 14:57:55 -0500

    Updated Mockito in the opensearch plugin. This fixes some incompatibilities with upcoming versions of OpenSearch. (#1339)
    Signed-off-by: David Venable <dlv@amazon.com>
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

--- data-prepper-plugins/opensearch/src/test/java/com/amazon/dataprepper/plugins/sink/opensearch/index/TraceAnalyticsServiceMapIndexManagerTests.java
+++ data-prepper-plugins/opensearch/src/test/java/com/amazon/dataprepper/plugins/sink/opensearch/index/TraceAnalyticsServiceMapIndexManagerTests.java
@@ -30,7 +30,7 @@ import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
