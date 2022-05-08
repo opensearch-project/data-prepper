8718bae636cf0909a74eddf348bbf54f0c7b64f3
Kai Sternad <kai.sternad@sap.com>
2022-05-08 13:10:30 +0200

    Fix OtelMetricsRawProcessorConfigTest
    Signed-off-by: Kai Sternad <kai.sternad@sap.com>
    Co-authored-by: Tomas Longo <tomas.longo@sap.com>
    Co-authored-by: Karsten Schnitter <k.schnitter@sap.com>

--- data-prepper-plugins/otel-metrics-raw-processor/src/test/java/org/opensearch/dataprepper/plugins/processor/otelmetrics/OtelMetricsRawProcessorConfigTest.java
+++ data-prepper-plugins/otel-metrics-raw-processor/src/test/java/org/opensearch/dataprepper/plugins/processor/otelmetrics/OtelMetricsRawProcessorConfigTest.java
@@ -17,8 +17,8 @@ public class OtelMetricsRawProcessorConfigTest {
     void testDefaultConfig() {
         final OtelMetricsRawProcessorConfig dateProcessorConfig = new OtelMetricsRawProcessorConfig();
 
-        assertThat(dateProcessorConfig.getCalculateExponentialHistogramBuckets(), equalTo(false));
-        assertThat(dateProcessorConfig.getCalculateHistogramBuckets(), equalTo(false));
+        assertThat(dateProcessorConfig.getCalculateExponentialHistogramBuckets(), equalTo(true));
+        assertThat(dateProcessorConfig.getCalculateHistogramBuckets(), equalTo(true));
         assertThat(dateProcessorConfig.getExponentialHistogramMaxAllowedScale(), equalTo(10));
     }
 
