f6583b8f0cf19f65879567f786c52d8f93186669
Qi Chen <19492223+chenqi0805@users.noreply.github.com>
2022-05-12 17:17:39 -0500

    TST: EMFMetricUtilsTest
    Signed-off-by: Qi Chen <19492223+chenqi0805@users.noreply.github.com>

---
+++ data-prepper-core/src/test/java/com/amazon/dataprepper/meter/EMFMetricUtilsTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.amazon.dataprepper.meter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class EMFMetricUtilsTest {
+    @Test
+    void testClampZero() {
+        final double testValue = -0;
+        assertThat(EMFMetricUtils.clampMetricValue(testValue), equalTo(Math.abs(testValue)));
+    }
+
+    @Test
+    void testClampMagnitudeWithinRange() {
+        final double testPositiveValue = 10.1;
+        final double testNegativeValue = -10.1;
+        assertThat(EMFMetricUtils.clampMetricValue(testPositiveValue), equalTo(testPositiveValue));
+        assertThat(EMFMetricUtils.clampMetricValue(testNegativeValue), equalTo(testNegativeValue));
+    }
+
+    @Test
+    void testClampMagnitudeOverFlow() {
+        assertThat(EMFMetricUtils.clampMetricValue(Double.POSITIVE_INFINITY), equalTo(EMFMetricUtils.MAXIMUM_ALLOWED_VALUE));
+        assertThat(EMFMetricUtils.clampMetricValue(Double.NEGATIVE_INFINITY), equalTo(-EMFMetricUtils.MAXIMUM_ALLOWED_VALUE));
+    }
+
+    @Test
+    void testClampMagnitudeTooSmall() {
+        assertThat(EMFMetricUtils.clampMetricValue(Double.MIN_VALUE), equalTo(EMFMetricUtils.MINIMUM_ALLOWED_VALUE));
+        assertThat(EMFMetricUtils.clampMetricValue(-Double.MIN_VALUE), equalTo(-EMFMetricUtils.MINIMUM_ALLOWED_VALUE));
+    }
+
+    @Test
+    void testClampNaN() {
+        assertThat(EMFMetricUtils.clampMetricValue(Double.NaN), equalTo(Double.NaN));
+    }
+}
