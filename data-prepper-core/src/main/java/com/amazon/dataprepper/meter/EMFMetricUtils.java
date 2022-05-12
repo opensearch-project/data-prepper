e740652f32fb41526a7e86f311bf401f6f7790ee
Qi Chen <19492223+chenqi0805@users.noreply.github.com>
2022-05-12 17:17:11 -0500

    MAINT: access modifier and style
    Signed-off-by: Qi Chen <19492223+chenqi0805@users.noreply.github.com>

--- data-prepper-core/src/main/java/com/amazon/dataprepper/meter/EMFMetricUtils.java
+++ data-prepper-core/src/main/java/com/amazon/dataprepper/meter/EMFMetricUtils.java
@@ -5,23 +5,23 @@
 
 package com.amazon.dataprepper.meter;
 
-public class EMFMetricUtils {
+class EMFMetricUtils {
 
-    private static final double MINIMUM_ALLOWED_VALUE = 8.515920e-109;
-    private static final double MAXIMUM_ALLOWED_VALUE = 1.174271e+108;
+    protected static final double MINIMUM_ALLOWED_VALUE = 8.515920e-109;
+    protected static final double MAXIMUM_ALLOWED_VALUE = 1.174271e+108;
 
-    static double clampMetricValue(double value) {
+    static double clampMetricValue(final double value) {
         // Leave as is and let the SDK reject it
         if (Double.isNaN(value)) {
             return value;
         }
-        double magnitude = Math.abs(value);
+        final double magnitude = Math.abs(value);
         if (magnitude == 0) {
             // Leave zero as zero
             return 0;
         }
         // Non-zero magnitude, clamp to allowed range
-        double clampedMag = Math.min(Math.max(magnitude, MINIMUM_ALLOWED_VALUE), MAXIMUM_ALLOWED_VALUE);
+        final double clampedMag = Math.min(Math.max(magnitude, MINIMUM_ALLOWED_VALUE), MAXIMUM_ALLOWED_VALUE);
         return Math.copySign(clampedMag, value);
     }
 }
