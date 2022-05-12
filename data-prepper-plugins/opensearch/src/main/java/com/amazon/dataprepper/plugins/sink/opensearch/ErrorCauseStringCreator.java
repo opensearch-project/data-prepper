d57315b1e70ca5afaec4dd9e37bad772ba1b4b2c
David Venable <dlv@amazon.com>
2022-05-12 09:15:51 -0500

    Migrated Data Prepper to use the opensearch-java client for bulk requests rather than the REST High Level Client. #1347
    Signed-off-by: David Venable <dlv@amazon.com>

---
+++ data-prepper-plugins/opensearch/src/main/java/com/amazon/dataprepper/plugins/sink/opensearch/ErrorCauseStringCreator.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.amazon.dataprepper.plugins.sink.opensearch;
+
+import org.opensearch.client.opensearch._types.ErrorCause;
+
+class ErrorCauseStringCreator {
+    static String toSingleLineDisplayString(ErrorCause errorCause) {
+        ErrorCause currentErrorCause = errorCause;
+
+        StringBuilder errorString = new StringBuilder();
+        while (currentErrorCause != null) {
+            final String reasonLine = currentErrorCause.reason() != null ? currentErrorCause.reason() : "unknown";
+            if (currentErrorCause != errorCause) {
+                errorString.append(" caused by ");
+            }
+            errorString.append(reasonLine);
+            currentErrorCause = currentErrorCause.causedBy();
+        }
+
+        return errorString.toString();
+    }
+}
