7cff3b0a31e03f2cce445c06a1ad690727748fd0
Asif Sohail Mohammed <nsifmoh@amazon.com>
2022-05-24 08:05:53 -0700

    Added aws authentication for s3-source
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>

--- data-prepper-plugins/s3-source/src/main/java/com/amazon/dataprepper/plugins/source/configuration/AWSAuthenticationOptions.java
+++ data-prepper-plugins/s3-source/src/main/java/com/amazon/dataprepper/plugins/source/configuration/AWSAuthenticationOptions.java
@@ -7,6 +7,16 @@ package com.amazon.dataprepper.plugins.source.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import software.amazon.awssdk.arns.Arn;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider;
+import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
+
+import java.util.UUID;
+
+import static com.google.common.base.Preconditions.checkArgument;
 
 public class AWSAuthenticationOptions {
     @JsonProperty("aws_region")
@@ -22,6 +32,34 @@ public class AWSAuthenticationOptions {
 
     public String getAwsStsRoleArn() {
         return awsStsRoleArn;
+    }
+
+    public AwsCredentialsProvider authenticateAWSConfiguration(final StsClient stsClient) {
+
+        AwsCredentialsProvider awsCredentialsProvider;
+        if (awsStsRoleArn != null && !awsStsRoleArn.isEmpty()) {
+            checkArgument(awsStsRoleArn.length() <= 2048, "awsStsRoleArn length cannot exceed 2048");
+            try {
+                Arn.fromString(awsStsRoleArn);
+            } catch (Exception e) {
+                throw new IllegalArgumentException("Invalid ARN format for awsStsRoleArn");
+            }
+
+            awsCredentialsProvider = StsAssumeRoleCredentialsProvider.builder()
+                    .stsClient(stsClient)
+                    .refreshRequest(AssumeRoleRequest.builder()
+                            .roleSessionName("S3-Source-" + UUID.randomUUID())
+                            .roleArn(awsStsRoleArn)
+                            .build())
+                    .build();
+
+        }
+        else {
+            // use default credential provider
+            awsCredentialsProvider = DefaultCredentialsProvider.create();
+        }
+
+        return awsCredentialsProvider;
     }
 }
 
