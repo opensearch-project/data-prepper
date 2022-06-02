40a581d8bac91ca1fd9ed35575fb803bbc446a01
Asif Sohail Mohammed <nsifmoh@amazon.com>
2022-06-02 14:07:07 -0500

    Added separate classes for sqs and s3
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>

--- data-prepper-plugins/s3-source/src/test/java/com/amazon/dataprepper/plugins/source/S3ServiceTest.java
+++ data-prepper-plugins/s3-source/src/test/java/com/amazon/dataprepper/plugins/source/S3ServiceTest.java
@@ -8,7 +8,7 @@ package com.amazon.dataprepper.plugins.source;
 import com.amazon.dataprepper.plugins.source.configuration.AwsAuthenticationOptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.sts.StsClient;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -17,30 +17,28 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class SqsClientAuthenticationTest {
+class S3ServiceTest {
 
     S3SourceConfig s3SourceConfig;
-    SqsClientAuthentication sqsClientAuthentication;
+    S3Service s3Service;
     AwsAuthenticationOptions awsAuthenticationOptions;
     StsClient stsClient = mock(StsClient.class);
 
     @BeforeEach
     void setUp() {
         s3SourceConfig = mock(S3SourceConfig.class);
-
         awsAuthenticationOptions = mock(AwsAuthenticationOptions.class);
 
-        when(s3SourceConfig.getAWSAuthentication()).thenReturn(awsAuthenticationOptions);
+        when(s3SourceConfig.getAWSAuthenticationOptions()).thenReturn(awsAuthenticationOptions);
         when(awsAuthenticationOptions.getAwsRegion()).thenReturn("us-east-1");
-        sqsClientAuthentication = new SqsClientAuthentication(s3SourceConfig);
+
+        s3Service = new S3Service(s3SourceConfig);
     }
 
     @Test
-    void createS3Client() {
-        when(s3SourceConfig.getAWSAuthentication()).thenReturn(mock(AwsAuthenticationOptions.class));
-        when(s3SourceConfig.getAWSAuthentication().getAwsRegion()).thenReturn("us-east-1");
-
-        assertThat(s3SourceConfig.getAWSAuthentication().getAwsRegion(), equalTo("us-east-1"));
-        assertThat(sqsClientAuthentication.createSqsClient(stsClient), instanceOf(SqsClient.class));
+    void createS3Client_should_return_instance_of_S3Client() {
+        assertThat(s3SourceConfig.getAWSAuthenticationOptions().getAwsRegion(), equalTo("us-east-1"));
+        assertThat(s3Service.createS3Client(stsClient), instanceOf(S3Client.class));
     }
+
 }
