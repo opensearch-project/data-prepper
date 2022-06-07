8d1454c110ae7ead1e6e26d2d46efc05c7e10393
Asif Sohail Mohammed <nsifmoh@amazon.com>
2022-06-07 15:37:09 -0500

    Added SQS interactions for S3 source (#1431)
    * Added sqs configuration and basic sqs interactions
    
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>

--- data-prepper-plugins/s3-source/src/main/java/com/amazon/dataprepper/plugins/source/S3SourceConfig.java
+++ data-prepper-plugins/s3-source/src/main/java/com/amazon/dataprepper/plugins/source/S3SourceConfig.java
@@ -5,12 +5,14 @@
 
 package com.amazon.dataprepper.plugins.source;
 
-import com.amazon.dataprepper.plugins.source.configuration.AwsAuthenticationOptions;
-import com.amazon.dataprepper.plugins.source.configuration.CodecOption;
-import com.amazon.dataprepper.plugins.source.configuration.CompressionOption;
 import com.amazon.dataprepper.plugins.source.configuration.NotificationTypeOption;
+import com.amazon.dataprepper.plugins.source.configuration.CompressionOption;
+import com.amazon.dataprepper.plugins.source.configuration.CodecOption;
+import com.amazon.dataprepper.plugins.source.configuration.SqsOptions;
+import com.amazon.dataprepper.plugins.source.configuration.AwsAuthenticationOptions;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public class S3SourceConfig {
@@ -27,10 +29,18 @@ public class S3SourceConfig {
     @Valid
     private CodecOption codec;
 
+    @JsonProperty("sqs")
+    @NotNull
+    private SqsOptions sqsOptions;
+
     @JsonProperty("aws")
     @NotNull
     @Valid
-    private AwsAuthenticationOptions awsAuthentication;
+    private AwsAuthenticationOptions awsAuthenticationOptions;
+
+    @JsonProperty("thread_count")
+    @Min(0)
+    private int threadCount;
 
     public NotificationTypeOption getNotificationType() {
         return notificationType;
@@ -44,7 +54,15 @@ public class S3SourceConfig {
         return codec;
     }
 
-    public AwsAuthenticationOptions getAWSAuthentication() {
-        return awsAuthentication;
+    public SqsOptions getSqsOptions() {
+        return sqsOptions;
+    }
+
+    public AwsAuthenticationOptions getAWSAuthenticationOptions() {
+        return awsAuthenticationOptions;
+    }
+
+    public int getThreadCount() {
+        return threadCount;
     }
 }
---
+++ data-prepper-plugins/s3-source/src/test/java/com/amazon/dataprepper/plugins/source/SqsWorkerTest.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.amazon.dataprepper.plugins.source;
+
+import com.amazon.dataprepper.plugins.source.configuration.AwsAuthenticationOptions;
+import com.amazon.dataprepper.plugins.source.configuration.SqsOptions;
+import com.amazon.dataprepper.plugins.source.filter.ObjectCreatedFilter;
+import com.amazon.dataprepper.plugins.source.filter.S3EventFilter;
+import com.amazonaws.services.s3.event.S3EventNotification;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class SqsWorkerTest {
+    private SqsWorker sqsWorker;
+    private SqsClient sqsClient;
+    private S3Service s3Service;
+    private S3SourceConfig s3SourceConfig;
+    private S3EventFilter objectCreatedFilter;
+
+    @BeforeEach
+    void setUp() {
+        sqsClient = mock(SqsClient.class);
+        s3Service = mock(S3Service.class);
+        s3SourceConfig = mock(S3SourceConfig.class);
+        objectCreatedFilter = new ObjectCreatedFilter();
+
+        AwsAuthenticationOptions awsAuthenticationOptions = mock(AwsAuthenticationOptions.class);
+        when(awsAuthenticationOptions.getAwsRegion()).thenReturn("us-east-1");
+        when(awsAuthenticationOptions.getAwsStsRoleArn()).thenReturn("arn:aws:iam::123456789012:iam-role");
+
+        SqsOptions sqsOptions = mock(SqsOptions.class);
+        when(sqsOptions.getSqsUrl()).thenReturn("https://sqs.us-east-2.amazonaws.com/123456789012/MyQueue");
+
+        when(s3SourceConfig.getAWSAuthenticationOptions()).thenReturn(awsAuthenticationOptions);
+        when(s3SourceConfig.getSqsOptions()).thenReturn(sqsOptions);
+
+        sqsWorker = new SqsWorker(sqsClient, s3Service, s3SourceConfig);
+    }
+
+    @Test
+    void getMessagesFromSqs_throws_null_pointer_exception_with_dummy_queue_url() {
+        assertThrows(NullPointerException.class, () -> sqsWorker.getMessagesFromSqs());
+    }
+
+    @Test
+    void createReceiveMessageRequest_should_return_ReceiveMessageRequest() {
+        assertThat(sqsWorker.createReceiveMessageRequest(), instanceOf(ReceiveMessageRequest.class));
+    }
+
+    @Test
+    void convertS3EventMessages_convert_message_to_S3EventNotificationRecord() {
+        Message message = mock(Message.class);
+        when(message.body()).thenReturn("{\"Records\":[{\"eventVersion\":\"2.1\",\"eventSource\":\"aws:s3\",\"awsRegion\":\"us-east-1\"," +
+                "\"eventTime\":\"2022-06-06T18:02:33.495Z\",\"eventName\":\"ObjectCreated:Put\",\"userIdentity\":{\"principalId\":\"AWS:AROAX:xxxxxx\"}," +
+                "\"requestParameters\":{\"sourceIPAddress\":\"99.99.999.99\"},\"responseElements\":{\"x-amz-request-id\":\"ABCD\"," +
+                "\"x-amz-id-2\":\"abcd\"},\"s3\":{\"s3SchemaVersion\":\"1.0\",\"configurationId\":\"s3SourceEventNotification\"," +
+                "\"bucket\":{\"name\":\"bucketName\",\"ownerIdentity\":{\"principalId\":\"ID\"},\"arn\":\"arn:aws:s3:::bucketName\"}," +
+                "\"object\":{\"key\":\"File.gz\",\"size\":72,\"eTag\":\"abcd\",\"sequencer\":\"ABCD\"}}}]}");
+        S3EventNotification.S3EventNotificationRecord actualS3EventNotificationRecord = sqsWorker.convertS3EventMessages(message);
+        assertThat(actualS3EventNotificationRecord, instanceOf(S3EventNotification.S3EventNotificationRecord.class));
+        assertThat(actualS3EventNotificationRecord.getAwsRegion(), equalTo("us-east-1"));
+        assertThat(actualS3EventNotificationRecord.getS3().getBucket().getName(), equalTo("bucketName"));
+        assertThat(actualS3EventNotificationRecord.getS3().getObject().getKey(), equalTo("File.gz"));
+    }
+
+    @Test
+    void isEventNameCreated_should_return_true_if_event_is_created() {
+        S3EventNotification.S3EventNotificationRecord s3EventNotificationRecord = mock(S3EventNotification.S3EventNotificationRecord.class);
+        when(s3EventNotificationRecord.getEventName()).thenReturn("ObjectCreated");
+        Assertions.assertTrue(sqsWorker.isEventNameCreated(s3EventNotificationRecord));
+    }
+
+    @Test
+    void isEventNameCreated_should_return_false_if_event_is_not_created() {
+        S3EventNotification.S3EventNotificationRecord s3EventNotificationRecord = mock(S3EventNotification.S3EventNotificationRecord.class);
+        when(s3EventNotificationRecord.getEventName()).thenReturn("ObjectRemoved");
+        Assertions.assertFalse(sqsWorker.isEventNameCreated(s3EventNotificationRecord));
+    }
+
+    @Test
+    void populateS3Reference_should_return_instance_of_S3ObjectReference() {
+        S3EventNotification.S3EventNotificationRecord s3EventNotificationRecord = mock(S3EventNotification.S3EventNotificationRecord.class);
+        S3EventNotification.S3Entity s3Entity = mock(S3EventNotification.S3Entity.class);
+        S3EventNotification.S3BucketEntity s3BucketEntity = mock(S3EventNotification.S3BucketEntity.class);
+        S3EventNotification.S3ObjectEntity s3ObjectEntity = mock(S3EventNotification.S3ObjectEntity.class);
+
+        when(s3EventNotificationRecord.getS3()).thenReturn(s3Entity);
+        when(s3Entity.getBucket()).thenReturn(s3BucketEntity);
+        when(s3Entity.getObject()).thenReturn(s3ObjectEntity);
+
+        when(s3EventNotificationRecord.getS3().getBucket().getName()).thenReturn("s3-source-test-bucket");
+        when(s3EventNotificationRecord.getS3().getObject().getKey()).thenReturn("s3-bucket-key");
+
+        S3ObjectReference s3ObjectReference = sqsWorker.populateS3Reference(s3EventNotificationRecord);
+
+        assertThat(s3ObjectReference, instanceOf(S3ObjectReference.class));
+        assertThat(s3ObjectReference.getBucketName(), equalTo("s3-source-test-bucket"));
+        assertThat(s3ObjectReference.getKey(), equalTo("s3-bucket-key"));
+    }
+}
