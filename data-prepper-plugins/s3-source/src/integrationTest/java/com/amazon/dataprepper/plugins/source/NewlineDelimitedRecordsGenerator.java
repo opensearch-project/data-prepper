9359082de044c9c021035a92d7281675dbe4ba4b
David Venable <dlv@amazon.com>
2022-06-14 15:20:00 -0500

    Updated the S3 source integration test to include JSON. This involved some refactoring to place some logic into the RecordsGenerator interface and implementations. (#1498)
    Signed-off-by: David Venable <dlv@amazon.com>
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

--- data-prepper-plugins/s3-source/src/integrationTest/java/com/amazon/dataprepper/plugins/source/NewlineDelimitedRecordsGenerator.java
+++ data-prepper-plugins/s3-source/src/integrationTest/java/com/amazon/dataprepper/plugins/source/NewlineDelimitedRecordsGenerator.java
@@ -5,22 +5,32 @@
 
 package com.amazon.dataprepper.plugins.source;
 
-import java.io.IOException;
+import com.amazon.dataprepper.model.event.Event;
+import com.amazon.dataprepper.plugins.source.codec.Codec;
+import com.amazon.dataprepper.plugins.source.codec.NewlineDelimitedCodec;
+import com.amazon.dataprepper.plugins.source.codec.NewlineDelimitedConfig;
+
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Generates records where each record is on a single line.
  */
-public class NewlineDelimitedRecordsGenerator implements RecordsGenerator {
+class NewlineDelimitedRecordsGenerator implements RecordsGenerator {
+    private final String KNOWN_HTTP_LINE = "GET /my/endpoint HTTP/1.1 200";
     private final Random random = new Random();
     private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MMM/yyyy:hh:mm:ss");
 
     @Override
-    public void write(final int numberOfRecords, final OutputStream outputStream) throws IOException {
+    public void write(final int numberOfRecords, final OutputStream outputStream) {
         try (final PrintWriter printWriter = new PrintWriter(outputStream)) {
             for (int i = 0; i < numberOfRecords; i++) {
                 writeLine(printWriter);
@@ -28,9 +38,32 @@ public class NewlineDelimitedRecordsGenerator implements RecordsGenerator {
         }
     }
 
+    @Override
+    public Codec getCodec() {
+        return new NewlineDelimitedCodec(new NewlineDelimitedConfig());
+    }
+
+    @Override
+    public String getFileExtension() {
+        return "txt";
+    }
+
+    @Override
+    public void assertEventIsCorrect(final Event event) {
+        final String message = event.get("message", String.class);
+        assertThat(event.toMap().size(), equalTo(1));
+        assertThat(message, notNullValue());
+        assertThat(message, containsString(KNOWN_HTTP_LINE));
+    }
+
     private void writeLine(final PrintWriter printWriter) {
         final String dateString = dateTimeFormatter.format(LocalDateTime.now());
-        final String line = "127.0.0.1 - - [" + dateString + "] GET /my/endpoint HTTP/1.1 200 " + random.nextInt(3000) + " \"http://localhost\" \"Mozilla/5.0\"";
+        final String line = "127.0.0.1 - - [" + dateString + "] " + KNOWN_HTTP_LINE + " " + random.nextInt(3000) + " \"http://localhost\" \"Mozilla/5.0\"";
         printWriter.write(line + "\n");
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getSimpleName();
     }
 }
