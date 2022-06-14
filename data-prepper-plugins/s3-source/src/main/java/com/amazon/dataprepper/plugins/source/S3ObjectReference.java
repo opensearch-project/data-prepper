712f5c8a66e60ebf594f4200b8a735e5a01b2726
David Venable <dlv@amazon.com>
2022-06-14 15:37:19 -0500

    When the bucket owner is available, then use the x-amz-expected-bucket-owner header on S3 GetObject requests to ensure that the bucket is owned by that expected owner. Resolves #1463
    Signed-off-by: David Venable <dlv@amazon.com>

--- data-prepper-plugins/s3-source/src/main/java/com/amazon/dataprepper/plugins/source/S3ObjectReference.java
+++ data-prepper-plugins/s3-source/src/main/java/com/amazon/dataprepper/plugins/source/S3ObjectReference.java
@@ -6,6 +6,7 @@
 package com.amazon.dataprepper.plugins.source;
 
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * Reference to an S3 object.
@@ -13,17 +14,18 @@ import java.util.Objects;
 class S3ObjectReference {
     private final String bucketName;
     private final String key;
+    private final String owner;
 
-    private S3ObjectReference(final String bucketName, final String key) {
-        Objects.requireNonNull(bucketName, "bucketName must be non null");
-        Objects.requireNonNull(key, "key must be non null");
-
+    private S3ObjectReference(final String bucketName, final String key, final String owner) {
         this.bucketName = bucketName;
         this.key = key;
+        this.owner = owner;
     }
 
-    static S3ObjectReference fromBucketAndKey(final String bucketName, final String key) {
-        return new S3ObjectReference(bucketName, key);
+    static Builder bucketAndKey(final String bucketName, final String key) {
+        Objects.requireNonNull(bucketName, "bucketName must be non null");
+        Objects.requireNonNull(key, "key must be non null");
+        return new Builder(bucketName, key);
     }
 
     String getBucketName() {
@@ -37,5 +39,30 @@ class S3ObjectReference {
     @Override
     public String toString() {
         return "[bucketName=" + bucketName + ", key=" + key + "]";
+    }
+
+    public Optional<String> getBucketOwner() {
+        return Optional.ofNullable(owner);
+    }
+
+    public static final class Builder {
+
+        private final String bucketName;
+        private final String key;
+        private String owner;
+
+        public Builder(final String bucketName, final String key) {
+            this.bucketName = bucketName;
+            this.key = key;
+        }
+
+        Builder owner(final String owner) {
+            this.owner = owner;
+            return this;
+        }
+
+        S3ObjectReference build() {
+            return new S3ObjectReference(bucketName, key, owner);
+        }
     }
 }
