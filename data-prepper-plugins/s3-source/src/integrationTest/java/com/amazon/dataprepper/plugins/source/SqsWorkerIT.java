ed1e60b4821a7d834064addeaa84c5533fb01391
Asif Sohail Mohammed <nsifmoh@amazon.com>
2022-06-17 17:31:26 -0500

    PR feedback
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>

--- data-prepper-plugins/s3-source/src/integrationTest/java/com/amazon/dataprepper/plugins/source/SqsWorkerIT.java
+++ data-prepper-plugins/s3-source/src/integrationTest/java/com/amazon/dataprepper/plugins/source/SqsWorkerIT.java
@@ -5,11 +5,6 @@
 
 package com.amazon.dataprepper.plugins.source;
 
-import com.amazon.dataprepper.metrics.PluginMetrics;
-import com.amazon.dataprepper.model.buffer.Buffer;
-import com.amazon.dataprepper.model.event.Event;
-import com.amazon.dataprepper.model.record.Record;
-import com.amazon.dataprepper.plugins.source.codec.Codec;
 import com.amazon.dataprepper.plugins.source.configuration.OnErrorOption;
 import com.amazon.dataprepper.plugins.source.configuration.SqsOptions;
 import org.junit.jupiter.api.AfterEach;
@@ -17,6 +12,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.ArgumentCaptor;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.sqs.SqsClient;
@@ -24,42 +20,38 @@ import software.amazon.awssdk.services.sqs.SqsClient;
 import java.io.IOException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.UUID;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.hamcrest.core.StringStartsWith.startsWith;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class SqsWorkerIT {
-    private S3Client s3Client;
     private SqsClient sqsClient;
     private S3Service s3Service;
-    private Buffer<Record<Event>> buffer;
     private S3SourceConfig s3SourceConfig;
     private S3ObjectGenerator s3ObjectGenerator;
     private String bucket;
-    private Codec codec;
-    private PluginMetrics pluginMetrics;
 
     @BeforeEach
     void setUp() {
-        s3Client = S3Client.builder()
+        final S3Client s3Client = S3Client.builder()
                 .region(Region.of(System.getProperty("tests.s3source.region")))
                 .build();
         bucket = System.getProperty("tests.s3source.bucket");
         s3ObjectGenerator = new S3ObjectGenerator(s3Client, bucket);
-
 
         sqsClient = SqsClient.builder()
                 .region(Region.of(System.getProperty("tests.s3source.region")))
                 .build();
 
         s3SourceConfig = mock(S3SourceConfig.class);
-        buffer = mock(Buffer.class);
-        codec = mock(Codec.class);
-        pluginMetrics = mock(PluginMetrics.class);
         s3Service = mock(S3Service.class);
 
         final SqsOptions sqsOptions = mock(SqsOptions.class);
@@ -91,16 +83,21 @@ public class SqsWorkerIT {
         writeToS3(numberOfObjectsToWrite);
 
         final SqsWorker objectUnderTest = createObjectUnderTest();
-        int sqsMessagesProcessed = objectUnderTest.processSqsMessages();
+        final int sqsMessagesProcessed = objectUnderTest.processSqsMessages();
 
+        final ArgumentCaptor<S3ObjectReference> s3ObjectReferenceArgumentCaptor = ArgumentCaptor.forClass(S3ObjectReference.class);
+        verify(s3Service, atLeastOnce()).addS3Object(s3ObjectReferenceArgumentCaptor.capture());
+
+        assertThat(s3ObjectReferenceArgumentCaptor.getValue().getBucketName(), equalTo(bucket));
+        assertThat(s3ObjectReferenceArgumentCaptor.getValue().getKey(), startsWith("s3source/sqs/"));
         assertThat(sqsMessagesProcessed, greaterThanOrEqualTo(1));
         assertThat(sqsMessagesProcessed, lessThanOrEqualTo(numberOfObjectsToWrite));
     }
 
     @Test
     void processSqsMessages_should_return_zero_if_no_objects_are_written() {
-        SqsWorker objectUnderTest = createObjectUnderTest();
-        int sqsMessagesProcessed = objectUnderTest.processSqsMessages();
+        final SqsWorker objectUnderTest = createObjectUnderTest();
+        final int sqsMessagesProcessed = objectUnderTest.processSqsMessages();
 
         assertThat(sqsMessagesProcessed, equalTo(0));
     }
@@ -109,7 +106,7 @@ public class SqsWorkerIT {
         final int numberOfRecords = 100;
         final NewlineDelimitedRecordsGenerator newlineDelimitedRecordsGenerator = new NewlineDelimitedRecordsGenerator();
         for (int i = 0; i < numberOfObjectsToWrite; i++) {
-            final String key = "s3source/s3/" + numberOfRecords + "_" + Instant.now().toString() + newlineDelimitedRecordsGenerator.getFileExtension();
+            final String key = "s3source/sqs/" + UUID.randomUUID() + "_" + Instant.now().toString() + newlineDelimitedRecordsGenerator.getFileExtension();
             s3ObjectGenerator.write(numberOfRecords, key, newlineDelimitedRecordsGenerator);
         }
     }
