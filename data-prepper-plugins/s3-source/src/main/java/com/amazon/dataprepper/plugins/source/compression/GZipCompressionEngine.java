a51f6deab3351b9f8caa094b04744c3050ead24e
Asif Sohail Mohammed <nsifmoh@amazon.com>
2022-07-07 19:21:02 -0500

    Fix: Updated GZipCompressionEngine to use GzipCompressorInputStream (#1570)
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>
    (cherry picked from commit bde1b52ea0b147fefc279b054a509c521bdf3ae7)

--- data-prepper-plugins/s3-source/src/main/java/com/amazon/dataprepper/plugins/source/compression/GZipCompressionEngine.java
+++ data-prepper-plugins/s3-source/src/main/java/com/amazon/dataprepper/plugins/source/compression/GZipCompressionEngine.java
@@ -5,13 +5,17 @@
 
 package com.amazon.dataprepper.plugins.source.compression;
 
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.zip.GZIPInputStream;
 
 public class GZipCompressionEngine implements CompressionEngine {
     @Override
     public InputStream createInputStream(final String s3Key, final InputStream responseInputStream) throws IOException {
-        return new GZIPInputStream(responseInputStream);
+        // We are using GzipCompressorInputStream here to decompress because GZIPInputStream doesn't decompress concatenated .gz files
+        // it stops after the first member and silently ignores the rest.
+        // It doesn't leave the read position to point to the beginning of the next member.
+        return new GzipCompressorInputStream(responseInputStream, true);
     }
 }
