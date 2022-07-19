bbc9efbd1ba50bb3f0d14e0f383c111c80d7afc0
Asif Sohail Mohammed <nsifmoh@amazon.com>
2022-07-18 21:07:17 -0500

    Added Core Peer Forwarder decorator and interface (#1592)
    * Added Core Peer Forwarder decorator and interface
    
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>

---
+++ data-prepper-core/src/main/java/com/amazon/dataprepper/peerforwarder/PeerForwardingProcessorDecorator.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.amazon.dataprepper.peerforwarder;
+
+import com.amazon.dataprepper.model.event.Event;
+import com.amazon.dataprepper.model.processor.Processor;
+import com.amazon.dataprepper.model.record.Record;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+
+public class PeerForwardingProcessorDecorator implements Processor<Record<Event>, Record<Event>> {
+    private static final Logger LOG = LoggerFactory.getLogger(PeerForwardingProcessorDecorator.class);
+
+
+    @Override
+    public Collection<Record<Event>> execute(final Collection<Record<Event>> collection) {
+        LOG.info("Peer Forwarder not implemented yet, skipping events for now.");
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void prepareForShutdown() {
+
+    }
+
+    @Override
+    public boolean isReadyForShutdown() {
+        return false;
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+}
