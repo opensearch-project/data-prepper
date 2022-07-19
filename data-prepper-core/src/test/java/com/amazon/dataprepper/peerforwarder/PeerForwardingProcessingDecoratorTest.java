385eb6c78dff6146cab30a84104e96a582718489
Asif Sohail Mohammed <nsifmoh@amazon.com>
2022-07-18 21:07:17 -0500

    Added Core Peer Forwarder decorator and interface (#1592)
    * Added Core Peer Forwarder decorator and interface
    
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

---
+++ data-prepper-core/src/test/java/com/amazon/dataprepper/peerforwarder/PeerForwardingProcessingDecoratorTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.amazon.dataprepper.peerforwarder;
+
+import com.amazon.dataprepper.model.event.Event;
+import com.amazon.dataprepper.model.record.Record;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+
+class PeerForwardingProcessingDecoratorTest {
+    private Record<Event> record;
+
+    @BeforeEach
+    void setUp() {
+        record = mock(Record.class);
+    }
+
+    private PeerForwardingProcessorDecorator createObjectUnderTest() {
+        return new PeerForwardingProcessorDecorator();
+    }
+
+    @Test
+    void PeerForwardingProcessingDecorator_will_throw_NotImplemented_exception() {
+        PeerForwardingProcessorDecorator objectUnderTest = createObjectUnderTest();
+        Collection<Record<Event>> testData = Collections.singletonList(record);
+        assertThrows(UnsupportedOperationException.class, () -> objectUnderTest.execute(testData));
+    }
+
+}
