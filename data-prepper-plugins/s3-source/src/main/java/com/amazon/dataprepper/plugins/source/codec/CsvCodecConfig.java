257a718e2b0eb9a16c69dfcb0a3bb1c6cf4dff69
Finn <67562851+finnroblin@users.noreply.github.com>
2022-08-09 13:40:28 -0700

    Implemented CSVCodec for S3 Source, config & unit tests (#1644)
    * Implemented CSVCodec for S3 Source, config & unit tests
    
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>
    
    * Addressed Travis's feedback
    
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>
    
    * Addressed David's feedback & fixed NPE if header is null
    
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>
    
    * Renamed all instances of CSVProcessor to CsvProcessor (and offshoots like CsvProcessorConfig)
    
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>
    Signed-off-by: Arunachalam Lakshmanan <arunachalam.l@gmail.com>

---
+++ data-prepper-plugins/s3-source/src/main/java/com/amazon/dataprepper/plugins/source/codec/CsvCodecConfig.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.amazon.dataprepper.plugins.source.codec;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.AssertTrue;
+
+import java.util.List;
+import java.util.Objects;
+
+public class CsvCodecConfig {
+    static final String DEFAULT_DELIMITER = ",";
+    static final String DEFAULT_QUOTE_CHARACTER = "\""; // double quote
+    static final Boolean DEFAULT_DETECT_HEADER = true;
+
+    @JsonProperty("delimiter")
+    private String delimiter = DEFAULT_DELIMITER;
+
+    @JsonProperty("quote_character")
+    private String quoteCharacter = DEFAULT_QUOTE_CHARACTER;
+
+    @JsonProperty("header")
+    private List<String> header;
+
+    @JsonProperty("detect_header")
+    private Boolean detectHeader = DEFAULT_DETECT_HEADER;
+
+    public String getDelimiter() {
+        return delimiter;
+    }
+
+    public String getQuoteCharacter() {
+        return quoteCharacter;
+    }
+
+    public List<String> getHeader() {
+        return header;
+    }
+
+    public Boolean isDetectHeader() {
+        return detectHeader;
+    }
+
+    @AssertTrue(message = "delimiter must be exactly one character.")
+    boolean isValidDelimiter() {
+        return delimiter.length() == 1;
+    }
+
+    @AssertTrue(message = "quote_character must be exactly one character.")
+    boolean isValidQuoteCharacter() {
+        return quoteCharacter.length() == 1;
+    }
+
+    @AssertTrue(message = "quote_character and delimiter cannot be the same character.")
+    boolean areDelimiterAndQuoteCharacterDifferent() {
+        return !(delimiter.equals(quoteCharacter));
+    }
+
+    @AssertTrue(message = "header must not be an empty list. To autogenerate columns, set detect_header: false and delete header " +
+            "from config.")
+    boolean isValidHeader() {
+        return Objects.isNull(header) || header.size() > 0;
+    }
+}
