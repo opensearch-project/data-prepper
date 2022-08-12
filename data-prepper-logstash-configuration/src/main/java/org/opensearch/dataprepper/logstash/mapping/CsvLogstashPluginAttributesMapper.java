0a9591dce6fdbe9caaba25f528ea87af7f62e9db
Finn Roblin <finnrobl@amazon.com>
2022-08-12 13:40:43 -0700

    Addressed Asif's feedback, including edge case where autogenerate_column_names == true and columns specified
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>

--- data-prepper-logstash-configuration/src/main/java/org/opensearch/dataprepper/logstash/mapping/CsvLogstashPluginAttributesMapper.java
+++ data-prepper-logstash-configuration/src/main/java/org/opensearch/dataprepper/logstash/mapping/CsvLogstashPluginAttributesMapper.java
@@ -5,10 +5,10 @@
 
 package org.opensearch.dataprepper.logstash.mapping;
 
-import org.opensearch.dataprepper.logstash.exception.LogstashConfigurationException;
 import org.opensearch.dataprepper.logstash.model.LogstashAttribute;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -20,70 +20,25 @@ import java.util.Optional;
  *
  */
 public class CsvLogstashPluginAttributesMapper extends AbstractLogstashPluginAttributesMapper{
-    protected static final String LOGSTASH_AUTODETECT_COLUMN_NAMES_ATTRIBUTE_NAME = "autodetect_column_names";
-    protected static final String DATA_PREPPER_COLUMN_NAMES_SOURCE_KEY = "column_names_source_key";
-    protected static final String DATA_PREPPER_COLUMN_NAMES = "column_names";
     protected static final String LOGSTASH_AUTOGENERATE_COLUMN_NAMES_ATTRIBUTE_NAME = "autogenerate_column_names";
+    protected static final String LOGSTASH_COLUMNS_ATTRIBUTE_NAME = "columns";
+    protected static final String DATA_PREPPER_COLUMN_NAMES = "column_names";
 
-    protected static final String LOGSTASH_SKIP_EMPTY_ROWS_ATTRIBUTE_NAME = "skip_empty_rows";
-    protected static final String LOGSTASH_SKIP_EMPTY_COLUMNS_ATTRIBUTE_NAME = "skip_empty_columns";
-    protected static final String LOGSTASH_SKIP_HEADER_ATTRIBUTE_NAME = "skip_header";
-    protected static final String LOGSTASH_CONVERT_ATTRIBUTE_NAME = "convert";
-    protected static final String LOGSTASH_AUTODETECT_COLUMN_NAMES_EXCEPTION_MESSAGE = "Data Prepper logstash conversion does not " +
-            "support the autodetect_column_names setting in Logstash configuration. Consider manually specifying a Data Prepper config " +
-            "file with a CSV Processor for header autodetection functionality.";
-    protected static final String LOGSTASH_SKIP_EMPTY_ROWS_EXCEPTION_MESSAGE = "Data Prepper logstash conversion does not support the " +
-            "skip_empty_columns setting in Logstash configuration. Consider using a delete_entries processor with the empty columns you " +
-            "wish to drop.";
-    protected static final String LOGSTASH_SKIP_EMPTY_COLUMNS_EXCEPTION_MESSAGE = "Data Prepper logstash conversion does not support the " +
-            "skip_empty_rows setting in Logstash configuration. Consider using a drop_events processor to drop empty rows.";
-    protected static final String LOGSTASH_SKIP_HEADER_EXCEPTION_MESSAGE = "Data Prepper logstash conversion does not support the " +
-            "skip_header setting in Logstash configuration. Consider using a drop_events processor to drop tbe first row.";
-    protected static final String LOGSTASH_CONVERT_EXCEPTION_MESSAGE = "Data Prepper logstash conversion does not support the convert " +
-            "setting in Logstash configuration. This feature may be supported in the future; a separate processor to typecast Event " +
-            "fields is in the Data Prepper roadmap.";
     @Override
     protected void mapCustomAttributes(final List<LogstashAttribute> logstashAttributes,
                                        final LogstashAttributesMappings logstashAttributesMappings,
                                        final Map<String, Object> pluginSettings) {
+        final Optional<LogstashAttribute> columnsAttribute = findLogstashAttribute(logstashAttributes,
+                LOGSTASH_COLUMNS_ATTRIBUTE_NAME);
+
         final Optional<LogstashAttribute> autogenerateColumnNamesAttribute = findLogstashAttribute(logstashAttributes,
                 LOGSTASH_AUTOGENERATE_COLUMN_NAMES_ATTRIBUTE_NAME);
 
-        if (logstashAttributeExistsAndIsTrue(autogenerateColumnNamesAttribute)) {
+        if (logstashAttributeExistsAndIsTrue(autogenerateColumnNamesAttribute) && logstashColumnsDoesNotExistOrIsEmpty(columnsAttribute)) {
             pluginSettings.put(
                     DATA_PREPPER_COLUMN_NAMES,
                     new ArrayList<String>()
             );
-        }
-
-        final Optional<LogstashAttribute> autodetectColumnNamesAttribute = findLogstashAttribute(logstashAttributes,
-                LOGSTASH_AUTODETECT_COLUMN_NAMES_ATTRIBUTE_NAME);
-
-        if (logstashAttributeExistsAndIsTrue(autodetectColumnNamesAttribute)) {
-            throw new LogstashConfigurationException(LOGSTASH_AUTODETECT_COLUMN_NAMES_EXCEPTION_MESSAGE);
-        }
-
-        final Optional<LogstashAttribute> skipEmptyRowsAttribute = findLogstashAttribute(logstashAttributes,
-                LOGSTASH_SKIP_EMPTY_ROWS_ATTRIBUTE_NAME);
-
-        if (skipEmptyRowsAttribute.isPresent()) {
-            throw new LogstashConfigurationException(LOGSTASH_SKIP_EMPTY_ROWS_EXCEPTION_MESSAGE);
-        }
-
-        final Optional<LogstashAttribute> skipEmptyColumnsAttribute = findLogstashAttribute(logstashAttributes,
-                LOGSTASH_SKIP_EMPTY_COLUMNS_ATTRIBUTE_NAME);
-        if (skipEmptyColumnsAttribute.isPresent()) {
-            throw new LogstashConfigurationException(LOGSTASH_SKIP_EMPTY_COLUMNS_EXCEPTION_MESSAGE);
-        }
-        final Optional<LogstashAttribute> skipHeaderAttribute = findLogstashAttribute(logstashAttributes,
-                LOGSTASH_SKIP_HEADER_ATTRIBUTE_NAME);
-        if (skipHeaderAttribute.isPresent()) {
-            throw new LogstashConfigurationException(LOGSTASH_SKIP_HEADER_EXCEPTION_MESSAGE);
-        }
-        final Optional<LogstashAttribute> convertAttribute = findLogstashAttribute(logstashAttributes,
-                LOGSTASH_CONVERT_ATTRIBUTE_NAME);
-        if (convertAttribute.isPresent()) {
-            throw new LogstashConfigurationException(LOGSTASH_CONVERT_EXCEPTION_MESSAGE);
         }
     }
 
@@ -94,20 +49,23 @@ public class CsvLogstashPluginAttributesMapper extends AbstractLogstashPluginAtt
                 .findFirst();
     }
 
+    private boolean logstashColumnsDoesNotExistOrIsEmpty(final Optional<LogstashAttribute> columnsLogstashAttribute) {
+        if (!columnsLogstashAttribute.isPresent()) {
+            return true;
+        }
+        final Object columnsFromLogstashConfiguration = columnsLogstashAttribute.get().getAttributeValue().getValue();
+        if (columnsFromLogstashConfiguration instanceof List) {
+            return ((List<?>) columnsFromLogstashConfiguration).isEmpty();
+        }
+        return true;
+    }
+
     private boolean logstashAttributeExistsAndIsTrue(final Optional<LogstashAttribute> optionalLogstashAttribute) {
         return optionalLogstashAttribute.isPresent() && optionalLogstashAttribute.get().getAttributeValue().getValue().equals(true);
     }
 
     @Override
     protected HashSet<String> getCustomMappedAttributeNames() {
-        final int numberOfCustomAttributeNames = 6;
-        final HashSet<String> names = new HashSet<>(numberOfCustomAttributeNames);
-        names.add(DATA_PREPPER_COLUMN_NAMES_SOURCE_KEY);
-        names.add(DATA_PREPPER_COLUMN_NAMES);
-        names.add(LOGSTASH_SKIP_EMPTY_ROWS_ATTRIBUTE_NAME);
-        names.add(LOGSTASH_SKIP_EMPTY_COLUMNS_ATTRIBUTE_NAME);
-        names.add(LOGSTASH_SKIP_HEADER_ATTRIBUTE_NAME);
-        names.add(LOGSTASH_CONVERT_ATTRIBUTE_NAME);
-        return names;
+        return new HashSet<>(Collections.singleton(LOGSTASH_AUTOGENERATE_COLUMN_NAMES_ATTRIBUTE_NAME));
     }
 }
--- data-prepper-logstash-configuration/src/test/java/org/opensearch/dataprepper/logstash/mapping/CsvLogstashPluginAttributesMapperTest.java
+++ data-prepper-logstash-configuration/src/test/java/org/opensearch/dataprepper/logstash/mapping/CsvLogstashPluginAttributesMapperTest.java
@@ -6,9 +6,7 @@
 package org.opensearch.dataprepper.logstash.mapping;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.opensearch.dataprepper.logstash.exception.LogstashConfigurationException;
 import org.opensearch.dataprepper.logstash.model.LogstashAttribute;
 import org.opensearch.dataprepper.logstash.model.LogstashAttributeValue;
 import org.opensearch.dataprepper.logstash.model.LogstashValueType;
@@ -19,21 +17,10 @@ import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.opensearch.dataprepper.logstash.mapping.CsvLogstashPluginAttributesMapper.DATA_PREPPER_COLUMN_NAMES;
-import static org.opensearch.dataprepper.logstash.mapping.CsvLogstashPluginAttributesMapper.LOGSTASH_AUTODETECT_COLUMN_NAMES_ATTRIBUTE_NAME;
-import static org.opensearch.dataprepper.logstash.mapping.CsvLogstashPluginAttributesMapper.LOGSTASH_AUTODETECT_COLUMN_NAMES_EXCEPTION_MESSAGE;
 import static org.opensearch.dataprepper.logstash.mapping.CsvLogstashPluginAttributesMapper.LOGSTASH_AUTOGENERATE_COLUMN_NAMES_ATTRIBUTE_NAME;
-import static org.opensearch.dataprepper.logstash.mapping.CsvLogstashPluginAttributesMapper.LOGSTASH_CONVERT_ATTRIBUTE_NAME;
-import static org.opensearch.dataprepper.logstash.mapping.CsvLogstashPluginAttributesMapper.LOGSTASH_CONVERT_EXCEPTION_MESSAGE;
-import static org.opensearch.dataprepper.logstash.mapping.CsvLogstashPluginAttributesMapper.LOGSTASH_SKIP_EMPTY_COLUMNS_ATTRIBUTE_NAME;
-import static org.opensearch.dataprepper.logstash.mapping.CsvLogstashPluginAttributesMapper.LOGSTASH_SKIP_EMPTY_COLUMNS_EXCEPTION_MESSAGE;
-import static org.opensearch.dataprepper.logstash.mapping.CsvLogstashPluginAttributesMapper.LOGSTASH_SKIP_EMPTY_ROWS_ATTRIBUTE_NAME;
-import static org.opensearch.dataprepper.logstash.mapping.CsvLogstashPluginAttributesMapper.LOGSTASH_SKIP_EMPTY_ROWS_EXCEPTION_MESSAGE;
-import static org.opensearch.dataprepper.logstash.mapping.CsvLogstashPluginAttributesMapper.LOGSTASH_SKIP_HEADER_ATTRIBUTE_NAME;
-import static org.opensearch.dataprepper.logstash.mapping.CsvLogstashPluginAttributesMapper.LOGSTASH_SKIP_HEADER_EXCEPTION_MESSAGE;
 
 class CsvLogstashPluginAttributesMapperTest {
     CsvLogstashPluginAttributesMapper csvLogstashPluginAttributesMapper;
@@ -58,89 +45,5 @@ class CsvLogstashPluginAttributesMapperTest {
 
         assertThat(pluginSettings.containsKey(DATA_PREPPER_COLUMN_NAMES), equalTo(true));
         assertThat(pluginSettings.containsValue(Collections.emptyList()), equalTo(true));
-    }
-    @Nested
-    class InvalidConfigurationExceptions {
-        final LogstashAttributesMappings mappings = mock(LogstashAttributesMappings.class);
-        final Map<String, Object> pluginSettings = new LinkedHashMap<>();
-
-        @Test
-        void when_autoDetectColumnNames_then_throws() {
-            when(mappings.getMappedAttributeNames()).thenReturn(Collections.emptyMap());
-            final LogstashAttribute autoDetectColumnNames = LogstashAttribute.builder()
-                    .attributeName(LOGSTASH_AUTODETECT_COLUMN_NAMES_ATTRIBUTE_NAME)
-                    .attributeValue(LogstashAttributeValue.builder().attributeValueType(LogstashValueType.BAREWORD).value(true).build())
-                    .build();
-
-            Exception autoDetectColumnNamesException = assertThrows(LogstashConfigurationException.class, () ->
-                    csvLogstashPluginAttributesMapper.mapCustomAttributes(Collections.singletonList(autoDetectColumnNames),
-                            mappings, pluginSettings)
-            );
-
-            assertThat(autoDetectColumnNamesException.getMessage(), equalTo(LOGSTASH_AUTODETECT_COLUMN_NAMES_EXCEPTION_MESSAGE));
-        }
-
-        @Test
-        void when_skipEmptyRows_then_throws() {
-            when(mappings.getMappedAttributeNames()).thenReturn(Collections.emptyMap());
-            final LogstashAttribute skipEmptyRows = LogstashAttribute.builder()
-                    .attributeName(LOGSTASH_SKIP_EMPTY_ROWS_ATTRIBUTE_NAME)
-                    .attributeValue(LogstashAttributeValue.builder().attributeValueType(LogstashValueType.BAREWORD).value(true).build())
-                    .build();
-
-            Exception skipEmptyRowsException = assertThrows(LogstashConfigurationException.class, () ->
-                    csvLogstashPluginAttributesMapper.mapCustomAttributes(Collections.singletonList(skipEmptyRows),
-                            mappings, pluginSettings)
-            );
-
-            assertThat(skipEmptyRowsException.getMessage(), equalTo(LOGSTASH_SKIP_EMPTY_ROWS_EXCEPTION_MESSAGE));
-        }
-
-        @Test
-        void when_skipEmptyColumns_then_throws() {
-            when(mappings.getMappedAttributeNames()).thenReturn(Collections.emptyMap());
-            final LogstashAttribute skipEmptyColumns = LogstashAttribute.builder()
-                    .attributeName(LOGSTASH_SKIP_EMPTY_COLUMNS_ATTRIBUTE_NAME)
-                    .attributeValue(LogstashAttributeValue.builder().attributeValueType(LogstashValueType.BAREWORD).value(true).build())
-                    .build();
-            Exception skipEmptyColumnsException = assertThrows(LogstashConfigurationException.class, () ->
-                    csvLogstashPluginAttributesMapper.mapCustomAttributes(Collections.singletonList(skipEmptyColumns),
-                            mappings, pluginSettings)
-            );
-
-            assertThat(skipEmptyColumnsException.getMessage(), equalTo(LOGSTASH_SKIP_EMPTY_COLUMNS_EXCEPTION_MESSAGE));
-        }
-
-        @Test
-        void when_skipHeader_then_throws() {
-            when(mappings.getMappedAttributeNames()).thenReturn(Collections.emptyMap());
-            final LogstashAttribute skipHeader = LogstashAttribute.builder()
-                    .attributeName(LOGSTASH_SKIP_HEADER_ATTRIBUTE_NAME)
-                    .attributeValue(LogstashAttributeValue.builder().attributeValueType(LogstashValueType.BAREWORD).value(true).build())
-                    .build();
-
-            Exception skipHeaderException = assertThrows(LogstashConfigurationException.class, () ->
-                    csvLogstashPluginAttributesMapper.mapCustomAttributes(Collections.singletonList(skipHeader),
-                            mappings, pluginSettings)
-            );
-
-            assertThat(skipHeaderException.getMessage(), equalTo(LOGSTASH_SKIP_HEADER_EXCEPTION_MESSAGE));
-        }
-
-        @Test
-        void when_convert_then_throws() {
-            when(mappings.getMappedAttributeNames()).thenReturn(Collections.emptyMap());
-            final LogstashAttribute convert = LogstashAttribute.builder()
-                    .attributeName(LOGSTASH_CONVERT_ATTRIBUTE_NAME)
-                    .attributeValue(LogstashAttributeValue.builder().attributeValueType(LogstashValueType.BAREWORD).value(true).build())
-                    .build();
-
-            Exception convertException = assertThrows(LogstashConfigurationException.class, () ->
-                    csvLogstashPluginAttributesMapper.mapCustomAttributes(Collections.singletonList(convert),
-                            mappings, pluginSettings)
-            );
-
-            assertThat(convertException.getMessage(), equalTo(LOGSTASH_CONVERT_EXCEPTION_MESSAGE));
-        }
     }
 }
