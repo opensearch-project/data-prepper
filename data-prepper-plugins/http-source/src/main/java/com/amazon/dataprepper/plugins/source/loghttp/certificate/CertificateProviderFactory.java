a8dddfca81ad14096791913512e1e1f6fa966957
Asif Sohail Mohammed <nsifmoh@amazon.com>
2022-08-22 15:24:40 -0500

    Added ACM and S3 certificate support for HTTP source (#1678)
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>

--- data-prepper-plugins/http-source/src/main/java/com/amazon/dataprepper/plugins/source/loghttp/certificate/CertificateProviderFactory.java
+++ data-prepper-plugins/http-source/src/main/java/com/amazon/dataprepper/plugins/source/loghttp/certificate/CertificateProviderFactory.java
@@ -6,10 +6,20 @@
 package com.amazon.dataprepper.plugins.source.loghttp.certificate;
 
 import com.amazon.dataprepper.plugins.certificate.CertificateProvider;
+import com.amazon.dataprepper.plugins.certificate.acm.ACMCertificateProvider;
 import com.amazon.dataprepper.plugins.certificate.file.FileCertificateProvider;
+import com.amazon.dataprepper.plugins.certificate.s3.S3CertificateProvider;
 import com.amazon.dataprepper.plugins.source.loghttp.HTTPSourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.core.retry.RetryMode;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.acm.AcmClient;
+import software.amazon.awssdk.services.s3.S3Client;
 
 public class CertificateProviderFactory {
     private static final Logger LOG = LoggerFactory.getLogger(CertificateProviderFactory.class);
@@ -20,8 +30,47 @@ public class CertificateProviderFactory {
     }
 
     public CertificateProvider getCertificateProvider() {
-        // TODO: support more certificate providers
-        LOG.info("Using local file system to get certificate and private key for SSL/TLS.");
-        return new FileCertificateProvider(httpSourceConfig.getSslCertificateFile(), httpSourceConfig.getSslKeyFile());
+        if (httpSourceConfig.isUseAcmCertificateForSsl()) {
+            LOG.info("Using ACM certificate and private key for SSL/TLS.");
+            final AwsCredentialsProvider credentialsProvider = AwsCredentialsProviderChain.builder()
+                    .addCredentialsProvider(DefaultCredentialsProvider.create())
+                    .build();
+
+            final ClientOverrideConfiguration clientConfig = ClientOverrideConfiguration.builder()
+                    .retryPolicy(RetryMode.STANDARD)
+                    .build();
+
+            final AcmClient awsCertificateManager = AcmClient.builder()
+                    .region(Region.of(httpSourceConfig.getAwsRegion()))
+                    .credentialsProvider(credentialsProvider)
+                    .overrideConfiguration(clientConfig)
+                    .build();
+
+            return new ACMCertificateProvider(awsCertificateManager,
+                    httpSourceConfig.getAcmCertificateArn(),
+                    httpSourceConfig.getAcmCertificateTimeoutMillis(),
+                    httpSourceConfig.getAcmPrivateKeyPassword());
+        } else if (httpSourceConfig.isSslCertAndKeyFileInS3()) {
+            LOG.info("Using S3 to fetch certificate and private key for SSL/TLS.");
+            final AwsCredentialsProvider credentialsProvider = AwsCredentialsProviderChain.builder()
+                    .addCredentialsProvider(DefaultCredentialsProvider.create()).build();
+
+            final S3Client s3Client = S3Client.builder()
+                    .region(Region.of(httpSourceConfig.getAwsRegion()))
+                    .credentialsProvider(credentialsProvider)
+                    .build();
+
+            return new S3CertificateProvider(
+                    s3Client,
+                    httpSourceConfig.getSslCertificateFile(),
+                    httpSourceConfig.getSslKeyFile()
+            );
+        } else {
+            LOG.info("Using local file system to get certificate and private key for SSL/TLS.");
+            return new FileCertificateProvider(
+                    httpSourceConfig.getSslCertificateFile(),
+                    httpSourceConfig.getSslKeyFile()
+            );
+        }
     }
 }
