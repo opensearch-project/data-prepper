3a4c66a5911e4defd3be6c81ca0826cdaa4d4547
David Venable <dlv@amazon.com>
2022-09-07 18:10:25 -0500

    Deletes PrepperStateTest which is no longer valid after #1707 (#1732)
    Signed-off-by: David Venable <dlv@amazon.com>
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

--- data-prepper-plugins/mapdb-prepper-state/src/test/java/com/amazon/dataprepper/plugins/prepper/state/MapDbPrepperStateTest.java
+++ data-prepper-plugins/mapdb-prepper-state/src/test/java/com/amazon/dataprepper/plugins/prepper/state/MapDbPrepperStateTest.java
@@ -5,7 +5,6 @@
 
 package com.amazon.dataprepper.plugins.prepper.state;
 
-import org.opensearch.dataprepper.plugins.prepper.state.PrepperStateTest;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
