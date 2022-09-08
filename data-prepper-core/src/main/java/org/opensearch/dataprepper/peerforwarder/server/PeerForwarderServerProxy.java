784adaa1cb9b836f73fc70deed248915915e5a67
Asif Sohail Mohammed <nsifmoh@amazon.com>
2022-09-08 11:58:20 -0500

    Moved conditional out of constructor
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>

--- data-prepper-core/src/main/java/org/opensearch/dataprepper/peerforwarder/server/PeerForwarderServerProxy.java
+++ data-prepper-core/src/main/java/org/opensearch/dataprepper/peerforwarder/server/PeerForwarderServerProxy.java
@@ -9,10 +9,18 @@ import com.linecorp.armeria.server.Server;
 import org.opensearch.dataprepper.peerforwarder.PeerForwarderConfiguration;
 
 public class PeerForwarderServerProxy implements PeerForwarderServer {
-    private final PeerForwarderServer peerForwarderServer;
+    private final PeerForwarderConfiguration peerForwarderConfiguration;
+    private final Server server;
+
+    private PeerForwarderServer peerForwarderServer;
 
     public PeerForwarderServerProxy(final PeerForwarderConfiguration peerForwarderConfiguration, final Server server) {
+        this.peerForwarderConfiguration = peerForwarderConfiguration;
+        this.server = server;
+    }
 
+    @Override
+    public void start() {
         // update this conditional based on PeerForwarderProvider
         if (true) {
             peerForwarderServer = new RemotePeerForwarderServer(peerForwarderConfiguration, server);
@@ -20,10 +28,6 @@ public class PeerForwarderServerProxy implements PeerForwarderServer {
         else {
             peerForwarderServer = new NoOpPeerForwarderServer();
         }
-    }
-
-    @Override
-    public void start() {
         peerForwarderServer.start();
     }
 
