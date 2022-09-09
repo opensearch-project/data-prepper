46859d24422845fd70a3db7edc38f7c19c94eafb
Asif Sohail Mohammed <nsifmoh@amazon.com>
2022-09-09 13:22:22 -0500

    Added server proxy for Peer Forwarder Server (#1738)
    * Added server proxy
    
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>

---
+++ data-prepper-core/src/main/java/org/opensearch/dataprepper/peerforwarder/server/PeerForwarderServerProxy.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.peerforwarder.server;
+
+import com.linecorp.armeria.server.Server;
+import org.opensearch.dataprepper.peerforwarder.PeerForwarderConfiguration;
+
+public class PeerForwarderServerProxy implements PeerForwarderServer {
+    private final PeerForwarderConfiguration peerForwarderConfiguration;
+    private final Server server;
+
+    private PeerForwarderServer peerForwarderServer;
+
+    public PeerForwarderServerProxy(final PeerForwarderConfiguration peerForwarderConfiguration, final Server server) {
+        this.peerForwarderConfiguration = peerForwarderConfiguration;
+        this.server = server;
+    }
+
+    @Override
+    public void start() {
+        // TODO: update this conditional based on PeerForwarderProvider
+        if (true) {
+            peerForwarderServer = new RemotePeerForwarderServer(peerForwarderConfiguration, server);
+        }
+        else {
+            peerForwarderServer = new NoOpPeerForwarderServer();
+        }
+        peerForwarderServer.start();
+    }
+
+    @Override
+    public void stop() {
+        if (peerForwarderServer != null) {
+            peerForwarderServer.stop();
+        }
+    }
+}
