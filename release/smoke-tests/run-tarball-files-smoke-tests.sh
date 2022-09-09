588950578ba0509a6eb3a707e14b0da2375beb34
David Venable <dlv@amazon.com>
2022-09-09 08:52:45 -0500

    Update Data Prepper to build using JDK 11 making this the minimum sup… (#1739)
    Update Data Prepper to build using JDK 11 making this the minimum supported Java version. Resolves #1422. Includes updated documentation.
    
    Signed-off-by: David Venable <dlv@amazon.com>

--- release/smoke-tests/run-tarball-files-smoke-tests.sh
+++ release/smoke-tests/run-tarball-files-smoke-tests.sh
@@ -195,5 +195,5 @@ fi
 
 echo -e "Using tarball source ${TAR_SOURCE_TYPE}"
 
-run_smoke_test "openjdk" "8" "opensearch-data-prepper"
+run_smoke_test "openjdk" "11" "opensearch-data-prepper"
 run_smoke_test "ubuntu" "latest" "opensearch-data-prepper-jdk"
