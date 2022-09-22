6de36db6516d0b637513a34e1d88874356998508
Hai Yan <8153134+oeyh@users.noreply.github.com>
2022-09-22 14:27:42 -0500

    Fix ignored Logstash config file when loading from directory (#1788)
    Signed-off-by: Hai Yan <oeyh@amazon.com>
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

--- data-prepper-core/src/main/java/org/opensearch/dataprepper/parser/config/DataPrepperArgs.java
+++ data-prepper-core/src/main/java/org/opensearch/dataprepper/parser/config/DataPrepperArgs.java
@@ -10,23 +10,49 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
+import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class DataPrepperArgs {
     private static final Logger LOG = LoggerFactory.getLogger(DataPrepperArgs.class);
-
     private static final Integer DATA_PREPPER_PIPELINE_CONFIG_POSITON = 0;
     private static final Integer DATA_PREPPER_CONFIG_POSITON = 1;
     private static final Integer MAXIMUM_SUPPORTED_NUMBER_OF_ARGS = 2;
 
+    /**
+     * Converts Logstash configuration to Data Prepper pipeline configuration. If input is a directory path, converts all Logstash
+     * configurations in the directory and return the directory path; if input is a file path, convert the file if necessary and
+     * return the pipeline configuration path.
+     *
+     * @param configurationFileLocation String path to a configuration or to a directory containing configurations
+     * @return String path to a pipeline configuration or a directory containing pipeline configurations
+     */
     private static String checkForLogstashConfigurationAndConvert(String configurationFileLocation) {
-        if (configurationFileLocation.endsWith(".conf")) {
+        final File configurationLocationAsFile = new File(configurationFileLocation);
+        final LogstashConfigConverter logstashConfigConverter = new LogstashConfigConverter();
+        final Path configurationDirectory;
+
+        if (configurationLocationAsFile.isDirectory()) {
+            configurationDirectory = Paths.get(configurationFileLocation).toAbsolutePath();
+            final FileFilter confFilter = pathname -> (pathname.getName().endsWith(".conf"));
+            for (final File file : configurationLocationAsFile.listFiles(confFilter)) {
+                LOG.info("Detected logstash configuration file {}, attempting to convert to Data Prepper pipeline", file.getName());
+
+                try {
+                    logstashConfigConverter.convertLogstashConfigurationToPipeline(
+                            file.getAbsolutePath(), String.valueOf(configurationDirectory));
+                } catch (final IOException e) {
+                    LOG.warn("Unable to read the Logstash configuration file", e);
+                    throw new IllegalArgumentException("Invalid Logstash configuration file", e);
+                }
+            }
+        } else if (configurationFileLocation.endsWith(".conf")) {
             LOG.debug("Detected logstash configuration file, attempting to convert to Data Prepper pipeline");
 
-            final LogstashConfigConverter logstashConfigConverter = new LogstashConfigConverter();
-            final Path configurationDirectory = Paths.get(configurationFileLocation).toAbsolutePath().getParent();
+            configurationDirectory = Paths.get(configurationFileLocation).toAbsolutePath().getParent();
 
             try {
                 configurationFileLocation = logstashConfigConverter.convertLogstashConfigurationToPipeline(
