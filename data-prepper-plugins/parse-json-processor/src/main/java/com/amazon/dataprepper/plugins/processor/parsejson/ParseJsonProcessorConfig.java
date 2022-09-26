5037b343eac6d496067638385ada639bd003d302
Finn <67562851+finnroblin@users.noreply.github.com>
2022-09-26 19:36:57 -0400

    Parse JSON Processor: README, more testing, support for JSON Pointer (#1696)
    Signed-off-by: Finn Roblin <finnrobl@amazon.com>
    Signed-off-by: Arunachalam Lakshmanan <arunachalam.l@gmail.com>

--- data-prepper-plugins/parse-json-processor/src/main/java/com/amazon/dataprepper/plugins/processor/parsejson/ParseJsonProcessorConfig.java
+++ data-prepper-plugins/parse-json-processor/src/main/java/com/amazon/dataprepper/plugins/processor/parsejson/ParseJsonProcessorConfig.java
@@ -6,7 +6,10 @@
 package com.amazon.dataprepper.plugins.processor.parsejson;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
+
+import java.util.Objects;
 
 public class ParseJsonProcessorConfig {
     static final String DEFAULT_SOURCE = "message";
@@ -17,6 +20,9 @@ public class ParseJsonProcessorConfig {
 
     @JsonProperty("destination")
     private String destination;
+
+    @JsonProperty("pointer")
+    private String pointer;
 
     /**
      * The field of the Event that contains the JSON data.
@@ -35,5 +41,25 @@ public class ParseJsonProcessorConfig {
      */
     public String getDestination() {
         return destination;
+    }
+
+    /**
+     * An optional setting used to specify a JSON Pointer. Pointer points to the JSON key that will be parsed into the destination.
+     * There is no pointer by default, meaning that the entirety of source will be parsed. If the target key would overwrite an existing
+     * key in the Event then the absolute path of the target key will be placed into destination
+     *
+     * Note: (should this be configurable/what about double conflicts?)
+     * @return String representing JSON Pointer
+     */
+    public String getPointer() {
+        return pointer;
+    }
+
+    @AssertTrue(message = "destination cannot be empty, whitespace, or a front slash (/)")
+    boolean isValidDestination() {
+        if (Objects.isNull(destination)) return true;
+
+        final String trimmedDestination = destination.trim();
+        return trimmedDestination.length() != 0 && !(trimmedDestination.equals("/"));
     }
 }
