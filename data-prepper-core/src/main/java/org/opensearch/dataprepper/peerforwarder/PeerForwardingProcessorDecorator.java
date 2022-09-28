d91ea23a10449811a42532013ae9307d1a1e6550
Asif Sohail Mohammed <nsifmoh@amazon.com>
2022-09-28 09:36:51 -0500

    Fix: Peer forwarder exception with single thread annotated processors (#1821)
    * Fix: Peer forwarder exception with single thread annotated processors
    
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>
    
    * Addressed feedback
    
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>
    Signed-off-by: Arunachalam Lakshmanan <arunachalam.l@gmail.com>

--- data-prepper-core/src/main/java/org/opensearch/dataprepper/peerforwarder/PeerForwardingProcessorDecorator.java
+++ data-prepper-core/src/main/java/org/opensearch/dataprepper/peerforwarder/PeerForwardingProcessorDecorator.java
@@ -14,31 +14,66 @@ import org.opensearch.dataprepper.peerforwarder.exception.EmptyPeerForwarderPlug
 import org.opensearch.dataprepper.peerforwarder.exception.UnsupportedPeerForwarderPluginException;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class PeerForwardingProcessorDecorator implements Processor<Record<Event>, Record<Event>> {
-    private final Processor innerProcessor;
     private final PeerForwarder peerForwarder;
-    private final String pluginId;
-    private final Set<String> identificationKeys;
+    private final Processor innerProcessor;
 
-    public PeerForwardingProcessorDecorator(final Processor innerProcessor,
-                                            final PeerForwarderProvider peerForwarderProvider,
-                                            final String pipelineName,
-                                            final String pluginId) {
-        this.innerProcessor = innerProcessor;
-        this.pluginId = pluginId;
+    public static List<Processor> decorateProcessors(
+            final List<Processor> processors,
+            final PeerForwarderProvider peerForwarderProvider,
+            final String pipelineName,
+            final String pluginId) {
 
-        if (innerProcessor instanceof RequiresPeerForwarding) {
-            identificationKeys = new HashSet<> (((RequiresPeerForwarding) innerProcessor).getIdentificationKeys());
-        } else {
-            throw new UnsupportedPeerForwarderPluginException("Peer Forwarding is only supported for plugins which implement RequiresPeerForwarding interface.");
+        Set<String> identificationKeys;
+        Processor firstInnerProcessor;
+        if (!processors.isEmpty()) {
+            firstInnerProcessor = processors.get(0);
         }
+        else {
+            return Collections.emptyList();
+        }
+
+        if (firstInnerProcessor instanceof RequiresPeerForwarding) {
+            identificationKeys = new HashSet<> (((RequiresPeerForwarding) firstInnerProcessor).getIdentificationKeys());
+        }
+        else {
+            throw new UnsupportedPeerForwarderPluginException(
+                    "Peer Forwarding is only supported for plugins which implement RequiresPeerForwarding interface."
+            );
+        }
+
+        // verify if identification keys of all processors are same with
+        // identification keys of first processor in case Processor is annotated with SingleThread.
+        processors.forEach(processor -> {
+            if (processor instanceof RequiresPeerForwarding) {
+                final Set<String> processorIdentificationKeys = new HashSet<>(((RequiresPeerForwarding) processor).getIdentificationKeys());
+                if (!identificationKeys.equals(processorIdentificationKeys)) {
+                    throw new RuntimeException(
+                            "All the processors of same type within a single pipeline should have same identification keys.");
+                }
+            }
+        });
+
         if (identificationKeys.isEmpty()) {
-            throw new EmptyPeerForwarderPluginIdentificationKeysException("Peer Forwarder Plugin: %s cannot have empty identification keys." + pluginId);
+            throw new EmptyPeerForwarderPluginIdentificationKeysException(
+                    "Peer Forwarder Plugin: %s cannot have empty identification keys." + pluginId);
         }
-        this.peerForwarder = peerForwarderProvider.register(pipelineName, pluginId, identificationKeys);
+
+        final PeerForwarder peerForwarder = peerForwarderProvider.register(pipelineName, pluginId, identificationKeys);
+
+        return processors.stream().map(processor -> new PeerForwardingProcessorDecorator(peerForwarder, processor))
+                .collect(Collectors.toList());
+    }
+
+    private PeerForwardingProcessorDecorator(final PeerForwarder peerForwarder, final Processor innerProcessor) {
+        this.peerForwarder = peerForwarder;
+        this.innerProcessor = innerProcessor;
     }
 
     @Override
@@ -46,7 +81,8 @@ public class PeerForwardingProcessorDecorator implements Processor<Record<Event>
         final Collection<Record<Event>> recordsToProcessOnLocalPeer = peerForwarder.forwardRecords(records);
         final Collection<Record<Event>> receivedRecordsFromBuffer = peerForwarder.receiveRecords();
 
-        final Collection<Record<Event>> recordsToProcessLocally = CollectionUtils.union(recordsToProcessOnLocalPeer, receivedRecordsFromBuffer);
+        final Collection<Record<Event>> recordsToProcessLocally = CollectionUtils.union(
+                recordsToProcessOnLocalPeer, receivedRecordsFromBuffer);
 
         return innerProcessor.execute(recordsToProcessLocally);
     }
