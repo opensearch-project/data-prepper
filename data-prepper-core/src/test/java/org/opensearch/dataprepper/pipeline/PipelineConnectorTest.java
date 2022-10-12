c7dad8a69dc1aa4dfcef0a1021016a5186f1bfba
Krishna Kondaka <krishkdk@amazon.com>
2022-10-12 18:40:02 +0000

    Addressed review comment and added parallel pipeline test to github/workflows
    Signed-off-by: Krishna Kondaka <krishkdk@amazon.com>

--- data-prepper-core/src/test/java/org/opensearch/dataprepper/pipeline/PipelineConnectorTest.java
+++ data-prepper-core/src/test/java/org/opensearch/dataprepper/pipeline/PipelineConnectorTest.java
@@ -34,7 +34,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.LinkedHashMap;
 import java.util.UUID;
 import java.util.Queue;
 import java.util.Map;
