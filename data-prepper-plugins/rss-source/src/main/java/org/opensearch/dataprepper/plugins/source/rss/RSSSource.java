e0c7bfdeb978770d14384aee850f51153225ab31
Shivani Shukla <sshkamz@amazon.com>
2022-11-30 23:14:26 -0600

    Adds boilerplate config and code for rss source Signed-off-by: Shivani Shukla <sshkamz@amazon.com>

---
+++ data-prepper-plugins/rss-source/src/main/java/org/opensearch/dataprepper/plugins/source/rss/RSSSource.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.plugins.source.rss;
+
+import org.opensearch.dataprepper.metrics.PluginMetrics;
+import org.opensearch.dataprepper.model.annotations.DataPrepperPlugin;
+import org.opensearch.dataprepper.model.annotations.DataPrepperPluginConstructor;
+import org.opensearch.dataprepper.model.buffer.Buffer;
+import org.opensearch.dataprepper.model.source.Source;
+
+@DataPrepperPlugin(name = "rss", pluginType = Source.class, pluginConfigurationType =  RSSSourceConfig.class)
+public class RSSSource implements Source {
+
+    private final PluginMetrics pluginMetrics;
+
+    private final RSSSourceConfig rssSourceConfig;
+
+    @DataPrepperPluginConstructor
+    public RSSSource(PluginMetrics pluginMetrics, final RSSSourceConfig rssSourceConfig) {
+        this.pluginMetrics = pluginMetrics;
+        this.rssSourceConfig = rssSourceConfig;
+
+
+    }
+
+
+
+    @Override
+    public void start(Buffer buffer) {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
