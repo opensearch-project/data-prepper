970a7eb7e396c585127c7dd46a806ce2757935b1
Shivani Shukla <sshkamz@amazon.com>
2022-12-09 00:17:29 -0600

    Extracts items from feed url Signed-off-by: Shivani Shukla <sshkamz@amazon.com>

--- data-prepper-plugins/rss-source/src/main/java/org/opensearch/dataprepper/plugins/source/rss/RSSSource.java
+++ data-prepper-plugins/rss-source/src/main/java/org/opensearch/dataprepper/plugins/source/rss/RSSSource.java
@@ -5,14 +5,26 @@
 
 package org.opensearch.dataprepper.plugins.source.rss;
 
+import com.apptasticsoftware.rssreader.Item;
+import com.apptasticsoftware.rssreader.RssReader;
 import org.opensearch.dataprepper.metrics.PluginMetrics;
 import org.opensearch.dataprepper.model.annotations.DataPrepperPlugin;
 import org.opensearch.dataprepper.model.annotations.DataPrepperPluginConstructor;
 import org.opensearch.dataprepper.model.buffer.Buffer;
+import org.opensearch.dataprepper.model.document.Document;
+import org.opensearch.dataprepper.model.document.JacksonDocument;
+import org.opensearch.dataprepper.model.event.Event;
+import org.opensearch.dataprepper.model.event.JacksonEvent;
+import org.opensearch.dataprepper.model.record.Record;
 import org.opensearch.dataprepper.model.source.Source;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @DataPrepperPlugin(name = "rss", pluginType = Source.class, pluginConfigurationType =  RSSSourceConfig.class)
 public class RSSSource implements Source {
@@ -39,5 +51,23 @@ public class RSSSource implements Source {
     @Override
     public void stop() {
         // TODO: Stop the service
+    }
+
+    private void extractItems(final RSSSourceConfig rssSourceConfig) throws IOException {
+        final String url = rssSourceConfig.getUrl();
+        RssReader reader = new RssReader();
+        Stream<Item> rssFeed = reader.read(url);
+        List<Item> items = rssFeed.collect(Collectors.toList());
+
+
+    }
+
+    private Record<Document> buildEventDocument(Event event) {
+        final JacksonDocument document = JacksonDocument.builder()
+                .withData(event)
+                .getThis()
+                .build();
+
+        return new Record<>(document);
     }
 }
