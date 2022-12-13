ce4d9d81213de4449919c7a8fb46831cb547db3a
Shivani Shukla <sshkamz@amazon.com>
2022-12-13 16:21:06 -0600

    Polls and parses RSS feed URL and converts to Data Prepper Event Signed-off-by: Shivani Shukla <sshkamz@amazon.com>

--- data-prepper-plugins/rss-source/src/main/java/org/opensearch/dataprepper/plugins/source/rss/RSSSource.java
+++ data-prepper-plugins/rss-source/src/main/java/org/opensearch/dataprepper/plugins/source/rss/RSSSource.java
@@ -13,11 +13,8 @@ import org.opensearch.dataprepper.model.annotations.DataPrepperPluginConstructor
 import org.opensearch.dataprepper.model.buffer.Buffer;
 import org.opensearch.dataprepper.model.document.Document;
 import org.opensearch.dataprepper.model.document.JacksonDocument;
-import org.opensearch.dataprepper.model.event.Event;
-import org.opensearch.dataprepper.model.event.JacksonEvent;
 import org.opensearch.dataprepper.model.record.Record;
 import org.opensearch.dataprepper.model.source.Source;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,7 +24,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @DataPrepperPlugin(name = "rss", pluginType = Source.class, pluginConfigurationType =  RSSSourceConfig.class)
-public class RSSSource implements Source {
+public class RSSSource implements Source<Record<Document>> {
 
     private static final Logger LOG = LoggerFactory.getLogger(RSSSource.class);
 
@@ -36,16 +33,17 @@ public class RSSSource implements Source {
     private final RSSSourceConfig rssSourceConfig;
 
     @DataPrepperPluginConstructor
-    public RSSSource(PluginMetrics pluginMetrics, final RSSSourceConfig rssSourceConfig) {
+    public RSSSource(final PluginMetrics pluginMetrics, final RSSSourceConfig rssSourceConfig) {
         this.pluginMetrics = pluginMetrics;
         this.rssSourceConfig = rssSourceConfig;
     }
 
-
-
     @Override
     public void start(Buffer buffer) {
         // TODO: Add to buffer
+        if(buffer == null) {
+            throw new IllegalStateException("Buffer is null");
+        }
     }
 
     @Override
@@ -53,21 +51,29 @@ public class RSSSource implements Source {
         // TODO: Stop the service
     }
 
-    private void extractItems(final RSSSourceConfig rssSourceConfig) throws IOException {
+    void extractItemsFromRssFeed(final RSSSourceConfig rssSourceConfig) {
         final String url = rssSourceConfig.getUrl();
-        RssReader reader = new RssReader();
-        Stream<Item> rssFeed = reader.read(url);
-        List<Item> items = rssFeed.collect(Collectors.toList());
 
+        if(url.isEmpty()) {
+            throw new IllegalArgumentException("No path specified for the RSS Feed URL");
+        }
 
+        final RssReader reader = new RssReader();
+        final Stream<Item> rssFeed;
+        try {
+            rssFeed = reader.read(url);
+        } catch (IOException e) {
+            throw new RuntimeException("IO Exception when reading RSS Feed URL", e);
+        }
+        final List<Item> items = rssFeed.collect(Collectors.toList());
+        items.forEach(this::buildEventDocument);
     }
 
-    private Record<Document> buildEventDocument(Event event) {
+    private Record<Document> buildEventDocument(final Item item) {
         final JacksonDocument document = JacksonDocument.builder()
-                .withData(event)
+                .withData(item)
                 .getThis()
                 .build();
-
         return new Record<>(document);
     }
 }
