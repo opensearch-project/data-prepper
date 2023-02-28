d11f8be90e0d2c2b5cc628249e4a194894dbfb76
Krishna Kondaka <krishkdk@amazon.com>
2023-02-28 22:44:28 +0000

    Fixed checkStyleMain  failure
    Signed-off-by: Krishna Kondaka <krishkdk@amazon.com>

--- data-prepper-plugins/grok-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/grok/GrokProcessor.java
+++ data-prepper-plugins/grok-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/grok/GrokProcessor.java
@@ -37,7 +37,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
