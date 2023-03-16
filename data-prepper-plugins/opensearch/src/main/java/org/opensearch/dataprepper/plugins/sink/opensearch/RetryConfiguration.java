5cf86590cfbee52ffec1ce8842f703c535240959
kkondaka <41027584+kkondaka@users.noreply.github.com>
2023-03-16 14:54:20 -0700

    OpenSearch Sink should make the number of retries configurable - Issue #2291 (#2339)
    OpenSearch Sink should make the number of retries configurable - Issue #2291
    Signed-off-by: Krishna Kondaka <krishkdk@amazon.com>
    
    ---------
    
    Signed-off-by: Krishna Kondaka <krishkdk@amazon.com>
    Co-authored-by: Krishna Kondaka <krishkdk@amazon.com>

--- data-prepper-plugins/opensearch/src/main/java/org/opensearch/dataprepper/plugins/sink/opensearch/RetryConfiguration.java
+++ data-prepper-plugins/opensearch/src/main/java/org/opensearch/dataprepper/plugins/sink/opensearch/RetryConfiguration.java
@@ -11,15 +11,25 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 public class RetryConfiguration {
   public static final String DLQ_FILE = "dlq_file";
+  public static final String MAX_RETRIES = "max_retries";
 
   private final String dlqFile;
+  private final int maxRetries;
 
   public String getDlqFile() {
     return dlqFile;
   }
 
+  public int getMaxRetries() {
+    if (maxRetries < 1) {
+        throw new IllegalArgumentException("max_retries must be > 1");
+    }
+    return maxRetries;
+  }
+
   public static class Builder {
     private String dlqFile;
+    private int maxRetries = Integer.MAX_VALUE;
 
     public Builder withDlqFile(final String dlqFile) {
       checkNotNull(dlqFile, "dlqFile cannot be null.");
@@ -27,6 +37,11 @@ public class RetryConfiguration {
       return this;
     }
 
+    public Builder withMaxRetries(final Integer maxRetries) {
+      checkNotNull(maxRetries, "maxRetries cannot be null.");
+      this.maxRetries = maxRetries;
+      return this;
+    }
     public RetryConfiguration build() {
       return new RetryConfiguration(this);
     }
@@ -34,6 +49,7 @@ public class RetryConfiguration {
 
   private RetryConfiguration(final Builder builder) {
     this.dlqFile = builder.dlqFile;
+    this.maxRetries = builder.maxRetries;
   }
 
   public static RetryConfiguration readRetryConfig(final PluginSetting pluginSetting) {
@@ -41,6 +57,10 @@ public class RetryConfiguration {
     final String dlqFile = (String) pluginSetting.getAttributeFromSettings(DLQ_FILE);
     if (dlqFile != null) {
       builder = builder.withDlqFile(dlqFile);
+    }
+    final Integer maxRetries = (Integer) pluginSetting.getAttributeFromSettings(MAX_RETRIES);
+    if (maxRetries != null) {
+      builder = builder.withMaxRetries(maxRetries);
     }
     return builder.build();
   }
