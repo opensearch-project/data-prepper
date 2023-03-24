83d2319703ee42ea586cdfe7b628d49e553e4403
Deepak Sahu <deepak.sahu562@gmail.com>
2023-03-24 22:08:21 +0530

    Integration Tests for s3-sink related to Github-issue#1048
    Signed-off-by: Deepak Sahu <deepak.sahu562@gmail.com>

---
+++ data-prepper-plugins/s3-sink/src/integrationTest/java/org/opensearch/dataprepper/plugins/sink/RecordsGenerator.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.plugins.sink;
+
+import org.opensearch.dataprepper.model.event.Event;
+import org.opensearch.dataprepper.plugins.sink.codec.Codec;
+import java.io.IOException;
+import java.io.OutputStream;
+
+interface RecordsGenerator {
+    void write(int numberOfRecords, OutputStream outputStream) throws IOException;
+
+    Codec getCodec();
+
+    String getFileExtension();
+
+    void assertEventIsCorrect(Event event);
+}
