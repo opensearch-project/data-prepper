d39f0a4199b6657c4472228b19af9c14f4376c0f
Chase Engelbrecht <engechas@amazon.com>
2023-04-03 11:07:07 -0500

    Fix spacing
    Signed-off-by: Chase Engelbrecht <engechas@amazon.com>

--- data-prepper-plugins/s3-source/src/main/java/org/opensearch/dataprepper/plugins/source/configuration/S3SelectOptions.java
+++ data-prepper-plugins/s3-source/src/main/java/org/opensearch/dataprepper/plugins/source/configuration/S3SelectOptions.java
@@ -16,16 +16,16 @@ public class S3SelectOptions {
 
 	static final String DEFAULT_EXPRESSION_TYPE = "SQL";
 	@JsonProperty("expression")
-    @NotBlank(message = "expression cannot be null or empty")
-    private String expression;
+	@NotBlank(message = "expression cannot be null or empty")
+	private String expression;
 
 	@JsonProperty("expression_type")
 	@NotBlank(message = "expression_type cannot be null or empty")
 	private String expressionType = DEFAULT_EXPRESSION_TYPE;
-	
+
 	@JsonProperty("input_serialization")
-    @NotBlank(message = "input serialization format cannot be null or empty")
-    private S3SelectSerializationFormatOption s3SelectSerializationFormatOption;
+	@NotBlank(message = "input serialization format cannot be null or empty")
+	private S3SelectSerializationFormatOption s3SelectSerializationFormatOption;
 
 	@JsonProperty("csv_file_header")
 	private String csvFileHeaderInfo = DEFAULT_CSV_HEADER;
@@ -37,7 +37,7 @@ public class S3SelectOptions {
 	public S3SelectSerializationFormatOption getS3SelectSerializationFormatOption() {
 		return s3SelectSerializationFormatOption;
 	}
-
+	
 	public String getCsvFileHeaderInfo() {
 		return csvFileHeaderInfo;
 	}
