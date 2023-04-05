0a874b25298f087b43d31df54cb4a5b08b3b2b38
Taylor Gray <tylgry@amazon.com>
2023-04-04 19:38:24 -0500

    Address PR comments and fix flaky dlq test
    Signed-off-by: Taylor Gray <tylgry@amazon.com>

--- data-prepper-api/src/test/java/org/opensearch/dataprepper/model/source/coordinator/SourcePartitionTest.java
+++ data-prepper-api/src/test/java/org/opensearch/dataprepper/model/source/coordinator/SourcePartitionTest.java
@@ -6,8 +6,6 @@
 package org.opensearch.dataprepper.model.source.coordinator;
 
 import org.junit.jupiter.api.Test;
-import org.opensearch.dataprepper.model.source.coordinator.PartitionIdentifier;
-import org.opensearch.dataprepper.model.source.coordinator.SourcePartition;
 
 import java.util.UUID;
 
