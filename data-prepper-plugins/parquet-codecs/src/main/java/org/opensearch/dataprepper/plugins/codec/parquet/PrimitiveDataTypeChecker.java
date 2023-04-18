865b92abf5c54fe7dd60b8fd4a101038e37872a7
umairofficial <umairhusain1010@gmail.com>
2023-04-18 14:24:26 +0530

    -Support for Source Codecs Signed-off-by: umairofficial <umairhusain1010@gmail.com>

---
+++ data-prepper-plugins/parquet-codecs/src/main/java/org/opensearch/dataprepper/plugins/codec/parquet/PrimitiveDataTypeChecker.java
@@ -0,0 +1,40 @@
+package org.opensearch.dataprepper.plugins.codec.parquet;
+
+import org.apache.parquet.example.data.simple.SimpleGroup;
+import org.apache.parquet.schema.PrimitiveType;
+import org.apache.parquet.schema.Type;
+
+public class PrimitiveDataTypeChecker {
+
+    private PrimitiveDataTypeChecker(){
+
+    }
+
+    public static Object checkPrimitiveDataType(Type field, SimpleGroup simpleGroup, int fieldIndex) {
+        String fieldPrimitiveDataType;
+        if (field.isPrimitive()) {
+            PrimitiveType primitiveType = field.asPrimitiveType();
+            fieldPrimitiveDataType = primitiveType.getPrimitiveTypeName().name();
+            switch (fieldPrimitiveDataType) {
+                case "INT32":
+                    return simpleGroup.getInteger(fieldIndex, 0);
+                case "BOOLEAN":
+                    return simpleGroup.getBoolean(fieldIndex, 0);
+                case "FLOAT":
+                    return simpleGroup.getFloat(fieldIndex, 0);
+                case "DOUBLE":
+                    return simpleGroup.getDouble(fieldIndex, 0);
+                case "INT96":
+                    return simpleGroup.getInt96(fieldIndex, 0);
+                case "INT64":
+                case "LONG":
+                    return simpleGroup.getLong(fieldIndex,0);
+                case "FIXED_LEN_BYTE_ARRAY":
+                    return simpleGroup.getValueToString(fieldIndex, 0);
+                default:
+                    //
+            }
+        }
+        return null;
+    }
+}
