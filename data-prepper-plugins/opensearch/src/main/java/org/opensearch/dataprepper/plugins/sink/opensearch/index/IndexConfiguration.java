f8350821161f296f96b4a48795c5c88b8ff19773
Asif Sohail Mohammed <nsifmoh@amazon.com>
2023-04-19 13:54:50 -0500

    Remove aws_serverless option
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>

--- data-prepper-plugins/opensearch/src/main/java/org/opensearch/dataprepper/plugins/sink/opensearch/index/IndexConfiguration.java
+++ data-prepper-plugins/opensearch/src/main/java/org/opensearch/dataprepper/plugins/sink/opensearch/index/IndexConfiguration.java
@@ -42,7 +42,7 @@ public class IndexConfiguration {
     public static final String ACTION = "action";
     public static final String S3_AWS_REGION = "s3_aws_region";
     public static final String S3_AWS_STS_ROLE_ARN = "s3_aws_sts_role_arn";
-    public static final String AWS_SERVERLESS = "aws_serverless";
+    public static final String SERVERLESS = "serverless";
     public static final String AWS_OPTION = "aws";
     public static final String DOCUMENT_ROOT_KEY = "document_root_key";
 
@@ -57,7 +57,7 @@ public class IndexConfiguration {
     private final String s3AwsRegion;
     private final String s3AwsStsRoleArn;
     private final S3Client s3Client;
-    private final boolean awsServerless;
+    private final boolean serverless;
     private final String documentRootKey;
 
     private static final String S3_PREFIX = "s3://";
@@ -65,7 +65,7 @@ public class IndexConfiguration {
 
     @SuppressWarnings("unchecked")
     private IndexConfiguration(final Builder builder) {
-        this.awsServerless = builder.awsServerless;
+        this.serverless = builder.serverless;
         determineIndexType(builder);
 
         this.s3AwsRegion = builder.s3AwsRegion;
@@ -114,7 +114,7 @@ public class IndexConfiguration {
             indexType = mappedIndexType.orElseThrow(
                     () -> new IllegalArgumentException("Value of the parameter, index_type, must be from the list: "
                     + IndexType.getIndexTypeValues()));
-        } else if (builder.awsServerless) {
+        } else if (builder.serverless) {
             this.indexType = IndexType.MANAGEMENT_DISABLED;
         } else {
             this.indexType  = IndexType.CUSTOM;
@@ -163,17 +163,10 @@ public class IndexConfiguration {
         }
 
         Map<String, Object> awsOption = pluginSetting.getTypedMap(AWS_OPTION, String.class, Object.class);
-        boolean awsOptionUsed = false;
         if (awsOption != null && !awsOption.isEmpty()) {
-            awsOptionUsed = true;
-            builder.withAwsServerless((Boolean)awsOption.getOrDefault(AWS_SERVERLESS.substring(4), false));
-        }
-        final boolean awsServerless = pluginSetting.getBooleanOrDefault(AWS_SERVERLESS, false);
-        if (awsServerless) {
-            if (awsOptionUsed) {
-                throw new RuntimeException(String.format("%s option cannot be used along with %s option", AWS_SERVERLESS, AWS_OPTION));
-            }
-            builder.withAwsServerless(awsServerless);
+            builder.withServerless((Boolean)awsOption.getOrDefault(SERVERLESS, false));
+        } else {
+            builder.withServerless(false);
         }
 
         final String documentRootKey = pluginSetting.getStringOrDefault(DOCUMENT_ROOT_KEY, null);
@@ -222,12 +215,8 @@ public class IndexConfiguration {
         return s3AwsStsRoleArn;
     }
 
-    public boolean getAwsServerless() {
-        return awsServerless;
-    }
-
-    public String getDocumentRootKey() {
-        return documentRootKey;
+    public boolean getServerless() {
+        return serverless;
     }
 
     /**
@@ -285,7 +274,7 @@ public class IndexConfiguration {
         private String s3AwsRegion;
         private String s3AwsStsRoleArn;
         private S3Client s3Client;
-        private boolean awsServerless;
+        private boolean serverless;
         private String documentRootKey;
 
         public Builder withIndexAlias(final String indexAlias) {
@@ -370,8 +359,8 @@ public class IndexConfiguration {
             return this;
         }
 
-        public Builder withAwsServerless(final boolean awsServerless) {
-            this.awsServerless = awsServerless;
+        public Builder withServerless(final boolean serverless) {
+            this.serverless = serverless;
             return this;
         }
 
