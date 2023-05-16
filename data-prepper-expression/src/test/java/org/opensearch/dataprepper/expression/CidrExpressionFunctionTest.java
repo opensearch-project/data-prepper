0d642511f3fef4d6f285f09a1595e85943d37057
Hai Yan <oeyh@amazon.com>
2023-05-15 22:09:05 -0500

    Use parameterized tests
    Signed-off-by: Hai Yan <oeyh@amazon.com>

--- data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/CidrExpressionFunctionTest.java
+++ data-prepper-expression/src/test/java/org/opensearch/dataprepper/expression/CidrExpressionFunctionTest.java
@@ -8,6 +8,9 @@ package org.opensearch.dataprepper.expression;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.opensearch.dataprepper.model.event.Event;
@@ -16,6 +19,8 @@ import org.opensearch.dataprepper.model.event.JacksonEvent;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -42,20 +47,22 @@ class CidrExpressionFunctionTest {
         cidrExpressionFunction = createObjectUnderTest();
     }
 
-    @Test
-    void testIpv4MatchWithSingleCidrBlock() {
-        String network = "\"192.0.2.0/24\"";
-        testEvent = createTestEvent(Map.of("sourceIp", "192.0.2.3"));
+    @ParameterizedTest
+    @MethodSource("ipv4AddressesInRange")
+    void testIpv4MatchWithSingleCidrBlock(String testIp) {
+        String network = "\"192.0.2.0/26\"";
+        testEvent = createTestEvent(Map.of("sourceIp", testIp));
         Object expressionResult = cidrExpressionFunction.evaluate(List.of("/sourceIp", network), testEvent, testFunction);
         assertThat((boolean)expressionResult, equalTo(true));
     }
 
-    @Test
-    void testIpv4MatchWithMultipleCidrBlocks() {
-        String network1 = "\"192.0.2.0/24\"";
+    @ParameterizedTest
+    @MethodSource("ipv4AddressesInRange")
+    void testIpv4MatchWithMultipleCidrBlocks(String testIp) {
+        String network1 = "\"192.0.2.0/26\"";
         String network2 = "\"192.168.1.0/24\"";
         String network3 = "\"10.0.1.0/24\"";
-        testEvent = createTestEvent(Map.of("sourceIp", "192.0.2.3"));
+        testEvent = createTestEvent(Map.of("sourceIp", testIp));
         Object expressionResult = cidrExpressionFunction.evaluate(
                 List.of("/sourceIp", network1, network2, network3), testEvent, testFunction);
         assertThat((boolean)expressionResult, equalTo(true));
@@ -72,20 +79,22 @@ class CidrExpressionFunctionTest {
         assertThat((boolean)expressionResult, equalTo(false));
     }
 
-    @Test
-    void testIpv6MatchWithSingleCidrBlock() {
+    @ParameterizedTest
+    @MethodSource("ipv6AddressesInRange")
+    void testIpv6MatchWithSingleCidrBlock(String testIp) {
         String network = "\"2001:0db8::/32\"";
-        testEvent = createTestEvent(Map.of("sourceIp", "2001:0db8:aaaa:bbbb::"));
+        testEvent = createTestEvent(Map.of("sourceIp", testIp));
         Object expressionResult = cidrExpressionFunction.evaluate(List.of("/sourceIp", network), testEvent, testFunction);
         assertThat((boolean)expressionResult, equalTo(true));
     }
 
-    @Test
-    void testIpv6MatchWithMultipleCidrBlocks() {
+    @ParameterizedTest
+    @MethodSource("ipv6AddressesInRange")
+    void testIpv6MatchWithMultipleCidrBlocks(String testIp) {
         String network1 = "\"2001:0db8::/32\"";
         String network2 = "\"2001:aaaa::/32\"";
         String network3 = "\"2001:bbbb:cccc::/48\"";
-        testEvent = createTestEvent(Map.of("sourceIp", "2001:0db8:aaaa:bbbb::"));
+        testEvent = createTestEvent(Map.of("sourceIp", testIp));
         Object expressionResult = cidrExpressionFunction.evaluate(List.of("/sourceIp", network1, network2, network3), testEvent, testFunction);
         assertThat((boolean)expressionResult, equalTo(true));
     }
@@ -120,5 +129,20 @@ class CidrExpressionFunctionTest {
         testEvent = createTestEvent(Map.of("sourceIp", "192.0.2.3"));
         assertThrows(NullPointerException.class,
                 () -> cidrExpressionFunction.evaluate(List.of("/destinationIp", network), testEvent, testFunction));
+    }
+
+    private static Stream<Arguments> ipv4AddressesInRange() {
+        final String prefix = "192.0.2.";
+        return IntStream.range(0, 64)
+                .boxed()
+                .map(num -> Arguments.of(prefix + num));
+    }
+
+    private static Stream<Arguments> ipv6AddressesInRange() {
+        return Stream.of(
+                Arguments.of("2001:0db8::"),
+                Arguments.of("2001:0db8:aaaa:bbbb::"),
+                Arguments.of("2001:0db8:ffff:ffff:ffff:ffff:ffff:ffff")
+        );
     }
 }
