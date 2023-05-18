d64b48cac45fc163c7e93c0761849068f3d1eaa0
Ajeesh Gopalakrishnakurup <61016936+ajeeshakd@users.noreply.github.com>
2023-05-18 21:13:56 +0530

    Kafka source multithreading (#2673)
    * Added kafka consumer multithreaded logic and it's junit
    
    Signed-off-by: Ajeesh Gopalakrishnakurup <ajeesh.akd@gmail.com>
    
    * Applied file formatting
    
    Signed-off-by: Ajeesh Gopalakrishnakurup <ajeesh.akd@gmail.com>
    
    * Fixed the build issue
    
    Signed-off-by: Ajeesh Gopalakrishnakurup <ajeesh.akd@gmail.com>
    
    * Incorporated the review comments
    
    Signed-off-by: Ajeesh Gopalakrishnakurup <ajeesh.akd@gmail.com>
    
    * Removed the topic config files
    
    Signed-off-by: Ajeesh Gopalakrishnakurup <ajeesh.akd@gmail.com>
    
    * Incorporated the review comments
    
    Signed-off-by: Ajeesh Gopalakrishnakurup <ajeesh.akd@gmail.com>
    
    ---------
    
    Signed-off-by: Ajeesh Gopalakrishnakurup <ajeesh.akd@gmail.com>

--- data-prepper-plugins/kafka-plugins/src/test/java/org/opensearch/dataprepper/plugins/kafka/configuration/SchemaConfigTest.java
+++ data-prepper-plugins/kafka-plugins/src/test/java/org/opensearch/dataprepper/plugins/kafka/configuration/SchemaConfigTest.java
@@ -1,11 +1,13 @@
 package org.opensearch.dataprepper.plugins.kafka.configuration;
 
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,61 +22,48 @@ class SchemaConfigTest {
 	@Mock
 	SchemaConfig schemaConfig;
 
-	private String keyDeserializer;
-
-	private String valueDeserializer;
-	private String recordType;
-	private String schemaType;
-	private String registryUrl;
-
-
-
 	@BeforeEach
 	void setUp() throws IOException {
 		schemaConfig = new SchemaConfig();
 		//Added to load Yaml file - Start
 		Yaml yaml = new Yaml();
-		FileReader reader = new FileReader(getClass().getClassLoader().getResource("sample-pipelines.yaml").getFile());
-		Object data = yaml.load(reader);
+		FileReader fileReader = new FileReader(getClass().getClassLoader().getResource("sample-pipelines.yaml").getFile());
+		Object data = yaml.load(fileReader);
 		ObjectMapper mapper = new ObjectMapper();
 		if(data instanceof Map){
-			Map<Object, Object> propertyMap = (Map<Object, Object>) data;
-			Map<Object, Object> nestedMap = new HashMap<>();
-			iterateMap(propertyMap, nestedMap);
-			List<TopicConfig> topicConfigList = (List<TopicConfig>) nestedMap.get("topics");
+			Map<String, Object> propertyMap = (Map<String, Object>) data;
+			Map<String, Object> logPipelineMap = (Map<String, Object>) propertyMap.get("log-pipeline");
+			Map<String, Object> sourceMap = (Map<String, Object>) logPipelineMap.get("source");
+			Map<String, Object> kafkaConfigMap = (Map<String, Object>) sourceMap.get("kafka");
 			mapper.registerModule(new JavaTimeModule());
-			TopicsConfig topicsConfig = mapper.convertValue(topicConfigList.get(0), TopicsConfig.class);
-			TopicConfig topicConfig = topicsConfig.getTopic();
-			schemaConfig = topicConfig.getSchemaConfig();
-			keyDeserializer = schemaConfig.getKeyDeserializer();
-			valueDeserializer = schemaConfig.getValueDeserializer();
-			recordType = schemaConfig.getRecordType();
-			schemaType = schemaConfig.getSchemaType();
-			registryUrl = schemaConfig.getRegistryURL();
+			String json = mapper.writeValueAsString(kafkaConfigMap);
+			Reader reader = new StringReader(json);
+			KafkaSourceConfig kafkaSourceConfig = mapper.readValue(reader, KafkaSourceConfig.class);
+			schemaConfig = kafkaSourceConfig.getSchemaConfig();
 		}
-	}
-
-	private static Map<Object, Object> iterateMap(Map<Object, Object> map, Map<Object, Object> nestedMap) {
-		for (Map.Entry<Object, Object> entry : map.entrySet()) {
-			String key = (String) entry.getKey();
-			Object value = entry.getValue();
-			if (value instanceof Map) {
-				iterateMap((Map<Object, Object>) value, nestedMap); // call recursively for nested maps
-			} else {
-				nestedMap.put(key, value);
-			}
-		}
-		return nestedMap;
 	}
 
 	@Test
-	void testConfigValues() {
-		assertEquals(schemaConfig.getKeyDeserializer(), keyDeserializer);
-		assertEquals(schemaConfig.getValueDeserializer(), valueDeserializer);
-		assertEquals(schemaConfig.getRecordType(), recordType);
-		assertEquals(schemaConfig.getSchemaType(), schemaType);
-		assertEquals(schemaConfig.getRegistryURL(), registryUrl);
+	void test_schemaConfig_not_null() {
+		assertThat(schemaConfig, notNullValue());
+		assertThat(schemaConfig.getVersion(), notNullValue());
+		assertThat(schemaConfig.getRegistryURL(), notNullValue());
+	}
 
+	@Test
+	void test_schemaConfig_values() {
+		assertEquals(1, schemaConfig.getVersion());
+		assertEquals("http://localhost:8081/", schemaConfig.getRegistryURL());
+	}
+
+	@Test
+	void testSetters(){
+		schemaConfig = new SchemaConfig();
+		schemaConfig.setVersion(2);
+		schemaConfig.setRegistryURL("http://localhost:8080/");
+
+		assertEquals(2, schemaConfig.getVersion());
+		assertEquals("http://localhost:8080/", schemaConfig.getRegistryURL());
 	}
 
 }
