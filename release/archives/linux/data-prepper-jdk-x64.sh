86e4bd3b9ece1056769a64f3b14815cebf96122d
David Venable <dlv@amazon.com>
2023-05-19 14:09:08 -0500

    Sets JAVA_OPTS after DATA_PREPPER_JAVA_OPTS to allow Data Prepper admins to override the Log4j configuration file setting. Resolves #2720.
    Signed-off-by: David Venable <dlv@amazon.com>

--- release/archives/linux/data-prepper-jdk-x64.sh
+++ release/archives/linux/data-prepper-jdk-x64.sh
@@ -37,9 +37,9 @@ DATA_PREPPER_HOME_OPTS="-Ddata-prepper.dir=$DATA_PREPPER_HOME"
 DATA_PREPPER_JAVA_OPTS="-Dlog4j.configurationFile=$DATA_PREPPER_HOME/config/log4j2-rolling.properties"
 
 if [[ $# == 0 ]]; then
-    exec java $JAVA_OPTS $DATA_PREPPER_HOME_OPTS $DATA_PREPPER_JAVA_OPTS -cp "$DATA_PREPPER_CLASSPATH" org.opensearch.dataprepper.DataPrepperExecute
+    exec java $DATA_PREPPER_JAVA_OPTS $JAVA_OPTS $DATA_PREPPER_HOME_OPTS -cp "$DATA_PREPPER_CLASSPATH" org.opensearch.dataprepper.DataPrepperExecute
 else
     PIPELINES_FILE_LOCATION=$1
     CONFIG_FILE_LOCATION=$2
-    exec java $JAVA_OPTS $DATA_PREPPER_HOME_OPTS $DATA_PREPPER_JAVA_OPTS -cp "$DATA_PREPPER_CLASSPATH" org.opensearch.dataprepper.DataPrepperExecute $PIPELINES_FILE_LOCATION $CONFIG_FILE_LOCATION
+    exec java $DATA_PREPPER_JAVA_OPTS $JAVA_OPTS $DATA_PREPPER_HOME_OPTS -cp "$DATA_PREPPER_CLASSPATH" org.opensearch.dataprepper.DataPrepperExecute $PIPELINES_FILE_LOCATION $CONFIG_FILE_LOCATION
 fi
