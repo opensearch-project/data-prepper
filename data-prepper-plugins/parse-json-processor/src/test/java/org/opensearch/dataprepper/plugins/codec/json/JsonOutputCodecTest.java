3956d3944927d38bef3b02a38a34b6f8379fbcaa
umairofficial <umairhusain1010@gmail.com>
2023-06-01 12:42:09 +0530

    -Support for Sink Codecs Signed-off-by: umairofficial <umairhusain1010@gmail.com>

--- data-prepper-plugins/parse-json-processor/src/test/java/org/opensearch/dataprepper/plugins/codec/json/JsonOutputCodecTest.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-package org.opensearch.dataprepper.plugins.codec.json;
-
-public class JsonOutputCodecTest {
-}
