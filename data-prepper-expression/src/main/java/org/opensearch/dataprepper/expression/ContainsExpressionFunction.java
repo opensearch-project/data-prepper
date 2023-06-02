c9abfb71d51a5228e42c34145fd6937781f373b0
Krishna Kondaka <krishkdk@amazon.com>
2023-06-02 18:20:03 +0000

    Addressed comments. Renamed containsSubstring() to contains()
    Signed-off-by: Krishna Kondaka <krishkdk@amazon.com>

--- data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/ContainsExpressionFunction.java
+++ data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/ContainsExpressionFunction.java
@@ -11,20 +11,20 @@ import javax.inject.Named;
 import java.util.function.Function;
 
 @Named
-public class ContainsSubstringExpressionFunction implements ExpressionFunction {
+public class ContainsExpressionFunction implements ExpressionFunction {
     public String getFunctionName() {
-        return "containsSubstring";
+        return "contains";
     }
 
     public Object evaluate(final List<Object> args, Event event, Function<Object, Object> convertLiteralType) {
         if (args.size() != 2) {
-            throw new RuntimeException("containsSubstring() takes exactly two arguments");
+            throw new RuntimeException("contains() takes exactly two arguments");
         }
         String[] strArgs = new String[2];
         for (int i = 0; i < 2; i++) {
             Object arg = args.get(i);
             if (!(arg instanceof String)) {
-                throw new RuntimeException("containsSubstring() takes only string type arguments");
+                throw new RuntimeException("contains() takes only string type arguments");
             }
             String str = (String) arg;
             if (str.charAt(0) == '"') {
@@ -35,11 +35,11 @@ public class ContainsSubstringExpressionFunction implements ExpressionFunction {
                     return false;
                 }
                 if (!(obj instanceof String)) {
-                    throw new RuntimeException("containsSubstring() takes only string type arguments");
+                    throw new RuntimeException("contains() takes only string type arguments");
                 }
                 strArgs[i] = (String)obj;
             } else {
-                throw new RuntimeException("Invalid argument passed to containsSubstring(). Argument must be a literal string or a Json Pointer");
+                throw new RuntimeException("Arguments to contains() must be a literal string or a Json Pointer");
             }
         }
         return strArgs[0].contains(strArgs[1]);
