d9c35da939e2f3ed0a46b33fa020092c88035059
Krishna Kondaka <krishkdk@amazon.com>
2023-06-02 22:12:47 +0000

    Addressed comments.
    Signed-off-by: Krishna Kondaka <krishkdk@amazon.com>

--- data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/ContainsExpressionFunction.java
+++ data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/ContainsExpressionFunction.java
@@ -12,19 +12,21 @@ import java.util.function.Function;
 
 @Named
 public class ContainsExpressionFunction implements ExpressionFunction {
+    private static final int NUMBER_OF_ARGS = 2;
+
     public String getFunctionName() {
         return "contains";
     }
 
     public Object evaluate(final List<Object> args, Event event, Function<Object, Object> convertLiteralType) {
-        if (args.size() != 2) {
+        if (args.size() != NUMBER_OF_ARGS) {
             throw new RuntimeException("contains() takes exactly two arguments");
         }
-        String[] strArgs = new String[2];
-        for (int i = 0; i < 2; i++) {
+        String[] strArgs = new String[NUMBER_OF_ARGS];
+        for (int i = 0; i < NUMBER_OF_ARGS; i++) {
             Object arg = args.get(i);
             if (!(arg instanceof String)) {
-                throw new RuntimeException("contains() takes only string type arguments");
+                throw new RuntimeException(String.format("containsSubstring() takes only string type arguments. \"%s\" is not of type string", arg));
             }
             String str = (String) arg;
             if (str.charAt(0) == '"') {
@@ -35,11 +37,11 @@ public class ContainsExpressionFunction implements ExpressionFunction {
                     return false;
                 }
                 if (!(obj instanceof String)) {
-                    throw new RuntimeException("contains() takes only string type arguments");
+                    throw new RuntimeException(String.format("containsSubstring() takes only string type arguments. \"%s\" is not of type string", obj));
                 }
                 strArgs[i] = (String)obj;
             } else {
-                throw new RuntimeException("Arguments to contains() must be a literal string or a Json Pointer");
+                throw new RuntimeException(String.format("Arguments to contains() must be a literal string or a Json Pointer. \"%s\" is not string literal or json pointer", str));
             }
         }
         return strArgs[0].contains(strArgs[1]);
