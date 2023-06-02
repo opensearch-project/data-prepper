f34627a16de1bf434a4223056b9d120b974fb26a
David Venable <dlv@amazon.com>
2023-06-02 11:11:40 -0500

    Adds missing unit tests as noted in PR feedback. Updates the testing approach to deserialize using Jackson to better simulate the final usage.
    Signed-off-by: David Venable <dlv@amazon.com>

--- data-prepper-plugins/s3-sink/src/test/java/org/opensearch/dataprepper/plugins/sink/configuration/AwsAuthenticationOptionsTest.java
+++ data-prepper-plugins/s3-sink/src/test/java/org/opensearch/dataprepper/plugins/sink/configuration/AwsAuthenticationOptionsTest.java
@@ -5,45 +5,72 @@
 
 package org.opensearch.dataprepper.plugins.sink.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import org.opensearch.dataprepper.test.helper.ReflectivelySetField;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import software.amazon.awssdk.regions.Region;
 
+import java.util.Collections;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
 
 class AwsAuthenticationOptionsTest {
-
-    private AwsAuthenticationOptions awsAuthenticationOptions;
+    private ObjectMapper objectMapper;
 
     @BeforeEach
     void setUp() {
-        awsAuthenticationOptions = new AwsAuthenticationOptions();
+        objectMapper = new ObjectMapper();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"us-east-1", "us-west-2", "eu-central-1"})
+    void getAwsRegion_returns_Region_of(final String regionString) {
+        final Region expectedRegionObject = Region.of(regionString);
+        final Map<String, Object> jsonMap = Map.of("region", regionString);
+        final AwsAuthenticationOptions objectUnderTest = objectMapper.convertValue(jsonMap, AwsAuthenticationOptions.class);
+        assertThat(objectUnderTest.getAwsRegion(), equalTo(expectedRegionObject));
     }
 
     @Test
-    void getAwsRegion_returns_Region_of() throws NoSuchFieldException, IllegalAccessException {
-        final String regionString = UUID.randomUUID().toString();
-        final Region expectedRegionObject = mock(Region.class);
-        ReflectivelySetField.setField(AwsAuthenticationOptions.class, awsAuthenticationOptions, "awsRegion", regionString);
-        final Region actualRegion;
-        try (final MockedStatic<Region> regionMockedStatic = mockStatic(Region.class)) {
-            regionMockedStatic.when(() -> Region.of(regionString)).thenReturn(expectedRegionObject);
-            actualRegion = awsAuthenticationOptions.getAwsRegion();
-        }
-        assertThat(actualRegion, equalTo(expectedRegionObject));
+    void getAwsRegion_returns_null_when_region_is_null() {
+        final Map<String, Object> jsonMap = Collections.emptyMap();
+        final AwsAuthenticationOptions objectUnderTest = objectMapper.convertValue(jsonMap, AwsAuthenticationOptions.class);
+        assertThat(objectUnderTest.getAwsRegion(), nullValue());
     }
 
     @Test
-    void getAwsRegion_returns_null_when_region_is_null() throws NoSuchFieldException, IllegalAccessException {
-        ReflectivelySetField.setField(AwsAuthenticationOptions.class, awsAuthenticationOptions, "awsRegion", null);
-        assertThat(awsAuthenticationOptions.getAwsRegion(), nullValue());
+    void getAwsStsRoleArn_returns_value_from_deserialized_JSON() {
+        final String stsRoleArn = UUID.randomUUID().toString();
+        final Map<String, Object> jsonMap = Map.of("sts_role_arn", stsRoleArn);
+        final AwsAuthenticationOptions objectUnderTest = objectMapper.convertValue(jsonMap, AwsAuthenticationOptions.class);
+        assertThat(objectUnderTest.getAwsStsRoleArn(), equalTo(stsRoleArn));
+    }
+
+    @Test
+    void getAwsStsRoleArn_returns_null_if_not_in_JSON() {
+        final Map<String, Object> jsonMap = Collections.emptyMap();
+        final AwsAuthenticationOptions objectUnderTest = objectMapper.convertValue(jsonMap, AwsAuthenticationOptions.class);
+        assertThat(objectUnderTest.getAwsStsRoleArn(), nullValue());
+    }
+
+    @Test
+    void getAwsStsHeaderOverrides_returns_value_from_deserialized_JSON() {
+        final Map<String, String> stsHeaderOverrides = Map.of(UUID.randomUUID().toString(), UUID.randomUUID().toString());
+        final Map<String, Object> jsonMap = Map.of("sts_header_overrides", stsHeaderOverrides);
+        final AwsAuthenticationOptions objectUnderTest = objectMapper.convertValue(jsonMap, AwsAuthenticationOptions.class);
+        assertThat(objectUnderTest.getAwsStsHeaderOverrides(), equalTo(stsHeaderOverrides));
+    }
+
+    @Test
+    void getAwsStsHeaderOverrides_returns_null_if_not_in_JSON() {
+        final Map<String, Object> jsonMap = Collections.emptyMap();
+        final AwsAuthenticationOptions objectUnderTest = objectMapper.convertValue(jsonMap, AwsAuthenticationOptions.class);
+        assertThat(objectUnderTest.getAwsStsHeaderOverrides(), nullValue());
     }
 }
