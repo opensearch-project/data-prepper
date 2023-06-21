3cc5a83a92c89b5f8ffe0238d5c844ec68194c96
Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
2023-06-21 20:09:34 +0000

    Added javadoc for SinkContext
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>

--- data-prepper-api/src/main/java/org/opensearch/dataprepper/model/sink/SinkContext.java
+++ data-prepper-api/src/main/java/org/opensearch/dataprepper/model/sink/SinkContext.java
@@ -7,6 +7,10 @@ package org.opensearch.dataprepper.model.sink;
 
 import java.util.Collection;
 
+/**
+ * Data Prepper Sink Context class. This the class for keeping global
+ * sink configuration as context so that individual sinks may use them.
+ */
 public class SinkContext {
     private final String tagsTargetKey;
     private final Collection<String> routes;
@@ -16,10 +20,18 @@ public class SinkContext {
         this.routes = routes;
     }
     
+    /**
+     * returns the target key name for tags if configured for a given sink
+     * @return tags target key
+     */
     public String getTagsTargetKey() {
         return tagsTargetKey;
     }
 
+    /**
+     * returns routes if configured for a given sink
+     * @return routes
+     */
     public Collection<String> getRoutes() {
         return routes;
     }
