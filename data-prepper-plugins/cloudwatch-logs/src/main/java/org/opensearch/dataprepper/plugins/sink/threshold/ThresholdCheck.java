d0ebb26e7bec910316cade6fe97a0b22593a92ce
Marcos Gonzalez Mayedo <alemayed@amazon.com>
2023-06-26 15:42:38 -0700

    Added tests for ThresholdCheck and function documentation
    Signed-off-by: Marcos Gonzalez Mayedo <alemayed@amazon.com>

--- data-prepper-plugins/cloudwatch-logs/src/main/java/org/opensearch/dataprepper/plugins/sink/threshold/ThresholdCheck.java
+++ data-prepper-plugins/cloudwatch-logs/src/main/java/org/opensearch/dataprepper/plugins/sink/threshold/ThresholdCheck.java
@@ -20,18 +20,40 @@ public class ThresholdCheck {
         this.logSendInterval = logSendInterval;
     }
 
+    /**
+     * Checks if the interval passed in is equal to or greater
+     * than the threshold interval for sending PutLogEvents.
+     * @param currentTime int
+     * @return boolean - true if greater than or equal to logInterval, false otherwise.
+     */
     public boolean checkLogSendInterval(int currentTime) {
         return currentTime >= logSendInterval;
     }
 
+    /**
+     * Determines if the event size is greater than the max event size.
+     * @param eventSize int denoting size of event.
+     * @return boolean - true if greater than MaxEventSize, false otherwise.
+     */
     public boolean checkMaxEventSize(int eventSize) {
         return eventSize > maxEventSize;
     }
 
+    /**
+     * Checks if the request size is greater than or equal to the current size passed in.
+     * @param currentRequestSize int denoting size of request(Sum of PutLogEvent messages).
+     * @return boolean - true if greater than or equal to the Max request size, smaller otherwise.
+     */
     public boolean checkMaxRequestSize(int currentRequestSize) {
         return currentRequestSize >= maxRequestSize;
     }
 
+    /**
+     * Checks if the current batch size is equal to the threshold
+     * batch size.
+     * @param batchSize int denoting the size of the batch of PutLogEvents.
+     * @return boolean - true if equal, false otherwise.
+     */
     public boolean checkBatchSize(int batchSize) {
         return batchSize == this.batchSize;
     }
--- data-prepper-plugins/cloudwatch-logs/src/test/java/org/opensearch/dataprepper/plugins/sink/threshold/ThresholdCheckTest.java
+++ data-prepper-plugins/cloudwatch-logs/src/test/java/org/opensearch/dataprepper/plugins/sink/threshold/ThresholdCheckTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.opensearch.dataprepper.plugins.sink.config.ThresholdConfig;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 public class ThresholdCheckTest {
     private ThresholdCheck thresholdCheck;
 
@@ -16,8 +19,49 @@ public class ThresholdCheckTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, })
-    void check_batchSize_valid() {
+    @ValueSource(ints = {1, 2, 4})
+    void check_batchSize_invalid(final int batch_size) {
+        assertThat(thresholdCheck.checkBatchSize(batch_size), is(false));
+    }
 
+    @Test
+    void check_batchSize_valid() {
+        assertThat(thresholdCheck.checkBatchSize(ThresholdConfig.DEFAULT_BATCH_SIZE), is(true));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {10, 30, 50})
+    void check_log_send_interval_invalid(final int send_interval) {
+        assertThat(thresholdCheck.checkLogSendInterval(send_interval), is(false));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {60, 80, 100})
+    void check_log_send_interval_valid(final int send_interval) {
+        assertThat(thresholdCheck.checkLogSendInterval(send_interval), is(true));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {55, 80, 100})
+    void check_max_event_size_invalid(final int event_size) {
+        assertThat(thresholdCheck.checkMaxEventSize(event_size), is(true));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {10, 30, 50})
+    void check_max_event_size_valid(final int event_size) {
+        assertThat(thresholdCheck.checkMaxEventSize(event_size), is(false));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {10000, 250000, 500000})
+    void check_max_request_size_invalid(final int request_size) {
+        assertThat(thresholdCheck.checkMaxRequestSize(request_size), is(false));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {550000, 750000, 1000000})
+    void check_max_request_size_valid(final int request_size) {
+        assertThat(thresholdCheck.checkMaxRequestSize(request_size), is(true));
     }
 }
