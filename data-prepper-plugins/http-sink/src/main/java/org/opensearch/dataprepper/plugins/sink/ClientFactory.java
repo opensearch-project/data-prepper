c78de957040e664ec8e49ff1fb3a193295c1b721
mallikagogoi7 <mallikagogoi7@gmail.com>
2023-07-05 21:57:57 +0530

    Boiler plate code of HttpSink for #874. (#2916)
    * Boiler plate code of HttpSink for #874.
    Signed-off-by: mallikagogoi7 <mallikagogoi7@gmail.com>
    
    * Added copyright on classes of HttpSink for #874.
    Signed-off-by: mallikagogoi7 <mallikagogoi7@gmail.com>
    
    * Moved Accumulator package to common for #874.
    Signed-off-by: mallikagogoi7 <mallikagogoi7@gmail.com>
    
    * Test cases added for accumulator related classes for #874.
    Signed-off-by: mallikagogoi7 <mallikagogoi7@gmail.com>
    
    * Added HttpSink related methos in accumulator for #874.
    Signed-off-by: mallikagogoi7 <mallikagogoi7@gmail.com>
    
    * Removed plugin specific methods from common for #874.
    Signed-off-by: mallikagogoi7 <mallikagogoi7@gmail.com>

---
+++ data-prepper-plugins/http-sink/src/main/java/org/opensearch/dataprepper/plugins/sink/ClientFactory.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.opensearch.dataprepper.plugins.sink;
+
+/**
+ * AWS factory class required to create AWS Http Endpoint client for this plugin.
+ */
+public final class ClientFactory {
+    private ClientFactory() {}
+
+}
