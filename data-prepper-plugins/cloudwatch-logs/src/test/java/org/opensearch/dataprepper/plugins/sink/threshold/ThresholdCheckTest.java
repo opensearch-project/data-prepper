976f06a235def11e745f7f0f5555908a98199df6
Marcos Gonzalez Mayedo <alemayed@amazon.com>
2023-07-10 12:45:57 -0700

    Removed unused methods/comments
    Signed-off-by: Marcos Gonzalez Mayedo <alemayed@amazon.com>

--- data-prepper-plugins/cloudwatch-logs/src/test/java/org/opensearch/dataprepper/plugins/sink/threshold/ThresholdCheckTest.java
+++ data-prepper-plugins/cloudwatch-logs/src/test/java/org/opensearch/dataprepper/plugins/sink/threshold/ThresholdCheckTest.java
@@ -24,31 +24,6 @@ public class ThresholdCheckTest {
                 ThresholdConfig.DEFAULT_SIZE_OF_REQUEST, ThresholdConfig.DEFAULT_LOG_SEND_INTERVAL_TIME);
     }
 
-//    @ParameterizedTest
-//    @ValueSource(ints = {1, 10, 24})
-//    void check_batchSize_invalid(final int batch_size) {
-//        assertThat(thresholdCheck.checkGreaterThanBatchSize(batch_size), is(false));
-//    }
-//
-//
-//
-//    @Test
-//    void check_batchSize_valid() {
-//        assertThat(thresholdCheck.checkGreaterThanBatchSize(ThresholdConfig.DEFAULT_BATCH_SIZE), is(false));
-//    }
-//
-//    @ParameterizedTest
-//    @ValueSource(ints = {1, 3, 4})
-//    void check_log_send_interval_invalid(final int send_interval) {
-//        assertThat(thresholdCheck.checkLogSendInterval(send_interval), is(false));
-//    }
-//
-//    @ParameterizedTest
-//    @ValueSource(ints = {60, 80, 100})
-//    void check_log_send_interval_valid(final int send_interval) {
-//        assertThat(thresholdCheck.checkLogSendInterval(send_interval), is(true));
-//    }
-
     @ParameterizedTest
     @ValueSource(ints = {55, 80, 100})
     void check_max_event_size_invalid(final int event_size) {
@@ -60,18 +35,6 @@ public class ThresholdCheckTest {
     void check_max_event_size_valid(final int event_size) {
         assertThat(thresholdCheck.checkGreaterThanMaxEventSize(event_size), is(false));
     }
-
-//    @ParameterizedTest
-//    @ValueSource(ints = {10000, 250000, 500000})
-//    void check_max_request_size_invalid(final int request_size) {
-//        assertThat(thresholdCheck.checkGreaterThanMaxRequestSize(request_size), is(false));
-//    }
-
-//    @ParameterizedTest
-//    @ValueSource(ints = {550000, 750000, 1000000})
-//    void check_max_request_size_valid(final int request_size) {
-//        assertThat(thresholdCheck.checkGreaterThanMaxRequestSize(request_size), is(true));
-//    }
 
     @ParameterizedTest
     @ValueSource(ints = {60, 80, 100})
