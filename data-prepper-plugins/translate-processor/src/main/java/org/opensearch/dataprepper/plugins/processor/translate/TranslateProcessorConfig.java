ec37e23ccc3875d458eca336c2bfeb6a6e56e35e
Vishal Boinapalli <vishalboinapalli3@gmail.com>
2023-07-13 22:18:06 -0700

    Translate Plugin: Simplified Config.  (#3022)
    * Translate Plugin: Simplified Config. Added functionality for multiple sources and multiple targets
    
    Signed-off-by: Vishal Boinapalli <vishalboinapalli3@gmail.com>
    
    * Moved helper methods out of config file
    
    Signed-off-by: Vishal Boinapalli <vishalboinapalli3@gmail.com>
    
    ---------
    
    Signed-off-by: Vishal Boinapalli <vishalboinapalli3@gmail.com>

--- data-prepper-plugins/translate-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/translate/TranslateProcessorConfig.java
+++ data-prepper-plugins/translate-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/translate/TranslateProcessorConfig.java
@@ -8,116 +8,32 @@ package org.opensearch.dataprepper.plugins.processor.translate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import org.opensearch.dataprepper.plugins.processor.mutateevent.TargetType;
-import org.opensearch.dataprepper.typeconverter.TypeConverter;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Stream;
 
 
 public class TranslateProcessorConfig {
 
-
-    @JsonProperty("source")
-    @NotNull
-    private Object source;
-
-    @JsonProperty("target")
-    @NotNull
-    @NotEmpty
-    private String target;
-
-    @JsonProperty("map")
-    private Map<String, Object> map;
-
     @JsonProperty("file_path")
     private String filePath;
 
-    @JsonProperty("default")
-    private String defaultValue;
+    @NotNull
+    @JsonProperty("mappings")
+    private List<MappingsParameterConfig> mappingsParameterConfigs;
 
-    @JsonProperty("translate_when")
-    private String translateWhen;
-
-    @JsonProperty("iterate_on")
-    private String iterateOn;
-
-    @JsonProperty("regex")
-    private RegexParameterConfiguration regexParameterConfiguration;
-
-    @JsonProperty("target_type")
-    private TargetType targetType = TargetType.STRING;
-
-
-    public Object getSource() { return source; }
-
-    public String getTarget() { return target; }
-
-    public Map<String, Object> getMap() { return map; }
-
-    public String getDefaultValue() { return defaultValue; }
-
-    public String getFilePath() { return filePath; }
-
-    public String getTranslateWhen() { return translateWhen; }
-
-    public String getIterateOn() { return iterateOn; }
-
-    public TargetType getTargetType() { return targetType; }
-
-    public RegexParameterConfiguration getRegexParameterConfiguration(){ return regexParameterConfiguration; }
-
-
-    @AssertTrue(message = "source field must be a string or list of strings")
-    public boolean isSourceFieldValid(){
-        if(source instanceof String){
-            return true;
-        }
-        if(source instanceof List<?>){
-            List<?>  sourceList = (List<?>) source;
-            return sourceList.stream().allMatch(sourceItem -> sourceItem instanceof String);
-        }
-        return false;
+    public String getFilePath() {
+        return filePath;
     }
 
-    @AssertTrue(message = "Either of map or patterns or file_path options need to be configured.")
+    public List<MappingsParameterConfig> getMappingsParameterConfigs() {
+        return mappingsParameterConfigs;
+    }
+
+    @AssertTrue(message = "Either mappings or file_path option needs to be configured.")
     public boolean hasMappings() {
-        return Stream.of(map, filePath, regexParameterConfiguration).filter(n -> n!=null).count() != 0;
+        return Stream.of(mappingsParameterConfigs, filePath).filter(n -> n != null).count() != 0;
     }
 
-    @AssertTrue(message = "pattern option is mandatory while configuring regex option")
-    public boolean isPatternPresent(){
-        return regexParameterConfiguration == null || regexParameterConfiguration.getPatterns() != null;
-    }
-
-    @AssertTrue(message = "The mapped values do not match the target type provided")
-    public boolean isMapTypeValid() {
-        return map.keySet().stream().allMatch(key -> checkTargetValueType(map.get(key)));
-    }
-
-    @AssertTrue(message = "The pattern values do not match the target type provided")
-    public boolean isPatternTypeValid() {
-        if (Objects.isNull(regexParameterConfiguration) || Objects.isNull(regexParameterConfiguration.getPatterns())) {
-            return true;
-        }
-        Map<String, Object> patterns = regexParameterConfiguration.getPatterns();
-        return patterns.keySet().stream().allMatch(key -> checkTargetValueType(patterns.get(key)));
-    }
-
-    private boolean checkTargetValueType(Object val) throws NumberFormatException {
-        if (Objects.isNull(targetType)) {
-            return true;
-        }
-        try {
-            final TypeConverter converter = targetType.getTargetConverter();
-            converter.convert(val);
-        } catch (Exception ex) {
-            return false;
-        }
-        return true;
-    }
 }
---
+++ data-prepper-plugins/translate-processor/src/test/java/org/opensearch/dataprepper/plugins/processor/translate/MappingsParameterConfigTest.java
@@ -0,0 +1,58 @@
+package org.opensearch.dataprepper.plugins.processor.translate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.opensearch.dataprepper.test.helper.ReflectivelySetField.setField;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.hamcrest.MatcherAssert.assertThat;
+import java.util.List;
+
+class MappingsParameterConfigTest {
+
+    private MappingsParameterConfig mappingsParameterConfig;
+
+    @BeforeEach
+    void setup() throws NoSuchFieldException, IllegalAccessException{
+        mappingsParameterConfig = new MappingsParameterConfig();
+        setField(MappingsParameterConfig.class, mappingsParameterConfig, "source", "sourceKey");
+    }
+
+    @Test
+    void test_get_iterate_on() throws NoSuchFieldException, IllegalAccessException{
+        assertNull(mappingsParameterConfig.getIterateOn());
+        setField(MappingsParameterConfig.class, mappingsParameterConfig, "iterateOn", "iteratorField");
+        assertThat(mappingsParameterConfig.getIterateOn(),is("iteratorField"));
+    }
+
+    @Test
+    void test_get_source() {
+        assertThat(mappingsParameterConfig.getSource(),is("sourceKey"));
+    }
+
+    @Test
+    void test_source_field_valid_types() throws NoSuchFieldException, IllegalAccessException{
+        setField(MappingsParameterConfig.class, mappingsParameterConfig, "source", "key1");
+        assertTrue(mappingsParameterConfig.isSourceFieldValid());
+        assertThat(mappingsParameterConfig.getSource(), is("key1"));
+        setField(MappingsParameterConfig.class, mappingsParameterConfig, "source", List.of("key1", "key2", "key3"));
+        assertTrue(mappingsParameterConfig.isSourceFieldValid());
+        assertThat(mappingsParameterConfig.getSource(), is(List.of("key1", "key2", "key3")));
+    }
+
+    @Test
+    void test_source_field_invalid_types() throws NoSuchFieldException, IllegalAccessException{
+        setField(MappingsParameterConfig.class, mappingsParameterConfig, "source", 200);
+        assertFalse(mappingsParameterConfig.isSourceFieldValid());
+        setField(MappingsParameterConfig.class, mappingsParameterConfig, "source", false);
+        assertFalse(mappingsParameterConfig.isSourceFieldValid());
+        setField(MappingsParameterConfig.class, mappingsParameterConfig, "source", 20.1);
+        assertFalse(mappingsParameterConfig.isSourceFieldValid());
+        setField(MappingsParameterConfig.class, mappingsParameterConfig, "source", List.of("key1", 200));
+        assertFalse(mappingsParameterConfig.isSourceFieldValid());
+    }
+
+}
