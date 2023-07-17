6f97c1631162b94d7cab12fd364a23c60df4c539
mallikagogoi7 <mallikagogoi7@gmail.com>
2023-07-17 20:19:32 +0530

    Connection code of HttpSink Plugin for #874. (#2987)
    Connection code of HttpSink Plugin.
    Signed-off-by: mallikagogoi7 <mallikagogoi7@gmail.com>
    Signed-off-by: George Chen <qchea@amazon.com>

--- data-prepper-plugins/http-sink/src/main/java/org/opensearch/dataprepper/plugins/sink/HTTPSink.java
+++ data-prepper-plugins/http-sink/src/main/java/org/opensearch/dataprepper/plugins/sink/HTTPSink.java
@@ -4,10 +4,15 @@
  */
 package org.opensearch.dataprepper.plugins.sink;
 
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.opensearch.dataprepper.aws.api.AwsCredentialsSupplier;
+import org.apache.hc.client5.http.HttpRequestRetryStrategy;
+import org.apache.hc.client5.http.impl.DefaultHttpRequestRetryStrategy;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.util.TimeValue;
 import org.opensearch.dataprepper.model.annotations.DataPrepperPlugin;
 import org.opensearch.dataprepper.model.annotations.DataPrepperPluginConstructor;
+import org.opensearch.dataprepper.model.configuration.PipelineDescription;
+import org.opensearch.dataprepper.model.configuration.PluginModel;
 import org.opensearch.dataprepper.model.configuration.PluginSetting;
 import org.opensearch.dataprepper.model.event.Event;
 import org.opensearch.dataprepper.model.plugin.InvalidPluginConfigurationException;
@@ -15,32 +20,57 @@ import org.opensearch.dataprepper.model.plugin.PluginFactory;
 import org.opensearch.dataprepper.model.record.Record;
 import org.opensearch.dataprepper.model.sink.AbstractSink;
 import org.opensearch.dataprepper.model.sink.Sink;
+import org.opensearch.dataprepper.plugins.accumulator.BufferFactory;
+import org.opensearch.dataprepper.plugins.accumulator.BufferTypeOptions;
+import org.opensearch.dataprepper.plugins.accumulator.InMemoryBufferFactory;
+import org.opensearch.dataprepper.plugins.accumulator.LocalFileBufferFactory;
 import org.opensearch.dataprepper.plugins.sink.configuration.HttpSinkConfiguration;
-import org.opensearch.dataprepper.plugins.sink.configuration.UrlConfigurationOption;
+import org.opensearch.dataprepper.plugins.sink.service.HttpSinkService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
 
 @DataPrepperPlugin(name = "http", pluginType = Sink.class, pluginConfigurationType = HttpSinkConfiguration.class)
 public class HTTPSink extends AbstractSink<Record<Event>> {
 
     private static final Logger LOG = LoggerFactory.getLogger(HTTPSink.class);
 
-    private final HttpSinkConfiguration httpSinkConfiguration;
-
     private volatile boolean sinkInitialized;
+
+    private final BufferFactory bufferFactory;
+
+    private final HttpSinkService httpSinkService;
 
     @DataPrepperPluginConstructor
     public HTTPSink(final PluginSetting pluginSetting,
                     final HttpSinkConfiguration httpSinkConfiguration,
                     final PluginFactory pluginFactory,
-                    final AwsCredentialsSupplier awsCredentialsSupplier) {
+                    final PipelineDescription pipelineDescription) {
         super(pluginSetting);
-        this.httpSinkConfiguration = httpSinkConfiguration;
-        sinkInitialized = Boolean.FALSE;
+        final PluginModel codecConfiguration = httpSinkConfiguration.getCodec();
+        final PluginSetting codecPluginSettings = new PluginSetting(codecConfiguration.getPluginName(),
+                codecConfiguration.getPluginSettings());
+        codecPluginSettings.setPipelineName(pipelineDescription.getPipelineName());
+        this.sinkInitialized = Boolean.FALSE;
+        if (httpSinkConfiguration.getBufferType().equals(BufferTypeOptions.LOCALFILE)) {
+            this.bufferFactory = new LocalFileBufferFactory();
+        } else {
+            this.bufferFactory = new InMemoryBufferFactory();
+        }
+        final HttpRequestRetryStrategy httpRequestRetryStrategy = new DefaultHttpRequestRetryStrategy(httpSinkConfiguration.getMaxUploadRetries(),
+                TimeValue.of(httpSinkConfiguration.getHttpRetryInterval()));
+
+        final HttpClientBuilder httpClientBuilder = HttpClients.custom()
+                .setRetryStrategy(httpRequestRetryStrategy);
+
+        this.httpSinkService = new HttpSinkService(
+                httpSinkConfiguration,
+                bufferFactory,
+                codecPluginSettings,
+                httpClientBuilder,
+                pluginMetrics);
     }
 
     @Override
@@ -75,23 +105,6 @@ public class HTTPSink extends AbstractSink<Record<Event>> {
         if (records.isEmpty()) {
             return;
         }
-        //TODO:  call Service call method
-    }
-
-
-    public Optional<CloseableHttpClient> getAuthHandlerByConfig(final HttpSinkConfiguration sinkConfiguration){
-        //TODO: AWS Sigv4 - check
-        // TODO: call Auth Handlers based on auth Type
-
-        return null;
-    }
-
-    public List<HttpAuthOptions> getClassicHttpRequestList(final List<UrlConfigurationOption> urlConfigurationOption){
-        // logic for create auth handler for each url based on provided configuration - getAuthHandlerByConfig()
-        // logic for request preparation for each url
-        // logic for worker is not there in url level then verify the global workers if global workers also not defined then default 1
-        // logic for get the Proxy object if url level proxy enabled else look the global proxy.
-        // Aws SageMaker headers if headers found in the configuration
-        return null;
+        httpSinkService.output(records);
     }
 }
