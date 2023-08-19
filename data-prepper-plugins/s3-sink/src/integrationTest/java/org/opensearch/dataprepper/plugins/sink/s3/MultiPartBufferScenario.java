f0649bb951df94cb147c61b55c92a12a6609f011
David Venable <dlv@amazon.com>
2023-08-18 19:44:35 -0500

    Improve the S3 sink integration tests combinations. The tests are now more consistent and avoid some redundant tests, thus also running faster. Sets up to have fewer combinations while testing all codecs.
    Signed-off-by: David Venable <dlv@amazon.com>

--- data-prepper-plugins/s3-sink/src/integrationTest/java/org/opensearch/dataprepper/plugins/sink/s3/MultiPartBufferScenario.java
+++ data-prepper-plugins/s3-sink/src/integrationTest/java/org/opensearch/dataprepper/plugins/sink/s3/MultiPartBufferScenario.java
@@ -7,8 +7,6 @@ package org.opensearch.dataprepper.plugins.sink.s3;
 
 import org.opensearch.dataprepper.plugins.sink.s3.accumulator.BufferTypeOptions;
 
-import static org.opensearch.dataprepper.plugins.sink.s3.S3SinkIT.LARGE_OBJECT_SIZE;
-
 public class MultiPartBufferScenario implements BufferScenario {
     @Override
     public BufferTypeOptions getBufferType() {
@@ -17,6 +15,6 @@ public class MultiPartBufferScenario implements BufferScenario {
 
     @Override
     public int getMaximumNumberOfEvents() {
-        return LARGE_OBJECT_SIZE;
+        return SizeCombination.LARGE.getTotalSize();
     }
 }
