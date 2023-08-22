3673d020675a40bf8fcb43e3eb03db0de576bb75
David Venable <dlv@amazon.com>
2023-08-22 10:25:03 -0700

    Adds an auto_schema flag to require a user to be explicit in using auto-schema generation. (#3206)
    Signed-off-by: David Venable <dlv@amazon.com>

--- data-prepper-plugins/avro-codecs/src/main/java/org/opensearch/dataprepper/plugins/codec/avro/AvroOutputCodecConfig.java
+++ data-prepper-plugins/avro-codecs/src/main/java/org/opensearch/dataprepper/plugins/codec/avro/AvroOutputCodecConfig.java
@@ -5,6 +5,7 @@
 package org.opensearch.dataprepper.plugins.codec.avro;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.AssertTrue;
 
 /**
  * Configuration class for {@link AvroOutputCodec}.
@@ -13,11 +14,27 @@ public class AvroOutputCodecConfig {
     @JsonProperty("schema")
     private String schema;
 
+    @JsonProperty("auto_schema")
+    private boolean autoSchema;
+
+    @AssertTrue(message = "The Avro codec requires either defining a schema or setting auto_schema to true to automatically generate a schema.")
+    boolean isSchemaOrAutoSchemaDefined() {
+        return schema != null ^ autoSchema;
+    }
+
     public String getSchema() {
         return schema;
     }
 
-    public void setSchema(String schema) {
+    void setSchema(final String schema) {
         this.schema = schema;
+    }
+
+    public boolean isAutoSchema() {
+        return autoSchema;
+    }
+
+    void setAutoSchema(final boolean autoSchema) {
+        this.autoSchema = autoSchema;
     }
 }
--- data-prepper-plugins/s3-sink/src/main/java/org/opensearch/dataprepper/plugins/codec/parquet/ParquetOutputCodecConfig.java
+++ data-prepper-plugins/s3-sink/src/main/java/org/opensearch/dataprepper/plugins/codec/parquet/ParquetOutputCodecConfig.java
@@ -5,17 +5,34 @@
 package org.opensearch.dataprepper.plugins.codec.parquet;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.AssertTrue;
 
 public class ParquetOutputCodecConfig {
     @JsonProperty("schema")
     private String schema;
 
+    @JsonProperty("auto_schema")
+    private boolean autoSchema;
+
+    @AssertTrue(message = "The Parquet codec requires either defining a schema or setting auto_schema to true to automatically generate a schema.")
+    boolean isSchemaOrAutoSchemaDefined() {
+        return schema != null ^ autoSchema;
+    }
+
     public String getSchema() {
         return schema;
     }
 
-    public void setSchema(String schema) {
+    public void setSchema(final String schema) {
         this.schema = schema;
+    }
+
+    public boolean isAutoSchema() {
+        return autoSchema;
+    }
+
+    void setAutoSchema(final boolean autoSchema) {
+        this.autoSchema = autoSchema;
     }
 }
 
