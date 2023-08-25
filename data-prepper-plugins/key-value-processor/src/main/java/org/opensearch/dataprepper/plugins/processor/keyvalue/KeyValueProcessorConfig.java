8ba2ed1e954a5dd0c6a8094d30560c9d3bc466ae
Kat Shen <katshen@amazon.com>
2023-08-25 20:21:23 +0000

    add import statement
    Signed-off-by: Kat Shen <katshen@amazon.com>

--- data-prepper-plugins/key-value-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/keyvalue/KeyValueProcessorConfig.java
+++ data-prepper-plugins/key-value-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/keyvalue/KeyValueProcessorConfig.java
@@ -10,6 +10,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
