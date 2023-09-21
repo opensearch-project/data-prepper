11ea4b4fd53a9c71b03c1e4692ebcdd2d3e4ea8f
Taylor Gray <tylgry@amazon.com>
2023-09-21 00:46:00 -0500

    Add _id as additional sort key for point-in-time and search_after
    Signed-off-by: Taylor Gray <tylgry@amazon.com>

--- data-prepper-plugins/opensearch-source/src/main/java/org/opensearch/dataprepper/plugins/source/opensearch/worker/client/model/SearchContextType.java
+++ data-prepper-plugins/opensearch-source/src/main/java/org/opensearch/dataprepper/plugins/source/opensearch/worker/client/model/SearchContextType.java
@@ -5,22 +5,13 @@
 
 package org.opensearch.dataprepper.plugins.source.opensearch.worker.client.model;
 
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum SearchContextType {
     SCROLL("scroll"),
     POINT_IN_TIME("point_in_time"),
     NONE("none");
 
     private final String searchContextType;
-
     SearchContextType(final String searchContextType) {
         this.searchContextType = searchContextType;
     }
-
-    @JsonValue
-    public String getSearchContextType() {
-        return searchContextType;
-    }
-
 }
