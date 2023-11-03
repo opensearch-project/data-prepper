230f02d2ee2be4396a2c6ba3a571d3fc03abae92
Roman Kvasnytskyi <roman@kvasnytskyi.net>
2023-11-03 15:43:29 +0100

    Replace Guava Cache with Caffeine for OpenSearch integration
    Signed-off-by: Roman Kvasnytskyi <roman@kvasnytskyi.net>

--- data-prepper-plugins/opensearch/src/main/java/org/opensearch/dataprepper/plugins/sink/opensearch/index/DynamicIndexManager.java
+++ data-prepper-plugins/opensearch/src/main/java/org/opensearch/dataprepper/plugins/sink/opensearch/index/DynamicIndexManager.java
@@ -5,21 +5,20 @@
 
 package org.opensearch.dataprepper.plugins.sink.opensearch.index;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.client.opensearch.OpenSearchClient;
 import org.opensearch.dataprepper.plugins.sink.opensearch.OpenSearchSinkConfiguration;
-
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class DynamicIndexManager extends AbstractIndexManager {
-    private Cache<String, IndexManager> indexManagerCache;
-    final int CACHE_EXPIRE_AFTER_ACCESS_TIME_MINUTES = 30;
-    final int APPROXIMATE_INDEX_MANAGER_SIZE = 32;
+    private final Cache<String, IndexManager> indexManagerCache;
+    private static final int CACHE_EXPIRE_AFTER_ACCESS_TIME_MINUTES = 30;
+    private static final int APPROXIMATE_INDEX_MANAGER_SIZE = 32;
     private final long cacheSizeInKB = 1024;
     protected RestHighLevelClient restHighLevelClient;
     protected OpenSearchClient openSearchClient;
@@ -47,9 +46,8 @@ public class DynamicIndexManager extends AbstractIndexManager {
         this.restHighLevelClient = restHighLevelClient;
         this.openSearchSinkConfiguration = openSearchSinkConfiguration;
         this.clusterSettingsParser = clusterSettingsParser;
-        CacheBuilder<String, IndexManager> cacheBuilder = CacheBuilder.newBuilder()
+        Caffeine<String, IndexManager> cacheBuilder = Caffeine.newBuilder()
                         .recordStats()
-                        .concurrencyLevel(1)
                         .maximumWeight(cacheSizeInKB)
                         .expireAfterAccess(CACHE_EXPIRE_AFTER_ACCESS_TIME_MINUTES, TimeUnit.MINUTES)
                         .weigher((k, v) -> APPROXIMATE_INDEX_MANAGER_SIZE);
