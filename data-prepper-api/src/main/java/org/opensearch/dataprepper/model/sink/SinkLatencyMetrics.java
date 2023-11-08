541a2a7798f97c5be563762853609f3453b4d35f
kkondaka <41027584+kkondaka@users.noreply.github.com>
2023-11-08 07:51:55 -0800

    Change Latency Metric names (#3603)
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    Co-authored-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>

--- data-prepper-api/src/main/java/org/opensearch/dataprepper/model/sink/SinkLatencyMetrics.java
+++ data-prepper-api/src/main/java/org/opensearch/dataprepper/model/sink/SinkLatencyMetrics.java
@@ -13,8 +13,8 @@ import java.time.Duration;
 import java.time.Instant;
 
 public class SinkLatencyMetrics {
-    public static final String INTERNAL_LATENCY = "internalLatency";
-    public static final String EXTERNAL_LATENCY = "externalLatency";
+    public static final String INTERNAL_LATENCY = "PipelineLatency";
+    public static final String EXTERNAL_LATENCY = "EndToEndLatency";
     private final DistributionSummary internalLatencySummary;
     private final DistributionSummary externalLatencySummary;
 
