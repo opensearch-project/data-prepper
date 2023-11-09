c09e0f6b2888d590440375d8cc11be2798665b73
David Venable <dlv@amazon.com>
2023-11-08 21:36:55 -0600

    Improves the DefaultPluginFactory class design by creating a new class for providing arguments from the Application Context to plugin parameters.
    Signed-off-by: David Venable <dlv@amazon.com>

---
+++ data-prepper-core/src/main/java/org/opensearch/dataprepper/plugin/ApplicationContextToTypedSuppliers.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.plugin;
+
+import org.opensearch.dataprepper.model.acknowledgements.AcknowledgementSetManager;
+import org.opensearch.dataprepper.model.breaker.CircuitBreaker;
+import org.opensearch.dataprepper.model.event.EventFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+/**
+ * Injects types from the Application Context and makes them available
+ * for plugin constructors.
+ * <p>
+ * If you are providing a direct pass-through from the Application Context
+ * to a plugin constructor, add it here. It is the simplest place for it.
+ * Otherwise, you can add it to the {@link DefaultPluginFactory} directly.
+ */
+@Named
+class ApplicationContextToTypedSuppliers {
+    private final Map<Class<?>, Supplier<Object>> typedSuppliers;
+
+    @Inject
+    ApplicationContextToTypedSuppliers(
+            final EventFactory eventFactory,
+            final AcknowledgementSetManager acknowledgementSetManager,
+            @Autowired(required = false) final CircuitBreaker circuitBreaker
+    ) {
+        Objects.requireNonNull(eventFactory);
+        Objects.requireNonNull(acknowledgementSetManager);
+
+        typedSuppliers = Map.of(
+                EventFactory.class, () -> eventFactory,
+                AcknowledgementSetManager.class, () -> acknowledgementSetManager,
+                CircuitBreaker.class, () -> circuitBreaker
+        );
+    }
+
+    /**
+     * Gets a map of {@link Class} to a {@link Supplier} for each class. The
+     * {@link Supplier} supplies an instance of that type. The supplied value
+     * may be <b>null</b> for some types.
+     *
+     * @return Map of types to suppliers of concrete implementations.
+     */
+    Map<Class<?>, Supplier<Object>> getArgumentsSuppliers() {
+        return typedSuppliers;
+    }
+}
--- data-prepper-core/src/main/java/org/opensearch/dataprepper/plugin/ComponentPluginArgumentsContext.java
+++ data-prepper-core/src/main/java/org/opensearch/dataprepper/plugin/ComponentPluginArgumentsContext.java
@@ -5,16 +5,13 @@
 
 package org.opensearch.dataprepper.plugin;
 
-import org.opensearch.dataprepper.model.breaker.CircuitBreaker;
-import org.opensearch.dataprepper.model.plugin.PluginConfigObservable;
-import org.opensearch.dataprepper.model.sink.SinkContext;
 import org.opensearch.dataprepper.metrics.PluginMetrics;
 import org.opensearch.dataprepper.model.configuration.PipelineDescription;
 import org.opensearch.dataprepper.model.configuration.PluginSetting;
 import org.opensearch.dataprepper.model.plugin.InvalidPluginDefinitionException;
+import org.opensearch.dataprepper.model.plugin.PluginConfigObservable;
 import org.opensearch.dataprepper.model.plugin.PluginFactory;
-import org.opensearch.dataprepper.model.event.EventFactory;
-import org.opensearch.dataprepper.model.acknowledgements.AcknowledgementSetManager;
+import org.opensearch.dataprepper.model.sink.SinkContext;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 
@@ -60,32 +57,26 @@ class ComponentPluginArgumentsContext implements PluginArgumentsContext {
             typedArgumentsSuppliers.put(PluginFactory.class, () -> builder.pluginFactory);
         }
 
-        if (builder.eventFactory != null) {
-            typedArgumentsSuppliers.put(EventFactory.class, () -> builder.eventFactory);
-        }
-
-        if (builder.acknowledgementSetManager != null) {
-            typedArgumentsSuppliers.put(AcknowledgementSetManager.class, () -> builder.acknowledgementSetManager);
+        if (builder.sinkContext != null) {
+            typedArgumentsSuppliers.put(SinkContext.class, () -> builder.sinkContext);
         }
 
         if (builder.pluginConfigObservable != null) {
             typedArgumentsSuppliers.put(PluginConfigObservable.class, () -> builder.pluginConfigObservable);
         }
 
-        if (builder.sinkContext != null) {
-            typedArgumentsSuppliers.put(SinkContext.class, () -> builder.sinkContext);
+        if(builder.additionalTypeArgumentSuppliers != null) {
+            typedArgumentsSuppliers.putAll(builder.additionalTypeArgumentSuppliers);
         }
-
-        typedArgumentsSuppliers.put(CircuitBreaker.class, () -> builder.circuitBreaker);
     }
 
     @Override
     public Object[] createArguments(final Class<?>[] parameterTypes, final Object ... args) {
-        Map<Class<?>, Supplier<Object>> optionalArgumentsSuppliers = new HashMap<>();
+        final Map<Class<?>, Supplier<Object>> optionalArgumentsSuppliers = new HashMap<>();
         for (final Object arg: args) {
             if (Objects.nonNull(arg)) {
                 optionalArgumentsSuppliers.put(arg.getClass(), () -> arg);
-                for (final Class interfaceClass: arg.getClass().getInterfaces()) {
+                for (final Class<?> interfaceClass: arg.getClass().getInterfaces()) {
                     optionalArgumentsSuppliers.put(interfaceClass, () -> arg);
                 }
             }
@@ -134,11 +125,9 @@ class ComponentPluginArgumentsContext implements PluginArgumentsContext {
         private PluginFactory pluginFactory;
         private PipelineDescription pipelineDescription;
         private BeanFactory beanFactory;
-        private EventFactory eventFactory;
-        private AcknowledgementSetManager acknowledgementSetManager;
-        private PluginConfigObservable pluginConfigObservable;
         private SinkContext sinkContext;
-        private CircuitBreaker circuitBreaker;
+        private PluginConfigObservable pluginConfigObservable;
+        private Map<Class<?>, Supplier<Object>> additionalTypeArgumentSuppliers;
 
         Builder withPluginConfiguration(final Object pluginConfiguration) {
             this.pluginConfiguration = pluginConfiguration;
@@ -147,16 +136,6 @@ class ComponentPluginArgumentsContext implements PluginArgumentsContext {
 
         Builder withPluginSetting(final PluginSetting pluginSetting) {
             this.pluginSetting = pluginSetting;
-            return this;
-        }
-
-        Builder withEventFactory(final EventFactory eventFactory) {
-            this.eventFactory = eventFactory;
-            return this;
-        }
-
-        Builder withAcknowledgementSetManager(final AcknowledgementSetManager acknowledgementSetManager) {
-            this.acknowledgementSetManager = acknowledgementSetManager;
             return this;
         }
 
@@ -185,8 +164,8 @@ class ComponentPluginArgumentsContext implements PluginArgumentsContext {
             return this;
         }
 
-        Builder withCircuitBreaker(final CircuitBreaker circuitBreaker) {
-            this.circuitBreaker = circuitBreaker;
+        Builder withTypeArgumentSuppliers(final Map<Class<?>, Supplier<Object>> additionalTypeArgumentSuppliers) {
+            this.additionalTypeArgumentSuppliers = additionalTypeArgumentSuppliers;
             return this;
         }
 
