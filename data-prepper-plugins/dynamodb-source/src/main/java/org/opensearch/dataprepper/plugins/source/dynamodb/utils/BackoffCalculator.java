92224c2d25a0b458eb81dd760f7f3b7b87a445a1
Taylor Gray <tylgry@amazon.com>
2023-11-16 16:59:00 -0600

    Improve logging message for no shards found to indicate that export m… (#3681)
    Improve logging message for no shards found to indicate that export may still be ongoing
    
    Signed-off-by: Taylor Gray <tylgry@amazon.com>

--- data-prepper-plugins/dynamodb-source/src/main/java/org/opensearch/dataprepper/plugins/source/dynamodb/utils/BackoffCalculator.java
+++ data-prepper-plugins/dynamodb-source/src/main/java/org/opensearch/dataprepper/plugins/source/dynamodb/utils/BackoffCalculator.java
@@ -30,17 +30,30 @@ public class BackoffCalculator {
     static final Duration MAX_JITTER = Duration.ofSeconds(2);
     static final Duration MIN_JITTER = Duration.ofSeconds(-2);
 
+    private final boolean isExportConfigured;
+
+
+
+    public BackoffCalculator(final boolean isExportConfigured) {
+        this.isExportConfigured = isExportConfigured;
+    }
+
     public long calculateBackoffToAcquireNextShard(final int noAvailableShardCount, final AtomicInteger shardsAcquired) {
 
         // When no shards are available to process we backoff exponentially based on how many consecutive attempts have been made without getting a shard
         // This limits calls to the coordination store
         if (noAvailableShardCount > 0) {
-            if (noAvailableShardCount % 10 == 0) {
-                LOG.info("No shards acquired after {} attempts", noAvailableShardCount);
+            if (noAvailableShardCount % 50 == 0 && shardsAcquired.get() == 0) {
+                String errorMessage = String.format("No new shards acquired after %s attempts. This means that all shards are currently being consumed", noAvailableShardCount);
+
+                if (isExportConfigured) {
+                    errorMessage += ", or that the export is still in progress. New shards will not be consumed until the export is fully processed.";
+                }
+                LOG.info(errorMessage);
             }
 
             final long jitterMillis = MIN_JITTER.toMillis() + RANDOM.nextInt((int) (MAX_JITTER.toMillis() - MIN_JITTER.toMillis() + 1));
-            return max(1, min(STARTING_BACKOFF.toMillis() * pow(BACKOFF_RATE, noAvailableShardCount - 1) + jitterMillis, MAX_BACKOFF_NO_SHARDS_ACQUIRED.toMillis()));
+            return max(1, min(STARTING_BACKOFF.toMillis() * pow(BACKOFF_RATE, (int) min(noAvailableShardCount - 1, 8)) + jitterMillis, MAX_BACKOFF_NO_SHARDS_ACQUIRED.toMillis()));
         }
 
         // When shards are being acquired we backoff linearly based on how many shards this node is actively processing, to encourage a fast start but still a balance of shards between nodes
