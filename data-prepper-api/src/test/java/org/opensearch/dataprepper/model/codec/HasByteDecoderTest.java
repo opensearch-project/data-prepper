63b7fc6d450e4ca684d6793676d068ade4b20de6
David Venable <dlv@amazon.com>
2023-11-29 10:12:55 -0800

    Updates data-prepper-api fully to JUnit 5 and removes JUnit vintage from the Gradle project. (#3627)
    Signed-off-by: David Venable <dlv@amazon.com>

--- data-prepper-api/src/test/java/org/opensearch/dataprepper/model/codec/HasByteDecoderTest.java
+++ data-prepper-api/src/test/java/org/opensearch/dataprepper/model/codec/HasByteDecoderTest.java
@@ -5,9 +5,9 @@
 
 package org.opensearch.dataprepper.model.codec;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.spy;
 
 public class HasByteDecoderTest {
@@ -16,7 +16,7 @@ public class HasByteDecoderTest {
     public void testGetDecoder() {
         final HasByteDecoder hasByteDecoder = spy(HasByteDecoder.class);
 
-        Assert.assertEquals(null, hasByteDecoder.getDecoder());
+        assertEquals(null, hasByteDecoder.getDecoder());
     }
 
 }
