a93d9a78b56813506e64f1eb6781b19acbb89a86
Asif Sohail Mohammed <nsifmoh@amazon.com>
2023-12-13 10:29:07 -0600

    Add Avro integration test to s3 source (#3852)
    Signed-off-by: Asif Sohail Mohammed <nsifmoh@amazon.com>

--- data-prepper-plugins/s3-source/src/integrationTest/java/org/opensearch/dataprepper/plugins/source/s3/S3ObjectWorkerIT.java
+++ data-prepper-plugins/s3-source/src/integrationTest/java/org/opensearch/dataprepper/plugins/source/s3/S3ObjectWorkerIT.java
@@ -158,7 +158,8 @@ class S3ObjectWorkerIT {
                     new NewlineDelimitedRecordsGenerator(),
                     new JsonRecordsGenerator(),
                     new CsvRecordsGenerator(),
-                    new ParquetRecordsGenerator());
+                    new ParquetRecordsGenerator(),
+                    new AvroRecordsGenerator());
             final List<Integer> numberOfRecordsList = List.of(0, 1, 25, 500, 5000);
             final List<Integer> recordsToAccumulateList = List.of(1, 100, 1000);
             final List<Boolean> booleanList = List.of(Boolean.TRUE, Boolean.FALSE);
