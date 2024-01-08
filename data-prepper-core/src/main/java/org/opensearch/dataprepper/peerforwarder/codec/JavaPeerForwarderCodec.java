1fa8fcf25e09c4468b65b011cd67f5b617e9ef4a
George Chen <qchea@amazon.com>
2024-01-08 16:36:49 -0600

    MAINT: missing copyright in data-prepper-core
    Signed-off-by: George Chen <qchea@amazon.com>

--- data-prepper-core/src/main/java/org/opensearch/dataprepper/peerforwarder/codec/JavaPeerForwarderCodec.java
+++ data-prepper-core/src/main/java/org/opensearch/dataprepper/peerforwarder/codec/JavaPeerForwarderCodec.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.dataprepper.peerforwarder.codec;
 
 import org.opensearch.dataprepper.peerforwarder.model.PeerForwardingEvents;
--- data-prepper-core/src/main/java/org/opensearch/dataprepper/plugin/PluginConfigurationObservableFactory.java
+++ data-prepper-core/src/main/java/org/opensearch/dataprepper/plugin/PluginConfigurationObservableFactory.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.dataprepper.plugin;
 
 import org.opensearch.dataprepper.model.configuration.PluginSetting;
