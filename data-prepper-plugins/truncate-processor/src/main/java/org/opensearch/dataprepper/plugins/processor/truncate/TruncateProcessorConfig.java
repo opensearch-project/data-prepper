de7369f8d0a0456bbc4eed2517b65ffd00bf09e4
kkondaka <41027584+kkondaka@users.noreply.github.com>
2024-01-11 09:14:43 -0800

    Add truncate string processor (#3924)
    * Add truncate string processor
    
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    
    * Addressed review comments
    
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    
    * Added check for negative numbers in the config input
    
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    
    * Fixed checkstyle error
    
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    
    * Modified to make truncate processor a top level processor, not specific to strings
    
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    
    * Addressed review comments
    
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    
    * Updated documentation with correct configuration
    
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    
    * Fixed typos in the documentation
    
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    
    * Modified to allow more than one source keys in the config
    
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    
    * Modified to allow multiple entries under configuration
    
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    
    ---------
    
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    Co-authored-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    (cherry picked from commit c3fe808e11b02968091609360199cb313b4ab7c3)

---
+++ data-prepper-plugins/truncate-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/truncate/TruncateProcessorConfig.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.plugins.processor.truncate;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.AssertTrue; 
+import jakarta.validation.Valid;
+
+import java.util.List;
+
+public class TruncateProcessorConfig {
+    public static class Entry {
+        @NotEmpty
+        @NotNull
+        @JsonProperty("source_keys")
+        private List<String> sourceKeys;
+
+        @JsonProperty("start_at")
+        private Integer startAt;
+
+        @JsonProperty("length")
+        private Integer length;
+
+        @JsonProperty("truncate_when")
+        private String truncateWhen;
+
+        public Entry(final List<String> sourceKeys, final Integer startAt, final Integer length, final String truncateWhen) {
+            this.sourceKeys = sourceKeys;
+            this.startAt = startAt;
+            this.length = length;
+            this.truncateWhen = truncateWhen;
+        }
+
+        public Entry() {}
+
+        public List<String> getSourceKeys() {
+            return sourceKeys;
+        }
+
+        public Integer getStartAt() {
+            return startAt;
+        }
+
+        public Integer getLength() {
+            return length;
+        }
+
+        public String getTruncateWhen() {
+            return truncateWhen;
+        }
+
+        @AssertTrue(message = "source_keys must be specified and at least one of start_at or length or both must be specified and the values must be positive integers")
+        public boolean isValidConfig() {
+            if (length == null && startAt == null) {
+                return false;
+            }
+            if (length != null && length < 0) {
+                return false;
+            }
+            if (startAt != null && startAt < 0) {
+                return false;
+            }
+            return true;
+        }
+    }
+
+    @NotEmpty
+    @NotNull
+    private List<@Valid Entry> entries;
+
+    public List<Entry> getEntries() {
+        return entries;
+    }
+
+}
+
---
+++ data-prepper-plugins/truncate-processor/src/test/java/org/opensearch/dataprepper/plugins/processor/truncate/TruncateProcessorTests.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.plugins.processor.truncate;
+
+import org.opensearch.dataprepper.expression.ExpressionEvaluator;
+import org.opensearch.dataprepper.metrics.PluginMetrics;
+import org.opensearch.dataprepper.model.event.Event;
+import org.opensearch.dataprepper.model.event.JacksonEvent;
+import org.opensearch.dataprepper.model.record.Record;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class TruncateProcessorTests {
+
+    @Mock
+    private PluginMetrics pluginMetrics;
+
+    @Mock
+    private TruncateProcessorConfig config;
+
+    @Mock
+    private ExpressionEvaluator expressionEvaluator;
+
+    private TruncateProcessor createObjectUnderTest() {
+        return new TruncateProcessor(pluginMetrics, config, expressionEvaluator);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(TruncateArgumentsProvider.class)
+    void testTruncateProcessor(final Object messageValue, final Integer startAt, final Integer truncateLength, final Object truncatedMessage) {
+
+        when(config.getEntries()).thenReturn(Collections.singletonList(createEntry(List.of("message"), startAt, truncateLength, null)));
+        final TruncateProcessor truncateProcessor = createObjectUnderTest();
+        final Record<Event> record = createEvent("message", messageValue);
+        final List<Record<Event>> truncatedRecords = (List<Record<Event>>) truncateProcessor.doExecute(Collections.singletonList(record));
+        assertThat(truncatedRecords.get(0).getData().get("message", Object.class), notNullValue());
+        assertThat(truncatedRecords.get(0).getData().get("message", Object.class), equalTo(truncatedMessage));
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(MultipleTruncateArgumentsProvider.class)
+    void testTruncateProcessorMultipleEntries(final Object messageValue, final Integer startAt1, final Integer truncateLength1, final Integer startAt2, final Integer truncateLength2, final Object truncatedMessage1, final Object truncatedMessage2) {
+        TruncateProcessorConfig.Entry entry1 = createEntry(List.of("message1"), startAt1, truncateLength1, null);
+        TruncateProcessorConfig.Entry entry2 = createEntry(List.of("message2"), startAt2, truncateLength2, null);
+        when(config.getEntries()).thenReturn(List.of(entry1, entry2));
+        final Record<Event> record1 = createEvent("message1", messageValue);
+        final Record<Event> record2 = createEvent("message2", messageValue);
+        final TruncateProcessor truncateProcessor = createObjectUnderTest();
+        final List<Record<Event>> truncatedRecords = (List<Record<Event>>) truncateProcessor.doExecute(List.of(record1, record2));
+        assertThat(truncatedRecords.get(0).getData().get("message1", Object.class), notNullValue());
+        assertThat(truncatedRecords.get(1).getData().get("message2", Object.class), notNullValue());
+        assertThat(truncatedRecords.get(0).getData().get("message1", Object.class), equalTo(truncatedMessage1));
+        assertThat(truncatedRecords.get(1).getData().get("message2", Object.class), equalTo(truncatedMessage2));
+    }
+
+    @Test
+    void test_event_is_the_same_when_truncateWhen_condition_returns_false() {
+        final String truncateWhen = UUID.randomUUID().toString();
+        final String message = UUID.randomUUID().toString();
+
+        when(config.getEntries()).thenReturn(Collections.singletonList(createEntry(List.of("message"), null, 5, truncateWhen)));
+
+        final TruncateProcessor truncateProcessor = createObjectUnderTest();
+        final Record<Event> record = createEvent("message", message);
+        when(expressionEvaluator.evaluateConditional(truncateWhen, record.getData())).thenReturn(false);
+        final List<Record<Event>> truncatedRecords = (List<Record<Event>>) truncateProcessor.doExecute(Collections.singletonList(record));
+
+        assertThat(truncatedRecords.get(0).getData().toMap(), equalTo(record.getData().toMap()));
+    }
+
+    private TruncateProcessorConfig.Entry createEntry(final List<String> sourceKeys, final Integer startAt, final Integer length, final String truncateWhen) {
+        return new TruncateProcessorConfig.Entry(sourceKeys, startAt, length, truncateWhen);
+    }
+
+    private Record<Event> createEvent(final String key, final Object value) {
+        final Map<String, Object> eventData = new HashMap<>();
+        eventData.put(key, value);
+        return new Record<>(JacksonEvent.builder()
+                .withEventType("event")
+                .withData(eventData)
+                .build());
+    }
+
+    static class TruncateArgumentsProvider implements ArgumentsProvider {
+
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    arguments("hello,world,no-truncate", 0, 100, "hello,world,no-truncate"),
+                    arguments("hello,world,no-truncate", 6, 100, "world,no-truncate"),
+                    arguments("hello,world,no-truncate", 6, 16, "world,no-truncat"),
+                    arguments("hello,world,no-truncate", 6, 17, "world,no-truncate"),
+                    arguments("hello,world,no-truncate", 6, 18, "world,no-truncate"),
+                    arguments("hello,world,no-truncate", 6, 5, "world"),
+                    arguments("hello,world,no-truncate", 6, null, "world,no-truncate"),
+
+                    arguments("hello,world,no-truncate", null, 100, "hello,world,no-truncate"),
+                    arguments("hello,world,truncate", null, 11, "hello,world"),
+                    arguments("hello,world", null, 1, "h"),
+                    arguments("hello", null, 0, ""),
+                    arguments(List.of("hello_one", "hello_two", "hello_three"), null, 5, List.of("hello", "hello", "hello")),
+                    arguments(List.of("hello_one", 2, "hello_three"), null, 5, List.of("hello", 2, "hello"))
+            );
+        }
+    }
+
+    static class MultipleTruncateArgumentsProvider implements ArgumentsProvider {
+
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    arguments("hello,world,no-truncate", 0, 100, 1, 10, "hello,world,no-truncate", "ello,world"),
+                    arguments("hello,world,no-truncate", 6, 100, null, 5, "world,no-truncate", "hello"),
+                    arguments("hello,world,no-truncate", 6, 16, 12, null, "world,no-truncat", "no-truncate")
+            );
+        }
+    }
+
+}
+
