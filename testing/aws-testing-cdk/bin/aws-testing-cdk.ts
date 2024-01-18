6191af077ca58b8c3351f8708014abc854890bfa
David Venable <dlv@amazon.com>
2024-01-18 13:52:34 -0600

    Adds a new integration test for using the Kafka buffer with KMS encryption. Includes a new KMS CDK stack for any projects that need KMS to use. Some improvements to the CDK stack. Resolves #3980
    Signed-off-by: David Venable <dlv@amazon.com>

--- testing/aws-testing-cdk/bin/aws-testing-cdk.ts
+++ testing/aws-testing-cdk/bin/aws-testing-cdk.ts
@@ -3,10 +3,15 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import {GitHubAccessStack} from '../lib/common/GitHubAccessStack';
 import {SecretsManagerStack} from '../lib/aws-secrets-manager/SecretsManagerStack';
+import {KmsStack} from "../lib/common/KmsStack";
 
 const app = new cdk.App();
 
 const githubStack = new GitHubAccessStack(app, 'GitHubStack', {});
+
+new KmsStack(app, 'CommonKmsStack', {
+  testingRole: githubStack.gitHubActionsTestingRole
+})
 
 new SecretsManagerStack(app, 'SecretsManagerStack', {
   testingRole: githubStack.gitHubActionsTestingRole
