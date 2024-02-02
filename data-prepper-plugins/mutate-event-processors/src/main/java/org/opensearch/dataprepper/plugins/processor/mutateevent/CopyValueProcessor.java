0801605b5f4d6cc43c96c82db8d7e62b16cd27f9
Hai Yan <oeyh@amazon.com>
2024-02-02 11:46:47 -0600

    initial experiment
    Signed-off-by: Hai Yan <oeyh@amazon.com>

--- data-prepper-plugins/mutate-event-processors/src/main/java/org/opensearch/dataprepper/plugins/processor/mutateevent/CopyValueProcessor.java
+++ data-prepper-plugins/mutate-event-processors/src/main/java/org/opensearch/dataprepper/plugins/processor/mutateevent/CopyValueProcessor.java
@@ -14,19 +14,23 @@ import org.opensearch.dataprepper.model.processor.AbstractProcessor;
 import org.opensearch.dataprepper.model.processor.Processor;
 import org.opensearch.dataprepper.model.record.Record;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @DataPrepperPlugin(name = "copy_values", pluginType = Processor.class, pluginConfigurationType = CopyValueProcessorConfig.class)
 public class CopyValueProcessor extends AbstractProcessor<Record<Event>, Record<Event>> {
+    private final CopyValueProcessorConfig config;
     private final List<CopyValueProcessorConfig.Entry> entries;
-
     private final ExpressionEvaluator expressionEvaluator;
 
     @DataPrepperPluginConstructor
     public CopyValueProcessor(final PluginMetrics pluginMetrics, final CopyValueProcessorConfig config, final ExpressionEvaluator expressionEvaluator) {
         super(pluginMetrics);
+        this.config = config;
         this.entries = config.getEntries();
         this.expressionEvaluator = expressionEvaluator;
     }
@@ -35,19 +39,44 @@ public class CopyValueProcessor extends AbstractProcessor<Record<Event>, Record<
     public Collection<Record<Event>> doExecute(final Collection<Record<Event>> records) {
         for(final Record<Event> record : records) {
             final Event recordEvent = record.getData();
-            for(CopyValueProcessorConfig.Entry entry : entries) {
-                if (Objects.nonNull(entry.getCopyWhen()) && !expressionEvaluator.evaluateConditional(entry.getCopyWhen(), recordEvent)) {
-                    continue;
-                }
+            if (config.getMode() == CopyValueProcessorConfig.Mode.NORMAL) {
+                for (CopyValueProcessorConfig.Entry entry : entries) {
+                    if (Objects.nonNull(entry.getCopyWhen()) && !expressionEvaluator.evaluateConditional(entry.getCopyWhen(), recordEvent)) {
+                        continue;
+                    }
 
-                if (entry.getFromKey().equals(entry.getToKey()) || !recordEvent.containsKey(entry.getFromKey())) {
-                    continue;
-                }
+                    if (entry.getFromKey().equals(entry.getToKey()) || !recordEvent.containsKey(entry.getFromKey())) {
+                        continue;
+                    }
 
-                if (!recordEvent.containsKey(entry.getToKey()) || entry.getOverwriteIfToKeyExists()) {
-                    final Object source = recordEvent.get(entry.getFromKey(), Object.class);
-                    recordEvent.put(entry.getToKey(), source);
+                    if (!recordEvent.containsKey(entry.getToKey()) || entry.getOverwriteIfToKeyExists()) {
+                        final Object source = recordEvent.get(entry.getFromKey(), Object.class);
+                        recordEvent.put(entry.getToKey(), source);
+                    }
                 }
+            } else if (config.getMode() == CopyValueProcessorConfig.Mode.LIST) {
+                List<Map<String, Object>> sourceList = recordEvent.get(config.getSource(), List.class);
+                List<Map<String, Object>> targetList = new ArrayList<>();
+
+                Map<CopyValueProcessorConfig.Entry, Boolean> whenConditions = new HashMap<>();
+                for (CopyValueProcessorConfig.Entry entry : entries) {
+                    if (Objects.nonNull(entry.getCopyWhen()) && !expressionEvaluator.evaluateConditional(entry.getCopyWhen(), recordEvent)) {
+                        whenConditions.put(entry, Boolean.FALSE);
+                    } else {
+                        whenConditions.put(entry, Boolean.TRUE);
+                    }
+                }
+                for (Map<String, Object> sourceField : sourceList) {
+                    final Map<String, Object> targetItem = new HashMap<>();
+                    for (CopyValueProcessorConfig.Entry entry : entries) {
+                        if (!whenConditions.get(entry) || !sourceField.containsKey(entry.getFromKey())) {
+                            continue;
+                        }
+                        targetItem.put(entry.getToKey(), sourceField.get(entry.getFromKey()));
+                    }
+                    targetList.add(targetItem);
+                }
+                recordEvent.put(config.getTarget(), targetList);
             }
         }
 
--- data-prepper-plugins/mutate-event-processors/src/main/java/org/opensearch/dataprepper/plugins/processor/mutateevent/CopyValueProcessorConfig.java
+++ data-prepper-plugins/mutate-event-processors/src/main/java/org/opensearch/dataprepper/plugins/processor/mutateevent/CopyValueProcessorConfig.java
@@ -5,15 +5,41 @@
 
 package org.opensearch.dataprepper.plugins.processor.mutateevent;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CopyValueProcessorConfig {
-   public static class Entry {
+    enum Mode {
+        NORMAL("normal"),
+        LIST("list");
+
+        private final String name;
+
+        private static final Map<String, Mode> ACTIONS_MAP = Arrays.stream(Mode.values())
+                .collect(Collectors.toMap(
+                        value -> value.name,
+                        value -> value
+                ));
+
+        Mode(String name) {
+            this.name = name.toLowerCase();
+        }
+
+        @JsonCreator
+        static Mode fromOptionValue(final String option) {
+            return ACTIONS_MAP.get(option);
+        }
+    }
+
+    public static class Entry {
         @NotEmpty
         @NotNull
         @JsonProperty("from_key")
@@ -61,7 +87,28 @@ public class CopyValueProcessorConfig {
     @Valid
     private List<Entry> entries;
 
+    @JsonProperty("source")
+    private String source;
+
+    @JsonProperty("target")
+    private String target;
+
+    @JsonProperty("mode")
+    private Mode mode = Mode.NORMAL;
+
     public List<Entry> getEntries() {
         return entries;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public Mode getMode() {
+        return mode;
     }
 }
