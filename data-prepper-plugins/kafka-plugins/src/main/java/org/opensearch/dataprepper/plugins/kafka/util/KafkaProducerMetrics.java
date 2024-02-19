845569c43299ebbdfd3a909dffda710a09fbbc57
David Venable <dlv@amazon.com>
2024-02-19 11:52:13 -0800

    Registers some common producer metrics in the Kafka buffer. (#4139)
    Signed-off-by: David Venable <dlv@amazon.com>

---
+++ data-prepper-plugins/kafka-plugins/src/main/java/org/opensearch/dataprepper/plugins/kafka/util/KafkaProducerMetrics.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.plugins.kafka.util;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.common.Metric;
+import org.apache.kafka.common.MetricName;
+import org.opensearch.dataprepper.metrics.PluginMetrics;
+
+import java.util.Map;
+
+/**
+ * Metrics for a Kafka producer. These span all topics, which makes it distinct from
+ * the {@link KafkaTopicProducerMetrics}.
+ */
+public final class KafkaProducerMetrics {
+    static final Map<String, String> METRICS_NAME_MAP = Map.of(
+            "record-queue-time-avg", "recordQueueTimeAvg",
+            "record-queue-time-max", "recordQueueTimeMax",
+            "buffer-exhausted-rate", "bufferExhaustedRate",
+            "buffer-available-bytes", "bufferAvailableBytes"
+    );
+
+    private KafkaProducerMetrics() { }
+
+    public static void registerProducer(final PluginMetrics pluginMetrics, final KafkaProducer<?, ?> kafkaProducer) {
+        final Map<MetricName, ? extends Metric> kafkaProducerMetrics = kafkaProducer.metrics();
+        for (final Map.Entry<MetricName, ? extends Metric> metricNameEntry : kafkaProducerMetrics.entrySet()) {
+            final MetricName kafkaMetricName = metricNameEntry.getKey();
+
+            final String dataPrepperMetricName = METRICS_NAME_MAP.get(kafkaMetricName.name());
+            if(dataPrepperMetricName == null)
+                continue;
+
+            final Metric metric = metricNameEntry.getValue();
+
+            pluginMetrics.gauge(dataPrepperMetricName, metric, m -> (double) m.metricValue());
+        }
+    }
+}
