afc85e0e314cb229bea3c2111101ea6136f8c1f2
Taylor Gray <tylgry@amazon.com>
2024-03-05 13:18:05 -0600

    Change grok performance_metadata to be disabled by default (#4235)
    Signed-off-by: Taylor Gray <tylgry@amazon.com>

--- data-prepper-plugins/grok-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/grok/GrokProcessorConfig.java
+++ data-prepper-plugins/grok-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/grok/GrokProcessorConfig.java
@@ -100,7 +100,7 @@ public class GrokProcessorConfig {
                 pluginSetting.getStringOrDefault(GROK_WHEN, null),
                 pluginSetting.getTypedList(TAGS_ON_MATCH_FAILURE, String.class),
                 pluginSetting.getTypedList(TAGS_ON_TIMEOUT, String.class),
-                pluginSetting.getBooleanOrDefault(INCLUDE_PERFORMANCE_METADATA, true));
+                pluginSetting.getBooleanOrDefault(INCLUDE_PERFORMANCE_METADATA, false));
     }
 
     public boolean isBreakOnMatch() {
