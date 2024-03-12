aa40256d341f6f88ea22fd7083da11f14f63caff
David Venable <dlv@amazon.com>
2024-03-12 08:40:21 -0700

    Adds a GitHub Action to verify the aws-testing-cdk project. It runs the linter and tests. Adds a unit test for this project. Correct the formatting to pass the linter. (#4263)
    Signed-off-by: David Venable <dlv@amazon.com>

--- testing/aws-testing-cdk/bin/aws-testing-cdk.ts
+++ testing/aws-testing-cdk/bin/aws-testing-cdk.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import {GitHubAccessStack} from '../lib/common/GitHubAccessStack';
 import {SecretsManagerStack} from '../lib/aws-secrets-manager/SecretsManagerStack';
-import {KmsStack} from "../lib/common/KmsStack";
+import {KmsStack} from '../lib/common/KmsStack';
 
 const app = new cdk.App();
 
