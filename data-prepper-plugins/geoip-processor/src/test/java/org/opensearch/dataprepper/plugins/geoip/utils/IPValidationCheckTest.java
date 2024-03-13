6611631b2eb4a9951641558bba55902e2dbf5406
Hai Yan <8153134+oeyh@users.noreply.github.com>
2024-03-13 16:54:33 -0500

    Fix ip check and add more tests (#4278)
    Signed-off-by: Hai Yan <oeyh@amazon.com>

--- data-prepper-plugins/geoip-processor/src/test/java/org/opensearch/dataprepper/plugins/geoip/utils/IPValidationCheckTest.java
+++ data-prepper-plugins/geoip-processor/src/test/java/org/opensearch/dataprepper/plugins/geoip/utils/IPValidationCheckTest.java
@@ -5,30 +5,32 @@
 
 package org.opensearch.dataprepper.plugins.geoip.utils;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 class IPValidationCheckTest {
 
-    private static final String PRIVATE_IP_ADDRESS = "192.168.29.233";
-    private static final String PUBLIC_IP_ADDRESS = "2001:4860:4860::8888";
-    private static final String INVALID_IP_ADDRESS = "255.255.255.999";
-
-    @Test
-    void ipValidationcheckTest_public() {
-        Assertions.assertTrue(IPValidationCheck.isPublicIpAddress(PUBLIC_IP_ADDRESS));
+    @ParameterizedTest
+    @ValueSource(strings = {"93.184.216.34", "172.217.0.0", "142.250.64.0", "2607:f8b0:4005:805::200e"})
+    void ipValidationcheckTest_public(String publicIpAddress) {
+        assertThat(IPValidationCheck.isPublicIpAddress(publicIpAddress), is(true));
     }
 
-    @Test
-    void ipValidationcheckTest_negative() {
-        Assertions.assertFalse(IPValidationCheck.isPublicIpAddress(PRIVATE_IP_ADDRESS));
+    @ParameterizedTest
+    @ValueSource(strings = {"10.0.0.0", "172.16.0.0", "192.168.0.0"})
+    void ipValidationcheckTest_negative(String privateIpAddress) {
+        assertThat(IPValidationCheck.isPublicIpAddress(privateIpAddress), is(false));
     }
 
-    @Test
-    void ipValidationcheckTest_invalid() {
-        Assertions.assertFalse(IPValidationCheck.isPublicIpAddress(INVALID_IP_ADDRESS));
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "255.255.255.999", "true", "[1,2,3]"})
+    void ipValidationcheckTest_invalid(String invalidIpAddress) {
+        assertThat(IPValidationCheck.isPublicIpAddress(invalidIpAddress), is(false));
     }
 }
