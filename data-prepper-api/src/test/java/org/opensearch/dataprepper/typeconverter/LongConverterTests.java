bd81e92472af1f0e6aed89cf0ed5c87ac68f5c72
Utkarsh Agarwal <utkarsh07379@gmail.com>
2024-03-30 14:07:52 +0530

    Added 'long' as a target type for 'convert_entry_type' processor
    Signed-off-by: Utkarsh Agarwal <utkarsh07379@gmail.com>

--- data-prepper-api/src/test/java/org/opensearch/dataprepper/typeconverter/LongConverterTests.java
+++ data-prepper-api/src/test/java/org/opensearch/dataprepper/typeconverter/LongConverterTests.java
@@ -5,50 +5,53 @@
 
  package org.opensearch.dataprepper.typeconverter;
 
- import org.junit.jupiter.api.Test;
- 
  import static org.hamcrest.CoreMatchers.equalTo;
- import static org.hamcrest.MatcherAssert.assertThat;
- import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
  
  public class LongConverterTests {
-     @Test
-     void testStringToLongConversion() {
+     @ParameterizedTest
+     @ValueSource(strings = {"1234", "5678"})
+     void testStringToLongConversion(String stringValue) {
          LongConverter converter = new LongConverter();
-         final String stringConstant = "1234";
-         assertThat(converter.convert(stringConstant), equalTo(Long.parseLong(stringConstant)));
+         assertThat(converter.convert(stringValue), equalTo(Long.parseLong(stringValue)));
      }
-     @Test
-     void testfloatToLongConversion() {
+     @ParameterizedTest
+     @ValueSource(floats = {(float)1234.56789, Float.MAX_VALUE, Float.MIN_VALUE})
+     void testfloatToLongConversion(float floatValue) {
          LongConverter converter = new LongConverter();
-         final Float floatConstant = (float)1234.56789;
-         assertThat(converter.convert(floatConstant), equalTo((long)(float)floatConstant));
+         assertThat(converter.convert(floatValue), equalTo((long)(float)floatValue));
      }
-     @Test
-     void testDoubleToLongConversion() {
+     @ParameterizedTest
+     @ValueSource(doubles = {12345678.12345678, 2.0 * Integer.MAX_VALUE, Double.MAX_VALUE, Double.MIN_VALUE})
+     void testDoubleToLongConversion(double doubleValue) {
         LongConverter converter = new LongConverter();
-        final Double doubleConstant = (double)12345678.12345678;
-        assertThat(converter.convert(doubleConstant), equalTo((long)(double)doubleConstant));
+        assertThat(converter.convert(doubleValue), equalTo((long)(double)doubleValue));
      }
-     @Test
-     void testDoubleToLongConversionWithBigValue() {
+     @ParameterizedTest
+     @ValueSource(booleans = {false,true})
+     void testBooleanToLongConversion(boolean booleanValue) {
+         LongConverter converter = new LongConverter();
+         if (booleanValue)
+            assertThat(converter.convert(booleanValue), equalTo(1L));
+         else
+            assertThat(converter.convert(booleanValue), equalTo(0L));
+     }
+     @ParameterizedTest
+     @ValueSource(ints = {1234, Integer.MAX_VALUE, Integer.MIN_VALUE})
+     void testIntToLongConverstion(int intValue){
         LongConverter converter = new LongConverter();
-        final Double doubleConstant = (double)2.0 * Integer.MAX_VALUE;
-        assertThat(converter.convert(doubleConstant), equalTo((long)(double)doubleConstant));
+        assertThat(converter.convert(intValue), equalTo((long)intValue));
      }
-     @Test
-     void testBooleanToLongConversion() {
+     @ParameterizedTest
+     @ValueSource(longs = {(long)1234, Long.MAX_VALUE, Long.MIN_VALUE})
+     void testLongToLongConversion(long longValue) {
          LongConverter converter = new LongConverter();
-         final Boolean boolFalseConstant = false;
-         assertThat(converter.convert(boolFalseConstant), equalTo(0L));
-         final Boolean boolTrueConstant = true;
-         assertThat(converter.convert(boolTrueConstant), equalTo(1L));
-     }
-     @Test
-     void testLongToLongConversion() {
-         LongConverter converter = new LongConverter();
-         final Long longConstant = (long)1234;
-         assertThat(converter.convert(longConstant), equalTo(longConstant));
+         assertThat(converter.convert(longValue), equalTo(longValue));
      }
      @Test
      void testInvalidStringConversion() {
