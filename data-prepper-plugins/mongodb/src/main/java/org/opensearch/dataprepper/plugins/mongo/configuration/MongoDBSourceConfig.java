2f856dbf2a7254319a9bc6891c0707907d8e722c
Dinu John <86094133+dinujoh@users.noreply.github.com>
2024-04-22 19:22:32 -0500

    Update s3 path prefix logic
    Signed-off-by: Dinu John <86094133+dinujoh@users.noreply.github.com>

--- data-prepper-plugins/mongodb/src/main/java/org/opensearch/dataprepper/plugins/mongo/configuration/MongoDBSourceConfig.java
+++ data-prepper-plugins/mongodb/src/main/java/org/opensearch/dataprepper/plugins/mongo/configuration/MongoDBSourceConfig.java
@@ -5,7 +5,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.Duration;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
