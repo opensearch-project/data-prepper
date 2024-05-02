0311a766351877e78a90bc644dffb2d8ae8cfaec
Souvik Bose <souvik04in@gmail.com>
2024-05-02 09:43:59 -0400

    Asynchronous implementation of opensearch api pipeline with pipeline transformation.
    Signed-off-by: Souvik Bose <souvik04in@gmail.com>

---
+++ data-prepper-plugins/opensearch-api-source/src/main/java/org/opensearch/dataprepper/plugins/source/opensearch_api/OpenSearchAPISourceConfig.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.plugins.source.opensearch_api;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import org.opensearch.dataprepper.http.common.HttpServerConfig;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class OpenSearchAPISourceConfig extends HttpServerConfig {
+    static final String DEFAULT_LOG_INGEST_URI = "/";
+    static final int DEFAULT_PORT = 9202;
+
+    @JsonProperty("port")
+    @Min(0)
+    @Max(65535)
+    private int port = DEFAULT_PORT;
+
+    @JsonProperty("path_prefix")
+    @Size(min = 1, message = "path length should be at least 1")
+    private String path_prefix = DEFAULT_LOG_INGEST_URI;
+
+    @AssertTrue(message = "path should start with /")
+    boolean isPathValid() {
+        return path_prefix.startsWith("/");
+    }
+
+    @JsonProperty("acknowledgments")
+    private Boolean acknowledgments = false;
+
+    @JsonProperty("s3_bucket")
+    private String s3Bucket;
+
+    @JsonProperty("s3_prefix")
+    private String s3Prefix;
+
+    @JsonProperty("s3_region")
+    private String s3Region;
+
+    @JsonProperty("aws")
+    @NotNull
+    @Valid
+    private AwsAuthenticationOptions awsConfig;
+
+    @JsonProperty("delete_s3_objects_on_read")
+    private boolean deleteS3ObjectsOnRead = false;
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getPath() {
+        return path_prefix;
+    }
+}
---
+++ data-prepper-plugins/opensearch-api-source/src/main/java/org/opensearch/dataprepper/plugins/source/opensearch_api/model/MetadataKeyAttributes.java
@@ -0,0 +1,11 @@
+package org.opensearch.dataprepper.plugins.source.opensearch_api.model;
+
+public class MetadataKeyAttributes {
+
+    public static final String EVENT_NAME_BULK_ACTION_METADATA_ATTRIBUTE = "opensearch_action";
+
+    public static final String EVENT_NAME_BULK_ACTION_METADATA_ATTRIBUTE_INDEX = "opensearch_index";
+
+    public static final String EVENT_NAME_BULK_ACTION_METADATA_ATTRIBUTE_ID = "opensearch_id";
+
+}
