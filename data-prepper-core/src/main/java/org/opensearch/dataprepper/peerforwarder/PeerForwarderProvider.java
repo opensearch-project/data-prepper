6102091471a80ac3ab8da8c99bee1a41d5d6f6f1
Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
2024-06-13 03:06:56 +0000

    Rebased to latest
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>

--- data-prepper-core/src/main/java/org/opensearch/dataprepper/peerforwarder/PeerForwarderProvider.java
+++ data-prepper-core/src/main/java/org/opensearch/dataprepper/peerforwarder/PeerForwarderProvider.java
@@ -5,97 +5,49 @@
 
 package org.opensearch.dataprepper.peerforwarder;
 
-import org.opensearch.dataprepper.metrics.PluginMetrics;
 import org.opensearch.dataprepper.model.event.Event;
 import org.opensearch.dataprepper.model.record.Record;
-import org.opensearch.dataprepper.peerforwarder.client.PeerForwarderClient;
-import org.opensearch.dataprepper.peerforwarder.discovery.DiscoveryMode;
+import org.opensearch.dataprepper.model.processor.Processor;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class PeerForwarderProvider {
+public interface PeerForwarderProvider {
+    /**
+     * Registers a pipeline and identification keys
+     *
+     * @param pipelineName pipeline name
+     * @param processor processor
+     * @param pluginId plugin id
+     * @param identificationKeys identification keys
+     * @param pipelineWorkerThreads number of pipeline worker threads
+     * @return peer forwarder
+     * @since 2.9
+     */
+    PeerForwarder register(final String pipelineName, final Processor processor, final String pluginId, final Set<String> identificationKeys, final Integer pipelineWorkerThreads);
 
-    private final PeerForwarderClientFactory peerForwarderClientFactory;
-    private final PeerForwarderClient peerForwarderClient;
-    private final PeerForwarderConfiguration peerForwarderConfiguration;
-    private final PluginMetrics pluginMetrics;
-    private final Map<String, Map<String, PeerForwarderReceiveBuffer<Record<Event>>>> pipelinePeerForwarderReceiveBufferMap = new HashMap<>();
-    private HashRing hashRing;
+    /**
+     * Returns if peer forwarding required
+     *
+     * @return returns if peer forwarding required or nto
+     * @since 2.9
+     */
+    boolean isPeerForwardingRequired();
 
-    PeerForwarderProvider(final PeerForwarderClientFactory peerForwarderClientFactory,
-                          final PeerForwarderClient peerForwarderClient,
-                          final PeerForwarderConfiguration peerForwarderConfiguration,
-                          final PluginMetrics pluginMetrics) {
-        this.peerForwarderClientFactory = peerForwarderClientFactory;
-        this.peerForwarderClient = peerForwarderClient;
-        this.peerForwarderConfiguration = peerForwarderConfiguration;
-        this.pluginMetrics = pluginMetrics;
-    }
+    /**
+     * Returns if peers configured
+     *
+     * @return returns if peers configured
+     * @since 2.9
+     */
+    boolean arePeersConfigured();
 
-    public PeerForwarder register(final String pipelineName, final String pluginId, final Set<String> identificationKeys,
-                                  final Integer pipelineWorkerThreads) {
-        if (pipelinePeerForwarderReceiveBufferMap.containsKey(pipelineName) &&
-                pipelinePeerForwarderReceiveBufferMap.get(pipelineName).containsKey(pluginId)) {
-            throw new RuntimeException("Data Prepper 2.0 will only support a single peer-forwarder per pipeline/plugin type");
-        }
-
-        final PeerForwarderReceiveBuffer<Record<Event>> peerForwarderReceiveBuffer = createBufferPerPipelineProcessor(pipelineName, pluginId);
-
-        if (isPeerForwardingRequired()) {
-            if (hashRing == null) {
-                hashRing = peerForwarderClientFactory.createHashRing();
-            }
-            return new RemotePeerForwarder(
-                    peerForwarderClient,
-                    hashRing,
-                    peerForwarderReceiveBuffer,
-                    pipelineName,
-                    pluginId,
-                    identificationKeys,
-                    pluginMetrics,
-                    peerForwarderConfiguration.getBatchDelay(),
-                    peerForwarderConfiguration.getFailedForwardingRequestLocalWriteTimeout(),
-                    peerForwarderConfiguration.getForwardingBatchSize(),
-                    peerForwarderConfiguration.getForwardingBatchQueueDepth(),
-                    peerForwarderConfiguration.getForwardingBatchTimeout(),
-                    pipelineWorkerThreads
-            );
-        }
-        else {
-            return new LocalPeerForwarder();
-        }
-    }
-
-    private PeerForwarderReceiveBuffer<Record<Event>> createBufferPerPipelineProcessor(final String pipelineName, final String pluginId) {
-        final PeerForwarderReceiveBuffer<Record<Event>> peerForwarderReceiveBuffer = new
-                PeerForwarderReceiveBuffer<>(peerForwarderConfiguration.getBufferSize(), peerForwarderConfiguration.getBatchSize(), pipelineName, pluginId);
-
-        final Map<String, PeerForwarderReceiveBuffer<Record<Event>>> pluginsBufferMap =
-                pipelinePeerForwarderReceiveBufferMap.computeIfAbsent(pipelineName, k -> new HashMap<>());
-
-        pluginsBufferMap.put(pluginId, peerForwarderReceiveBuffer);
-
-        return peerForwarderReceiveBuffer;
-    }
-
-    public boolean isPeerForwardingRequired() {
-        return arePeersConfigured() && pipelinePeerForwarderReceiveBufferMap.size() > 0;
-    }
-
-    private boolean arePeersConfigured() {
-        final DiscoveryMode discoveryMode = peerForwarderConfiguration.getDiscoveryMode();
-        if (discoveryMode.equals(DiscoveryMode.LOCAL_NODE)) {
-            return false;
-        }
-        else if (discoveryMode.equals(DiscoveryMode.STATIC) && peerForwarderConfiguration.getStaticEndpoints().size() <= 1) {
-            return false;
-        }
-        return true;
-    }
-
-    public Map<String, Map<String, PeerForwarderReceiveBuffer<Record<Event>>>> getPipelinePeerForwarderReceiveBufferMap() {
-        return pipelinePeerForwarderReceiveBufferMap;
-    }
+    /**
+     * Returns pipeline peer forwarder receive buffer map
+     *
+     * @return Map of buffer per pipeline per pluginId
+     * @since 2.9
+     */
+    Map<String, Map<String, PeerForwarderReceiveBuffer<Record<Event>>>> getPipelinePeerForwarderReceiveBufferMap();
 }
+
