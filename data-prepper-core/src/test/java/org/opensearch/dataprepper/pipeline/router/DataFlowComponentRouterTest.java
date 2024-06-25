1de66978b2e5998fa8cf1ecc95a2b984c026988d
Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
2024-06-25 20:19:05 +0000

    Added another test case as per review comments
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>

--- data-prepper-core/src/test/java/org/opensearch/dataprepper/pipeline/router/DataFlowComponentRouterTest.java
+++ data-prepper-core/src/test/java/org/opensearch/dataprepper/pipeline/router/DataFlowComponentRouterTest.java
@@ -258,6 +258,22 @@ class DataFlowComponentRouterTest {
             verify(componentRecordsConsumer).accept(testComponent, recordsIn);
         }
 
+        @Test
+        void route_matched_events_with_none_to_default_route() {
+            DataFlowComponent<TestComponent> dataFlowComponent2 = mock(DataFlowComponent.class);
+            when(dataFlowComponent2.getRoutes()).thenReturn(Set.of(DataFlowComponentRouter.DEFAULT_ROUTE));
+            final Map<Record, Set<String>> allMatchingRoutes = recordsIn.stream()
+                    .collect(Collectors.toMap(Function.identity(), r -> Collections.singleton(knownRoute)));
+
+            createObjectUnderTest().route(recordsIn, dataFlowComponent2, allMatchingRoutes, getRecordStrategy, componentRecordsConsumer);
+            verify(componentRecordsConsumer).accept(null, Collections.emptyList());
+            createObjectUnderTest().route(recordsIn, dataFlowComponent, allMatchingRoutes, getRecordStrategy, componentRecordsConsumer);
+
+            verify(componentRecordsConsumer).accept(testComponent, recordsIn);
+
+        }
+
+
 
         @Test
         void route_all_Events_when_all_have_matched_route() {
