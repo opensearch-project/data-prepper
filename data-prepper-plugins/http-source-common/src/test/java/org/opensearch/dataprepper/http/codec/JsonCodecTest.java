d29954c25363b827b7697e6073e724c2d427ec1d
David Venable <dlv@amazon.com>
2024-06-25 17:09:27 -0500

    Updates the chunking algorithm for http source's JsonCodec to account for actual byte size. Test using Unicode characters to prove this was incorrectly chunking and verify against future changes. (#4656)
    Signed-off-by: David Venable <dlv@amazon.com>

--- data-prepper-plugins/http-source-common/src/test/java/org/opensearch/dataprepper/http/codec/JsonCodecTest.java
+++ data-prepper-plugins/http-source-common/src/test/java/org/opensearch/dataprepper/http/codec/JsonCodecTest.java
@@ -7,16 +7,31 @@ package org.opensearch.dataprepper.http.codec;
 
 import com.linecorp.armeria.common.HttpData;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class JsonCodecTest {
     private final HttpData goodTestData = HttpData.ofUtf8("[{\"a\":\"b\"}, {\"c\":\"d\"}]");
     private final HttpData goodLargeTestData = HttpData.ofUtf8("[{\"a1\":\"b1\"}, {\"a2\":\"b2\"}, {\"a3\":\"b3\"}, {\"a4\":\"b4\"}, {\"a5\":\"b5\"}]");
+    private final HttpData goodLargeTestDataUnicode = HttpData.ofUtf8("[{\"ὊὊὊ1\":\"ὊὊὊ1\"}, {\"ὊὊὊ2\":\"ὊὊὊ2\"}, {\"a3\":\"b3\"}, {\"ὊὊὊ4\":\"ὊὊὊ4\"}]");
     private final HttpData badTestDataJsonLine = HttpData.ofUtf8("{\"a\":\"b\"}");
     private final HttpData badTestDataMultiJsonLines = HttpData.ofUtf8("{\"a\":\"b\"}{\"c\":\"d\"}");
     private final HttpData badTestDataNonJson = HttpData.ofUtf8("non json content");
@@ -51,6 +66,25 @@ class JsonCodecTest {
         assertEquals("{\"a5\":\"b5\"}", res.get(2).get(0));
     }
 
+    @ParameterizedTest
+    @ArgumentsSource(JsonArrayWithKnownFirstArgumentsProvider.class)
+    public void parse_should_return_lists_smaller_than_provided_length(
+            final String inputJsonArray, final String knownFirstPart) throws IOException {
+        final int knownSingleBodySize = knownFirstPart.getBytes(Charset.defaultCharset()).length;
+        final int maxSize = (knownSingleBodySize * 2) + 3;
+        final List<List<String>> chunkedBodies = objectUnderTest.parse(HttpData.ofUtf8(inputJsonArray),
+                maxSize);
+
+        assertThat(chunkedBodies, notNullValue());
+        assertThat(chunkedBodies.size(), greaterThanOrEqualTo(1));
+        final String firstReconstructed = chunkedBodies.get(0).stream().collect(Collectors.joining(",", "[", "]"));
+        assertThat(firstReconstructed.getBytes(Charset.defaultCharset()).length,
+                lessThanOrEqualTo(maxSize));
+
+        assertThat(chunkedBodies.get(0).size(), greaterThanOrEqualTo(1));
+        assertThat(chunkedBodies.get(0).get(0), equalTo(knownFirstPart));
+    }
+
     @Test
     public void testParseJsonLineFailure() {
         assertThrows(IOException.class, () -> objectUnderTest.parse(badTestDataJsonLine));
@@ -64,5 +98,19 @@ class JsonCodecTest {
     @Test
     public void testParseNonJsonFailure() {
         assertThrows(IOException.class, () -> objectUnderTest.parse(badTestDataNonJson));
+    }
+
+    static class JsonArrayWithKnownFirstArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+            return Stream.of(
+                    arguments(
+                            "[{\"ὊὊὊ1\":\"ὊὊὊ1\"}, {\"ὊὊὊ2\":\"ὊὊὊ2\"}, {\"a3\":\"b3\"}, {\"ὊὊὊ4\":\"ὊὊὊ4\"}]",
+                            "{\"ὊὊὊ1\":\"ὊὊὊ1\"}"),
+                    arguments(
+                            "[{\"aaa1\":\"aaa1\"}, {\"aaa2\":\"aaa2\"}, {\"a3\":\"b3\"}, {\"bbb4\":\"bbb4\"}]",
+                            "{\"aaa1\":\"aaa1\"}")
+            );
+        }
     }
 }
