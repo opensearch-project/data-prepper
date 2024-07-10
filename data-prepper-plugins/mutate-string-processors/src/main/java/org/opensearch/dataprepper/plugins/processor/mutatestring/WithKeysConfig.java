688c90cd7b0e0716f93eb234e5a95473e07cf0b0
George Chen <qchea@amazon.com>
2024-07-10 18:10:16 -0500

    MAINT: backfill doc into json property for trim_string
    Signed-off-by: George Chen <qchea@amazon.com>

--- data-prepper-plugins/mutate-string-processors/src/main/java/org/opensearch/dataprepper/plugins/processor/mutatestring/WithKeysConfig.java
+++ data-prepper-plugins/mutate-string-processors/src/main/java/org/opensearch/dataprepper/plugins/processor/mutatestring/WithKeysConfig.java
@@ -5,18 +5,22 @@
 
 package org.opensearch.dataprepper.plugins.processor.mutatestring;
 
+import com.fasterxml.jackson.annotation.JsonClassDescription;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import org.opensearch.dataprepper.model.event.EventKey;
 
 import java.util.List;
 
+@JsonClassDescription("https://opensearch.org/docs/latest/data-prepper/pipelines/configuration/processors/trim-string/")
 public class WithKeysConfig implements StringProcessorConfig<EventKey> {
 
     @NotNull
     @NotEmpty
     @JsonProperty("with_keys")
+    @JsonPropertyDescription("A list of keys to trim the white space from.")
     private List<EventKey> withKeys;
 
     @Override
