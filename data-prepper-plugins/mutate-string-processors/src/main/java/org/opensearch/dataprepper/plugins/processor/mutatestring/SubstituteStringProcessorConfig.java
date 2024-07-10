e3b518a4e79441d05c0d84e1ce8ab189a239bf53
George Chen <qchea@amazon.com>
2024-07-10 18:07:07 -0500

    MAINT: backfill documentation into json property for substitute_string
    Signed-off-by: George Chen <qchea@amazon.com>

--- data-prepper-plugins/mutate-string-processors/src/main/java/org/opensearch/dataprepper/plugins/processor/mutatestring/SubstituteStringProcessorConfig.java
+++ data-prepper-plugins/mutate-string-processors/src/main/java/org/opensearch/dataprepper/plugins/processor/mutatestring/SubstituteStringProcessorConfig.java
@@ -5,15 +5,24 @@
 
 package org.opensearch.dataprepper.plugins.processor.mutatestring;
 
+import com.fasterxml.jackson.annotation.JsonClassDescription;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import org.opensearch.dataprepper.model.event.EventKey;
 
 import java.util.List;
 
+@JsonClassDescription("https://opensearch.org/docs/latest/data-prepper/pipelines/configuration/processors/substitute-string/#configuration")
 public class SubstituteStringProcessorConfig implements StringProcessorConfig<SubstituteStringProcessorConfig.Entry> {
     public static class Entry {
+        @JsonPropertyDescription("The key to modify.")
         private EventKey source;
+        @JsonPropertyDescription("The Regex String to be replaced. Special regex characters such as `[` and `]` must " +
+                "be escaped using `\\\\` when using double quotes and `\\ ` when using single quotes. " +
+                "See [Java Patterns](https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/regex/Pattern.html) " +
+                "for more information.")
         private String from;
+        @JsonPropertyDescription("The String to be substituted for each match of `from`.")
         private String to;
 
         @JsonProperty("substitute_when")
@@ -43,6 +52,7 @@ public class SubstituteStringProcessorConfig implements StringProcessorConfig<Su
         public Entry() {}
     }
 
+    @JsonPropertyDescription("List of entries. Valid values are `source`, `from`, and `to`.")
     private List<Entry> entries;
 
     public List<Entry> getEntries() {
