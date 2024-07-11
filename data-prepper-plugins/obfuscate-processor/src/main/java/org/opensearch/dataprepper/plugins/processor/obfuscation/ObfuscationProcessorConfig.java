1911fdc7dfb8acf3b1fe8c147cd9a7ba4cb54c91
George Chen <qchea@amazon.com>
2024-07-11 17:15:25 -0500

    MAINT: remove doc link
    Signed-off-by: George Chen <qchea@amazon.com>

--- data-prepper-plugins/obfuscate-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/obfuscation/ObfuscationProcessorConfig.java
+++ data-prepper-plugins/obfuscate-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/obfuscation/ObfuscationProcessorConfig.java
@@ -5,7 +5,6 @@
 
 package org.opensearch.dataprepper.plugins.processor.obfuscation;
 
-import com.fasterxml.jackson.annotation.JsonClassDescription;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import jakarta.validation.constraints.NotEmpty;
@@ -16,7 +15,6 @@ import org.opensearch.dataprepper.model.plugin.InvalidPluginConfigurationExcepti
 
 import java.util.List;
 
-@JsonClassDescription("https://opensearch.org/docs/latest/data-prepper/pipelines/configuration/processors/obfuscate/#configuration")
 public class ObfuscationProcessorConfig {
 
     @JsonProperty("source")
