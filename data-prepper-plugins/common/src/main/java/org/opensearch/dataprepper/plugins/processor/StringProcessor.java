5b1edb6951025bbe9eb19f636d992cdbc74647fa
Qi Chen <qchea@amazon.com>
2024-07-12 12:40:24 -0500

    MAINT: backfill documentation into json description for string_converter (#4725)
    * MAINT: backfill documentation into json description for string_converter
    
    Signed-off-by: George Chen <qchea@amazon.com>

--- data-prepper-plugins/common/src/main/java/org/opensearch/dataprepper/plugins/processor/StringProcessor.java
+++ data-prepper-plugins/common/src/main/java/org/opensearch/dataprepper/plugins/processor/StringProcessor.java
@@ -5,6 +5,7 @@
 
 package org.opensearch.dataprepper.plugins.processor;
 
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import org.opensearch.dataprepper.model.annotations.DataPrepperPlugin;
 import org.opensearch.dataprepper.model.annotations.DataPrepperPluginConstructor;
 import org.opensearch.dataprepper.model.configuration.PluginSetting;
@@ -40,6 +41,7 @@ public class StringProcessor implements Processor<Record<Event>, Record<Event>> 
     private final boolean upperCase;
 
     public static class Configuration {
+        @JsonPropertyDescription("Whether to convert to uppercase (`true`) or lowercase (`false`).")
         private boolean upperCase = true;
 
         public boolean getUpperCase() {
