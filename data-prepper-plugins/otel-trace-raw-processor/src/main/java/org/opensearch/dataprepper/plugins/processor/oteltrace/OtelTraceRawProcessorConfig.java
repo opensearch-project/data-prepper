f9dc806d55f8f9a40970cf57c9eae40b20f2a8af
Qi Chen <qchea@amazon.com>
2024-07-12 12:40:52 -0500

    MAINT: backfill documentation in json description for otel_traces (#4724)
    * MAINT: backfill documentation in json property description for otel_traces
    
    Signed-off-by: George Chen <qchea@amazon.com>

--- data-prepper-plugins/otel-trace-raw-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/oteltrace/OtelTraceRawProcessorConfig.java
+++ data-prepper-plugins/otel-trace-raw-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/oteltrace/OtelTraceRawProcessorConfig.java
@@ -6,6 +6,7 @@
 package org.opensearch.dataprepper.plugins.processor.oteltrace;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
 import java.time.Duration;
 
@@ -14,12 +15,17 @@ public class OtelTraceRawProcessorConfig {
     static final Duration DEFAULT_TRACE_ID_TTL = Duration.ofSeconds(15L);
     static final long MAX_TRACE_ID_CACHE_SIZE = 1_000_000L;
     @JsonProperty("trace_flush_interval")
+    @JsonPropertyDescription("Represents the time interval in seconds to flush all the descendant spans without any " +
+            "root span. Default is 180.")
     private long traceFlushInterval = DEFAULT_TG_FLUSH_INTERVAL_SEC;
 
     @JsonProperty("trace_group_cache_ttl")
+    @JsonPropertyDescription("Represents the time-to-live to cache a trace group details. Default is 15 seconds.")
     private Duration traceGroupCacheTimeToLive = DEFAULT_TRACE_ID_TTL;
 
     @JsonProperty("trace_group_cache_max_size")
+    @JsonPropertyDescription("Represents the maximum size of the cache to store the trace group details from root spans. " +
+            "Default is 1000000.")
     private long traceGroupCacheMaxSize = MAX_TRACE_ID_CACHE_SIZE;
 
     public long getTraceFlushIntervalSeconds() {
