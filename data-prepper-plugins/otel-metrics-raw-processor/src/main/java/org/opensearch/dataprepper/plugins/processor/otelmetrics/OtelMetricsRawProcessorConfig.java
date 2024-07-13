a988177974764424f31dd27a3b1f12c435ed77ac
Qi Chen <qchea@amazon.com>
2024-07-13 00:30:56 -0500

    MAINT: backfill doc in json property description for otel_metrics (#4722)
    * MAINT: backfill doc in json property description for otel_metrics
    
    Signed-off-by: George Chen <qchea@amazon.com>

--- data-prepper-plugins/otel-metrics-raw-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/otelmetrics/OtelMetricsRawProcessorConfig.java
+++ data-prepper-plugins/otel-metrics-raw-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/otelmetrics/OtelMetricsRawProcessorConfig.java
@@ -6,17 +6,23 @@
 package org.opensearch.dataprepper.plugins.processor.otelmetrics;
 
 import static org.opensearch.dataprepper.plugins.otel.codec.OTelProtoCodec.DEFAULT_EXPONENTIAL_HISTOGRAM_MAX_ALLOWED_SCALE;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
 public class OtelMetricsRawProcessorConfig {
 
     @JsonProperty("flatten_attributes")
+    @JsonPropertyDescription("Whether or not to flatten the `attributes` field in the JSON data.")
     boolean flattenAttributesFlag = true;
 
+    @JsonPropertyDescription("Whether or not to calculate histogram buckets.")
     private Boolean calculateHistogramBuckets = true;
 
+    @JsonPropertyDescription("Whether or not to calculate exponential histogram buckets.")
     private Boolean calculateExponentialHistogramBuckets = true;
 
+    @JsonPropertyDescription("Maximum allowed scale in exponential histogram calculation.")
     private Integer exponentialHistogramMaxAllowedScale = DEFAULT_EXPONENTIAL_HISTOGRAM_MAX_ALLOWED_SCALE;
 
     public Boolean getCalculateExponentialHistogramBuckets() {
