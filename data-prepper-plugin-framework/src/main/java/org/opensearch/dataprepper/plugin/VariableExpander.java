d2bded8238d10584361f8fa727ca7ec510513382
George Chen <qchea@amazon.com>
2024-07-16 10:24:27 -0500

    MAINT: better log message and level
    Signed-off-by: George Chen <qchea@amazon.com>

--- data-prepper-plugin-framework/src/main/java/org/opensearch/dataprepper/plugin/VariableExpander.java
+++ data-prepper-plugin-framework/src/main/java/org/opensearch/dataprepper/plugin/VariableExpander.java
@@ -17,6 +17,7 @@ import javax.inject.Named;
 import java.io.IOException;
 import java.util.AbstractMap;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -34,6 +35,7 @@ public class VariableExpander {
             String.format(REFERENCE_PATTERN_STRING, REFERENCE_KEY));
     static final String SECRETS_REFERENCE_PATTERN_STRING = "^\\$\\{\\{%s\\:(?<%s>.*)\\}\\}$";
     private final Map<Pattern, PluginConfigValueTranslator> patternPluginConfigValueTranslatorMap;
+    private final Set<String> pluginConfigValueTranslatorPrefixes;
     private final ObjectMapper objectMapper;
 
     @Inject
@@ -43,6 +45,7 @@ public class VariableExpander {
             final Set<PluginConfigValueTranslator> pluginConfigValueTranslators) {
         this.objectMapper = objectMapper;
         patternPluginConfigValueTranslatorMap = new HashMap<>();
+        pluginConfigValueTranslatorPrefixes = new HashSet<>();
         pluginConfigValueTranslators.forEach(pluginConfigValueTranslator -> {
             if (!DEFAULT_DEPRECATED_PREFIX.equals(
                     pluginConfigValueTranslator.getDeprecatedPrefix())) {
@@ -51,12 +54,14 @@ public class VariableExpander {
                                 String.format(SECRETS_REFERENCE_PATTERN_STRING,
                                         pluginConfigValueTranslator.getDeprecatedPrefix(), VALUE_REFERENCE_KEY)),
                         pluginConfigValueTranslator);
+                pluginConfigValueTranslatorPrefixes.add(pluginConfigValueTranslator.getDeprecatedPrefix());
             }
             patternPluginConfigValueTranslatorMap.put(
                     Pattern.compile(
                             String.format(SECRETS_REFERENCE_PATTERN_STRING,
                                     pluginConfigValueTranslator.getPrefix(), VALUE_REFERENCE_KEY)),
                     pluginConfigValueTranslator);
+            pluginConfigValueTranslatorPrefixes.add(pluginConfigValueTranslator.getPrefix());
         });
     }
 
@@ -65,9 +70,12 @@ public class VariableExpander {
             final String rawValue = jsonParser.getValueAsString();
             final Matcher referenceMatcher = REFERENCE_PATTERN.matcher(rawValue);
             if (referenceMatcher.matches()) {
-                LOG.debug("Searching for PluginConfigValueTranslator to match prefix on the raw value: {}", rawValue);
+                LOG.debug("Searching for available pluginConfigValueTranslator prefixes: {} " +
+                        "to match prefix on the raw value: {}",
+                        pluginConfigValueTranslatorPrefixes,
+                        rawValue);
                 return patternPluginConfigValueTranslatorMap.entrySet().stream()
-                        .map(entry -> new AbstractMap.SimpleEntry<>(entry.getKey().matcher(rawValue), entry.getValue()))
+                        .map(entry -> Map.entry(entry.getKey().matcher(rawValue), entry.getValue()))
                         .filter(entry -> entry.getKey().matches())
                         .map(entry -> {
                             final String valueReferenceKey = entry.getKey().group(VALUE_REFERENCE_KEY);
@@ -76,8 +84,10 @@ public class VariableExpander {
                         })
                         .findFirst()
                         .orElseGet(() -> {
-                            LOG.debug("No PluginConfigValueTranslator matches prefix on the raw value: {}, " +
-                                            "it will be treated as literal value", rawValue);
+                            LOG.warn("No available PluginConfigValueTranslator prefixes: {} " +
+                                    "match prefix on the raw value: {}, it will be treated as literal value",
+                                    pluginConfigValueTranslatorPrefixes,
+                                    rawValue);
                             return objectMapper.convertValue(rawValue, destinationType);
                         });
             }
