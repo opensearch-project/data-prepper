6a9757f0933dfae36f83e4fb3941072ffd27ac13
Santhosh Gandhe <1909520+san81@users.noreply.github.com>
2024-07-22 14:40:18 -0700

    doc strings for enhanced UI view auto-generation (#4755)
    Signed-off-by: Santhosh Gandhe <1909520+san81@users.noreply.github.com>

--- data-prepper-plugins/split-event-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/splitevent/SplitEventProcessorConfig.java
+++ data-prepper-plugins/split-event-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/splitevent/SplitEventProcessorConfig.java
@@ -11,6 +11,7 @@
 package org.opensearch.dataprepper.plugins.processor.splitevent;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size; 
@@ -20,12 +21,15 @@ public class SplitEventProcessorConfig {
     @NotEmpty
     @NotNull
     @JsonProperty("field")
+    @JsonPropertyDescription("The event field to be split")
     private String field;
 
     @JsonProperty("delimiter_regex")
+    @JsonPropertyDescription("The regular expression used as the delimiter for splitting the field")
     private String delimiterRegex;
 
     @Size(min = 1, max = 1)
+    @JsonPropertyDescription("The delimiter used for splitting the field. If not specified, the default delimiter is used")
     private String delimiter;
 
     public String getField() {
