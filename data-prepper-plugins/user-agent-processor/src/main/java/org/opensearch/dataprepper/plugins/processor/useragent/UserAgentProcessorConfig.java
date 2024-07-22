76a2ec3cc465b3319547fc05022808f264199edd
Hai Yan <oeyh@amazon.com>
2024-07-22 11:08:58 -0500

    Add json property description for list-to-map, map-to-list and user-agent processor
    Signed-off-by: Hai Yan <oeyh@amazon.com>

--- data-prepper-plugins/user-agent-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/useragent/UserAgentProcessorConfig.java
+++ data-prepper-plugins/user-agent-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/useragent/UserAgentProcessorConfig.java
@@ -6,6 +6,7 @@
 package org.opensearch.dataprepper.plugins.processor.useragent;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import org.opensearch.dataprepper.model.event.EventKey;
@@ -21,21 +22,26 @@ public class UserAgentProcessorConfig {
     @NotEmpty
     @NotNull
     @JsonProperty("source")
+    @JsonPropertyDescription("The field in the event that will be parsed.")
     @EventKeyConfiguration(EventKeyFactory.EventAction.GET)
     private EventKey source;
 
     @NotNull
     @JsonProperty("target")
+    @JsonPropertyDescription("The field to which the parsed event will write. Default is `user_agent`.")
     private String target = "user_agent";
 
     @NotNull
     @JsonProperty("exclude_original")
+    @JsonPropertyDescription("Determines whether to exclude the original UA string from the parsing result. Defaults to `false`. ")
     private boolean excludeOriginal = false;
 
     @JsonProperty("cache_size")
+    @JsonPropertyDescription("The cache size of the parser in megabytes. Defaults to `1000`.")
     private int cacheSize = DEFAULT_CACHE_SIZE;
 
     @JsonProperty("tags_on_parse_failure")
+    @JsonPropertyDescription("The tag to add to an event if the `user_agent` processor fails to parse the UA string.")
     private List<String> tagsOnParseFailure;
 
     public EventKey getSource() {
