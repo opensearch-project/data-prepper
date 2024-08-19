d291289f29c2f900163792a8dbfc01b517be67af
Kondaka <krishkdk@amazon.com>
2024-08-19 13:21:27 -0700

    Added new  tests for KeyGenerator
    Signed-off-by: Kondaka <krishkdk@amazon.com>

--- data-prepper-plugins/s3-sink/src/test/java/org/opensearch/dataprepper/plugins/sink/s3/KeyGeneratorTest.java
+++ data-prepper-plugins/s3-sink/src/test/java/org/opensearch/dataprepper/plugins/sink/s3/KeyGeneratorTest.java
@@ -5,6 +5,7 @@
 
 package org.opensearch.dataprepper.plugins.sink.s3;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -41,8 +42,8 @@ class KeyGeneratorTest {
 
     }
 
-    private KeyGenerator createObjectUnderTest() {
-        return new KeyGenerator(s3SinkConfig, null, extensionProvider, expressionEvaluator);
+    private KeyGenerator createObjectUnderTest(S3BucketSelector s3BucketSelector) {
+        return new KeyGenerator(s3SinkConfig, s3BucketSelector, extensionProvider, expressionEvaluator);
     }
 
     @Test
@@ -51,7 +52,7 @@ class KeyGeneratorTest {
         final String objectName = UUID.randomUUID().toString();
         when(extensionProvider.getExtension()).thenReturn(null);
 
-        final KeyGenerator objectUnderTest = createObjectUnderTest();
+        final KeyGenerator objectUnderTest = createObjectUnderTest(null);
 
         final Event event = mock(Event.class);
 
@@ -78,11 +79,66 @@ class KeyGeneratorTest {
         String pathPrefix = "events/";
 
         final Event event = mock(Event.class);
-        final KeyGenerator objectUnderTest = createObjectUnderTest();
+        final KeyGenerator objectUnderTest = createObjectUnderTest(null);
         try (final MockedStatic<ObjectKey> objectKeyMockedStatic = mockStatic(ObjectKey.class)) {
 
             objectKeyMockedStatic.when(() -> ObjectKey.buildingPathPrefix(s3SinkConfig, event, expressionEvaluator))
                     .thenReturn(pathPrefix);
+            objectKeyMockedStatic.when(() -> ObjectKey.objectFileName(s3SinkConfig, extension, event, expressionEvaluator))
+                    .thenReturn(objectName);
+
+            String key = objectUnderTest.generateKeyForEvent(event);
+            assertThat(key, notNullValue());
+            assertThat(key.contains(pathPrefix), equalTo(true));
+            assertThat(key.contains(objectName), equalTo(true));
+        }
+
+    }
+
+    @Test
+    void test_generateKey_with_date_prefix_with_bucketSelector() {
+        String objectKeyPathPrefix = "logdata/";
+        final String objectName = UUID.randomUUID().toString();
+        when(extensionProvider.getExtension()).thenReturn(null);
+
+        String pathPrefix = RandomStringUtils.randomAlphabetic(5);
+        S3BucketSelector s3BucketSelector = mock(S3BucketSelector.class);
+        when(s3BucketSelector.getPathPrefix()).thenReturn(pathPrefix);
+        final KeyGenerator objectUnderTest = createObjectUnderTest(s3BucketSelector);
+
+        final Event event = mock(Event.class);
+
+        try (final MockedStatic<ObjectKey> objectKeyMockedStatic = mockStatic(ObjectKey.class)) {
+
+            objectKeyMockedStatic.when(() -> ObjectKey.buildingPathPrefix(s3SinkConfig, event, expressionEvaluator))
+                    .thenReturn(objectKeyPathPrefix);
+            objectKeyMockedStatic.when(() -> ObjectKey.objectFileName(s3SinkConfig, null, event, expressionEvaluator))
+                    .thenReturn(objectName);
+
+            String key = objectUnderTest.generateKeyForEvent(event);
+            assertNotNull(key);
+            assertThat(key, true);
+            assertThat(key.contains(pathPrefix), equalTo(true));
+            assertThat(key.contains(objectName), equalTo(true));
+        }
+    }
+
+    @Test
+    void generateKey_with_extension_with_bucketSelector() {
+        String extension = UUID.randomUUID().toString();
+        final String objectName = UUID.randomUUID().toString();
+        when(extensionProvider.getExtension()).thenReturn(extension);
+        String objectKeyPathPrefix = "events/";
+
+        final Event event = mock(Event.class);
+        String pathPrefix = RandomStringUtils.randomAlphabetic(5);
+        S3BucketSelector s3BucketSelector = mock(S3BucketSelector.class);
+        when(s3BucketSelector.getPathPrefix()).thenReturn(pathPrefix);
+        final KeyGenerator objectUnderTest = createObjectUnderTest(s3BucketSelector);
+        try (final MockedStatic<ObjectKey> objectKeyMockedStatic = mockStatic(ObjectKey.class)) {
+
+            objectKeyMockedStatic.when(() -> ObjectKey.buildingPathPrefix(s3SinkConfig, event, expressionEvaluator))
+                    .thenReturn(objectKeyPathPrefix);
             objectKeyMockedStatic.when(() -> ObjectKey.objectFileName(s3SinkConfig, extension, event, expressionEvaluator))
                     .thenReturn(objectName);
 
