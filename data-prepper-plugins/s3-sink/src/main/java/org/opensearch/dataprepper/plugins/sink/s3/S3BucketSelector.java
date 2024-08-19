4f7ac5860207b2b1459c2faf3886b13ff3580220
Kondaka <krishkdk@amazon.com>
2024-08-19 13:04:27 -0700

    Added javadoc for S3BucketSelector
    Signed-off-by: Kondaka <krishkdk@amazon.com>

--- data-prepper-plugins/s3-sink/src/main/java/org/opensearch/dataprepper/plugins/sink/s3/S3BucketSelector.java
+++ data-prepper-plugins/s3-sink/src/main/java/org/opensearch/dataprepper/plugins/sink/s3/S3BucketSelector.java
@@ -9,22 +9,30 @@ import java.util.stream.Collectors;
 
 public interface S3BucketSelector {
     /**
-     *
+     * initialize - initializes the selector
+     * @param s3SinkConfig - s3 sink configuration
      */
     void initialize(S3SinkConfig s3SinkConfig);
 
     /**
+     * getBucketName - returns the name of the bucket created by the bucket selector
      *
+     * @return - bucket name
      */
     String getBucketName();
 
     /**
+     * getPathPrefix - returns the prefix to be used for the objects created in the bucket
      *
+     * @return path prefix
      */
     String getPathPrefix();
 
     /**
+     * getMetadata - returns metadata to be used when creating the objects
+     * @param eventCount - count of number of events
      *
+     * @return - returns a map of metadata
      */
     Map<String, String> getMetadata(int eventCount);
 }
