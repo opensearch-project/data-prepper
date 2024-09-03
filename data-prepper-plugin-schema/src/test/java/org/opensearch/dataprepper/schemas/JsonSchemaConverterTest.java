9a04b4162bdfacbc9ed1e7649196e98a2998fcf3
Qi Chen <qchea@amazon.com>
2024-09-03 11:38:08 -0400

    ENH: respect JsonProperty defaultValue in JsonSchemaConverter (#4889)
    * ENH: respect JsonProperty defaultValue in JsonSchemaConverter
    
    Signed-off-by: George Chen <qchea@amazon.com>

--- data-prepper-plugin-schema/src/test/java/org/opensearch/dataprepper/schemas/JsonSchemaConverterTest.java
+++ data-prepper-plugin-schema/src/test/java/org/opensearch/dataprepper/schemas/JsonSchemaConverterTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
 import com.github.victools.jsonschema.generator.Module;
 import com.github.victools.jsonschema.generator.OptionPreset;
 import com.github.victools.jsonschema.generator.SchemaVersion;
@@ -14,6 +15,7 @@ import org.opensearch.dataprepper.schemas.module.CustomJacksonModule;
 import java.util.Collections;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -30,6 +32,13 @@ class JsonSchemaConverterTest {
         final ObjectNode jsonSchemaNode = jsonSchemaConverter.convertIntoJsonSchema(
                 SchemaVersion.DRAFT_2020_12, OptionPreset.PLAIN_JSON, TestConfig.class);
         assertThat(jsonSchemaNode, instanceOf(ObjectNode.class));
+        final JsonNode propertiesNode = jsonSchemaNode.at("/properties");
+        assertThat(propertiesNode, instanceOf(ObjectNode.class));
+        assertThat(propertiesNode.has("testAttributeWithDefaultValue"), is(true));
+        final JsonNode testAttributeWithDefaultValueNode = propertiesNode.at("/testAttributeWithDefaultValue");
+        assertThat(testAttributeWithDefaultValueNode, instanceOf(ObjectNode.class));
+        assertThat(testAttributeWithDefaultValueNode.has("default"), is(true));
+        assertThat(testAttributeWithDefaultValueNode.get("default"), equalTo(TextNode.valueOf("default_value")));
     }
 
     @Test
@@ -52,6 +61,9 @@ class JsonSchemaConverterTest {
 
         @JsonProperty("custom_test_attribute")
         private String testAttributeWithJsonPropertyAnnotation;
+
+        @JsonProperty(defaultValue = "default_value")
+        private String testAttributeWithDefaultValue;
 
         public String getTestAttributeWithGetter() {
             return testAttributeWithGetter;
