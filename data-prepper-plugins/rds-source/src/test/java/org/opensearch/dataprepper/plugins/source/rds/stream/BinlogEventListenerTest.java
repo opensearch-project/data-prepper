b72ca4034aeced3e65e1ff21ffdefbf7fb772c85
Hai Yan <oeyh@amazon.com>
2024-09-04 16:07:26 -0500

    Move event processing to separate threads and add event processing timer
    Signed-off-by: Hai Yan <oeyh@amazon.com>

--- data-prepper-plugins/rds-source/src/test/java/org/opensearch/dataprepper/plugins/source/rds/stream/BinlogEventListenerTest.java
+++ data-prepper-plugins/rds-source/src/test/java/org/opensearch/dataprepper/plugins/source/rds/stream/BinlogEventListenerTest.java
@@ -7,13 +7,17 @@ package org.opensearch.dataprepper.plugins.source.rds.stream;
 
 import com.github.shyiko.mysql.binlog.BinaryLogClient;
 import com.github.shyiko.mysql.binlog.event.EventType;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.Timer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.Answers;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.opensearch.dataprepper.metrics.PluginMetrics;
 import org.opensearch.dataprepper.model.acknowledgements.AcknowledgementSetManager;
@@ -22,10 +26,15 @@ import org.opensearch.dataprepper.model.event.Event;
 import org.opensearch.dataprepper.model.record.Record;
 import org.opensearch.dataprepper.plugins.source.rds.RdsSourceConfig;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.opensearch.dataprepper.plugins.source.rds.stream.BinlogEventListener.REPLICATION_LOG_EVENT_PROCESSING_TIME;
 
 @ExtendWith(MockitoExtension.class)
 class BinlogEventListenerTest {
@@ -48,14 +57,28 @@ class BinlogEventListenerTest {
     @Mock
     private AcknowledgementSetManager acknowledgementSetManager;
 
+    @Mock
+    private ExecutorService eventListnerExecutorService;
+
+    @Mock
+    private ExecutorService checkpointManagerExecutorService;
+
     @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     private com.github.shyiko.mysql.binlog.event.Event binlogEvent;
 
-    private static BinlogEventListener objectUnderTest;
+    private BinlogEventListener objectUnderTest;
+
+    private Timer eventProcessingTimer;
 
     @BeforeEach
     void setUp() {
-        objectUnderTest = spy(createObjectUnderTest());
+        eventProcessingTimer = Metrics.timer("test-timer");
+        when(pluginMetrics.timer(REPLICATION_LOG_EVENT_PROCESSING_TIME)).thenReturn(eventProcessingTimer);
+        try (final MockedStatic<Executors> executorsMockedStatic = mockStatic(Executors.class)) {
+            executorsMockedStatic.when(Executors::newCachedThreadPool).thenReturn(eventListnerExecutorService);
+            executorsMockedStatic.when(Executors::newSingleThreadExecutor).thenReturn(checkpointManagerExecutorService);
+            objectUnderTest = spy(createObjectUnderTest());
+        }
     }
 
     @Test
@@ -65,6 +88,7 @@ class BinlogEventListenerTest {
 
         objectUnderTest.onEvent(binlogEvent);
 
+        verifyHandlerCallHelper();
         verify(objectUnderTest).handleTableMapEvent(binlogEvent);
     }
 
@@ -76,6 +100,7 @@ class BinlogEventListenerTest {
 
         objectUnderTest.onEvent(binlogEvent);
 
+        verifyHandlerCallHelper();
         verify(objectUnderTest).handleInsertEvent(binlogEvent);
     }
 
@@ -87,6 +112,7 @@ class BinlogEventListenerTest {
 
         objectUnderTest.onEvent(binlogEvent);
 
+        verifyHandlerCallHelper();
         verify(objectUnderTest).handleUpdateEvent(binlogEvent);
     }
 
@@ -98,10 +124,19 @@ class BinlogEventListenerTest {
 
         objectUnderTest.onEvent(binlogEvent);
 
+        verifyHandlerCallHelper();
         verify(objectUnderTest).handleDeleteEvent(binlogEvent);
     }
 
     private BinlogEventListener createObjectUnderTest() {
         return new BinlogEventListener(buffer, sourceConfig, pluginMetrics, binaryLogClient, streamCheckpointer, acknowledgementSetManager);
+    }
+
+    private void verifyHandlerCallHelper() {
+        ArgumentCaptor<Runnable> runnableArgumentCaptor = ArgumentCaptor.forClass(Runnable.class);
+        verify(eventListnerExecutorService).submit(runnableArgumentCaptor.capture());
+
+        Runnable capturedRunnable = runnableArgumentCaptor.getValue();
+        capturedRunnable.run();
     }
 }
