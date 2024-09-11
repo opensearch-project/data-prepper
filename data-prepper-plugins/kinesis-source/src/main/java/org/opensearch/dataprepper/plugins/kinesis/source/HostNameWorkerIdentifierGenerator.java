0fd763b4125633f1a564b6a4c4acf893d6dce703
Souvik Bose <souvbose@amazon.com>
2024-09-10 17:01:05 -0700

    Add license header to all the classes
    Signed-off-by: Souvik Bose <souvbose@amazon.com>

--- data-prepper-plugins/kinesis-source/src/main/java/org/opensearch/dataprepper/plugins/kinesis/source/HostNameWorkerIdentifierGenerator.java
+++ data-prepper-plugins/kinesis-source/src/main/java/org/opensearch/dataprepper/plugins/kinesis/source/HostNameWorkerIdentifierGenerator.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ */
+
 package org.opensearch.dataprepper.plugins.kinesis.source;
 
 import java.net.InetAddress;
