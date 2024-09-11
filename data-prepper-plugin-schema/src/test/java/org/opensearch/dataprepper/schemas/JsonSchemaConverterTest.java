9a00ecb0c8b95806a51bce4589695b3ac7e3f2f4
George Chen <qchea@amazon.com>
2024-09-11 11:32:22 -0500

    MAINT: unused import
    Signed-off-by: George Chen <qchea@amazon.com>

--- data-prepper-plugin-schema/src/test/java/org/opensearch/dataprepper/schemas/JsonSchemaConverterTest.java
+++ data-prepper-plugin-schema/src/test/java/org/opensearch/dataprepper/schemas/JsonSchemaConverterTest.java
@@ -22,7 +22,6 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
 
 class JsonSchemaConverterTest {
 
