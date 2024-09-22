04115dd4b708d79e4d85a8d5441dae65576c0695
Souvik Bose <souvbose@amazon.com>
2024-09-21 18:27:47 -0700

    Fix the code.
    Signed-off-by: Souvik Bose <souvbose@amazon.com>

--- data-prepper-plugins/http-source-common/src/main/java/org/opensearch/dataprepper/http/codec/MultiLineJsonCodec.java
+++ data-prepper-plugins/http-source-common/src/main/java/org/opensearch/dataprepper/http/codec/MultiLineJsonCodec.java
@@ -9,6 +9,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.regex.Pattern;
 
 public class MultiLineJsonCodec implements Codec<List<Map<String, Object>>> {
@@ -36,5 +37,14 @@ public class MultiLineJsonCodec implements Codec<List<Map<String, Object>>> {
             jsonListData.add(objectMapper.readValue(jsonLine, MAP_TYPE_REFERENCE));
         }
         return jsonListData;
+    }
+
+    @Override
+    public void validate(HttpData content) throws IOException {
+
+    }
+
+    @Override
+    public void serializeSplit(final HttpData content, final Consumer<String> serializedBodyConsumer, final int splitLength) throws IOException {
     }
 }
