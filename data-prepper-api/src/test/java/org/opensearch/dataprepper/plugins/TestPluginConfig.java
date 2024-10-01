323773fa12dbdc4fd29503142429625d2599cd14
George Chen <qchea@amazon.com>
2024-10-01 17:25:51 -0500

    RMV: unused files
    Signed-off-by: George Chen <qchea@amazon.com>

--- data-prepper-api/src/test/java/org/opensearch/dataprepper/plugins/TestPluginConfig.java
+++
@@ -1,8 +0,0 @@
-package org.opensearch.dataprepper.plugins;
-
-import org.opensearch.dataprepper.model.annotations.DataPrepperPlugin;
-import org.opensearch.dataprepper.model.processor.Processor;
-
-@DataPrepperPlugin(name = "test_plugin", pluginType = Processor.class)
-public class TestPluginConfig {
-}
--- data-prepper-api/src/test/java/org/opensearch/dataprepper/plugins/TestPluginConfigWithDeprecatedName.java
+++
@@ -1,9 +0,0 @@
-package org.opensearch.dataprepper.plugins;
-
-import org.opensearch.dataprepper.model.annotations.DataPrepperPlugin;
-import org.opensearch.dataprepper.model.processor.Processor;
-
-@DataPrepperPlugin(name = "test_plugin_config_with_deprecated_name",
-        deprecatedName = "test_plugin_deprecated", pluginType = Processor.class)
-public class TestPluginConfigWithDeprecatedName {
-}
