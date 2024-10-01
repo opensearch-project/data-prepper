60ad37d6be4b5b65af084ce4d41b5d48e30a9660
George Chen <qchea@amazon.com>
2024-10-01 17:36:49 -0500

    FIX: sty
    Signed-off-by: George Chen <qchea@amazon.com>

--- data-prepper-plugin-framework/src/main/java/org/opensearch/dataprepper/plugin/ClasspathPluginProvider.java
+++ data-prepper-plugin-framework/src/main/java/org/opensearch/dataprepper/plugin/ClasspathPluginProvider.java
@@ -11,9 +11,7 @@ import org.reflections.util.ConfigurationBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
