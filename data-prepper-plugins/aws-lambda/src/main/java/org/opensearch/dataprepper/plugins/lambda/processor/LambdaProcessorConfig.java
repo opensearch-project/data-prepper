abc2c61c6be306a3f84f01172caecb06267dfb1f
Srikanth Govindarajan <srigovs@amazon.com>
2024-10-08 13:56:44 -0700

    Add JsonPropertyDescription to all Config, add debug logs and add ITs
    Signed-off-by: Srikanth Govindarajan <srigovs@amazon.com>

--- data-prepper-plugins/aws-lambda/src/main/java/org/opensearch/dataprepper/plugins/lambda/processor/LambdaProcessorConfig.java
+++ data-prepper-plugins/aws-lambda/src/main/java/org/opensearch/dataprepper/plugins/lambda/processor/LambdaProcessorConfig.java
@@ -5,6 +5,7 @@
 package org.opensearch.dataprepper.plugins.lambda.processor;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -26,26 +27,32 @@ public class LambdaProcessorConfig {
     @Valid
     private AwsAuthenticationOptions awsAuthenticationOptions;
 
+    @JsonPropertyDescription("Lambda Function Name")
     @JsonProperty("function_name")
     @NotEmpty
     @Size(min = 3, max = 500, message = "function name length should be at least 3 characters")
     private String functionName;
 
+    @JsonPropertyDescription("Total retries we want before failing")
     @JsonProperty("max_retries")
     private int maxConnectionRetries = DEFAULT_CONNECTION_RETRIES;
 
+    @JsonPropertyDescription("invocation type defines the way we want to call lambda function")
     @JsonProperty("invocation_type")
     private String invocationType = REQUEST_RESPONSE;
 
+    @JsonPropertyDescription("payload model defines whether we want to batch the events together or use them as single events")
     @JsonProperty("payload_model")
     private String payloadModel = BATCH_EVENT;
 
+    @JsonPropertyDescription("sdk timeout defines the time sdk maintains the connection to the client before timing out")
     @JsonProperty("sdk_timeout")
     private Duration sdkTimeout = DEFAULT_SDK_TIMEOUT;
 
     @JsonProperty("batch")
     private BatchOptions batchOptions;
 
+    @JsonPropertyDescription("defines a condition for event to use this processor")
     @JsonProperty("lambda_when")
     private String whenCondition;
 
--- data-prepper-plugins/aws-lambda/src/main/java/org/opensearch/dataprepper/plugins/lambda/sink/LambdaSinkConfig.java
+++ data-prepper-plugins/aws-lambda/src/main/java/org/opensearch/dataprepper/plugins/lambda/sink/LambdaSinkConfig.java
@@ -5,6 +5,7 @@
 package org.opensearch.dataprepper.plugins.lambda.sink;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -30,18 +31,22 @@ public class LambdaSinkConfig {
     @Valid
     private AwsAuthenticationOptions awsAuthenticationOptions;
 
+    @JsonPropertyDescription("Lambda Function Name")
     @JsonProperty("function_name")
     @NotEmpty
     @NotNull
     @Size(min = 3, max = 500, message = "function name length should be at least 3 characters")
     private String functionName;
 
+    @JsonPropertyDescription("Total retries we want before failing")
     @JsonProperty("max_retries")
     private int maxConnectionRetries = DEFAULT_CONNECTION_RETRIES;
 
+    @JsonPropertyDescription("invocation type defines the way we want to call lambda function")
     @JsonProperty("invocation_type")
     private String invocationType = EVENT;
 
+    @JsonPropertyDescription("payload model defines whether we want to batch the events together or use them as single events")
     @JsonProperty("payload_model")
     private String payloadModel = BATCH_EVENT;
 
@@ -51,9 +56,11 @@ public class LambdaSinkConfig {
     @JsonProperty("batch")
     private BatchOptions batchOptions;
 
+    @JsonPropertyDescription("defines a condition for event to use this processor")
     @JsonProperty("lambda_when")
     private String whenCondition;
 
+    @JsonPropertyDescription("sdk timeout defines the time sdk maintains the connection to the client before timing out")
     @JsonProperty("sdk_timeout")
     private Duration sdkTimeout = DEFAULT_SDK_TIMEOUT;
 
