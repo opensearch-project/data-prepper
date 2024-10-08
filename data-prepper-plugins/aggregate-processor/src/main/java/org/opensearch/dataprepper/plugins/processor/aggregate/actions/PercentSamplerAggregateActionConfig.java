ae3c96bd00f8df34b42ef00a3b9b4bdbb333a2be
David Venable <dlv@amazon.com>
2024-10-08 16:51:36 -0500

    Documentation improvements for the aggregate processor.
    Adds property and class description to configurations. Corrects property order. Adds configuration classes with documentation for put_all and remove_duplicates which now allows for including these. Corrects use of enums and using @JsonValue to have usable documentation on these enums.
    
    Signed-off-by: David Venable <dlv@amazon.com>

--- data-prepper-plugins/aggregate-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/aggregate/actions/PercentSamplerAggregateActionConfig.java
+++ data-prepper-plugins/aggregate-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/aggregate/actions/PercentSamplerAggregateActionConfig.java
@@ -5,14 +5,18 @@
 
 package org.opensearch.dataprepper.plugins.processor.aggregate.actions;
 
+import com.fasterxml.jackson.annotation.JsonClassDescription;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jakarta.validation.constraints.NotNull;
-import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import jakarta.validation.constraints.AssertTrue;
 
+@JsonPropertyOrder
+@JsonClassDescription("The <code>percent_sampler</code> action controls the number of events aggregated based " +
+        "on a percentage of events. The action drops any events not included in the percentage.")
 public class PercentSamplerAggregateActionConfig {
-    @JsonPropertyDescription("The percentage of events to be processed during a one second interval. Must be greater than 0.0 and less than 100.0")
+    @JsonPropertyDescription("The percentage of events to be processed during a one second interval. Must be greater than 0.0 and less than 100.0.")
     @JsonProperty("percent")
     @NotNull
     private double percent;
--- data-prepper-plugins/aggregate-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/aggregate/actions/TailSamplerAggregateActionConfig.java
+++ data-prepper-plugins/aggregate-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/aggregate/actions/TailSamplerAggregateActionConfig.java
@@ -5,25 +5,29 @@
 
 package org.opensearch.dataprepper.plugins.processor.aggregate.actions;
 
+import com.fasterxml.jackson.annotation.JsonClassDescription;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.AssertTrue;
 
 import java.time.Duration;
 
+@JsonPropertyOrder
+@JsonClassDescription("The <code>tail_sampler</code> action samples OpenTelemetry traces after collecting spans for a trace.")
 public class TailSamplerAggregateActionConfig {
     @JsonPropertyDescription("Period to wait before considering that a trace event is complete")
     @JsonProperty("wait_period")
     @NotNull
     private Duration waitPeriod;
     
-    @JsonPropertyDescription("Percent value to use for sampling non error events. 0.0 &lt; percent &lt; 100.0")
+    @JsonPropertyDescription("Percent value to use for sampling non error events. Must be greater than 0.0 and less than 100.0")
     @JsonProperty("percent")
     @NotNull
     private Integer percent;
     
-    @JsonPropertyDescription("A Data Prepper <a href=\"https://opensearch.org/docs/latest/data-prepper/pipelines/expression-syntax/\">conditional expression</a>, such as '/some-key == \"test\"', that will be evaluated to determine whether the event is an error event or not")
+    @JsonPropertyDescription("A <a href=\"https://opensearch.org/docs/latest/data-prepper/pipelines/expression-syntax/\">conditional expression</a>, such as '/some-key == \"test\"', that will be evaluated to determine whether the event is an error event or not")
     @JsonProperty("condition")
     private String condition;
 
