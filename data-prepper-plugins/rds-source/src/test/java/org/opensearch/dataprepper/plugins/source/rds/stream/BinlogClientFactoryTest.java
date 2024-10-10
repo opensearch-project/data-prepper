2f3a2804a456805107699721734d246be105405a
Hai Yan <8153134+oeyh@users.noreply.github.com>
2024-10-10 12:04:07 -0500

    Support secret rotation in RDS source (#5016)
    * Add stream worker refresher
    
    Signed-off-by: Hai Yan <oeyh@amazon.com>
    
    * Update unit tests
    
    Signed-off-by: Hai Yan <oeyh@amazon.com>
    
    * Address comments
    
    Signed-off-by: Hai Yan <oeyh@amazon.com>
    
    ---------
    
    Signed-off-by: Hai Yan <oeyh@amazon.com>

--- data-prepper-plugins/rds-source/src/test/java/org/opensearch/dataprepper/plugins/source/rds/stream/BinlogClientFactoryTest.java
+++ data-prepper-plugins/rds-source/src/test/java/org/opensearch/dataprepper/plugins/source/rds/stream/BinlogClientFactoryTest.java
@@ -5,25 +5,24 @@
 
 package org.opensearch.dataprepper.plugins.source.rds.stream;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Answers;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.opensearch.dataprepper.plugins.source.rds.RdsSourceConfig;
 import org.opensearch.dataprepper.plugins.source.rds.model.DbMetadata;
 import software.amazon.awssdk.services.rds.RdsClient;
 
-import java.util.Random;
+import java.util.UUID;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class BinlogClientFactoryTest {
 
-    @Mock
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     private RdsSourceConfig sourceConfig;
 
     @Mock
@@ -33,28 +32,22 @@ class BinlogClientFactoryTest {
     private DbMetadata dbMetadata;
 
     private BinlogClientFactory binlogClientFactory;
-    private Random random;
-
-    @BeforeEach
-    void setUp() {
-        binlogClientFactory = createBinlogClientFactory();
-        random = new Random();
-    }
 
     @Test
     void test_create() {
-        final RdsSourceConfig.AuthenticationConfig authenticationConfig = mock(RdsSourceConfig.AuthenticationConfig.class);
-        when(sourceConfig.getAuthenticationConfig()).thenReturn(authenticationConfig);
+        final String username = UUID.randomUUID().toString();
+        final String password = UUID.randomUUID().toString();
+        when(sourceConfig.getAuthenticationConfig().getUsername()).thenReturn(username);
+        when(sourceConfig.getAuthenticationConfig().getPassword()).thenReturn(password);
 
+        binlogClientFactory = createObjectUnderTest();
         binlogClientFactory.create();
 
         verify(dbMetadata).getHostName();
         verify(dbMetadata).getPort();
-        verify(authenticationConfig).getUsername();
-        verify(authenticationConfig).getPassword();
     }
 
-    private BinlogClientFactory createBinlogClientFactory() {
+    private BinlogClientFactory createObjectUnderTest() {
         return new BinlogClientFactory(sourceConfig, rdsClient, dbMetadata);
     }
 }
