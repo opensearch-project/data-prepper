86010bb8e6b248c38cd6211ef3ca6570dd1762d4
Souvik Bose <souvbose@amazon.com>
2024-10-09 17:57:24 -0700

    Disable the health check endpoint.
    Signed-off-by: Souvik Bose <souvbose@amazon.com>

--- data-prepper-plugins/opensearch-api-source/src/main/java/org/opensearch/dataprepper/plugins/source/opensearchapi/OpenSearchAPISourceConfig.java
+++ data-prepper-plugins/opensearch-api-source/src/main/java/org/opensearch/dataprepper/plugins/source/opensearchapi/OpenSearchAPISourceConfig.java
@@ -21,4 +21,9 @@ public class OpenSearchAPISourceConfig extends BaseHttpServerConfig {
     public String getDefaultPath() {
         return DEFAULT_ENDPOINT_URI;
     }
+
+    @Override
+    public boolean hasHealthCheckService() {
+        return false;
+    }
 }
