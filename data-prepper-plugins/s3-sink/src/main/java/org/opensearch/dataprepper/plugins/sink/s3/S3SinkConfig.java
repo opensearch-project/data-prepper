47fdbda0f7fff077f8725f00f2eb42a8f07816d2
Krishna Kondaka <41027584+kkondaka@users.noreply.github.com>
2024-10-14 17:12:21 -0700

    Fixed s3 sink config validation (#5063)
    * Fixed s3 sink config validation
    
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    
    * Added test cases
    
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    
    ---------
    
    Signed-off-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>
    Co-authored-by: Krishna Kondaka <krishkdk@dev-dsk-krishkdk-2c-bd29c437.us-west-2.amazon.com>

--- data-prepper-plugins/s3-sink/src/main/java/org/opensearch/dataprepper/plugins/sink/s3/S3SinkConfig.java
+++ data-prepper-plugins/s3-sink/src/main/java/org/opensearch/dataprepper/plugins/sink/s3/S3SinkConfig.java
@@ -50,9 +50,9 @@ public class S3SinkConfig {
     private ObjectMetadataConfig objectMetadataConfig;
 
     @AssertTrue(message = "Only one of object_metadata and predefined_object_metadata can be used.")
-    private boolean isValidMetadataConfig() {
-       return (objectMetadataConfig != null && predefinedObjectMetadata == null) ||
-              (objectMetadataConfig == null && predefinedObjectMetadata != null);
+    boolean isValidMetadataConfig() {
+        // One of them or both should be null
+        return (objectMetadataConfig == null || predefinedObjectMetadata == null);
     }
 
     @AssertTrue(message = "You may not use both bucket and bucket_selector together in one S3 sink.")
@@ -151,8 +151,8 @@ public class S3SinkConfig {
         return objectKeyOptions;
     }
 
-    public ObjectMetadataConfig getObjectMetadataConfig() {
-        return objectMetadataConfig;
+    public Object getObjectMetadataConfig() {
+        return objectMetadataConfig != null ? objectMetadataConfig : predefinedObjectMetadata;
     }
 
     /**
