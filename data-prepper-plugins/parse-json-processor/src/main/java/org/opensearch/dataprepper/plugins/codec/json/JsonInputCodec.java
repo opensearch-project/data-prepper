7da5d5123322380a7e5ab3c7e9fc83493060fcb8
Souvik Bose <souvik04in@gmail.com>
2024-10-14 20:46:35 -0700

    Json codec changes with specific json input codec config (#5054)
    * Json codec changes with specific json input codec config
    
    Signed-off-by: Souvik Bose <souvbose@amazon.com>
    
    * Modify the tests and address comments
    
    Signed-off-by: Souvik Bose <souvbose@amazon.com>
    
    ---------
    
    Signed-off-by: Souvik Bose <souvbose@amazon.com>
    Co-authored-by: Souvik Bose <souvbose@amazon.com>

--- data-prepper-plugins/parse-json-processor/src/main/java/org/opensearch/dataprepper/plugins/codec/json/JsonInputCodec.java
+++ data-prepper-plugins/parse-json-processor/src/main/java/org/opensearch/dataprepper/plugins/codec/json/JsonInputCodec.java
@@ -6,6 +6,7 @@
 package org.opensearch.dataprepper.plugins.codec.json;
 
 import org.opensearch.dataprepper.model.annotations.DataPrepperPlugin;
+import org.opensearch.dataprepper.model.annotations.DataPrepperPluginConstructor;
 import org.opensearch.dataprepper.model.codec.InputCodec;
 import org.opensearch.dataprepper.model.codec.JsonDecoder;
 import org.opensearch.dataprepper.model.event.Event;
@@ -13,13 +14,20 @@ import org.opensearch.dataprepper.model.record.Record;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 /**
  * An implementation of {@link InputCodec} which parses JSON Objects for arrays.
  */
-@DataPrepperPlugin(name = "json", pluginType = InputCodec.class)
+@DataPrepperPlugin(name = "json", pluginType = InputCodec.class, pluginConfigurationType = JsonInputCodecConfig.class)
 public class JsonInputCodec extends JsonDecoder implements InputCodec {
+
+    @DataPrepperPluginConstructor
+    public JsonInputCodec(final JsonInputCodecConfig config) {
+        super(Objects.requireNonNull(config).getKeyName(), config.getIncludeKeys(), config.getIncludeKeysMetadata());
+    }
+
     public void parse(InputStream inputStream, Consumer<Record<Event>> eventConsumer) throws IOException {
         parse(inputStream, null, eventConsumer);
     }
