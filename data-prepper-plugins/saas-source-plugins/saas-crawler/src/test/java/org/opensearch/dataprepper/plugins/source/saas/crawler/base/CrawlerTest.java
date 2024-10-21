44828f82dc699b819eb798260175387c980db335
Maxwell Brown <mxwelwbr@amazon.com>
2024-10-21 16:16:01 -0700

    full test coverage for base folder, spotless fixes
    Signed-off-by: Maxwell Brown <mxwelwbr@amazon.com>

--- data-prepper-plugins/saas-source-plugins/saas-crawler/src/test/java/org/opensearch/dataprepper/plugins/source/saas/crawler/base/CrawlerTest.java
+++ data-prepper-plugins/saas-source-plugins/saas-crawler/src/test/java/org/opensearch/dataprepper/plugins/source/saas/crawler/base/CrawlerTest.java
@@ -16,7 +16,9 @@ import org.opensearch.dataprepper.plugins.source.saas.crawler.model.ItemInfo;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
@@ -45,9 +47,7 @@ public class CrawlerTest {
     @Mock
     private ItemInfo item;
 
-
     private Crawler crawler;
-
 
     @BeforeEach
     public void setup() {
@@ -101,10 +101,86 @@ public class CrawlerTest {
 
     }
 
+    @Test
+    void testCrawlWithNullItemsInList() throws NoSuchFieldException, IllegalAccessException {
+        long lastPollTime = 0;
+        List<ItemInfo> itemInfoList = new ArrayList<>();
+        int maxItemsPerPage = getMaxItemsPerPage();
+        itemInfoList.add(null);
+        for (int i = 0; i < maxItemsPerPage-1; i++) {
+            itemInfoList.add(item);
+        }
+        when(client.listItems()).thenReturn(itemInfoList.iterator());
+        crawler.crawl(lastPollTime, coordinator);
+        verify(coordinator, times(1)).createPartition(any(SaasSourcePartition.class));
+    }
+
+    @Test
+    void testUpdatingPollTimeNullMetaData() {
+        long lastPollTime = 0;
+        List<ItemInfo> itemInfoList = new ArrayList<>();
+        ItemInfo testItem = createTestItemInfo("1",null, null);
+        itemInfoList.add(testItem);
+        when(client.listItems()).thenReturn(itemInfoList.iterator());
+        long updatedPollTime = crawler.crawl(lastPollTime, coordinator);
+        assert(updatedPollTime != 0);
+    }
+
+    @Test
+    void testUpdatedPollTimeNiCreatedLarger() {
+        long lastPollTime = 0;
+        List<ItemInfo> itemInfoList = new ArrayList<>();
+        ItemInfo testItem = createTestItemInfo("1","10" , "5");
+        itemInfoList.add(testItem);
+        when(client.listItems()).thenReturn(itemInfoList.iterator());
+        long updatedPollTime = crawler.crawl(lastPollTime, coordinator);
+        assert(updatedPollTime == 11);
+    }
+    @Test
+    void testUpdatedPollTimeNiUpdatedLarger() {
+        long lastPollTime = 0;
+        List<ItemInfo> itemInfoList = new ArrayList<>();
+        ItemInfo testItem = createTestItemInfo("1","5" , "10");
+        itemInfoList.add(testItem);
+        when(client.listItems()).thenReturn(itemInfoList.iterator());
+        long updatedPollTime = crawler.crawl(lastPollTime, coordinator);
+        assert(updatedPollTime == 11);
+    }
+
+
     private int getMaxItemsPerPage() throws NoSuchFieldException, IllegalAccessException {
-    Field maxItemsPerPageField = Crawler.class.getDeclaredField("maxItemsPerPage");
-    maxItemsPerPageField.setAccessible(true);
-    return (int) maxItemsPerPageField.get(null);
-}
+        Field maxItemsPerPageField = Crawler.class.getDeclaredField("maxItemsPerPage");
+        maxItemsPerPageField.setAccessible(true);
+        return (int) maxItemsPerPageField.get(null);
+    }
+
+    private static class TestItemInfo extends ItemInfo {
+        public TestItemInfo(String itemId, Map<String, String> metadata, Long eventTime) {
+            super(itemId, metadata, eventTime);
+
+        }
+        @Override
+        public String getPartitionKey() {
+            return getItemId();
+        }
+
+        @Override
+        public String getId() {
+            return getItemId();
+        }
+
+        @Override
+        public Map<String, String> getKeyAttributes() {
+            return new HashMap<>();
+        }
+    }
+
+    private ItemInfo createTestItemInfo(String id, String created, String updated) {
+        Map<String, String> metadata = new HashMap<>();
+        if (created != null) metadata.put(Crawler.CREATED, created);
+        if (updated != null) metadata.put(Crawler.UPDATED, updated);
+
+        return new TestItemInfo(id, metadata, System.currentTimeMillis());
+    }
 
 }
