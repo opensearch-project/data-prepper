094a332a107f550ded01515486b5bdd9cf67f68c
Maxwell Brown <mxwelwbr@amazon.com>
2024-10-23 10:30:25 -0700

    IssueItems.java isn't used, removed
    Signed-off-by: Maxwell Brown <mxwelwbr@amazon.com>

--- data-prepper-plugins/saas-source-plugins/jira-source/src/test/java/org/opensearch/dataprepper/plugins/source/saas/jira/models/IssueItemTest.java
+++
@@ -1,8 +0,0 @@
-package org.opensearch.dataprepper.plugins.source.saas.jira.models;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-public class IssueItemTest {
-}
