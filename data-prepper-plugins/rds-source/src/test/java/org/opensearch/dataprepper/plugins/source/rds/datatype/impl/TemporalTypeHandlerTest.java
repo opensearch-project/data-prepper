390acc1d790e48da602fb915546f194f26c25243
Dinu John <86094133+dinujoh@users.noreply.github.com>
2024-11-15 10:08:07 -0600

    update test
    Signed-off-by: Dinu John <86094133+dinujoh@users.noreply.github.com>

--- data-prepper-plugins/rds-source/src/test/java/org/opensearch/dataprepper/plugins/source/rds/datatype/impl/TemporalTypeHandlerTest.java
+++ data-prepper-plugins/rds-source/src/test/java/org/opensearch/dataprepper/plugins/source/rds/datatype/impl/TemporalTypeHandlerTest.java
@@ -59,7 +59,7 @@ class TemporalTypeHandlerTest {
                 Arguments.of("2023-12-25", getEpochMillisFromDate(2023, 12, 25)),
                 Arguments.of("1970-01-01", getEpochMillisFromDate(1970, 1, 1)),
                 Arguments.of("2024-02-29", getEpochMillisFromDate(2024, 2, 29)), // Leap year
-                Arguments.of("0000-00-00", null), // Leap year
+                Arguments.of("0000-00-00", null),
                 Arguments.of("1684108800000", getEpochMillisFromDate(2023, 5, 15))
         );
     }
