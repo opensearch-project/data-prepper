c3a6be6d9f83d7ab3f240216481f0b50aaccae38
Katherine Shen <katshen@amazon.com>
2024-12-12 16:22:18 -0600

    update lambda processor descriptions
    Signed-off-by: Katherine Shen <katshen@amazon.com>

--- data-prepper-plugins/aws-lambda/src/main/java/org/opensearch/dataprepper/plugins/lambda/processor/LambdaProcessorConfig.java
+++ data-prepper-plugins/aws-lambda/src/main/java/org/opensearch/dataprepper/plugins/lambda/processor/LambdaProcessorConfig.java
@@ -7,32 +7,43 @@ package org.opensearch.dataprepper.plugins.lambda.processor;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonClassDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.opensearch.dataprepper.model.annotations.ExampleValues;
+import org.opensearch.dataprepper.model.annotations.ExampleValues.Example;
 import java.util.Collections;
 import java.util.List;
 import org.opensearch.dataprepper.plugins.lambda.common.config.InvocationType;
 import org.opensearch.dataprepper.plugins.lambda.common.config.LambdaCommonConfig;
 
+@JsonPropertyOrder
+@JsonClassDescription("The <code>aws_lambda</code> processor enables invocation of an AWS Lambda function within your Data Prepper pipeline in order to process events." + 
+        "It supports both synchronous and asynchronous invocations based on your use case.")
 public class LambdaProcessorConfig extends LambdaCommonConfig {
+  static final String DEFAULT_INVOCATION_TYPE = "request-response"
 
-  @JsonPropertyDescription("invocation type defines the way we want to call lambda function")
-  @JsonProperty("invocation_type")
+  @JsonPropertyDescription("Specifies the invocation type, either <code>request-response</code> or <code>event</code>. Default is <code>request-response</code>.")
+  @JsonProperty("invocation_type", defaultValue = DEFAULT_INVOCATION_TYPE)
   private InvocationType invocationType = InvocationType.REQUEST_RESPONSE;
 
-  @JsonPropertyDescription("Defines the way Data Prepper treats the response from Lambda")
+  @JsonPropertyDescription("Specifies how Data Prepper interprets and processes Lambda function responses. Default is <code>false</code>.")
   @JsonProperty("response_events_match")
   private boolean responseEventsMatch = false;
-
-  @JsonPropertyDescription("defines a condition for event to use this processor")
-  @JsonProperty("lambda_when")
-  private String whenCondition;
 
   @JsonProperty("tags_on_failure")
   @JsonPropertyDescription(
       "A <code>List</code> of <code>String</code>s that specifies the tags to be set in the event when lambda fails to "
           +
           "or exception occurs. This tag may be used in conditional expressions in " +
-          "other parts of the configuration")
+          "other parts of the configuration.")
   private List<String> tagsOnFailure = Collections.emptyList();
+
+  @JsonPropertyDescription("Defines a condition for event to use this processor.")
+  @ExampleValues({
+          @Example(value = "event['status'] == 'process'", description = "The processor will only run on this condition.")
+  })
+  @JsonProperty("lambda_when")
+  private String whenCondition;
 
   public List<String> getTagsOnFailure() {
     return tagsOnFailure;
