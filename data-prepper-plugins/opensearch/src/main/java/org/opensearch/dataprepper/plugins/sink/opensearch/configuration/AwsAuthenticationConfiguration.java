53398fb104d2b829ac6c9931fb3b80e77e5a9c68
Maxwell Brown <mxwelwbr@amazon.com>
2024-12-18 16:46:10 -0800

    reset awsauthconfig to normal
    Signed-off-by: Maxwell Brown <mxwelwbr@amazon.com>

--- data-prepper-plugins/opensearch/src/main/java/org/opensearch/dataprepper/plugins/sink/opensearch/configuration/AwsAuthenticationConfiguration.java
+++ data-prepper-plugins/opensearch/src/main/java/org/opensearch/dataprepper/plugins/sink/opensearch/configuration/AwsAuthenticationConfiguration.java
@@ -15,14 +15,15 @@ import java.util.Map;
 public class AwsAuthenticationConfiguration {
 
     @JsonProperty("region")
+    @Size(min = 1, message = "Region cannot be empty string")
     private String awsRegion;
 
     @JsonProperty("sts_role_arn")
-    @Size(max = 2048, message = "awsStsRoleArn length should be less than 2048 characters")
+    @Size(min = 20, max = 2048, message = "awsStsRoleArn length should be between 1 and 2048 characters")
     private String awsStsRoleArn;
 
     @JsonProperty("sts_external_id")
-    @Size(max = 1224, message = "awsStsExternalId length should be between less than 1224 characters")
+    @Size(min = 2, max = 1224, message = "awsStsExternalId length should be between 2 and 1224 characters")
     private String awsStsExternalId;
 
     @JsonProperty("sts_header_overrides")
