bad172c3f10b277ea888e8dc910f8baf961baf8b
Maxwell Brown <55033421+Galactus22625@users.noreply.github.com>
2025-01-08 16:38:23 -0800

    Jira Source Configuration and Filter Changes and add license headers (#5306)
    * jira yaml changes and tests, authentication and hosts
    
    Signed-off-by: Maxwell Brown <mxwelwbr@amazon.com>
    
    * filter config file changes
    
    Signed-off-by: Maxwell Brown <mxwelwbr@amazon.com>
    
    * jira filter config changes and implementation
    
    Signed-off-by: Maxwell Brown <mxwelwbr@amazon.com>
    
    * filter change test corrections
    
    Signed-off-by: Maxwell Brown <mxwelwbr@amazon.com>
    
    * project name -> key for now
    
    Signed-off-by: Maxwell Brown <mxwelwbr@amazon.com>
    
    * removed unfishished function
    
    Signed-off-by: Maxwell Brown <mxwelwbr@amazon.com>
    
    * address pr comments
    
    Signed-off-by: Maxwell Brown <mxwelwbr@amazon.com>
    
    * Add license headers to all jira source files
    
    Signed-off-by: Maxwell Brown <mxwelwbr@amazon.com>
    
    ---------
    
    Signed-off-by: Maxwell Brown <mxwelwbr@amazon.com>

--- data-prepper-plugins/saas-source-plugins/jira-source/src/main/java/org/opensearch/dataprepper/plugins/source/jira/JiraSourceConfig.java
+++ data-prepper-plugins/saas-source-plugins/jira-source/src/main/java/org/opensearch/dataprepper/plugins/source/jira/JiraSourceConfig.java
@@ -1,16 +1,24 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ */
+
 package org.opensearch.dataprepper.plugins.source.jira;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.Valid;
 import lombok.Getter;
+import org.opensearch.dataprepper.plugins.source.jira.configuration.AuthenticationConfig;
+import org.opensearch.dataprepper.plugins.source.jira.configuration.FilterConfig;
 import org.opensearch.dataprepper.plugins.source.source_crawler.base.CrawlerSourceConfig;
 
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
-import static org.opensearch.dataprepper.plugins.source.jira.utils.Constants.OAUTH2;
 
 @Getter
 public class JiraSourceConfig implements CrawlerSourceConfig {
@@ -20,50 +28,23 @@ public class JiraSourceConfig implements CrawlerSourceConfig {
     /**
      * Jira account url
      */
-    @JsonProperty("account_url")
-    private String accountUrl;
+    @JsonProperty("hosts")
+    private List<String> hosts;
 
     /**
-     * A map of connector credentials specific to this source
+     * Authentication Config to Access Jira
      */
-    @JsonProperty("connector_credentials")
-    private Map<String, String> connectorCredentials;
+    @JsonProperty("authentication")
+    @Valid
+    private AuthenticationConfig authenticationConfig;
+
 
     /**
-     * List of projects to ingest
+     * Filter Config to filter what tickets get ingested
      */
-    @JsonProperty("projects")
-    @Size(max = 1000, message = "Project type filter should not be more than 1000")
-    private List<String> project = new ArrayList<>();
+    @JsonProperty("filter")
+    private FilterConfig filterConfig;
 
-    /**
-     * List of specific issue types to ingest.
-     * Ex: Story, Epic, Task etc
-     */
-    @JsonProperty("issue_types")
-    @Size(max = 1000, message = "Issue type filter should be less than 1000")
-    private List<String> issueType = new ArrayList<>();
-
-    /**
-     * Optional Inclusion patterns for filtering some tickets
-     */
-    @JsonProperty("inclusion_patterns")
-    @Size(max = 100, message = "inclusion pattern filters should not be more than 1000")
-    private List<String> inclusionPatterns;
-
-    /**
-     * Optional Exclusion patterns for excluding some tickets
-     */
-    @JsonProperty("exclusion_patterns")
-    @Size(max = 1000, message = "exclusion pattern filter should be less than 1000")
-    private List<String> exclusionPatterns;
-
-    /**
-     * Optional Status filter to ingest the tickets
-     */
-    @JsonProperty("statuses")
-    @Size(max = 1000, message = "Status filter should be less than 1000")
-    private List<String> status = new ArrayList<>();
 
     /**
      * Number of worker threads to spawn to parallel source fetching
@@ -78,43 +59,11 @@ public class JiraSourceConfig implements CrawlerSourceConfig {
     @JsonProperty("backoff_time")
     private Duration backOff = DEFAULT_BACKOFF_MILLIS;
 
-    public String getJiraId() {
-        return this.getConnectorCredentials().get("jira_id");
-    }
-
-    public String getJiraCredential() {
-        return this.getConnectorCredentials().get("jira_credential");
+    public String getAccountUrl() {
+        return this.getHosts().get(0);
     }
 
     public String getAuthType() {
-        return this.getConnectorCredentials().get("auth_type");
+        return this.getAuthenticationConfig().getAuthType();
     }
-
-    public String getAccessToken() {
-        return fetchGivenOAuthAttribute("access_token");
-    }
-
-    public String getRefreshToken() {
-        return fetchGivenOAuthAttribute("refresh_token");
-    }
-
-    public String getClientId() {
-        return fetchGivenOAuthAttribute("client_id");
-    }
-
-    public String getClientSecret() {
-        return fetchGivenOAuthAttribute("client_secret");
-    }
-
-    private String fetchGivenOAuthAttribute(String givenAttribute) {
-        if (!OAUTH2.equals(getAuthType())) {
-            throw new RuntimeException("Authentication Type is not OAuth2.");
-        }
-        String attributeValue = this.getConnectorCredentials().get(givenAttribute);
-        if (attributeValue == null || attributeValue.isEmpty()) {
-            throw new RuntimeException(String.format("%s is required for OAuth2 AuthType", givenAttribute));
-        }
-        return attributeValue;
-    }
-
 }
