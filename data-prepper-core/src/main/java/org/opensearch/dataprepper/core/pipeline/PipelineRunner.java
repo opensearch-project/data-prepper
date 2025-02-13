ef2fd341a79c83ed9bb38a0baaa0f07ca4dcada9
Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>
2025-02-13 10:37:11 +0000

    Pipeline Runner Implementation, ProcessWorker Refactoring and tests
    Signed-off-by: Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>

--- data-prepper-core/src/main/java/org/opensearch/dataprepper/core/pipeline/PipelineRunner.java
+++ data-prepper-core/src/main/java/org/opensearch/dataprepper/core/pipeline/PipelineRunner.java
@@ -1,5 +1,10 @@
 package org.opensearch.dataprepper.core.pipeline;
 
+/**
+ * Pipeline Runner interface encapsulates the functionalities of reading from buffer,
+ * executing the processors and publishing to sinks to provide both synchronous and
+ * asynchronous mechanism for running a pipeline.
+ */
 public interface PipelineRunner {
     void runAllProcessorsAndPublishToSinks();
 }
