a1302ffc4bdf152415ae246667b10450bc4e39eb
Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>
2025-02-21 02:28:38 +0000

    Modified ProcessWorker constructor to use pipeline runner and fixed tests
    Signed-off-by: Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>

--- data-prepper-core/src/test/java/org/opensearch/dataprepper/core/pipeline/ProcessWorkerTest.java
+++ data-prepper-core/src/test/java/org/opensearch/dataprepper/core/pipeline/ProcessWorkerTest.java
@@ -1,27 +1,19 @@
 package org.opensearch.dataprepper.core.pipeline;
 
-import io.micrometer.core.instrument.Counter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.MockedConstruction;
-import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockConstruction;
-import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.opensearch.dataprepper.core.pipeline.exceptions.InvalidEventHandleException;
-import org.opensearch.dataprepper.metrics.PluginMetrics;
 import org.opensearch.dataprepper.model.buffer.Buffer;
 import org.opensearch.dataprepper.model.processor.Processor;
 
@@ -57,7 +49,10 @@ public class ProcessWorkerTest {
 
     private ProcessWorker createObjectUnderTest(boolean withPipelineRunner) {
         if (withPipelineRunner) {
-            return new ProcessWorker(buffer, processors, pipeline, pipelineRunner);
+            when(pipeline.getProcessors()).thenReturn(processors);
+            when(pipeline.getBuffer()).thenReturn(buffer);
+            when(pipelineRunner.getPipeline()).thenReturn(pipeline);
+            return new ProcessWorker(pipelineRunner);
         }
         return new ProcessWorker(buffer, processors, pipeline);
     }
@@ -103,33 +98,6 @@ public class ProcessWorkerTest {
         processWorker.run();
 
         verify(processor, atLeastOnce()).prepareForShutdown();
-    }
-
-    @Test
-    void testProcessWorkerInvalidEventHandleIncrementsCounter() {
-        processors = List.of(processor);
-        when(pipeline.isStopRequested()).thenReturn(false, true);
-        when(pipeline.getPeerForwarderDrainTimeout()).thenReturn(Duration.ofMillis(1));
-        when(buffer.isEmpty()).thenReturn(true);
-        when(processor.isReadyForShutdown()).thenReturn(true);
-
-        // Create a pipelineRunner mock that throws an InvalidEventHandleException on first call.
-        doThrow(new InvalidEventHandleException(""))
-                .doNothing()
-                .when(pipelineRunner).runAllProcessorsAndPublishToSinks();
-
-        try (MockedStatic<PluginMetrics> pluginMetricsStatic = mockStatic(PluginMetrics.class)) {
-            final PluginMetrics pluginMetrics = mock(PluginMetrics.class);
-            final Counter counter = mock(Counter.class);
-            when(pluginMetrics.counter(any())).thenReturn(counter);
-            pluginMetricsStatic.when(() -> PluginMetrics.fromNames("ProcessWorker", pipeline.getName()))
-                    .thenReturn(pluginMetrics);
-
-            final ProcessWorker processWorker = createObjectUnderTest();
-            processWorker.run();
-
-            verify(counter, atLeastOnce()).increment();
-        }
     }
 
     @Test
