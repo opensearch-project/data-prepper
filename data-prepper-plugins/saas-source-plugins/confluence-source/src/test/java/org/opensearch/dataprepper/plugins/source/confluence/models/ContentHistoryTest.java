be83084829c817af439866ded63d6557419ceb23
Santhosh Gandhe <1909520+san81@users.noreply.github.com>
2025-02-21 17:15:52 -0800

    page headers
    Signed-off-by: Santhosh Gandhe <1909520+san81@users.noreply.github.com>

--- data-prepper-plugins/saas-source-plugins/confluence-source/src/test/java/org/opensearch/dataprepper/plugins/source/confluence/models/ContentHistoryTest.java
+++ data-prepper-plugins/saas-source-plugins/confluence-source/src/test/java/org/opensearch/dataprepper/plugins/source/confluence/models/ContentHistoryTest.java
@@ -1,7 +1,15 @@
-package org.opensearch.dataprepper.plugins.source.confluence.configuration;
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ */
+package org.opensearch.dataprepper.plugins.source.confluence.models;
 
 import org.junit.jupiter.api.Test;
-import org.opensearch.dataprepper.plugins.source.confluence.models.ContentHistory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
