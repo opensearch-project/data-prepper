60321ee56ddf1165ae04430a79265a1dc69ec667
Hai Yan <oeyh@amazon.com>
2025-02-26 14:46:13 -0600

    Update message types
    Signed-off-by: Hai Yan <oeyh@amazon.com>

--- data-prepper-plugins/rds-source/src/main/java/org/opensearch/dataprepper/plugins/source/rds/stream/StreamWorker.java
+++ data-prepper-plugins/rds-source/src/main/java/org/opensearch/dataprepper/plugins/source/rds/stream/StreamWorker.java
@@ -66,11 +66,7 @@ public class StreamWorker {
             sourceCoordinator.giveUpPartition(streamPartition);
             throw new RuntimeException(e);
         } finally {
-            try {
-                replicationLogClient.disconnect();
-            } catch (Exception e) {
-                LOG.error("Replication log client failed to disconnect.", e);
-            }
+            shutdown();
         }
     }
 
