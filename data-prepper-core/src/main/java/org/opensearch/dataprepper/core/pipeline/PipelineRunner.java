b17ce8dd9c2e6f64f7c18ad2436f98c967b91a06
Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>
2025-03-09 14:09:12 -0700

    Pipeline Runner Implementation and Tests (#5435)
    * Pipeline Runner Implementation, ProcessWorker Refactoring and tests
    
    Signed-off-by: Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>
    
    * Modified ProcessWorker constructor to use pipeline runner and fixed tests
    
    Signed-off-by: Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>
    
    * Modified AggregateProcessorITWithAcks tests to use pipeline runner
    
    Signed-off-by: Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>
    
    * Increased test timeout for AggregateProcessorITWithAcks
    
    Signed-off-by: Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>
    
    * Added copyright header to PipelineRunner
    
    Signed-off-by: Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>

--- data-prepper-core/src/main/java/org/opensearch/dataprepper/core/pipeline/PipelineRunner.java
+++ data-prepper-core/src/main/java/org/opensearch/dataprepper/core/pipeline/PipelineRunner.java
@@ -1,5 +1,17 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.opensearch.dataprepper.core.pipeline;
 
+/**
+ * Pipeline Runner interface encapsulates the functionalities of reading from buffer,
+ * executing the processors and publishing to sinks to provide both synchronous and
+ * asynchronous mechanism for running a pipeline.
+ */
 public interface PipelineRunner {
     void runAllProcessorsAndPublishToSinks();
+
+    Pipeline getPipeline();
 }
--- data-prepper-core/src/main/java/org/opensearch/dataprepper/core/pipeline/ProcessWorker.java
+++ data-prepper-core/src/main/java/org/opensearch/dataprepper/core/pipeline/ProcessWorker.java
@@ -5,41 +5,23 @@
 
 package org.opensearch.dataprepper.core.pipeline;
 
-import io.micrometer.core.instrument.Counter;
-import org.opensearch.dataprepper.core.pipeline.common.FutureHelper;
-import org.opensearch.dataprepper.core.pipeline.common.FutureHelperResult;
-import org.opensearch.dataprepper.metrics.PluginMetrics;
-import org.opensearch.dataprepper.model.CheckpointState;
 import org.opensearch.dataprepper.model.buffer.Buffer;
-import org.opensearch.dataprepper.model.event.DefaultEventHandle;
-import org.opensearch.dataprepper.model.event.Event;
-import org.opensearch.dataprepper.model.event.EventHandle;
-import org.opensearch.dataprepper.model.event.InternalEventHandle;
 import org.opensearch.dataprepper.model.processor.Processor;
-import org.opensearch.dataprepper.model.record.Record;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.Future;
-import java.util.stream.Collectors;
 
 @SuppressWarnings({"rawtypes", "unchecked"})
 public class ProcessWorker implements Runnable {
     private static final Logger LOG = LoggerFactory.getLogger(ProcessWorker.class);
 
-    private static final String INVALID_EVENT_HANDLES = "invalidEventHandles";
     private final Buffer readBuffer;
     private final List<Processor> processors;
     private final Pipeline pipeline;
-    private boolean isEmptyRecordsLogged = false;
-    private PluginMetrics pluginMetrics;
-    private final Counter invalidEventHandlesCounter;
-    private boolean acknowledgementsEnabled;
+
+    private final PipelineRunner pipelineRunner;
 
     public ProcessWorker(
             final Buffer readBuffer,
@@ -48,9 +30,14 @@ public class ProcessWorker implements Runnable {
         this.readBuffer = readBuffer;
         this.processors = processors;
         this.pipeline = pipeline;
-        this.pluginMetrics = PluginMetrics.fromNames("ProcessWorker", pipeline.getName());
-        this.invalidEventHandlesCounter = pluginMetrics.counter(INVALID_EVENT_HANDLES);
-        this.acknowledgementsEnabled = pipeline.getSource().areAcknowledgementsEnabled() || readBuffer.areAcknowledgementsEnabled();
+        this.pipelineRunner = new PipelineRunnerImpl(pipeline);
+    }
+
+    public ProcessWorker(PipelineRunner pipelineRunner) {
+        this.pipelineRunner = pipelineRunner;
+        this.readBuffer = pipelineRunner.getPipeline().getBuffer();
+        this.processors = pipelineRunner.getPipeline().getProcessors();
+        this.pipeline = pipelineRunner.getPipeline();
     }
 
     @Override
@@ -97,64 +84,8 @@ public class ProcessWorker implements Runnable {
         LOG.info("Processor shutdown phase 5 complete.");
     }
 
-    private void processAcknowledgements(List<Event> inputEvents, Collection<Record<Event>> outputRecords) {
-        Set<Event> outputEventsSet = outputRecords.stream().map(Record::getData).collect(Collectors.toSet());
-        // For each event in the input events list that is not present in the output events, send positive acknowledgement, if acknowledgements are enabled for it
-        inputEvents.forEach(event -> {
-            EventHandle eventHandle = event.getEventHandle();
-            if (eventHandle != null && eventHandle instanceof DefaultEventHandle) {
-                InternalEventHandle internalEventHandle = (InternalEventHandle)(DefaultEventHandle)eventHandle;
-                if (!outputEventsSet.contains(event)) {
-                    eventHandle.release(true);
-                }
-            } else if (eventHandle != null) {
-                invalidEventHandlesCounter.increment();
-                throw new RuntimeException("Unexpected EventHandle");
-            }
-        });
-    }
-
     private void doRun() {
-        final Map.Entry<Collection, CheckpointState> readResult = readBuffer.read(pipeline.getReadBatchTimeoutInMillis());
-        Collection records = readResult.getKey();
-        final CheckpointState checkpointState = readResult.getValue();
-        //TODO Hacky way to avoid logging continuously - Will be removed as part of metrics implementation
-        if (records.isEmpty()) {
-            if(!isEmptyRecordsLogged) {
-                LOG.debug(" {} Worker: No records received from buffer", pipeline.getName());
-                isEmptyRecordsLogged = true;
-            }
-        } else {
-            LOG.debug(" {} Worker: Processing {} records from buffer", pipeline.getName(), records.size());
-        }
-        //Should Empty list from buffer should be sent to the processors? For now sending as the Stateful processors expects it.
-        for (final Processor processor : processors) {
-
-            List<Event> inputEvents = null;
-            if (acknowledgementsEnabled) {
-                inputEvents = ((List<Record<Event>>) records).stream().map(Record::getData).collect(Collectors.toList());
-            }
-
-            try {
-                records = processor.execute(records);
-                // acknowledge missing events only if the processor is not holding events
-                if (!processor.holdsEvents() && inputEvents != null) {
-                    processAcknowledgements(inputEvents, records);
-                }
-            } catch (final Exception e) {
-                LOG.error("A processor threw an exception. This batch of Events will be dropped, and their EventHandles will be released: ", e);
-                if (inputEvents != null) {
-                    processAcknowledgements(inputEvents, Collections.emptyList());
-                }
-
-                records = Collections.emptyList();
-                break;
-            }
-        }
-
-        postToSink(records);
-        // Checkpoint the current batch read from the buffer after being processed by processors and sinks.
-        readBuffer.checkpoint(checkpointState);
+        pipelineRunner.runAllProcessorsAndPublishToSinks();
     }
 
     private boolean areComponentsReadyForShutdown() {
@@ -169,17 +100,5 @@ public class ProcessWorker implements Runnable {
         final boolean isBufferReadyForShutdown = isBufferEmpty || forceStopReadingBuffers;
         LOG.debug("isBufferReadyForShutdown={}, isBufferEmpty={}, forceStopReadingBuffers={}", isBufferReadyForShutdown, isBufferEmpty, forceStopReadingBuffers);
         return isBufferReadyForShutdown;
-    }
-
-    /**
-     * TODO Add isolator pattern - Fail if one of the Sink fails [isolator Pattern]
-     * Uses the pipeline method to publish to sinks, waits for each of the sink result to be true before attempting to
-     * process more records from buffer.
-     */
-    private boolean postToSink(final Collection<Record> records) {
-        LOG.debug("Pipeline Worker: Submitting {} processed records to sinks", records.size());
-        final List<Future<Void>> sinkFutures = pipeline.publishToSinks(records);
-        final FutureHelperResult<Void> futureResults = FutureHelper.awaitFuturesIndefinitely(sinkFutures);
-        return futureResults.getFailedReasons().size() == 0;
     }
 }
