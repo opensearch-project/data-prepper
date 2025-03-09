fa19f25d40f65dab1d803131e6ca07fcafa6aab1
Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>
2025-03-09 14:09:12 -0700

    Pipeline Runner Implementation and Tests (#5435)
    * Pipeline Runner Implementation, ProcessWorker Refactoring and tests
    
    Signed-off-by: Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>
    
    * Modified ProcessWorker constructor to use pipeline runner and fixed tests
    
    Signed-off-by: Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>
    
    * Modified AggregateProcessorITWithAcks tests to use pipeline runner
    
    Signed-off-by: Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>
    
    * Increased test timeout for AggregateProcessorITWithAcks
    
    Signed-off-by: Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>
    
    * Added copyright header to PipelineRunner
    
    Signed-off-by: Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Mohammed Aghil Puthiyottil <57040494+MohammedAghil@users.noreply.github.com>
    Signed-off-by: George Chen <qchea@amazon.com>

--- data-prepper-core/src/test/java/org/opensearch/dataprepper/core/pipeline/ProcessWorkerTest.java
+++ data-prepper-core/src/test/java/org/opensearch/dataprepper/core/pipeline/ProcessWorkerTest.java
@@ -4,307 +4,132 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
+import org.mockito.MockedConstruction;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.mockito.Mockito.lenient;
+
 import static org.mockito.Mockito.atLeast;
-import org.opensearch.dataprepper.core.pipeline.common.FutureHelper;
-import org.opensearch.dataprepper.core.pipeline.common.FutureHelperResult;
-import org.opensearch.dataprepper.model.CheckpointState;
-import org.opensearch.dataprepper.model.acknowledgements.AcknowledgementSet;
-import org.opensearch.dataprepper.model.buffer.Buffer;
-import org.opensearch.dataprepper.model.event.DefaultEventHandle;
-import org.opensearch.dataprepper.model.event.Event;
-import org.opensearch.dataprepper.model.event.EventHandle;
-import org.opensearch.dataprepper.model.event.InternalEventHandle;
-import org.opensearch.dataprepper.model.processor.Processor;
-import org.opensearch.dataprepper.model.record.Record;
-import org.opensearch.dataprepper.model.source.Source;
-
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.Future;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.mockStatic;
-import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mockConstruction;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import org.opensearch.dataprepper.model.buffer.Buffer;
+import org.opensearch.dataprepper.model.processor.Processor;
+
+import java.time.Duration;
+import java.util.List;
+
 @ExtendWith(MockitoExtension.class)
 public class ProcessWorkerTest {
-
     @Mock
     private Pipeline pipeline;
+
+    @Mock
+    private Processor processor;
 
     @Mock
     private Buffer buffer;
 
     @Mock
-    private Source source;
-
-    private List<Future<Void>> sinkFutures;
+    private PipelineRunnerImpl pipelineRunner;
 
     private List<Processor> processors;
 
     @BeforeEach
     void setup() {
         when(pipeline.isStopRequested()).thenReturn(false).thenReturn(true);
-        when(source.areAcknowledgementsEnabled()).thenReturn(false);
-        when(pipeline.getSource()).thenReturn(source);
-        when(buffer.isEmpty()).thenReturn(true);
         when(pipeline.getPeerForwarderDrainTimeout()).thenReturn(Duration.ofMillis(100));
-        when(pipeline.getReadBatchTimeoutInMillis()).thenReturn(500);
-
-        final Future<Void> sinkFuture = mock(Future.class);
-        sinkFutures = List.of(sinkFuture);
-        when(pipeline.publishToSinks(any())).thenReturn(sinkFutures);
+        when(buffer.isEmpty()).thenReturn(true);
     }
 
     private ProcessWorker createObjectUnderTest() {
+        return createObjectUnderTest(true);
+    }
+
+    private ProcessWorker createObjectUnderTest(boolean withPipelineRunner) {
+        if (withPipelineRunner) {
+            when(pipeline.getProcessors()).thenReturn(processors);
+            when(pipeline.getBuffer()).thenReturn(buffer);
+            when(pipelineRunner.getPipeline()).thenReturn(pipeline);
+            return new ProcessWorker(pipelineRunner);
+        }
         return new ProcessWorker(buffer, processors, pipeline);
     }
 
     @Test
     void testProcessWorkerHappyPath() {
+        when(processor.isReadyForShutdown()).thenReturn(true);
+        doNothing().when(pipelineRunner).runAllProcessorsAndPublishToSinks();
+        processors = List.of(processor);
 
-        final List<Record> records = List.of(mock(Record.class));
-        final CheckpointState checkpointState = mock(CheckpointState.class);
-        final Map.Entry<Collection, CheckpointState> readResult = Map.entry(records, checkpointState);
-        when(buffer.read(pipeline.getReadBatchTimeoutInMillis())).thenReturn(readResult);
+       final ProcessWorker processWorker = createObjectUnderTest();
+       processWorker.run();
 
-        final Processor processor = mock(Processor.class);
-        when(processor.execute(records)).thenReturn(records);
+        verify(pipelineRunner, atLeastOnce()).runAllProcessorsAndPublishToSinks();
+    }
+
+    @Test
+    void testProcessWorkerInitializesPipelineRunnerCorrectly() {
         when(processor.isReadyForShutdown()).thenReturn(true);
         processors = List.of(processor);
 
-        final FutureHelperResult<Void> futureHelperResult = mock(FutureHelperResult.class);
-        when(futureHelperResult.getFailedReasons()).thenReturn(Collections.emptyList());
-
-
-        try (final MockedStatic<FutureHelper> futureHelperMockedStatic = mockStatic(FutureHelper.class)) {
-            futureHelperMockedStatic.when(() -> FutureHelper.awaitFuturesIndefinitely(sinkFutures))
-                    .thenReturn(futureHelperResult);
-
-            final ProcessWorker processWorker = createObjectUnderTest();
-
+        try (MockedConstruction<PipelineRunnerImpl> pipelineRunnerMockedConstruction = mockConstruction(PipelineRunnerImpl.class,
+                (mock, context) -> doNothing().when(mock).runAllProcessorsAndPublishToSinks())) {
+            final ProcessWorker processWorker = createObjectUnderTest(false);
             processWorker.run();
+
+            pipelineRunnerMockedConstruction.constructed().forEach(pipelineRunner -> {
+                verify(pipelineRunner, atLeastOnce()).runAllProcessorsAndPublishToSinks();
+            });
         }
     }
 
     @Test
-    void testProcessWorkerHappyPathWithAcknowledgments() {
-
-        when(source.areAcknowledgementsEnabled()).thenReturn(true);
-
-        final List<Record<Event>> records = new ArrayList<>();
-        final Record<Event> mockRecord = mock(Record.class);
-        final Event mockEvent = mock(Event.class);
-        final EventHandle eventHandle = mock(DefaultEventHandle.class);
-        when(mockRecord.getData()).thenReturn(mockEvent);
-        when(mockEvent.getEventHandle()).thenReturn(eventHandle);
-
-        records.add(mockRecord);
-
-        final CheckpointState checkpointState = mock(CheckpointState.class);
-        final Map.Entry<Collection, CheckpointState> readResult = Map.entry(records, checkpointState);
-        when(buffer.read(pipeline.getReadBatchTimeoutInMillis())).thenReturn(readResult);
-
-        final Processor processor = mock(Processor.class);
-        when(processor.execute(records)).thenReturn(records);
-        when(processor.isReadyForShutdown()).thenReturn(true);
+    void testProcessWorkerShutdownProcessPreparesProcessorsForShutdown() {
         processors = List.of(processor);
-
-        final FutureHelperResult<Void> futureHelperResult = mock(FutureHelperResult.class);
-        when(futureHelperResult.getFailedReasons()).thenReturn(Collections.emptyList());
-
-
-        try (final MockedStatic<FutureHelper> futureHelperMockedStatic = mockStatic(FutureHelper.class)) {
-            futureHelperMockedStatic.when(() -> FutureHelper.awaitFuturesIndefinitely(sinkFutures))
-                    .thenReturn(futureHelperResult);
-
-            final ProcessWorker processWorker = createObjectUnderTest();
-
-            processWorker.run();
-        }
-    }
-
-    @Test
-    void testProcessWorkerWithProcessorsNotHoldingEvents() {
-        DefaultEventHandle eventHandle = mock(DefaultEventHandle.class);
-        Event event = mock(Event.class);
-        Record record = mock(Record.class);
-        when(eventHandle.release(true)).thenReturn(true);
-        lenient().when(event.getEventHandle()).thenReturn(eventHandle);
-        when(record.getData()).thenReturn(event);
-        final List<Record> records = List.of(record);
-        final CheckpointState checkpointState = mock(CheckpointState.class);
-        final Map.Entry<Collection, CheckpointState> readResult = Map.entry(records, checkpointState);
-        when(buffer.read(pipeline.getReadBatchTimeoutInMillis())).thenReturn(readResult);
-
-        final Processor processor1 = mock(Processor.class);
-        when(processor1.holdsEvents()).thenReturn(false);
-        when(processor1.execute(records)).thenReturn(List.of());
-        when(processor1.isReadyForShutdown()).thenReturn(true);
-        processors = List.of(processor1);
-        when(source.areAcknowledgementsEnabled()).thenReturn(true);
+        when(pipeline.isStopRequested()).thenReturn(false, true);
+        when(pipeline.getPeerForwarderDrainTimeout()).thenReturn(Duration.ofMillis(1));
+        when(buffer.isEmpty()).thenReturn(true);
+        when(processor.isReadyForShutdown()).thenReturn(true);
+        doNothing().when(pipelineRunner).runAllProcessorsAndPublishToSinks();
 
         final ProcessWorker processWorker = createObjectUnderTest();
-
         processWorker.run();
 
-        verify(eventHandle, atLeast(1)).release(true);
+        verify(processor, atLeastOnce()).prepareForShutdown();
     }
 
-
     @Test
-    void testProcessWorkerWithProcessorsHoldingEvents() {
-        EventHandle eventHandle = mock(EventHandle.class);
-        Event event = mock(Event.class);
-        Record record = mock(Record.class);
-        lenient().when(event.getEventHandle()).thenReturn(eventHandle);
-        when(record.getData()).thenReturn(event);
-        final List<Record> records = List.of(record);
-        final CheckpointState checkpointState = mock(CheckpointState.class);
-        final Map.Entry<Collection, CheckpointState> readResult = Map.entry(records, checkpointState);
-        when(buffer.read(pipeline.getReadBatchTimeoutInMillis())).thenReturn(readResult);
-
-        final Processor processor1 = mock(Processor.class);
-        when(processor1.holdsEvents()).thenReturn(true);
-        when(processor1.execute(records)).thenReturn(List.of());
-        when(processor1.isReadyForShutdown()).thenReturn(true);
-
-        processors = List.of(processor1);
-        when(source.areAcknowledgementsEnabled()).thenReturn(true);
+    void testProcessWorkerShutdownProcessWaitsUntilBufferEmpty() {
+        processors = List.of(processor);
+        // First call returns false, then true
+        when(pipeline.isStopRequested()).thenReturn(false, true);
+        when(pipeline.getPeerForwarderDrainTimeout()).thenReturn(Duration.ofMillis(1));
+        when(buffer.isEmpty()).thenReturn(false, true);
+        when(processor.isReadyForShutdown()).thenReturn(true);
+        doNothing().when(pipelineRunner).runAllProcessorsAndPublishToSinks();
 
         final ProcessWorker processWorker = createObjectUnderTest();
-
         processWorker.run();
 
-        verify(eventHandle, never()).release(true);
+        // Verify multiple invocations due to the wait loop in shutdown phase 2.
+        verify(pipelineRunner, atLeast(2)).runAllProcessorsAndPublishToSinks();
     }
 
     @Test
-    void testProcessWorkerWithProcessorThrowingExceptionIsCaughtProperly() {
-
-        final List<Record> records = List.of(mock(Record.class));
-        final CheckpointState checkpointState = mock(CheckpointState.class);
-        final Map.Entry<Collection, CheckpointState> readResult = Map.entry(records, checkpointState);
-        when(buffer.read(pipeline.getReadBatchTimeoutInMillis())).thenReturn(readResult);
-
-        final Processor processor = mock(Processor.class);
-        when(processor.execute(records)).thenThrow(RuntimeException.class);
+    void testProcessWorkerShutdownProcessForceStopReadingBuffers() {
+        processors = List.of(processor);
+        when(pipeline.isStopRequested()).thenReturn(false, true);
+        when(pipeline.getPeerForwarderDrainTimeout()).thenReturn(Duration.ofMillis(1));
+        when(buffer.isEmpty()).thenReturn(false);
+        when(pipeline.isForceStopReadingBuffers()).thenReturn(true);
         when(processor.isReadyForShutdown()).thenReturn(true);
+        doNothing().when(pipelineRunner).runAllProcessorsAndPublishToSinks();
 
-        final Processor skippedProcessor = mock(Processor.class);
-        when(skippedProcessor.isReadyForShutdown()).thenReturn(true);
-        processors = List.of(processor, skippedProcessor);
+        final ProcessWorker processWorker = createObjectUnderTest();
+        processWorker.run();
 
-        final FutureHelperResult<Void> futureHelperResult = mock(FutureHelperResult.class);
-        when(futureHelperResult.getFailedReasons()).thenReturn(Collections.emptyList());
-
-
-        try (final MockedStatic<FutureHelper> futureHelperMockedStatic = mockStatic(FutureHelper.class)) {
-            futureHelperMockedStatic.when(() -> FutureHelper.awaitFuturesIndefinitely(sinkFutures))
-                    .thenReturn(futureHelperResult);
-
-            final ProcessWorker processWorker = createObjectUnderTest();
-
-            processWorker.run();
-        }
-
-        verify(skippedProcessor, never()).execute(any());
-    }
-
-    @Test
-    void testProcessWorkerWithProcessorThrowingExceptionAndAcknowledgmentsEnabledIsHandledProperly() {
-
-        when(source.areAcknowledgementsEnabled()).thenReturn(true);
-
-        final List<Record<Event>> records = new ArrayList<>();
-        final Record<Event> mockRecord = mock(Record.class);
-        final Event mockEvent = mock(Event.class);
-        final EventHandle eventHandle = mock(DefaultEventHandle.class);
-        final AcknowledgementSet acknowledgementSet = mock(AcknowledgementSet.class);
-        ((InternalEventHandle)eventHandle).addAcknowledgementSet(acknowledgementSet);
-        when(mockRecord.getData()).thenReturn(mockEvent);
-        when(mockEvent.getEventHandle()).thenReturn(eventHandle);
-
-        records.add(mockRecord);
-
-        final CheckpointState checkpointState = mock(CheckpointState.class);
-        final Map.Entry<Collection, CheckpointState> readResult = Map.entry(records, checkpointState);
-        when(buffer.read(pipeline.getReadBatchTimeoutInMillis())).thenReturn(readResult);
-
-        final Processor processor = mock(Processor.class);
-        when(processor.execute(records)).thenThrow(RuntimeException.class);
-        when(processor.isReadyForShutdown()).thenReturn(true);
-
-        final Processor skippedProcessor = mock(Processor.class);
-        when(skippedProcessor.isReadyForShutdown()).thenReturn(true);
-        processors = List.of(processor, skippedProcessor);
-
-        final FutureHelperResult<Void> futureHelperResult = mock(FutureHelperResult.class);
-        when(futureHelperResult.getFailedReasons()).thenReturn(Collections.emptyList());
-
-
-        try (final MockedStatic<FutureHelper> futureHelperMockedStatic = mockStatic(FutureHelper.class)) {
-            futureHelperMockedStatic.when(() -> FutureHelper.awaitFuturesIndefinitely(sinkFutures))
-                    .thenReturn(futureHelperResult);
-
-            final ProcessWorker processWorker = createObjectUnderTest();
-
-            processWorker.run();
-        }
-
-        verify(skippedProcessor, never()).execute(any());
-    }
-
-    @Test
-    void testProcessWorkerWithProcessorDroppingAllRecordsAndAcknowledgmentsEnabledIsHandledProperly() {
-
-        when(source.areAcknowledgementsEnabled()).thenReturn(true);
-
-        final List<Record<Event>> records = new ArrayList<>();
-        final Record<Event> mockRecord = mock(Record.class);
-        final Event mockEvent = mock(Event.class);
-        final EventHandle eventHandle = mock(DefaultEventHandle.class);
-        final AcknowledgementSet acknowledgementSet = mock(AcknowledgementSet.class);
-        ((InternalEventHandle)eventHandle).addAcknowledgementSet(acknowledgementSet);
-        when(mockRecord.getData()).thenReturn(mockEvent);
-        when(mockEvent.getEventHandle()).thenReturn(eventHandle);
-
-        records.add(mockRecord);
-
-        final CheckpointState checkpointState = mock(CheckpointState.class);
-        final Map.Entry<Collection, CheckpointState> readResult = Map.entry(records, checkpointState);
-        when(buffer.read(pipeline.getReadBatchTimeoutInMillis())).thenReturn(readResult);
-
-        final Processor processor = mock(Processor.class);
-        when(processor.execute(records)).thenReturn(Collections.emptyList());
-        when(processor.isReadyForShutdown()).thenReturn(true);
-
-        final Processor secondProcessor = mock(Processor.class);
-        when(secondProcessor.isReadyForShutdown()).thenReturn(true);
-        when(secondProcessor.execute(Collections.emptyList())).thenReturn(Collections.emptyList());
-        processors = List.of(processor, secondProcessor);
-
-        final FutureHelperResult<Void> futureHelperResult = mock(FutureHelperResult.class);
-        when(futureHelperResult.getFailedReasons()).thenReturn(Collections.emptyList());
-
-
-        try (final MockedStatic<FutureHelper> futureHelperMockedStatic = mockStatic(FutureHelper.class)) {
-            futureHelperMockedStatic.when(() -> FutureHelper.awaitFuturesIndefinitely(sinkFutures))
-                    .thenReturn(futureHelperResult);
-
-            final ProcessWorker processWorker = createObjectUnderTest();
-
-            processWorker.run();
-        }
+        verify(pipelineRunner, atLeastOnce()).runAllProcessorsAndPublishToSinks();
     }
 }
