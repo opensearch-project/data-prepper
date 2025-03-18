c80accc045ffad252b02df6b46e529b1387c1358
Qi Chen <qchea@amazon.com>
2025-03-18 11:06:19 -0500

    FIX: invalid Jakarta annotation on enum type in ActionConfiguration (#5528)
    * FIX: invalid Jakarta annotation on enum type
    
    Signed-off-by: George Chen <qchea@amazon.com>

--- data-prepper-plugins/opensearch/src/main/java/org/opensearch/dataprepper/plugins/sink/opensearch/configuration/ActionConfiguration.java
+++ data-prepper-plugins/opensearch/src/main/java/org/opensearch/dataprepper/plugins/sink/opensearch/configuration/ActionConfiguration.java
@@ -7,12 +7,10 @@ package org.opensearch.dataprepper.plugins.sink.opensearch.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.AssertTrue;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import org.opensearch.dataprepper.model.opensearch.OpenSearchBulkActions;
 
 public class ActionConfiguration {
-    @Size(min = 1, message = "type cannot be empty")
     @JsonProperty("type")
     private OpenSearchBulkActions type;
 
