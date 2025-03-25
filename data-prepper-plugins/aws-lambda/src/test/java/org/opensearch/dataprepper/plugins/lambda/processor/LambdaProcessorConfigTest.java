4f23f93fef371076c0ac499a49c415b8c1cf82b2
Srikanth Govindarajan <srigovs@amazon.com>
2025-03-25 10:47:51 -0700

    Add circuit breaker support for lambda processor (#5540)
    * Add circuit breaker support for lambda processor
    
    Signed-off-by: Srikanth Govindarajan <srigovs@amazon.com>

--- data-prepper-plugins/aws-lambda/src/test/java/org/opensearch/dataprepper/plugins/lambda/processor/LambdaProcessorConfigTest.java
+++ data-prepper-plugins/aws-lambda/src/test/java/org/opensearch/dataprepper/plugins/lambda/processor/LambdaProcessorConfigTest.java
@@ -34,7 +34,8 @@ public class LambdaProcessorConfigTest {
         assertThat(lambdaProcessorConfig.getWhenCondition(), equalTo(null));
         assertThat(lambdaProcessorConfig.getResponseEventsMatch(), equalTo(false));
 
-        // Test default client options
+        // Test default client options  private static final int DEFAULT_CIRCUIT_BREAKER_RETRIES = 15; // 15 seconds with 1000ms sleep
+        //  private static final long DEFAULT_CIRCUIT_BREAKER_WAIT_INTERVAL_MS = 1000;
         assertThat(lambdaProcessorConfig.getClientOptions(), notNullValue());
         assertThat(lambdaProcessorConfig.getClientOptions().getMaxConnectionRetries(), equalTo(ClientOptions.DEFAULT_CONNECTION_RETRIES));
         assertThat(lambdaProcessorConfig.getClientOptions().getApiCallTimeout(), equalTo(ClientOptions.DEFAULT_API_TIMEOUT));
@@ -42,6 +43,8 @@ public class LambdaProcessorConfigTest {
         assertThat(lambdaProcessorConfig.getClientOptions().getMaxConcurrency(), equalTo(ClientOptions.DEFAULT_MAXIMUM_CONCURRENCY));
         assertThat(lambdaProcessorConfig.getClientOptions().getBaseDelay(), equalTo(ClientOptions.DEFAULT_BASE_DELAY));
         assertThat(lambdaProcessorConfig.getClientOptions().getMaxBackoff(), equalTo(ClientOptions.DEFAULT_MAX_BACKOFF));
+        assertThat(lambdaProcessorConfig.getCircuitBreakerRetries(), equalTo(LambdaProcessorConfig.DEFAULT_CIRCUIT_BREAKER_RETRIES));
+        assertThat(lambdaProcessorConfig.getCircuitBreakerWaitInterval(), equalTo(LambdaProcessorConfig.DEFAULT_CIRCUIT_BREAKER_WAIT_INTERVAL_MS));
     }
 
     @Test
