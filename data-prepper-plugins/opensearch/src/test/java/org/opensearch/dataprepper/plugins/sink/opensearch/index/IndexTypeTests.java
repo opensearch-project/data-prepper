c31486caf5a275df7a7de854a4dfcf061d864d62
Krishna Kondaka <krishkdk@amazon.com>
2025-04-08 00:29:09 +0000

    Fixed tests
    Signed-off-by: Krishna Kondaka <krishkdk@amazon.com>

--- data-prepper-plugins/opensearch/src/test/java/org/opensearch/dataprepper/plugins/sink/opensearch/index/IndexTypeTests.java
+++ data-prepper-plugins/opensearch/src/test/java/org/opensearch/dataprepper/plugins/sink/opensearch/index/IndexTypeTests.java
@@ -26,17 +26,17 @@ public class IndexTypeTests {
         assertEquals(Optional.of(IndexType.CUSTOM), IndexType.getByValue("custom"));
         assertEquals(Optional.of(IndexType.MANAGEMENT_DISABLED), IndexType.getByValue("management_disabled"));
         assertEquals(Optional.of(IndexType.TRACE_ANALYTICS_RAW), IndexType.getByValue("trace-analytics-raw"));
-        assertEquals(Optional.of(IndexType.TRACE_ANALYTICS_RAW_STANDARD), IndexType.getByValue("trace-analytics-standard-raw"));
+        assertEquals(Optional.of(IndexType.TRACE_ANALYTICS_RAW_OTEL), IndexType.getByValue("trace-analytics-otel-raw"));
         assertEquals(Optional.of(IndexType.TRACE_ANALYTICS_SERVICE_MAP), IndexType.getByValue("trace-analytics-service-map"));
         assertEquals(Optional.of(IndexType.LOG_ANALYTICS), IndexType.getByValue("log-analytics"));
-        assertEquals(Optional.of(IndexType.LOG_ANALYTICS_STANDARD), IndexType.getByValue("log-analytics-standard"));
+        assertEquals(Optional.of(IndexType.LOG_ANALYTICS_OTEL), IndexType.getByValue("log-analytics-otel"));
         assertEquals(Optional.of(IndexType.METRIC_ANALYTICS), IndexType.getByValue("metric-analytics"));
-        assertEquals(Optional.of(IndexType.METRIC_ANALYTICS_STANDARD), IndexType.getByValue("metric-analytics-standard"));
+        assertEquals(Optional.of(IndexType.METRIC_ANALYTICS_OTEL), IndexType.getByValue("metric-analytics-otel"));
     }
 
     @Test
     public void getIndexTypeValues() {
-        assertEquals("[trace-analytics-raw, trace-analytics-standard-raw, trace-analytics-service-map, log-analytics, log-analytics-standard, metric-analytics, metric-analytics-standard, custom, management_disabled]", IndexType.getIndexTypeValues());
+        assertEquals("[trace-analytics-raw, trace-analytics-otel-raw, trace-analytics-service-map, log-analytics, log-analytics-otel, metric-analytics, metric-analytics-otel, custom, management_disabled]", IndexType.getIndexTypeValues());
     }
 
     @ParameterizedTest
