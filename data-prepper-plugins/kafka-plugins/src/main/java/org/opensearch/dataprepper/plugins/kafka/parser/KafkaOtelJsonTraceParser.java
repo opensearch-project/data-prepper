1600d538465dc8bad1553ece1b883b1420b82b0d
mamol27 <mamol27@yandex.ru>
2025-04-14 10:40:21 +0300

    KafkaOtelJsonTraceParser extends OTelProtoOpensearchCodec.OTelProtoDecoder
    Signed-off-by: mamol27 <mamol27@yandex.ru>

--- data-prepper-plugins/kafka-plugins/src/main/java/org/opensearch/dataprepper/plugins/kafka/parser/KafkaOtelJsonTraceParser.java
+++ data-prepper-plugins/kafka-plugins/src/main/java/org/opensearch/dataprepper/plugins/kafka/parser/KafkaOtelJsonTraceParser.java
@@ -1,28 +1,33 @@
 package org.opensearch.dataprepper.plugins.kafka.parser;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.util.JsonFormat;
+import io.opentelemetry.proto.common.v1.KeyValue;
+import io.opentelemetry.proto.resource.v1.Resource;
+import io.opentelemetry.proto.trace.v1.ResourceSpans;
+import io.opentelemetry.proto.trace.v1.ScopeSpans;
+import io.opentelemetry.proto.trace.v1.TracesData;
 import org.opensearch.dataprepper.model.event.Event;
 import org.opensearch.dataprepper.model.record.Record;
-import org.opensearch.dataprepper.model.trace.*;
+import org.opensearch.dataprepper.model.trace.Span;
+import org.opensearch.dataprepper.plugins.otel.codec.OTelProtoOpensearchCodec;
 
 import java.io.IOException;
 import java.time.Instant;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
 
 /**
  * Parsing Open Telemetry trace json format from kafka topic to Spans.
  */
-public class KafkaOtelJsonTraceParser {
+public class KafkaOtelJsonTraceParser extends OTelProtoOpensearchCodec.OTelProtoDecoder {
 
-    private static final String STATUS_CODE = "status.code";
-    private static final String STATUS_MESSAGE = "status.message";
     static final String INSTRUMENTATION_SCOPE_NAME = "instrumentationScope.name";
     static final String INSTRUMENTATION_SCOPE_VERSION = "instrumentationScope.version";
     static final String INSTRUMENTATION_SCOPE_ATTRIBUTES = "instrumentationScope.attributes";
-    ObjectMapper objectMapper = new ObjectMapper();
 
     public List<Span> parse(String inputJson, Instant receivedTimeStamp, Consumer<Record<Event>> eventConsumer) throws IOException {
 
@@ -34,167 +39,51 @@ public class KafkaOtelJsonTraceParser {
         return spans;
     }
 
-    private List<Span> parseSpans(String inputJson, Instant receivedTimeStamp) throws IOException {
-        JsonOtelTracePojo otelTracePojo = objectMapper.readValue(inputJson, JsonOtelTracePojo.class);
+    private List<Span> parseSpans(String inputJson, Instant timeReceived) throws IOException {
+        TracesData.Builder builder = TracesData.newBuilder();
+        JsonFormat.parser().merge(inputJson, builder);
+        TracesData build = builder.build();
+        List<ResourceSpans> resourceSpansList = build.getResourceSpansList();
 
-        String serviceName = readServiceName(otelTracePojo);
-        Map<String, Object> resourceAttributes = getResourcesAttributes(otelTracePojo);
+
         List<Span> spans = new ArrayList<>();
-        for (JsonOtelTracePojo.ResourceSpan resourceSpan : otelTracePojo.resourceSpans) {
-            for (JsonOtelTracePojo.ScopeSpan scopeSpan : resourceSpan.scopeSpans) {
-                Map<String, Object> instrumentationScopeAttributes = getInstrumentationScopeAttributes(scopeSpan);
-                for (JsonOtelTracePojo.Span jsonSpan : scopeSpan.spans) {
-                    spans.add(parseSpan(jsonSpan, serviceName, resourceAttributes, instrumentationScopeAttributes, receivedTimeStamp));
+        for (ResourceSpans resourceSpans : resourceSpansList) {
+            String serviceName = readServiceName(resourceSpans.getResource());
+            Map<String, Object> resourceAttributes = getResourceAttributes(resourceSpans.getResource());
+
+            for (ScopeSpans scopeSpans : resourceSpans.getScopeSpansList()) {
+                Map<String, Object> instrumentationScopeAttributes = getInstrumentationScopeAttributes(scopeSpans);
+                for (io.opentelemetry.proto.trace.v1.Span span : scopeSpans.getSpansList()) {
+                    spans.add(super.parseSpan(span, instrumentationScopeAttributes, serviceName, resourceAttributes, timeReceived));
                 }
             }
         }
-
-
         return spans;
     }
 
-    public static Map<String, Object> getInstrumentationScopeAttributes(final JsonOtelTracePojo.ScopeSpan scopeSpan) {
+    public static Map<String, Object> getInstrumentationScopeAttributes(final ScopeSpans scopeSpans) {
         final Map<String, Object> instrumentationScopeAttr = new HashMap<>();
-        if (scopeSpan.scope.name != null && !scopeSpan.scope.name.isEmpty()) {
-            instrumentationScopeAttr.put(INSTRUMENTATION_SCOPE_NAME, scopeSpan.scope.name);
+        scopeSpans.getScope().getName();
+        if (!scopeSpans.getScope().getName().isEmpty()) {
+            instrumentationScopeAttr.put(INSTRUMENTATION_SCOPE_NAME, scopeSpans.getScope().getName());
         }
-        if (scopeSpan.scope.version != null && !scopeSpan.scope.version.isEmpty()) {
-            instrumentationScopeAttr.put(INSTRUMENTATION_SCOPE_VERSION, scopeSpan.scope.version);
+        if (!scopeSpans.getScope().getVersion().isEmpty()) {
+            instrumentationScopeAttr.put(INSTRUMENTATION_SCOPE_VERSION, scopeSpans.getScope().getVersion());
         }
-        if (scopeSpan.scope.attributes != null && !scopeSpan.scope.attributes.isEmpty()) {
-            for (JsonOtelTracePojo.Attribute attribute : scopeSpan.scope.attributes) {
-                instrumentationScopeAttr.put(INSTRUMENTATION_SCOPE_ATTRIBUTES + attribute.key, attribute.value.getValue());
+        if (!scopeSpans.getScope().getAttributesList().isEmpty()) {
+            for (KeyValue attribute : scopeSpans.getScope().getAttributesList()) {
+                instrumentationScopeAttr.put(INSTRUMENTATION_SCOPE_ATTRIBUTES + attribute.getKey(), attribute.getValue());
             }
         }
         return instrumentationScopeAttr;
     }
 
-    private Span parseSpan(JsonOtelTracePojo.Span jsonSpan, String serviceName, Map<String, Object> resourceAttributes,
-                           Map<String, Object> instrumentationScopeAttributes, Instant receivedTime) {
-
-        try {
-            return JacksonSpan.builder()
-                    .withSpanId(jsonSpan.spanId)
-                    .withTraceId(jsonSpan.traceId)
-                    .withTraceState(null)
-                    .withParentSpanId(jsonSpan.parentSpanId)
-                    .withName(jsonSpan.name)
-                    .withServiceName(serviceName)
-                    .withKind(jsonSpan.kind)
-                    .withStartTime(convertUnixNanosToISO8601(jsonSpan.startTimeUnixNano))
-                    .withEndTime(convertUnixNanosToISO8601(jsonSpan.endTimeUnixNano))
-                    .withAttributes(mergeAllAttributes(
-                            Arrays.asList(
-                                    getSpanAttributes(jsonSpan),
-                                    resourceAttributes,
-                                    instrumentationScopeAttributes,
-                                    getSpanStatusAttributes(jsonSpan.status)
-                            )
-                    ))
-//                .withDroppedAttributesCount(sp.getDroppedAttributesCount())
-                    .withEvents(getSpanEvents(jsonSpan))
-//                .withDroppedEventsCount(0)
-//                .withLinks(new ArrayList<>())
-//                .withDroppedLinksCount(0)
-                    .withTraceGroup(getTraceGroup(jsonSpan))
-                    .withDurationInNanos(jsonSpan.endTimeUnixNano - jsonSpan.startTimeUnixNano)
-                    .withTraceGroupFields(getTraceGroupFields(jsonSpan))
-                    .withTimeReceived(receivedTime)
-                    .build();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            throw new RuntimeException(e);
-        }
-    }
-
-    private String readServiceName(JsonOtelTracePojo otelTracePojo) {
-        for (JsonOtelTracePojo.ResourceSpan resourceSpan : otelTracePojo.resourceSpans) {
-            for (JsonOtelTracePojo.Attribute attribute : resourceSpan.resource.attributes) {
-                if ("service.name".equals(attribute.key)) {
-                    return attribute.value.stringValue;
-                }
+    private String readServiceName(Resource resource) {
+        for (KeyValue attribute : resource.getAttributesList()) {
+            if ("service.name".equals(attribute.getKey())) {
+                return attribute.getValue().getStringValue();
             }
         }
         return "";
     }
-
-    private Map<String, Object> getResourcesAttributes(JsonOtelTracePojo otelTracePojo) {
-        Map<String, Object> resourceAttributes = new HashMap<>();
-        for (JsonOtelTracePojo.ResourceSpan resourceSpan : otelTracePojo.resourceSpans) {
-            for (JsonOtelTracePojo.Attribute attribute : resourceSpan.resource.attributes) {
-                resourceAttributes.put(attribute.key, attribute.value.getValue());
-            }
-        }
-        return resourceAttributes;
-    }
-
-    private Map<String, Object> getSpanAttributes(JsonOtelTracePojo.Span jsonSpan) {
-        Map<String, Object> spanAttributes = new HashMap<>();
-        if (jsonSpan.attributes == null) {
-            return spanAttributes;
-        }
-        for (JsonOtelTracePojo.Attribute attribute : jsonSpan.attributes) {
-            spanAttributes.put(attribute.key, attribute.value.getValue());
-        }
-        return spanAttributes;
-    }
-
-    private Map<String, Object> getSpanStatusAttributes(final JsonOtelTracePojo.Status status) {
-        final Map<String, Object> statusAttr = new HashMap<>();
-        statusAttr.put(STATUS_CODE, status.code);
-        if (status.message != null && !status.message.isEmpty()) {
-            statusAttr.put(STATUS_MESSAGE, status.message);
-        }
-        return statusAttr;
-    }
-
-    protected Map<String, Object> mergeAllAttributes(final Collection<Map<String, Object>> attributes) {
-        return attributes.stream()
-                .flatMap(map -> map.entrySet().stream())
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-    }
-
-    private List<SpanEvent> getSpanEvents(JsonOtelTracePojo.Span jsonSpan) {
-
-        if (jsonSpan.events == null || jsonSpan.events.isEmpty()) {
-            return new ArrayList<>();
-        }
-
-        List<SpanEvent> spanEvents = new ArrayList<>();
-        for (JsonOtelTracePojo.Event event : jsonSpan.events) {
-
-            Map<String, Object> attributes = new HashMap<>();
-            for (JsonOtelTracePojo.Attribute attribute : event.attributes) {
-                attributes.put(attribute.key, attribute.value.getValue());
-            }
-
-            spanEvents.add(DefaultSpanEvent.builder()
-                    .withName(event.name)
-                    .withTime(convertUnixNanosToISO8601(jsonSpan.startTimeUnixNano))
-                    .withAttributes(attributes)
-                    .build());
-        }
-
-        return spanEvents;
-    }
-
-    private String getTraceGroup(JsonOtelTracePojo.Span jsonSpan) {
-        return jsonSpan.parentSpanId.isEmpty() ? jsonSpan.name : null;
-    }
-
-    private TraceGroupFields getTraceGroupFields(JsonOtelTracePojo.Span jsonSpan) {
-        DefaultTraceGroupFields.Builder traceGroupFieldsBuilder = DefaultTraceGroupFields.builder();
-        if (jsonSpan.parentSpanId.isEmpty()) {
-            traceGroupFieldsBuilder = traceGroupFieldsBuilder
-                    .withDurationInNanos(jsonSpan.endTimeUnixNano - jsonSpan.startTimeUnixNano)
-                    .withEndTime(convertUnixNanosToISO8601(jsonSpan.endTimeUnixNano))
-                    .withStatusCode(jsonSpan.status.code);
-        }
-        return traceGroupFieldsBuilder.build();
-    }
-
-    private static String convertUnixNanosToISO8601(final long unixNano) {
-        return Instant.ofEpochSecond(0L, unixNano).toString();
-    }
-
 }
