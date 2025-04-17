0639b149842e4921016f920c115d1bbe8323ba00
Pallempati Saketh <pallempati.saketh@fmr.com>
2025-04-17 12:44:03 +0530

    Fixed Spotless Format Error
    Signed-off-by: Pallempati Saketh <pallempati.saketh@fmr.com>

--- data-prepper-pipeline-parser/src/main/java/org/opensearch/dataprepper/pipeline/parser/PipelineConfigurationValidator.java
+++ data-prepper-pipeline-parser/src/main/java/org/opensearch/dataprepper/pipeline/parser/PipelineConfigurationValidator.java
@@ -9,7 +9,6 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.opensearch.dataprepper.model.configuration.PluginSetting;
 import org.opensearch.dataprepper.pipeline.parser.model.PipelineConfiguration;
 import org.opensearch.dataprepper.pipeline.parser.model.SinkContextPluginSetting;
-import org.opensearch.dataprepper.pipeline.parser.InvalidPipelineConfigurationException;
 import org.opensearch.dataprepper.model.configuration.ConditionalRoute;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
