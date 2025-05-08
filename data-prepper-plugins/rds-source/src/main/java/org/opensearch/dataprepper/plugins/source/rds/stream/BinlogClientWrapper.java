77f4528b0b68e87e4a89823672b21510ad3b66a3
Hai Yan <oeyh@amazon.com>
2025-05-08 14:48:41 -0500

    Add aggregated metrics for rds source
    Signed-off-by: Hai Yan <oeyh@amazon.com>

--- data-prepper-plugins/rds-source/src/main/java/org/opensearch/dataprepper/plugins/source/rds/stream/BinlogClientWrapper.java
+++ data-prepper-plugins/rds-source/src/main/java/org/opensearch/dataprepper/plugins/source/rds/stream/BinlogClientWrapper.java
@@ -11,6 +11,8 @@
 package org.opensearch.dataprepper.plugins.source.rds.stream;
 
 import com.github.shyiko.mysql.binlog.BinaryLogClient;
+import com.github.shyiko.mysql.binlog.network.AuthenticationException;
+import org.opensearch.dataprepper.plugins.source.rds.utils.RdsSourceAggregateMetrics;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,15 +22,27 @@ import java.util.List;
 public class BinlogClientWrapper implements ReplicationLogClient {
 
     private static final Logger LOG = LoggerFactory.getLogger(BinlogClientWrapper.class);
+    static final String CONNECTION_REFUSED = "Connection refused";
+    static final String FAILED_TO_DETERMINE_BINLOG_FILENAME = "Failed to determine binlog filename";
+    static final String ACCESS_DENIED = "Access denied";
     private final BinaryLogClient binlogClient;
+    private final RdsSourceAggregateMetrics rdsSourceAggregateMetrics;
 
-    public BinlogClientWrapper(final BinaryLogClient binlogClient) {
+    public BinlogClientWrapper(final BinaryLogClient binlogClient, final RdsSourceAggregateMetrics rdsSourceAggregateMetrics) {
         this.binlogClient = binlogClient;
+        this.rdsSourceAggregateMetrics = rdsSourceAggregateMetrics;
     }
 
     @Override
     public void connect() throws IOException {
-        binlogClient.connect();
+        try {
+            rdsSourceAggregateMetrics.getStreamApiInvocations().increment();
+            binlogClient.connect();
+        } catch (Exception e) {
+            LOG.error("Failed to connect to replication stream due to {}", e.getMessage());
+            categorizeError(e);
+            throw e;
+        }
     }
 
     @Override
@@ -53,5 +67,24 @@ public class BinlogClientWrapper implements ReplicationLogClient {
 
     public BinaryLogClient getBinlogClient() {
         return binlogClient;
+    }
+
+    private void categorizeError(Exception e) {
+        if (e instanceof AuthenticationException) {
+            rdsSourceAggregateMetrics.getStream4xxErrors().increment();
+            LOG.error("Failed to connect to replication stream: Authentication failed. [{}]", e.getMessage());
+        } else if (e.getCause() != null && e.getCause().getMessage() != null && e.getCause().getMessage().contains(CONNECTION_REFUSED)) {
+            rdsSourceAggregateMetrics.getStream4xxErrors().increment();
+            LOG.error("Failed to connect to replication stream: Cannot connect to MySQL server. [{}]", e.getMessage());
+        } else if (e.getMessage() != null && e.getMessage().contains(FAILED_TO_DETERMINE_BINLOG_FILENAME)) {
+            rdsSourceAggregateMetrics.getStream4xxErrors().increment();
+            LOG.error("Failed to connect to replication stream: Binary logging not enabled on the server. [{}]", e.getMessage());
+        } else if (e.getMessage() != null && e.getMessage().contains(ACCESS_DENIED)) {
+            rdsSourceAggregateMetrics.getStream4xxErrors().increment();
+            LOG.error("Failed to connect to replication stream: Insufficient privileges. [{}]", e.getMessage());
+        } else {
+            rdsSourceAggregateMetrics.getStream5xxErrors().increment();
+            LOG.error("Failed to connect to replication stream. ", e);
+        }
     }
 }
