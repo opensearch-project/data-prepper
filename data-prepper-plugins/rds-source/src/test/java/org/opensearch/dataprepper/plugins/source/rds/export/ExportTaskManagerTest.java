05a5e257b31c5c85214a036c26f4657df22ad2be
Hai Yan <8153134+oeyh@users.noreply.github.com>
2025-05-19 16:55:35 -0500

    Add aggregate metrics for rds source (#5697)
    * Add aggregated metrics for rds source
    
    Signed-off-by: Hai Yan <oeyh@amazon.com>
    
    * Add more granular error metrics
    
    Signed-off-by: Hai Yan <oeyh@amazon.com>
    
    * Add more tests
    
    Signed-off-by: Hai Yan <oeyh@amazon.com>
    
    ---------
    
    Signed-off-by: Hai Yan <oeyh@amazon.com>

--- data-prepper-plugins/rds-source/src/test/java/org/opensearch/dataprepper/plugins/source/rds/export/ExportTaskManagerTest.java
+++ data-prepper-plugins/rds-source/src/test/java/org/opensearch/dataprepper/plugins/source/rds/export/ExportTaskManagerTest.java
@@ -5,6 +5,7 @@
 
 package org.opensearch.dataprepper.plugins.source.rds.export;
 
+import io.micrometer.core.instrument.Counter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,11 +15,14 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.opensearch.dataprepper.plugins.source.rds.utils.RdsSourceAggregateMetrics;
+import software.amazon.awssdk.core.exception.SdkException;
 import software.amazon.awssdk.services.rds.RdsClient;
 import software.amazon.awssdk.services.rds.model.DescribeExportTasksRequest;
 import software.amazon.awssdk.services.rds.model.DescribeExportTasksResponse;
 import software.amazon.awssdk.services.rds.model.ExportTask;
 import software.amazon.awssdk.services.rds.model.StartExportTaskRequest;
+import software.amazon.awssdk.services.rds.model.StartExportTaskResponse;
 
 import java.util.List;
 import java.util.UUID;
@@ -26,7 +30,9 @@ import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.startsWith;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -38,28 +44,49 @@ class ExportTaskManagerTest {
     @Mock
     private RdsClient rdsClient;
 
+    @Mock
+    private RdsSourceAggregateMetrics rdsSourceAggregateMetrics;
+
+    @Mock
+    private Counter exportApiInvocations;
+
+    @Mock
+    private Counter export4xxErrors;
+
+    @Mock
+    private Counter export5xxErrors;
+
     private ExportTaskManager exportTaskManager;
 
     @BeforeEach
     void setUp() {
         exportTaskManager = createObjectUnderTest();
+        lenient().when(rdsSourceAggregateMetrics.getExportApiInvocations()).thenReturn(exportApiInvocations);
+        lenient().when(rdsSourceAggregateMetrics.getExport4xxErrors()).thenReturn(export4xxErrors);
+        lenient().when(rdsSourceAggregateMetrics.getExport5xxErrors()).thenReturn(export5xxErrors);
     }
 
     @ParameterizedTest
     @MethodSource("provideStartExportTaskTestParameters")
     void test_start_export_task(List<String> exportOnly) {
-        final String snapshotArn = "arn:aws:rds:us-east-1:123456789012:snapshot:" + UUID.randomUUID();
+        final String snapshotId = UUID.randomUUID().toString().substring(0, 5);
+        final String snapshotArn = "arn:aws:rds:us-east-1:123456789012:snapshot:" + snapshotId;
         final String iamRoleArn = "arn:aws:iam:us-east-1:123456789012:role:" +  UUID.randomUUID();
         final String bucket = UUID.randomUUID().toString();
         final String prefix = UUID.randomUUID().toString();
         final String kmsKey = UUID.randomUUID().toString();
+        final StartExportTaskResponse response = mock(StartExportTaskResponse.class);
+        when(rdsClient.startExportTask(any(StartExportTaskRequest.class))).thenReturn(response);
+        when(response.status()).thenReturn(UUID.randomUUID().toString());
 
-        exportTaskManager.startExportTask(snapshotArn, iamRoleArn, bucket, prefix, kmsKey, exportOnly);
+        final String exportTaskId = exportTaskManager.startExportTask(snapshotArn, iamRoleArn, bucket, prefix, kmsKey, exportOnly);
 
         final ArgumentCaptor<StartExportTaskRequest> exportTaskRequestArgumentCaptor =
                 ArgumentCaptor.forClass(StartExportTaskRequest.class);
 
+        assertThat(exportTaskId, startsWith(snapshotId));
         verify(rdsClient).startExportTask(exportTaskRequestArgumentCaptor.capture());
+        verify(exportApiInvocations).increment();
 
         final StartExportTaskRequest actualRequest = exportTaskRequestArgumentCaptor.getValue();
         assertThat(actualRequest.sourceArn(), equalTo(snapshotArn));
@@ -68,6 +95,40 @@ class ExportTaskManagerTest {
         assertThat(actualRequest.s3Prefix(), equalTo(prefix));
         assertThat(actualRequest.kmsKeyId(), equalTo(kmsKey));
         assertThat(actualRequest.exportOnly(), equalTo(exportOnly));
+    }
+
+    @Test
+    void test_start_export_task_with_sdkexception_returns_null() {
+        final String snapshotArn = "arn:aws:rds:us-east-1:123456789012:snapshot:" + UUID.randomUUID();
+        final String iamRoleArn = "arn:aws:iam:us-east-1:123456789012:role:" +  UUID.randomUUID();
+        final String bucket = UUID.randomUUID().toString();
+        final String prefix = UUID.randomUUID().toString();
+        final String kmsKey = UUID.randomUUID().toString();
+
+        when(rdsClient.startExportTask(any(StartExportTaskRequest.class))).thenThrow(SdkException.class);
+
+        final String exportTaskId = exportTaskManager.startExportTask(snapshotArn, iamRoleArn, bucket, prefix, kmsKey, List.of());
+
+        assertThat(exportTaskId, equalTo(null));
+        verify(exportApiInvocations).increment();
+        verify(export4xxErrors).increment();
+    }
+
+    @Test
+    void test_start_export_task_with_other_exception_returns_null() {
+        final String snapshotArn = "arn:aws:rds:us-east-1:123456789012:snapshot:" + UUID.randomUUID();
+        final String iamRoleArn = "arn:aws:iam:us-east-1:123456789012:role:" +  UUID.randomUUID();
+        final String bucket = UUID.randomUUID().toString();
+        final String prefix = UUID.randomUUID().toString();
+        final String kmsKey = UUID.randomUUID().toString();
+
+        when(rdsClient.startExportTask(any(StartExportTaskRequest.class))).thenThrow(RuntimeException.class);
+
+        final String exportTaskId = exportTaskManager.startExportTask(snapshotArn, iamRoleArn, bucket, prefix, kmsKey, List.of());
+
+        assertThat(exportTaskId, equalTo(null));
+        verify(exportApiInvocations).increment();
+        verify(export5xxErrors).increment();
     }
 
     @Test
@@ -99,6 +160,6 @@ class ExportTaskManagerTest {
     }
 
     private ExportTaskManager createObjectUnderTest() {
-        return new ExportTaskManager(rdsClient);
+        return new ExportTaskManager(rdsClient, rdsSourceAggregateMetrics);
     }
 }
