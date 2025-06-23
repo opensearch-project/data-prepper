9b7e66458afb7b6124bd4186122c92668f93a893
Jeffrey Aaron Jeyasingh <jeffreyaaron06@gmail.com>
2025-06-23 12:34:13 -0700

    Update KafkaCustomProducerTest.java
    Signed-off-by: Jeffrey Aaron Jeyasingh <jeffreyaaron06@gmail.com>

--- data-prepper-plugins/kafka-plugins/src/test/java/org/opensearch/dataprepper/plugins/kafka/producer/KafkaCustomProducerTest.java
+++ data-prepper-plugins/kafka-plugins/src/test/java/org/opensearch/dataprepper/plugins/kafka/producer/KafkaCustomProducerTest.java
@@ -42,6 +42,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
