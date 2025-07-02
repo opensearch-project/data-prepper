3d36eeef084773c73818abc5a92069e93b4767d6
Jeffrey Aaron Jeyasingh <jeffreyaaron06@gmail.com>
2025-07-02 16:13:22 -0700

    Fix equality checks
    Signed-off-by: Jeffrey Aaron Jeyasingh <jeffreyaaron06@gmail.com>

--- data-prepper-plugins/kafka-plugins/src/main/java/org/opensearch/dataprepper/plugins/kafka/buffer/KafkaBuffer.java
+++ data-prepper-plugins/kafka-plugins/src/main/java/org/opensearch/dataprepper/plugins/kafka/buffer/KafkaBuffer.java
@@ -41,7 +41,10 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
 import java.time.Duration;
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
