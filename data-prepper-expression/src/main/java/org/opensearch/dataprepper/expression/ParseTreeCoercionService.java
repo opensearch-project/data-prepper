a7f4435d596fdbc9fb0b361d25eeaa6fc02dfdf6
Santhosh Gandhe <1909520+san81@users.noreply.github.com>
2025-08-28 21:09:23 -0700

    some more improvements
    Signed-off-by: Santhosh Gandhe <1909520+san81@users.noreply.github.com>

--- data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/ParseTreeCoercionService.java
+++ data-prepper-expression/src/main/java/org/opensearch/dataprepper/expression/ParseTreeCoercionService.java
@@ -25,7 +25,11 @@ import java.util.regex.Pattern;
 class ParseTreeCoercionService {
     private static final Pattern QUOTE_PATTERN = Pattern.compile("^\"{1,3}|\"{1,3}$");
     private static final Pattern ARGUMENT_SPLITTER = Pattern.compile("(?<!\\\\),");
-    private static final int INITIAL_ARG_LIST_SIZE = 4;
+    private static final int INITIAL_ARG_LIST_SIZE = 8;
+    private static final String INVALID_FUNCTION_FORMAT_OPEN = "Invalid function format: missing opening parenthesis";
+    private static final String INVALID_FUNCTION_FORMAT_CLOSE = "Invalid function format: missing closing parenthesis";
+    private static final String INVALID_STRING_ARG = "Invalid string argument: check if any argument is missing a closing double quote or contains comma that's not escaped with `\\`.";
+    private static final String UNSUPPORTED_ARG_TYPE = "Unsupported type passed as function argument";
     private final Map<Class<? extends Serializable>, Function<Object, Object>> literalTypeConversions;
     private final ExpressionFunctionProvider expressionFunctionProvider;
     private final Function<Object, Object> convertLiteralType;
@@ -96,12 +100,12 @@ class ParseTreeCoercionService {
     private FunctionMetadata parseFunctionMetadata(final String nodeStringValue) {
         final int funcNameIndex = nodeStringValue.indexOf("(");
         if (funcNameIndex == -1) {
-            throw new ExpressionCoercionException("Invalid function format: missing opening parenthesis");
+            throw new ExpressionCoercionException(INVALID_FUNCTION_FORMAT_OPEN);
         }
         final String functionName = nodeStringValue.substring(0, funcNameIndex);
         final int argsEndIndex = nodeStringValue.indexOf(")", funcNameIndex);
         if (argsEndIndex == -1) {
-            throw new ExpressionCoercionException("Invalid function format: missing closing parenthesis");
+            throw new ExpressionCoercionException(INVALID_FUNCTION_FORMAT_CLOSE);
         }
 
         List<Object> argList = new ArrayList<>(INITIAL_ARG_LIST_SIZE);
@@ -119,11 +123,11 @@ class ParseTreeCoercionService {
                 } else if (trimmedArg.charAt(0) == '"') {
                     if (trimmedArg.length() < 2 || trimmedArg.charAt(trimmedArg.length() - 1) != '"') {
                         throw new ExpressionCoercionException(
-                                "Invalid string argument: check if any argument is missing a closing double quote or contains comma that's not escaped with `\\`.");
+                                INVALID_STRING_ARG);
                     }
                     argList.add(trimmedArg);
                 } else {
-                    throw new ExpressionCoercionException("Unsupported type passed as function argument");
+                    throw new ExpressionCoercionException(UNSUPPORTED_ARG_TYPE);
                 }
             }
         }
@@ -131,11 +135,11 @@ class ParseTreeCoercionService {
         return new FunctionMetadata(functionName, argList);
     }
 
-    private static class FunctionMetadata {
+    private static final class FunctionMetadata {
         final String functionName;
         final List<Object> argList;
 
-        private FunctionMetadata(String functionName, List<Object> argList) {
+        private FunctionMetadata(final String functionName, final List<Object> argList) {
             this.functionName = functionName.intern();
             this.argList = argList;
         }
