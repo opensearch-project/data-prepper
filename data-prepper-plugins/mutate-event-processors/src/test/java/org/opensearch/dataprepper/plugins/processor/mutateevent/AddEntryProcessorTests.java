d5ec944cb93cd2301c106f043a1767cd14b608cc
Santhosh Gandhe <1909520+san81@users.noreply.github.com>
2025-09-15 12:29:52 -0700

    removing the print ln
    Signed-off-by: Santhosh Gandhe <1909520+san81@users.noreply.github.com>

--- data-prepper-plugins/mutate-event-processors/src/test/java/org/opensearch/dataprepper/plugins/processor/mutateevent/AddEntryProcessorTests.java
+++ data-prepper-plugins/mutate-event-processors/src/test/java/org/opensearch/dataprepper/plugins/processor/mutateevent/AddEntryProcessorTests.java
@@ -96,8 +96,6 @@ public class AddEntryProcessorTests {
         final Record<Event> record = getEvent(mapList);
 
         final List<Record<Event>> editedRecords = (List<Record<Event>>) processor.doExecute(Collections.singletonList(record));
-
-        System.out.println();
         assertEquals("{\"message\":[{\"testKey\":\"testValue\"}],\"actor\":{\"user\":{\"groups\":[{\"name\":\"\"},[{\"second\":\"slistval\"}]]}}}",
                 editedRecords.get(0).getData().toJsonString());
     }
