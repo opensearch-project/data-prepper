2d788e0d4651ff9a7e18d098540f6ce44023c269
David Venable <dlv@amazon.com>
2025-10-03 10:05:15 -0500

    Adding Javadocs to the PluginConfigurationFile annotation.
    Signed-off-by: David Venable <dlv@amazon.com>

--- data-prepper-test/plugin-test-framework/src/main/java/org/opensearch/dataprepper/test/plugins/PluginConfigurationFile.java
+++ data-prepper-test/plugin-test-framework/src/main/java/org/opensearch/dataprepper/test/plugins/PluginConfigurationFile.java
@@ -14,8 +14,24 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Specifies a configuration file for loading a plugin. Use this on a parameter of your
+ * plugin type to define how to load the plugin from a configuration file.
+ *
+ * @since 2.13
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
 public @interface PluginConfigurationFile {
+    /**
+     * The name of the configuration file to load from. This must be
+     * in the same directory path as your test code package.
+     * <p>
+     * For example, <code>my_configuration.yaml</code>.
+     *
+     * @return The configuration file
+     *
+     * @since 2.13
+     */
     String value();
 }
