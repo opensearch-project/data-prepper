41e2bc026b5ea5c747afb1d19b08f6c8a695a2b4
Neeraj Kumar <kneeraj@amazon.com>
2025-12-02 12:33:47 -0800

    Add TimeSeries based Service Map Processor
    Implements time-series based APM service map generation from OpenTelemetry traces using three-window sliding architecture with off-heap storage for scalability. Generates service relationship events and performance metrics.

---
+++ data-prepper-plugins/otel-apm-service-map-processor/src/main/java/org/opensearch/dataprepper/plugins/processor/model/internal/ThreeWindowTraceData.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.plugins.processor.model.internal;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Data structure to hold three-window trace processing data
+ */
+public class ThreeWindowTraceData {
+    public final Collection<SpanStateData> processingSpans;
+    public final Collection<SpanStateData> lookupSpans;
+    public final Map<String, SpanStateData> spansBySpanId;
+    public final Map<String, Collection<SpanStateData>> childrenByParentId;
+    public final Set<String> processingSpanIds;
+
+    public ThreeWindowTraceData(final Collection<SpanStateData> processingSpans,
+                                final Collection<SpanStateData> lookupSpans,
+                                final Map<String, SpanStateData> spansBySpanId,
+                                final Map<String, Collection<SpanStateData>> childrenByParentId,
+                                final Set<String> processingSpanIds) {
+        this.processingSpans = processingSpans;
+        this.lookupSpans = lookupSpans;
+        this.spansBySpanId = spansBySpanId;
+        this.childrenByParentId = childrenByParentId;
+        this.processingSpanIds = processingSpanIds;
+    }
+}
