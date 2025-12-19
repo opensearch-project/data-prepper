e9ebc47cba8877c99d4ec5d89cdd115ba4af7d59
Tomas Longo <tlongo@sternad.de>
2025-12-19 11:07:52 +0100

    Remove unused imports
    Signed-off-by: Tomas Longo <tlongo@sternad.de>

--- e2e-test/log/src/integrationTest/java/org/opensearch/dataprepper/integration/log/EndToEndOtelLogsSourceGrpcTest.java
+++ e2e-test/log/src/integrationTest/java/org/opensearch/dataprepper/integration/log/EndToEndOtelLogsSourceGrpcTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
