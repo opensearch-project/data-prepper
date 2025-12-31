6cb701b935e0c2a1e5795675b9c1223bb3ad2cef
Souvik Bose <souvbose@amazon.com>
2025-12-31 14:54:44 -0800

    Minor code change for passing override config
    Signed-off-by: Souvik Bose <souvbose@amazon.com>

--- data-prepper-plugins/kafka-plugins/src/main/java/org/opensearch/dataprepper/plugins/kafka/util/KafkaSecurityConfigurer.java
+++ data-prepper-plugins/kafka-plugins/src/main/java/org/opensearch/dataprepper/plugins/kafka/util/KafkaSecurityConfigurer.java
@@ -255,22 +255,21 @@ public class KafkaSecurityConfigurer {
                     .region(Region.of(awsConfig.getRegion()))
                     .credentialsProvider(mskCredentialsProvider)
                     .build();
+            AssumeRoleRequest.Builder assumeRequestBuilder = AssumeRoleRequest
+                    .builder()
+                    .roleArn(awsConfig.getStsRoleArn())
+                    .roleSessionName(sessionName);
+            Map<String, String> headers = awsConfig.getAwsStsHeaderOverrides();
+            if (Objects.nonNull(headers)) {
+                assumeRequestBuilder.overrideConfiguration(configuration -> {
+                    headers.forEach(configuration::putHeader);
+                });
+            }
             mskCredentialsProvider = StsAssumeRoleCredentialsProvider
                     .builder()
                     .stsClient(stsClient)
-                    .refreshRequest(
-                            AssumeRoleRequest
-                                    .builder()
-                                    .roleArn(awsConfig.getStsRoleArn())
-                                    .roleSessionName(sessionName)
-                                    .overrideConfiguration(configuration -> {
-                                        Map<String, String> headers = awsConfig.getAwsStsHeaderOverrides();
-                                        if (headers != null) {
-                                            headers.forEach(configuration::putHeader);
-                                        }
-                                    })
-                                    .build()
-                    ).build();
+                    .refreshRequest(assumeRequestBuilder.build())
+                    .build();
         }
     }
 
