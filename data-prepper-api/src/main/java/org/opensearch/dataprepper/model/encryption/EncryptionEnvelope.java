347ecd83039b27ea0f216859ddfeec2433f01f20
chrisale000 <alchrisk@amazon.com>
2026-01-07 14:42:35 -0800

    Fix multiple javadoc warnings to reduce build log clutter (#6364)
    Signed-off-by: Alexander Christensen <alchrisk@amazon.com>

--- data-prepper-api/src/main/java/org/opensearch/dataprepper/model/encryption/EncryptionEnvelope.java
+++ data-prepper-api/src/main/java/org/opensearch/dataprepper/model/encryption/EncryptionEnvelope.java
@@ -12,11 +12,13 @@ package org.opensearch.dataprepper.model.encryption;
 public interface EncryptionEnvelope {
     /**
      * The encrypted data.
+     * @return the encrypted data as a byte array
      */
     byte[] getEncryptedData();
 
     /**
      * The encrypted data key.
+     * @return the encrypted data key as a String
      */
     String getEncryptedDataKey();
 }
