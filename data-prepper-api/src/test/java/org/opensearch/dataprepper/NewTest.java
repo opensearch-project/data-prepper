6a7a836180651c93fa03fd9fb9b798b60cc71456
David Venable <dlv@amazon.com>
2026-03-12 09:42:37 -0500

    PR with bad license
    Signed-off-by: David Venable <dlv@amazon.com>

---
+++ data-prepper-api/src/test/java/org/opensearch/dataprepper/NewTest.java
@@ -0,0 +1,13 @@
+package org.opensearch.dataprepper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class NewTest {
+    @Test
+    public void testSomething() {
+        assertThat(true, equalTo(true));
+    }
+}
