77d1a5f145ab46bb5c82738eae835544c0434491
Sotaro Hikita <bering1814@gmail.com>
2026-03-29 11:52:42 +0900

    spotless apply
    Signed-off-by: Sotaro Hikita <bering1814@gmail.com>

--- data-prepper-plugins/iceberg-source/src/main/java/org/opensearch/dataprepper/plugins/source/iceberg/leader/LeaderScheduler.java
+++ data-prepper-plugins/iceberg-source/src/main/java/org/opensearch/dataprepper/plugins/source/iceberg/leader/LeaderScheduler.java
@@ -47,7 +47,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class LeaderScheduler implements Runnable {
 
