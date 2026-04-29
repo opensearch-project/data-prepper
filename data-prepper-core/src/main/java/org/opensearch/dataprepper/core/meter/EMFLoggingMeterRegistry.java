cbba6613eb906c4a4f9b7abb38759791c177ea53
Jannik Brand <jannik.brand@sap.com>
2026-04-29 19:09:05 +0200

    Update Armeria to 1.32.6 (#6343)
    This Armeria patch version was requested explicitly:
    https://github.com/line/armeria/issues/6733
    
    Fixes #6271 since the current Ameria version is affected by a couple
    of bugs (see comment https://github.com/opensearch-project/data-prepper/issues/6271#issuecomment-3627395389).
    
    Furthermore, replace io.micrometer.core.lang annotation which is going
    to be deprecated in Micrometer 1.16.0 - see release notes:
    https://github.com/micrometer-metrics/micrometer/releases/tag/v1.16.0.
    
    Signed-off-by: Jannik Brand <jannik.brand@sap.com>

--- data-prepper-core/src/main/java/org/opensearch/dataprepper/core/meter/EMFLoggingMeterRegistry.java
+++ data-prepper-core/src/main/java/org/opensearch/dataprepper/core/meter/EMFLoggingMeterRegistry.java
@@ -24,7 +24,6 @@ import io.micrometer.core.instrument.Timer;
 import io.micrometer.core.instrument.step.StepMeterRegistry;
 import io.micrometer.core.instrument.util.NamedThreadFactory;
 import io.micrometer.core.instrument.util.StringUtils;
-import io.micrometer.core.lang.Nullable;
 import io.micrometer.core.util.internal.logging.WarnThenDebugLogger;
 import software.amazon.cloudwatchlogs.emf.environment.Environment;
 import software.amazon.cloudwatchlogs.emf.environment.EnvironmentProvider;
@@ -32,6 +31,7 @@ import software.amazon.cloudwatchlogs.emf.logger.MetricsLogger;
 import software.amazon.cloudwatchlogs.emf.model.DimensionSet;
 import software.amazon.cloudwatchlogs.emf.model.Unit;
 
+import javax.annotation.Nullable;
 import java.time.Instant;
 import java.util.Collections;
 import java.util.HashMap;
