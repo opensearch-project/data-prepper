e33821d0e8c50186a5e89385b893025fe2520d59
David Venable <dlv@amazon.com>
2026-05-07 02:38:44 +0800

    Initial support for experimental features within Data Prepper plugins. (#6811)
    Initial support for experimental features within Data Prepper plugins.
    
    This allows plugin authors to add an @Experimental annotation to configuration fields where the default value is null. This works well especially for nested configurations.
    
    The approach to implementing this is to expand our Hibernate Validator to be able to get custom ConstraintValidator instances from the Spring application context. This adds an ExperimentalFeatureValidator which implements ConstraintValidator for the @Experimental annotation. This allows data-prepper-api to retain the @Experimental annotation but not have any knowledge of how the constraint is implemented.
    
    Exclude the new annotation elements added to @Experimental from the compatibility test. These elements are not breaking backward compatibility. They have default values and Java handles them at runtime. However, the byte code does generate abstract methods, which are generally backward incompatible. That is why it is flagged as a breaking change.
    
    Signed-off-by: David Venable <dlv@amazon.com>

--- data-prepper-plugin-framework/src/main/java/org/opensearch/dataprepper/plugin/ValidatorConfiguration.java
+++ data-prepper-plugin-framework/src/main/java/org/opensearch/dataprepper/plugin/ValidatorConfiguration.java
@@ -5,14 +5,21 @@
 
 package org.opensearch.dataprepper.plugin;
 
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorFactory;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import org.apache.commons.text.similarity.LevenshteinDistance;
+import org.hibernate.validator.HibernateValidator;
+import org.hibernate.validator.HibernateValidatorConfiguration;
+import org.hibernate.validator.internal.engine.constraintvalidation.ConstraintValidatorFactoryImpl;
 import org.hibernate.validator.messageinterpolation.ParameterMessageInterpolator;
 import org.springframework.context.annotation.Bean;
 
 import javax.inject.Named;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Application context for internal plugin framework beans.
@@ -20,11 +27,21 @@ import javax.inject.Named;
 @Named
 class ValidatorConfiguration {
     @Bean
-    Validator validator() {
-        final ValidatorFactory validationFactory = Validation.byDefaultProvider()
+    ConstraintValidatorFactory constraintValidatorFactory(final Collection<ConstraintValidator<?, ?>> constraintValidators) {
+        return new BeanAwareConstraintValidatorFactory(new ConstraintValidatorFactoryImpl(), constraintValidators);
+    }
+
+    @Bean
+    Validator validator(final ConstraintValidatorFactory constraintValidatorFactory,
+                        final List<ConstraintMappingContributor> constraintMappingContributors) {
+        final HibernateValidatorConfiguration configuration = Validation.byProvider(HibernateValidator.class)
                 .configure()
                 .messageInterpolator(new ParameterMessageInterpolator())
-                .buildValidatorFactory();
+                .constraintValidatorFactory(constraintValidatorFactory);
+
+        constraintMappingContributors.forEach(contributor -> contributor.addConstraintMapping(configuration));
+
+        final ValidatorFactory validationFactory = configuration.buildValidatorFactory();
         return validationFactory.getValidator();
     }
 
