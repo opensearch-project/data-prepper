f6f9983860bdcc3fd1d7eb73fd4f43c61962bd0d
Dinu John <86094133+dinujoh@users.noreply.github.com>
2026-06-05 21:04:16 -0500

    Optimize MongoDBExportPartitionSupplier for uniform _id type collections
    For collections with uniform _id types, replace the 8-clause $or query
    with a simple Filters.gt("_id", value) for finding partition boundaries.
    This allows DocumentDB to use a single B-tree index seek instead of
    multi-index scan.
    
    Changes:
    - Add isUniformIdType() that checks first/last doc _id types
    - Add buildNextStartFilter() with simple $gt for uniform types,
      falling back to $or-based query for mixed types
    - Use fresh Filters.gte() + skip() per iteration for partition end
    - Extract addPartition() helper to reduce duplication
    - Make BsonHelper.isClassNumber() public for numeric type grouping
    
    Performance: 14M docs (10GB) partitioned in ~30 seconds.
    
    Signed-off-by: Dinu John <86094133+dinujoh@users.noreply.github.com>

--- data-prepper-plugins/mongodb/src/main/java/org/opensearch/dataprepper/plugins/mongo/export/MongoDBExportPartitionSupplier.java
+++ data-prepper-plugins/mongodb/src/main/java/org/opensearch/dataprepper/plugins/mongo/export/MongoDBExportPartitionSupplier.java
@@ -6,13 +6,14 @@
 package org.opensearch.dataprepper.plugins.mongo.export;
 
 import com.mongodb.MongoClientException;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.opensearch.dataprepper.model.source.coordinator.PartitionIdentifier;
 import org.opensearch.dataprepper.model.source.coordinator.enhanced.EnhancedSourceCoordinator;
 import org.opensearch.dataprepper.plugins.mongo.client.BsonHelper;
@@ -37,6 +38,9 @@ public class MongoDBExportPartitionSupplier implements Function<ExportPartition,
     private static final Logger LOG = LoggerFactory.getLogger(MongoDBExportPartitionSupplier.class);
     private static final String MONGODB_PARTITION_KEY_FORMAT = "%s|%s|%s|%s|%s"; // partition format: <db.collection>|<gte>|<lt>|<gteClassName>|<lteClassName>
     private static final String COLLECTION_SPLITTER = "\\.";
+    private static final Bson ID_PROJECTION = Projections.include("_id");
+    private static final Bson ID_ASC = Sorts.ascending("_id");
+    private static final Bson ID_DESC = Sorts.descending("_id");
 
     private final MongoDBSourceConfig sourceConfig;
     private final EnhancedSourceCoordinator enhancedSourceCoordinator;
@@ -50,95 +54,143 @@ public class MongoDBExportPartitionSupplier implements Function<ExportPartition,
         this.documentDBAggregateMetrics = documentDBAggregateMetrics;
     }
 
+    /**
+     * Detects whether the collection has a uniform _id type by checking the first and last documents.
+     * If uniform, we can use a simple Filters.gt() instead of the complex $or query across all BSON types.
+     */
+    boolean isUniformIdType(final MongoCollection<Document> col) {
+        final Document first = col.find().projection(ID_PROJECTION).sort(ID_ASC).limit(1).first();
+        final Document last = col.find().projection(ID_PROJECTION).sort(ID_DESC).limit(1).first();
+        if (first == null || last == null) {
+            return true;
+        }
+        final String firstType = first.get("_id").getClass().getName();
+        final String lastType = last.get("_id").getClass().getName();
+        if (BsonHelper.isClassNumber(firstType) && BsonHelper.isClassNumber(lastType)) {
+            return true;
+        }
+        return firstType.equals(lastType);
+    }
+
+    private Bson buildNextStartFilter(final Object lastLteValue, final String lteClassName, final boolean uniformType) {
+        if (uniformType) {
+            return Filters.gt("_id", lastLteValue);
+        }
+        final String lteValueString = BsonHelper.getPartitionStringFromMongoDBId(lastLteValue, lteClassName);
+        return buildGtQuery(lteValueString, lteClassName, MAX_KEY);
+    }
+
+    private void addPartition(final List<PartitionIdentifier> partitions, final String collectionDbName,
+                              final Object gteValue, final String gteClassName,
+                              final Object lteValue, final String lteClassName) {
+        final String gteValueString = BsonHelper.getPartitionStringFromMongoDBId(gteValue, gteClassName);
+        final String lteValueString = BsonHelper.getPartitionStringFromMongoDBId(lteValue, lteClassName);
+        LOG.debug("Partition of {} : { gte: {} class: {}, lte: {} class {} }",
+                collectionDbName, gteValueString, gteClassName, lteValueString, lteClassName);
+        partitions.add(PartitionIdentifier.builder()
+                .withPartitionKey(String.format(MONGODB_PARTITION_KEY_FORMAT,
+                        collectionDbName, gteValueString, lteValueString, gteClassName, lteClassName))
+                .build());
+    }
+
     private PartitionIdentifierBatch buildPartitions(final ExportPartition exportPartition) {
         documentDBAggregateMetrics.getExportApiInvocations().increment();
         final List<PartitionIdentifier> collectionPartitions = new ArrayList<>();
         final String collectionDbName = exportPartition.getCollection();
-        List<String> collection = List.of(collectionDbName.split(COLLECTION_SPLITTER));
+        final List<String> collection = List.of(collectionDbName.split(COLLECTION_SPLITTER));
         if (collection.size() < 2) {
             documentDBAggregateMetrics.getExport4xxErrors().increment();
             throw new IllegalArgumentException("Invalid Collection Name. Must be in db.collection format");
         }
-        final Optional<ExportProgressState> exportProgressStateOptional = exportPartition
-                .getProgressState();
-        final Object lastEndDocId = exportProgressStateOptional.map(
-                ExportProgressState::getLastEndDocId).orElse(null);
+
+        final Optional<ExportProgressState> exportProgressStateOptional = exportPartition.getProgressState();
+        final Object lastEndDocId = exportProgressStateOptional.map(ExportProgressState::getLastEndDocId).orElse(null);
         boolean isLastBatch = false;
         Object endDocId = lastEndDocId;
+
         try (MongoClient mongoClient = MongoDBConnection.getMongoClient(sourceConfig)) {
             final MongoDatabase db = mongoClient.getDatabase(collection.get(0));
-            final MongoCollection<Document> col = db.getCollection(collectionDbName.substring(collection.get(0).length()+1));
+            final MongoCollection<Document> col = db.getCollection(
+                    collectionDbName.substring(collection.get(0).length() + 1));
             final int partitionSize = exportPartition.getPartitionSize();
-            FindIterable<Document> startIterable;
+
+            final boolean uniformType = isUniformIdType(col);
+            LOG.info("Collection {} has {} _id type. Using {} partition query strategy.",
+                    collectionDbName, uniformType ? "uniform" : "mixed", uniformType ? "simple $gt" : "$or-based");
+
+            Bson startFilter;
             if (lastEndDocId != null) {
-                startIterable = col.find(Filters.gt("_id", lastEndDocId))
-                        .projection(new Document("_id", 1))
-                        .sort(new Document("_id", 1))
-                        .limit(1);
+                startFilter = Filters.gt("_id", lastEndDocId);
             } else {
-                startIterable = col.find()
-                        .projection(new Document("_id", 1))
-                        .sort(new Document("_id", 1))
-                        .limit(1);
+                startFilter = new Document();
             }
+
             while (!Thread.currentThread().isInterrupted()) {
-                try (final MongoCursor<Document> startCursor = startIterable.iterator()) {
-                    if (!startCursor.hasNext()) {
-                        LOG.info("No records to process or has reached end of the export partition.");
-                        isLastBatch = true;
-                        break;
-                    }
-                    final Document startDoc = startCursor.next();
-                    final Object gteValue = startDoc.get("_id");
-                    final String gteClassName = gteValue.getClass().getName();
+                final Document startDoc = col.find(startFilter)
+                        .projection(ID_PROJECTION)
+                        .sort(ID_ASC)
+                        .limit(1)
+                        .first();
 
-                    // Get end doc
-                    Document endDoc = startIterable.skip(partitionSize - 1).limit(1).first();
-                    if (endDoc == null) {
-                        // this means we have reached the end of the doc
-                        endDoc = col.find()
-                                .projection(new Document("_id", 1))
-                                .sort(new Document("_id", -1))
-                                .limit(1)
-                                .first();
-                        isLastBatch = true;
-                    }
-
-                    final Object lteValue = endDoc.get("_id");
-                    final String lteClassName = lteValue.getClass().getName();
-                    endDocId = lteValue;
-                    final String gteValueString = BsonHelper.getPartitionStringFromMongoDBId(gteValue, gteClassName);
-                    final String lteValueString = BsonHelper.getPartitionStringFromMongoDBId(lteValue, lteClassName);
-                    LOG.debug("Partition of {} : { gte: {} class: {}, lte: {} class {} }", collectionDbName, gteValueString, gteClassName, lteValueString, lteClassName);
-                    collectionPartitions.add(
-                        PartitionIdentifier
-                            .builder()
-                            .withPartitionKey(String.format(MONGODB_PARTITION_KEY_FORMAT, collectionDbName, gteValueString, lteValueString, gteClassName, lteClassName))
-                            .build());
-                    documentDBAggregateMetrics.getExportPartitionQueryCount().increment();
-
-                    if (isLastBatch) {
-                        break;
-                    }
-
-                    // extend the ownership of the partition
-                    enhancedSourceCoordinator.saveProgressStateForPartition(exportPartition, null);
-
-                    startIterable = col.find(buildGtQuery(lteValueString, lteClassName, MAX_KEY))
-                            .projection(new Document("_id", 1))
-                            .sort(new Document("_id", 1))
-                            .limit(1);
+                if (startDoc == null) {
+                    LOG.info("No records to process or has reached end of the export partition.");
+                    isLastBatch = true;
+                    break;
                 }
+
+                final Object gteValue = startDoc.get("_id");
+                final String gteClassName = gteValue.getClass().getName();
+
+                final Document endOfPageDoc = col.find(Filters.gte("_id", gteValue))
+                        .projection(ID_PROJECTION)
+                        .sort(ID_ASC)
+                        .skip(partitionSize - 1)
+                        .limit(1)
+                        .first();
+
+                final Object lteValue;
+                final String lteClassName;
+
+                if (endOfPageDoc == null) {
+                    final Document lastDoc = col.find()
+                            .projection(ID_PROJECTION)
+                            .sort(ID_DESC)
+                            .limit(1)
+                            .first();
+                    if (lastDoc == null) {
+                        isLastBatch = true;
+                        break;
+                    }
+                    lteValue = lastDoc.get("_id");
+                    lteClassName = lteValue.getClass().getName();
+                    isLastBatch = true;
+                } else {
+                    lteValue = endOfPageDoc.get("_id");
+                    lteClassName = lteValue.getClass().getName();
+                }
+
+                endDocId = lteValue;
+                addPartition(collectionPartitions, collectionDbName, gteValue, gteClassName, lteValue, lteClassName);
+                documentDBAggregateMetrics.getExportPartitionQueryCount().increment();
+
+                if (isLastBatch) {
+                    break;
+                }
+
+                // extend the ownership of the partition
+                enhancedSourceCoordinator.saveProgressStateForPartition(exportPartition, null);
+
+                startFilter = buildNextStartFilter(lteValue, lteClassName, uniformType);
             }
         } catch (final IllegalArgumentException | MongoClientException e) {
             // IllegalArgumentException is thrown when database or collection name is not valid
             // MongoClientException is thrown for exceptions indicating a failure condition with the MongoClient
             documentDBAggregateMetrics.getExport4xxErrors().increment();
-            LOG.error("Client side exception while build partitions.", e);
+            LOG.error("Client side exception while building partitions.", e);
             throw new RuntimeException(e);
         } catch (final Exception e) {
             documentDBAggregateMetrics.getExport5xxErrors().increment();
-            LOG.error("Server side exception while build partitions.", e);
+            LOG.error("Server side exception while building partitions.", e);
             throw new RuntimeException(e);
         }
 
